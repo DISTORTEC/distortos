cf07080d388f0d7536bfb0d0a8d509f2d731a0eb
Kamil Szczygiel <distortec@distortec.com>
2014-07-22 11:52:54 +0200

    source: add stub main()

---
+++ source/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * \file
+ * \brief
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-07-21
+ */
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief main code block
+ */
+
+int main()
+{
+	return 0;
+}
