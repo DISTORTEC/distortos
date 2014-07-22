aea5984d3f246fd1c056b31569471ad14784ccb8
Kamil Szczygiel <distortec@distortec.com>
2014-07-22 12:49:50 +0200

    build: make it possible to use CMSIS in source/architecture/ARM/ARMv7-M

--- source/architecture/ARM/ARMv7-M/Tupfile.lua
+++ source/architecture/ARM/ARMv7-M/Tupfile.lua
@@ -6,9 +6,15 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- date: 2014-07-21
+-- date: 2014-07-22
 --
 
 ASFLAGS["ARMv7-M-startup.S"] = "-D__USES_TWO_STACKS -D__USES_CXX"
+
+CXXFLAGS += "-DSTM32F407xx"
+
+CXXFLAGS += "-I" .. TOP .. "/source/chip/STMicroelectronics/STM32F4/include"
+CXXFLAGS += "-I" .. TOP .. "/source/chip/STMicroelectronics/STM32F4/external/CMSIS"
+CXXFLAGS += "-I" .. TOP .. "/source/architecture/ARM/external/CMSIS"
 
 tup.include(TOP .. "/compile.lua")
