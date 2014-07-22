3f87da027000955aadca9ddd8a7c9f39028d87ad
Kamil Szczygiel <distortec@distortec.com>
2014-07-22 11:52:32 +0200

    include: add distortosConfiguration.h

---
+++ include/distortos/distortosConfiguration.h
@@ -0,0 +1,19 @@
+/**
+ * \file
+ * \brief distortos configuration
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-07-22
+ */
+
+#ifndef INCLUDE_DISTORTOS_DISTORTOSCONFIGURATION_H_
+#define INCLUDE_DISTORTOS_DISTORTOSCONFIGURATION_H_
+
+#define CONFIG_CHIP_STM32F407
+
+#endif	// INCLUDE_DISTORTOS_DISTORTOSCONFIGURATION_H_
