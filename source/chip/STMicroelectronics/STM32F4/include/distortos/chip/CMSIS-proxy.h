2c232d24ed581d46fc9643a5e5e14265b5fbe570
Kamil Szczygiel <distortec@distortec.com>
2014-07-22 12:44:54 +0200

    source/chip/STMicroelectronics/STM32F4: add include folder include/distortos/chip with CMSIS-proxy.h

---
+++ source/chip/STMicroelectronics/STM32F4/include/distortos/chip/CMSIS-proxy.h
@@ -0,0 +1,19 @@
+/**
+ * \file
+ * \brief CMSIS proxy header for architecture.
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-07-22
+ */
+
+#ifndef SOURCE_CHIP_STMICROELECTRONICS_STM32F4_INCLUDE_DISTORTOS_CHIP_CMSIS_PROXY_H_
+#define SOURCE_CHIP_STMICROELECTRONICS_STM32F4_INCLUDE_DISTORTOS_CHIP_CMSIS_PROXY_H_
+
+#include "stm32f4xx.h"
+
+#endif	// SOURCE_CHIP_STMICROELECTRONICS_STM32F4_INCLUDE_DISTORTOS_CHIP_CMSIS_PROXY_H_
