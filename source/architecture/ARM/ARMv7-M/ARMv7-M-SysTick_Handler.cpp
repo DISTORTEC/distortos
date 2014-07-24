449ab3e7625a9ffd5bf25b86a396646b25e9ed34
Kamil Szczygiel <distortec@distortec.com>
2014-07-24 13:52:01 +0200

    source/architecture/ARM/ARMv7-M: add ARMv7-M-SysTick_Handler.cpp with stub implementation of SysTick_Handler() - just calls yield()

---
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-SysTick_Handler.cpp
@@ -0,0 +1,30 @@
+/**
+ * \file
+ * \brief SysTick_Handler() for ARMv7-M (Cortex-M3 / Cortex-M4)
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-07-24
+ */
+
+#include "distortos/scheduler/schedulerInstance.hpp"
+#include "distortos/scheduler/Scheduler.hpp"
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief SysTick_Handler() for ARMv7-M (Cortex-M3 / Cortex-M4)
+ *
+ * Tick interrupt of scheduler.
+ */
+
+extern "C" void SysTick_Handler()
+{
+	distortos::scheduler::schedulerInstance.yield();
+}
