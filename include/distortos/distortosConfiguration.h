91292fd62b1e374dcd5ac56c869f2aacdc9ce49c
Kamil Szczygiel <distortec@distortec.com>
2014-07-24 13:12:42 +0200

    include/distortos/distortosConfiguration.h: add #define with kernel BASEPRI value

--- include/distortos/distortosConfiguration.h
+++ include/distortos/distortosConfiguration.h
@@ -8,12 +8,14 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-07-22
+ * \date 2014-07-24
  */
 
 #ifndef INCLUDE_DISTORTOS_DISTORTOSCONFIGURATION_H_
 #define INCLUDE_DISTORTOS_DISTORTOSCONFIGURATION_H_
 
 #define CONFIG_CHIP_STM32F407
+
+#define CONFIG_ARCHITECTURE_ARMV7_M_KERNEL_BASEPRI 8
 
 #endif	// INCLUDE_DISTORTOS_DISTORTOSCONFIGURATION_H_
