2e4e2c8314f8aa5c044555c839e14d47a92a620e
Kamil Szczygiel <distortec@distortec.com>
2014-07-29 14:22:22 +0200

    main: add two more test threads, improve all test threads to use pseudo-random numbers (without seed)

--- source/main.cpp
+++ source/main.cpp
@@ -16,6 +16,8 @@
 #include "distortos/scheduler/Scheduler.hpp"
 #include "distortos/architecture.hpp"
 
+#include <random>
+
 #include <cstdint>
 #include <cmath>
 
@@ -33,12 +35,14 @@ namespace
 void * test1(void *)
 {
 	volatile uint32_t i = 0;
-	volatile double d = 1;
+	volatile double d;
+	std::default_random_engine generator;
+	std::uniform_real_distribution<> distribution {0, M_PI};
 
 	while (1)
 	{
 		++i;
-		++d;
+		d = distribution(generator);
 		d = sin(cos(d *  M_PI / 180.0) * M_PI / 180.0);
 	}
 
@@ -52,13 +56,57 @@ void * test1(void *)
 void * test2(void *)
 {
 	volatile uint32_t i = 0;
-	volatile double d = 1;
+	volatile double d;
+	std::default_random_engine generator;
+	std::uniform_real_distribution<> distribution {0, M_PI};
 
 	while (1)
 	{
 		++i;
-		++d;
+		d = distribution(generator);
 		d = tan(1 / (tan(d *  M_PI / 180.0) * M_PI / 180.0));
+	}
+
+	return nullptr;
+}
+
+/**
+ * \brief Test thread
+ */
+
+void * test3(void *)
+{
+	volatile uint32_t i = 0;
+	volatile double d;
+	std::default_random_engine generator;
+	std::uniform_real_distribution<> distribution {0, M_PI};
+
+	while (1)
+	{
+		++i;
+		d = distribution(generator);
+		d = log10(d * 11);
+	}
+
+	return nullptr;
+}
+
+/**
+ * \brief Test thread
+ */
+
+void * test4(void *)
+{
+	volatile uint32_t i = 0;
+	volatile double d;
+	std::default_random_engine generator;
+	std::uniform_real_distribution<> distribution {0, M_PI};
+
+	while (1)
+	{
+		++i;
+		d = distribution(generator);
+		d = log2(d * 3);
 	}
 
 	return nullptr;
@@ -74,11 +122,23 @@ uint64_t stack1[128];
 /// stack for thread2
 uint64_t stack2[128];
 
+/// stack for thread3
+uint64_t stack3[128];
+
+/// stack for thread4
+uint64_t stack4[128];
+
 /// TCB with thread1
 distortos::scheduler::ThreadControlBlock tcb1 {stack1, sizeof(stack1), test1, nullptr};
 
 /// TCB with thread2
 distortos::scheduler::ThreadControlBlock tcb2 {stack2, sizeof(stack2), test2, nullptr};
+
+/// TCB with thread3
+distortos::scheduler::ThreadControlBlock tcb3 {stack3, sizeof(stack3), test3, nullptr};
+
+/// TCB with thread4
+distortos::scheduler::ThreadControlBlock tcb4 {stack4, sizeof(stack4), test4, nullptr};
 
 }	// namespace
 
@@ -94,6 +154,8 @@ int main()
 {
 	distortos::scheduler::schedulerInstance.threadControlBlockList.emplace_back(tcb1);
 	distortos::scheduler::schedulerInstance.threadControlBlockList.emplace_back(tcb2);
+	distortos::scheduler::schedulerInstance.threadControlBlockList.emplace_back(tcb3);
+	distortos::scheduler::schedulerInstance.threadControlBlockList.emplace_back(tcb4);
 
 	distortos::scheduler::schedulerInstance.currentThreadControlBlock =
 			distortos::scheduler::schedulerInstance.threadControlBlockList.begin();
