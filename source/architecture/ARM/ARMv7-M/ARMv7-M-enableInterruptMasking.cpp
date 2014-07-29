b3d788cf036afb1e154cf38e92a725f87e400f0b
Kamil Szczygiel <distortec@distortec.com>
2014-07-29 19:41:32 +0200

    source/architecture/ARM/ARMv7-M: add implementations of disableInterruptMasking() and enableInterruptMasking()

---
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-enableInterruptMasking.cpp
@@ -0,0 +1,39 @@
+/**
+ * \file
+ * \brief enableInterruptMasking() implementation for ARMv7-M (Cortex-M3 / Cortex-M4)
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-07-29
+ */
+
+#include "distortos/architecture.hpp"
+
+#include "distortos/distortosConfiguration.h"
+
+#include "distortos/chip/CMSIS-proxy.h"
+
+namespace distortos
+{
+
+namespace architecture
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+InterruptMask enableInterruptMasking()
+{
+	const auto interrupt_mask = __get_BASEPRI();
+	__set_BASEPRI(CONFIG_ARCHITECTURE_ARMV7_M_KERNEL_BASEPRI << __NVIC_PRIO_BITS);
+	return interrupt_mask;
+}
+
+}	// namespace architecture
+
+}	// namespace distortos
