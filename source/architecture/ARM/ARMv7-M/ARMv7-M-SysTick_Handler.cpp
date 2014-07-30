5bd2edfb7e733e3ae3191cc295258310bd14a9ec
Kamil Szczygiel <distortec@distortec.com>
2014-07-30 14:04:02 +0200

    source/architecture/ARM/ARMv7-M: call Scheduler::tickInterruptHandler() in SysTick_Handler()

--- source/architecture/ARM/ARMv7-M/ARMv7-M-SysTick_Handler.cpp
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-SysTick_Handler.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-07-24
+ * \date 2014-07-30
  */
 
 #include "distortos/scheduler/schedulerInstance.hpp"
@@ -26,5 +26,7 @@
 
 extern "C" void SysTick_Handler()
 {
-	distortos::scheduler::schedulerInstance.yield();
+	const auto context_switch_required = distortos::scheduler::schedulerInstance.tickInterruptHandler();
+	if (context_switch_required == true)
+		distortos::scheduler::schedulerInstance.yield();
 }
