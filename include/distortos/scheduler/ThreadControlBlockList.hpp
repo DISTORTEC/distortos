ecc4b3126bfb7c37cd1e3d91f6e76ab058e49db3
Kamil Szczygiel <distortec@distortec.com>
2014-08-06 12:35:07 +0200

    include/distortos: comment fix in containers/SortedContainer.hpp and scheduler/ThreadControlBlockList.hpp

--- include/distortos/scheduler/ThreadControlBlockList.hpp
+++ include/distortos/scheduler/ThreadControlBlockList.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-08-01
+ * \date 2014-08-06
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCKLIST_HPP_
@@ -76,6 +76,8 @@ public:
 	 * \param Args are types of argument for value_type constructor
 	 *
 	 * \param [in] args are arguments for value_type constructor
+	 *
+	 * \return iterator to emplaced element
 	 */
 
 	template<typename... Args>
