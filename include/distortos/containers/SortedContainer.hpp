36f0f6ef9c49ab54bd05e0b0cafc15f0736a0c52
Kamil Szczygiel <distortec@distortec.com>
2014-08-07 20:08:09 +0200

    include/distortos/containers/SortedContainer.hpp: make erase() function public

--- include/distortos/containers/SortedContainer.hpp
+++ include/distortos/containers/SortedContainer.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-08-06
+ * \date 2014-08-07
  */
 
 #ifndef INCLUDE_DISTORTOS_CONTAINERS_SORTEDCONTAINER_HPP_
@@ -79,6 +79,7 @@ public:
 
 	using Base::begin;
 	using Base::empty;
+	using Base::erase;
 	using Base::end;
 	using Base::size;
 
