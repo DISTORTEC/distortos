83e00d14e7c84da32d1357a54217c3e8aa1c41ac
Kamil Szczygiel <distortec@distortec.com>
2014-08-08 23:35:56 +0200

    syscalls: add implementation of __cxa_pure_virtual()

---
+++ source/syscalls/cxa_pure_virtual.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file
+ * \brief __cxa_pure_virtual() system call implementation
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-08-08
+ */
+
+#include <cstdlib>
+
+extern "C"
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Replacement of original __cxa_pure_virtual().
+ *
+ * This function is called on pure virtual function call. It does not return.
+ *
+ * It just calls abort().
+ */
+
+__attribute__ ((noreturn))
+void __cxa_pure_virtual()
+{
+	abort();
+}
+
+}	// extern "C"
