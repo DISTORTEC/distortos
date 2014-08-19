f6547c8e58524d5ba051828c16af3c77c4fc3915
Kamil Szczygiel <distortec@distortec.com>
2014-08-19 10:51:57 +0200

    ThreadControlBlock: add new State - BlockedOnMutex

--- include/distortos/scheduler/ThreadControlBlock.hpp
+++ include/distortos/scheduler/ThreadControlBlock.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-08-16
+ * \date 2014-08-19
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCK_HPP_
@@ -47,6 +47,8 @@ public:
 		Suspended,
 		/// thread is terminated
 		Terminated,
+		/// thread is blocked on Mutex
+		BlockedOnMutex,
 	};
 
 	/**
