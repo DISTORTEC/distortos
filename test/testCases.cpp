ecf05207ed114e560b27ae27e9d797c7d5832821
Kamil Szczygiel <distortec@distortec.com>
2014-08-24 16:20:06 +0200

    test: add testCases object definition

---
+++ test/testCases.cpp
@@ -0,0 +1,47 @@
+/**
+ * \file
+ * \brief testCases object definition
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-08-24
+ */
+
+#include "testCases.hpp"
+
+#include "Thread/threadTestCases.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// array with references to TestCaseRange objects
+const TestCaseRangeRange::value_type testCases_[]
+{
+		threadTestCases,
+};
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const TestCaseRangeRange testCases {testCases_};
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/testCases.hpp
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \brief testCases object declaration
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-08-24
+ */
+
+#ifndef TEST_TESTCASES_HPP_
+#define TEST_TESTCASES_HPP_
+
+#include "TestCaseRangeRange.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// range of references to TestCaseRange objects
+extern const TestCaseRangeRange testCases;
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_TESTCASES_HPP_
