811b3a5e74a18b4d7976be008d9ad8a1a841323f
Kamil Szczygiel <distortec@distortec.com>
2014-08-24 16:17:08 +0200

    test: add threadTestCases object definition

---
+++ test/Thread/threadTestCases.cpp
@@ -0,0 +1,50 @@
+/**
+ * \file
+ * \brief threadTestCases object definition
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-08-24
+ */
+
+#include "threadTestCases.hpp"
+
+#include "ThreadPriorityTestCase.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// ThreadPriorityTestCase instance
+const ThreadPriorityTestCase priorityTestCase;
+
+/// array with references to TestCase objects related to threads
+const TestCaseRange::value_type threadTestCases_[]
+{
+		priorityTestCase,
+};
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const TestCaseRange threadTestCases {threadTestCases_};
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/Thread/threadTestCases.hpp
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \brief threadTestCases object declaration
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-08-24
+ */
+
+#ifndef TEST_THREAD_THREADTESTCASES_HPP_
+#define TEST_THREAD_THREADTESTCASES_HPP_
+
+#include "TestCaseRange.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// range of references to TestCase objects related to threads
+extern const TestCaseRange threadTestCases;
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_THREAD_THREADTESTCASES_HPP_
