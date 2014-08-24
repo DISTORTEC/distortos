df9c5bd2dc1117897ec10c65c695cc4aeee244b9
Kamil Szczygiel <distortec@distortec.com>
2014-08-24 12:02:42 +0200

    build: enable compilation of sources in test/

---
+++ test/Tupfile.lua
@@ -0,0 +1,16 @@
+--
+-- file: Tupfile.lua
+--
+-- author: Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+-- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- date: 2014-08-24
+--
+
+CXXFLAGS += "-I."
+CXXFLAGS += "-I" .. TOP .. "/include"
+CXXFLAGS += "-I" .. TOP .. "/source/architecture/ARM/ARMv7-M/include"
+
+tup.include(TOP .. "/compile.lua")
