a41304cd5686db1bdf5c6f7f57a31d36c633df9f
Kamil Szczygiel <distortec@distortec.com>
2014-08-27 23:24:37 +0200

    ThreadControlBlock: add pretected constructor which moves stack instead of initializing it

--- include/distortos/scheduler/ThreadControlBlock.hpp
+++ include/distortos/scheduler/ThreadControlBlock.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-08-24
+ * \date 2014-08-27
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCK_HPP_
@@ -96,6 +96,17 @@ public:
 	}
 
 protected:
+
+	/**
+	 * \brief ThreadControlBlock constructor.
+	 *
+	 * This constructor is meant for MainThreadControlBlock.
+	 *
+	 * \param [in] stack is an rvalue reference to stack of main()
+	 * \param [in] priority is the thread's priority, 0 - lowest, UINT8_MAX - highest
+	 */
+
+	ThreadControlBlock(architecture::Stack&& stack, uint8_t priority);
 
 	/**
 	 * \brief ThreadControlBlock's destructor
--- source/scheduler/ThreadControlBlock.cpp
+++ source/scheduler/ThreadControlBlock.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-08-16
+ * \date 2014-08-27
  */
 
 #include "distortos/scheduler/ThreadControlBlock.hpp"
@@ -27,6 +27,19 @@ namespace scheduler
 
 ThreadControlBlock::ThreadControlBlock(void* const buffer, const size_t size, const uint8_t priority) :
 		stack_{buffer, size, threadRunner, *this, threadReturnTrap},
+		priority_{priority},
+		roundRobinQuantum_{},
+		state_{State::New}
+{
+
+}
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| protected functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+ThreadControlBlock::ThreadControlBlock(architecture::Stack&& stack, const uint8_t priority) :
+		stack_{stack},
 		priority_{priority},
 		roundRobinQuantum_{},
 		state_{State::New}
