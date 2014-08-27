4839054583902e6c8a1e53d93014a4abf838fb84
Kamil Szczygiel <distortec@distortec.com>
2014-08-27 22:42:50 +0200

    source/architecture/ARM/ARMv7-M: add implementation of getMainStack()

---
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-getMainStack.cpp
@@ -0,0 +1,44 @@
+/**
+ * \file
+ * \brief getMainStack() implementation for ARMv7-M (Cortex-M3 / Cortex-M4)
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-08-27
+ */
+
+#include "distortos/architecture.hpp"
+
+namespace distortos
+{
+
+namespace architecture
+{
+
+extern "C"
+{
+
+/// beginning of process stack - imported from linker script
+extern char __process_stack_start[];
+
+/// size of process stack, bytes - imported from linker script
+extern char __process_stack_size[];
+
+}
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+std::pair<void*, size_t> getMainStack()
+{
+	return {__process_stack_start, reinterpret_cast<size_t>(__process_stack_size)};
+}
+
+}	// namespace architecture
+
+}	// namespace distortos
