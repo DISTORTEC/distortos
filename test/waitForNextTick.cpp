7865fc984738f7514726efe6577a25fad8c7d2c6
Kamil Szczygiel <distortec@distortec.com>
2014-09-27 13:06:39 +0200

    test: add waitForNextTick() helper function

---
+++ test/waitForNextTick.cpp
@@ -0,0 +1,35 @@
+/**
+ * \file
+ * \brief waitForNextTick() implementation
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-09-27
+ */
+
+#include "waitForNextTick.hpp"
+
+#include "distortos/scheduler/ThisThread.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void waitForNextTick()
+{
+	distortos::scheduler::ThisThread::sleepFor({});
+}
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/waitForNextTick.hpp
@@ -0,0 +1,33 @@
+/**
+ * \file
+ * \brief waitForNextTick() header
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-09-27
+ */
+
+#ifndef TEST_WAITFORNEXTTICK_HPP_
+#define TEST_WAITFORNEXTTICK_HPP_
+
+namespace distortos
+{
+
+namespace test
+{
+
+/**
+ * \brief Waits for beginning of next tick.
+ */
+
+void waitForNextTick();
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_WAITFORNEXTTICK_HPP_
