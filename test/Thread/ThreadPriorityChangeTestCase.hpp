cc5aa5b8725216a421673bb2095f05ddd397bae7
Kamil Szczygiel <distortec@distortec.com>
2014-10-24 21:09:08 +0200

    test: add ThreadPriorityChangeTestCase class

---
+++ test/Thread/ThreadPriorityChangeTestCase.hpp
@@ -0,0 +1,50 @@
+/**
+ * \file
+ * \brief ThreadPriorityChangeTestCase class header
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-10-24
+ */
+
+#ifndef TEST_THREAD_THREADPRIORITYCHANGETESTCASE_HPP_
+#define TEST_THREAD_THREADPRIORITYCHANGETESTCASE_HPP_
+
+#include "TestCase.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/**
+ * \brief Tests changing of thread priority.
+ *
+ * Creates 10 threads with the same priority. Their priority is changed and the order in which they execute is checked.
+ * Change of the main test thread priority is also tested - it is used as a kind of "semaphore" which allows other
+ * threads to run.
+ */
+
+class ThreadPriorityChangeTestCase : public TestCase
+{
+private:
+
+	/**
+	 * \brief Runs the test case.
+	 *
+	 * \return true if the test case succeeded, false otherwise
+	 */
+
+	virtual bool run_() const override;
+};
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_THREAD_THREADPRIORITYCHANGETESTCASE_HPP_
