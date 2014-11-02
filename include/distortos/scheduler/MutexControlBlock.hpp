c3e9fbf4a8ef4bff273676ef3f3c8ed6ea7e61a7
Kamil Szczygiel <distortec@distortec.com>
2014-11-02 18:47:32 +0100

    add (empty) MutexControlBlock class

---
+++ include/distortos/scheduler/MutexControlBlock.hpp
@@ -0,0 +1,33 @@
+/**
+ * \file
+ * \brief MutexControlBlock class header
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-11-02
+ */
+
+#ifndef INCLUDE_DISTORTOS_SCHEDULER_MUTEXCONTROLBLOCK_HPP_
+#define INCLUDE_DISTORTOS_SCHEDULER_MUTEXCONTROLBLOCK_HPP_
+
+namespace distortos
+{
+
+namespace scheduler
+{
+
+/// MutexControlBlock class is a control block for Mutex
+class MutexControlBlock
+{
+
+};
+
+}	// namespace scheduler
+
+}	// namespace distortos
+
+#endif	// INCLUDE_DISTORTOS_SCHEDULER_MUTEXCONTROLBLOCK_HPP_
