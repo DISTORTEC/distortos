3f1ec1329aee5a4cd67641ce8d26817d3e39f5be
Kamil Szczygiel <distortec@distortec.com>
2014-11-09 18:38:36 +0100

    test: execute MutexRecursiveOperationsTestCase for all valid combinations of mutex protocols and priority ceilings

--- test/Mutex/MutexRecursiveOperationsTestCase.cpp
+++ test/Mutex/MutexRecursiveOperationsTestCase.cpp
@@ -55,153 +55,165 @@ constexpr uint8_t testThreadPriority {UINT8_MAX - 1};
 
 bool testRunner_()
 {
-	Mutex mutex {Mutex::Type::Recursive};
-
-	size_t lockCount {};
-
+	using Parameters = std::pair<Mutex::Protocol, uint8_t>;
+	static const Parameters parametersArray[]
 	{
-		// simple lock - must succeed immediately
-		++lockCount;
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.lock();
-		if (ret != 0 || start != TickClock::now())
+			Parameters{Mutex::Protocol::None, {}},
+			Parameters{Mutex::Protocol::PriorityProtect, UINT8_MAX},
+			Parameters{Mutex::Protocol::PriorityProtect, testThreadPriority},
+			Parameters{Mutex::Protocol::PriorityInheritance, {}},
+	};
+
+	for (const auto& parameters : parametersArray)
+	{
+		Mutex mutex {Mutex::Type::Recursive, parameters.first, parameters.second};
+
+		size_t lockCount {};
+
+		{
+			// simple lock - must succeed immediately
+			++lockCount;
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.lock();
+			if (ret != 0 || start != TickClock::now())
+				return false;
+		}
+
+		const auto maxRecursiveLocks = mutex.getMaxRecursiveLocks();
+		if (maxRecursiveLocks < 4)
 			return false;
-	}
 
-	const auto maxRecursiveLocks = mutex.getMaxRecursiveLocks();
-	if (maxRecursiveLocks < 4)
-		return false;
+		{
+			// recursive lock - must succeed immediately
+			++lockCount;
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.lock();
+			if (ret != 0 || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// recursive lock - must succeed immediately
-		++lockCount;
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.lock();
-		if (ret != 0 || start != TickClock::now())
-			return false;
-	}
+		{
+			// recursive lock - must succeed immediately
+			++lockCount;
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.tryLock();
+			if (ret != 0 || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// recursive lock - must succeed immediately
-		++lockCount;
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.tryLock();
-		if (ret != 0 || start != TickClock::now())
-			return false;
-	}
+		{
+			// recursive lock - must succeed immediately
+			++lockCount;
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.tryLockFor(singleDuration);
+			if (ret != 0 || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// recursive lock - must succeed immediately
-		++lockCount;
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.tryLockFor(singleDuration);
-		if (ret != 0 || start != TickClock::now())
-			return false;
-	}
+		{
+			// recursive lock - must succeed immediately
+			++lockCount;
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.tryLockUntil(start + singleDuration);
+			if (ret != 0 || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// recursive lock - must succeed immediately
-		++lockCount;
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.tryLockUntil(start + singleDuration);
-		if (ret != 0 || start != TickClock::now())
-			return false;
-	}
+		while (lockCount < maxRecursiveLocks + 1)
+		{
+			// recursive lock - must succeed
+			++lockCount;
+			const auto ret = mutex.tryLock();
+			if (ret != 0)
+				return false;
+		}
 
-	while (lockCount < maxRecursiveLocks + 1)
-	{
-		// recursive lock - must succeed
-		++lockCount;
-		const auto ret = mutex.tryLock();
-		if (ret != 0)
-			return false;
-	}
+		{
+			// excessive recursive lock - must fail with EAGAIN immediately
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.lock();
+			if (ret != EAGAIN || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// excessive recursive lock - must fail with EAGAIN immediately
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.lock();
-		if (ret != EAGAIN || start != TickClock::now())
-			return false;
-	}
+		{
+			// excessive recursive lock - must fail with EAGAIN immediately
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.tryLock();
+			if (ret != EAGAIN || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// excessive recursive lock - must fail with EAGAIN immediately
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.tryLock();
-		if (ret != EAGAIN || start != TickClock::now())
-			return false;
-	}
+		{
+			// excessive recursive lock - must fail with EAGAIN immediately
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.tryLockFor(singleDuration);
+			if (ret != EAGAIN || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// excessive recursive lock - must fail with EAGAIN immediately
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.tryLockFor(singleDuration);
-		if (ret != EAGAIN || start != TickClock::now())
-			return false;
-	}
+		{
+			// excessive recursive lock - must fail with EAGAIN immediately
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.tryLockUntil(start + singleDuration);
+			if (ret != EAGAIN || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// excessive recursive lock - must fail with EAGAIN immediately
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.tryLockUntil(start + singleDuration);
-		if (ret != EAGAIN || start != TickClock::now())
-			return false;
-	}
+		{
+			const auto ret = mutexTestTryLockWhenLocked(mutex, testThreadPriority);
+			if (ret != true)
+				return ret;
+		}
 
-	{
-		const auto ret = mutexTestTryLockWhenLocked(mutex, testThreadPriority);
-		if (ret != true)
-			return ret;
-	}
+		{
+			const auto ret = mutexTestUnlockFromWrongThread(mutex);
+			if (ret != true)
+				return ret;
+		}
 
-	{
-		const auto ret = mutexTestUnlockFromWrongThread(mutex);
-		if (ret != true)
-			return ret;
-	}
+		{
+			// simple unlock - must succeed immediately
+			--lockCount;
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.unlock();
+			if (ret != 0 || start != TickClock::now())
+				return false;
+		}
 
-	{
-		// simple unlock - must succeed immediately
-		--lockCount;
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.unlock();
-		if (ret != 0 || start != TickClock::now())
-			return false;
-	}
+		{
+			const auto ret = mutexTestTryLockWhenLocked(mutex, testThreadPriority);
+			if (ret != true)
+				return ret;
+		}
 
-	{
-		const auto ret = mutexTestTryLockWhenLocked(mutex, testThreadPriority);
-		if (ret != true)
-			return ret;
-	}
+		while (lockCount > 0)
+		{
+			// simple unlock - must succeed
+			--lockCount;
+			const auto ret = mutex.unlock();
+			if (ret != 0)
+				return false;
+		}
 
-	while (lockCount > 0)
-	{
-		// simple unlock - must succeed
-		--lockCount;
-		const auto ret = mutex.unlock();
-		if (ret != 0)
-			return false;
-	}
-
-	{
-		// excessive unlock - must fail with EPERM immediately
-		waitForNextTick();
-		const auto start = TickClock::now();
-		const auto ret = mutex.unlock();
-		if (ret != EPERM || start != TickClock::now())
-			return false;
+		{
+			// excessive unlock - must fail with EPERM immediately
+			waitForNextTick();
+			const auto start = TickClock::now();
+			const auto ret = mutex.unlock();
+			if (ret != EPERM || start != TickClock::now())
+				return false;
+		}
 	}
 
 	return true;
