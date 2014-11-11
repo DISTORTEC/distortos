10511cb6a660b6d226d30b2e1038a4f6a8a86216
Kamil Szczygiel <distortec@distortec.com>
2014-11-11 20:48:40 +0100

    StaticThread: expose getEffectivePriority() from base class

--- include/distortos/StaticThread.hpp
+++ include/distortos/StaticThread.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-10-27
+ * \date 2014-11-11
  */
 
 #ifndef INCLUDE_DISTORTOS_STATICTHREAD_HPP_
@@ -48,6 +48,8 @@ public:
 	{
 
 	}
+
+	using Base::getEffectivePriority;
 
 	using Base::getPriority;
 
