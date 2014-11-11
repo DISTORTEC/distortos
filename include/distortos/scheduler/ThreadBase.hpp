7137c1f1af8cc1bb15b417efe577ac57dd84605f
Kamil Szczygiel <distortec@distortec.com>
2014-11-11 20:47:04 +0100

    ThreadBase: expose getEffectivePriority() from base class

--- include/distortos/scheduler/ThreadBase.hpp
+++ include/distortos/scheduler/ThreadBase.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-10-25
+ * \date 2014-11-11
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADBASE_HPP_
@@ -66,6 +66,8 @@ public:
 	 */
 
 	int start();
+
+	using ThreadControlBlock::getEffectivePriority;
 
 	using ThreadControlBlock::getPriority;
 
