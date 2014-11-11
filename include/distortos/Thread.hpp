bc0aaff6d1e8300151c71093fde26f0855f67bb7
Kamil Szczygiel <distortec@distortec.com>
2014-11-11 20:48:21 +0100

    Thread: expose getEffectivePriority() from base class

--- include/distortos/Thread.hpp
+++ include/distortos/Thread.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-10-27
+ * \date 2014-11-11
  */
 
 #ifndef INCLUDE_DISTORTOS_THREAD_HPP_
@@ -50,6 +50,8 @@ public:
 	{
 
 	}
+
+	using ThreadBase::getEffectivePriority;
 
 	using ThreadBase::getPriority;
 
