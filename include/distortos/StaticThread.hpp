459dd495bf01250774221fc81b978668eae25a7c
Kamil Szczygiel <distortec@distortec.com>
2014-11-16 21:06:45 +0100

    StaticThread: expose ThreadControlBlock::getSchedulingPolicy() and ThreadControlBlock::setSchedulingPolicy()

--- include/distortos/StaticThread.hpp
+++ include/distortos/StaticThread.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-11
+ * \date 2014-11-16
  */
 
 #ifndef INCLUDE_DISTORTOS_STATICTHREAD_HPP_
@@ -53,9 +53,13 @@ public:
 
 	using Base::getPriority;
 
+	using Base::getSchedulingPolicy;
+
 	using Base::join;
 
 	using Base::setPriority;
+
+	using Base::setSchedulingPolicy;
 
 	using Base::start;
 
