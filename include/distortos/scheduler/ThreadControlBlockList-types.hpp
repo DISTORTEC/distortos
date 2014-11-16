4cc1bbd0204fa162e65530d611fe3f113c99ce52
Kamil Szczygiel <distortec@distortec.com>
2014-11-16 18:37:10 +0100

    ThreadControlBlockList-types.hpp: remove use of containers::SortedContainerBase to get type of ThreadControlBlockListIterator

--- include/distortos/scheduler/ThreadControlBlockList-types.hpp
+++ include/distortos/scheduler/ThreadControlBlockList-types.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-09-09
+ * \date 2014-11-16
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCKLIST_TYPES_HPP_
@@ -40,7 +40,7 @@ using ThreadControlBlockListAllocator =
 using ThreadControlBlockListContainer = std::list<ThreadControlBlockListValueType, ThreadControlBlockListAllocator>;
 
 /// generic iterator for ThreadControlBlockList
-using ThreadControlBlockListIterator = containers::SortedContainerBase<ThreadControlBlockListContainer>::iterator;
+using ThreadControlBlockListIterator = ThreadControlBlockListContainer::iterator;
 
 }	// namespace scheduler
 
