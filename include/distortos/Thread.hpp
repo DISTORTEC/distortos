242a882ed5b244fc09cd71f9899cbac0e4df9bf8
Kamil Szczygiel <distortec@distortec.com>
2014-11-16 21:06:30 +0100

    Thread: expose ThreadControlBlock::getSchedulingPolicy() and ThreadControlBlock::setSchedulingPolicy()

--- include/distortos/Thread.hpp
+++ include/distortos/Thread.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-11
+ * \date 2014-11-16
  */
 
 #ifndef INCLUDE_DISTORTOS_THREAD_HPP_
@@ -55,9 +55,13 @@ public:
 
 	using ThreadBase::getPriority;
 
+	using ThreadBase::getSchedulingPolicy;
+
 	using ThreadBase::join;
 
 	using ThreadBase::setPriority;
+
+	using ThreadBase::setSchedulingPolicy;
 
 	using ThreadBase::start;
 
