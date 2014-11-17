f3775ad5dbec6eb2479e01c70fa5610ee0fb5ebe
Kamil Szczygiel <distortec@distortec.com>
2014-11-17 20:59:08 +0100

    test: simplify round-robin test in ThreadSchedulingPolicyTestCase to make it more generic

--- test/Thread/ThreadSchedulingPolicyTestCase.cpp
+++ test/Thread/ThreadSchedulingPolicyTestCase.cpp
@@ -14,10 +14,10 @@
 #include "ThreadSchedulingPolicyTestCase.hpp"
 
 #include "SequenceAsserter.hpp"
+#include "wasteTime.hpp"
 
 #include "distortos/StaticThread.hpp"
 #include "distortos/ThisThread.hpp"
-#include "distortos/statistics.hpp"
 
 #include "distortos/architecture/InterruptMaskingLock.hpp"
 
@@ -29,6 +29,13 @@ namespace test
 
 namespace
 {
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local types
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// pair of sequence points
+using SequencePoints = std::pair<unsigned int, unsigned int>;
 
 /*---------------------------------------------------------------------------------------------------------------------+
 | local constants
@@ -43,14 +50,14 @@ constexpr uint8_t testThreadPriority {1};
 /// number of test threads
 constexpr size_t totalThreads {10};
 
-/// number of loops in test thread
-constexpr size_t loops {5};
+/// duration of single test thread - significantly longer than single round-robin quantum
+constexpr auto testThreadDuration = scheduler::RoundRobinQuantum::getInitial() * 2;
 
 /*---------------------------------------------------------------------------------------------------------------------+
 | local functions' declarations
 +---------------------------------------------------------------------------------------------------------------------*/
 
-void thread(SequenceAsserter& sequenceAsserter, unsigned int sequencePoint, unsigned int sequenceStep);
+void thread(SequenceAsserter& sequenceAsserter, SequencePoints sequencePoints);
 
 /*---------------------------------------------------------------------------------------------------------------------+
 | local types
@@ -58,7 +65,7 @@ void thread(SequenceAsserter& sequenceAsserter, unsigned int sequencePoint, unsi
 
 /// type of test thread
 using TestThread = decltype(makeStaticThread<testThreadStackSize>({}, thread,
-		std::ref(std::declval<SequenceAsserter&>()), std::declval<unsigned int>(), std::declval<unsigned int>()));
+		std::ref(std::declval<SequenceAsserter&>()), std::declval<SequencePoints>()));
 
 /*---------------------------------------------------------------------------------------------------------------------+
 | local functions
@@ -67,39 +74,33 @@ using TestThread = decltype(makeStaticThread<testThreadStackSize>({}, thread,
 /**
  * \brief Test thread.
  *
- * Marks the sequence point in SequenceAsserter and waits for context switch. This is repeated several times.
+ * Marks the first sequence point in SequenceAsserter, wastes some time and marks the second sequence point in
+ * SequenceAsserter.
  *
  * \param [in] sequenceAsserter is a reference to SequenceAsserter shared object
- * \param [in] sequencePoint is the sequence point of this instance
- * \param [in] sequenceStep is the step added to sequence point in each iteration of the loop
+ * \param [in] sequencePoints is a pair of sequence points for this instance
  */
 
-void thread(SequenceAsserter& sequenceAsserter, const unsigned int sequencePoint, const unsigned int sequenceStep)
+void thread(SequenceAsserter& sequenceAsserter, const SequencePoints sequencePoints)
 {
-	for (size_t i = 0; i < loops; ++i)
-	{
-		sequenceAsserter.sequencePoint(sequencePoint + i * sequenceStep);
-
-		const auto contextSwitchCount = statistics::getContextSwitchCount();
-		while (contextSwitchCount == statistics::getContextSwitchCount());
-	}
+	sequenceAsserter.sequencePoint(sequencePoints.first);
+	wasteTime(testThreadDuration);
+	sequenceAsserter.sequencePoint(sequencePoints.second);
 }
 
 /**
  * \brief Builder of TestThread objects.
  *
  * \param [in] sequenceAsserter is a reference to SequenceAsserter shared object
- * \param [in] sequencePoint is the sequence point of this instance
- * \param [in] sequenceStep is the step added to sequence point in each iteration of the loop
+ * \param [in] sequencePoints is a pair of sequence points for this instance
  *
  * \return constructed TestThread object
  */
 
-TestThread makeTestThread(SequenceAsserter& sequenceAsserter, const unsigned int sequencePoint,
-		const unsigned int sequenceStep)
+TestThread makeTestThread(SequenceAsserter& sequenceAsserter, const SequencePoints sequencePoints)
 {
 	return makeStaticThread<testThreadStackSize>(testThreadPriority, thread, std::ref(sequenceAsserter),
-			static_cast<unsigned int>(sequencePoint), static_cast<unsigned int>(sequenceStep));
+			static_cast<SequencePoints>(sequencePoints));
 }
 
 }	// namespace
@@ -114,27 +115,17 @@ bool ThreadSchedulingPolicyTestCase::run_() const
 
 	std::array<TestThread, totalThreads> threads
 	{{
-			makeTestThread(sequenceAsserter, 0, totalThreads),
-			makeTestThread(sequenceAsserter, 1, totalThreads),
-			makeTestThread(sequenceAsserter, 2, totalThreads),
-			makeTestThread(sequenceAsserter, 3, totalThreads),
-			makeTestThread(sequenceAsserter, 4, totalThreads),
-			makeTestThread(sequenceAsserter, 5, totalThreads),
-			makeTestThread(sequenceAsserter, 6, totalThreads),
-			makeTestThread(sequenceAsserter, 7, totalThreads),
-			makeTestThread(sequenceAsserter, 8, totalThreads),
-			makeTestThread(sequenceAsserter, 9, totalThreads),
+			makeTestThread(sequenceAsserter, {0, 0 + totalThreads}),
+			makeTestThread(sequenceAsserter, {1, 1 + totalThreads}),
+			makeTestThread(sequenceAsserter, {2, 2 + totalThreads}),
+			makeTestThread(sequenceAsserter, {3, 3 + totalThreads}),
+			makeTestThread(sequenceAsserter, {4, 4 + totalThreads}),
+			makeTestThread(sequenceAsserter, {5, 5 + totalThreads}),
+			makeTestThread(sequenceAsserter, {6, 6 + totalThreads}),
+			makeTestThread(sequenceAsserter, {7, 7 + totalThreads}),
+			makeTestThread(sequenceAsserter, {8, 8 + totalThreads}),
+			makeTestThread(sequenceAsserter, {9, 9 + totalThreads}),
 	}};
-
-	volatile bool dummyThreadTerminate {};
-
-	// this thread is needed, because test threads need at least one other thread (which would preempt them) to
-	// exit their wait loop, so there is a problem with ending the very last iteration of the very last thread...
-	auto dummyThread = makeStaticThread<testThreadStackSize>(testThreadPriority,
-			[&dummyThreadTerminate]()
-			{
-				while (dummyThreadTerminate != true);
-			});
 
 	decltype(TickClock::now()) testStart;
 
@@ -147,8 +138,6 @@ bool ThreadSchedulingPolicyTestCase::run_() const
 		for (auto& thread : threads)
 			thread.start();
 
-		dummyThread.start();
-
 		testStart = TickClock::now();
 	}
 
@@ -157,14 +146,11 @@ bool ThreadSchedulingPolicyTestCase::run_() const
 
 	const auto testDuration = TickClock::now() - testStart;
 
-	dummyThreadTerminate = true;
-	dummyThread.join();
-
-	if (sequenceAsserter.assertSequence(totalThreads * loops) == false)
+	if (sequenceAsserter.assertSequence(totalThreads * 2) == false)
 		return false;
 
-	// calculations need to take dummyThread into account - this is the "+ 1"
-	if (testDuration != scheduler::RoundRobinQuantum::getInitial() * (totalThreads + 1) * loops)
+	// exact time cannot be calculated, because wasteTime() is not precise when context switches occur during it
+	if (testDuration < testThreadDuration * totalThreads)
 		return false;
 
 	return true;
