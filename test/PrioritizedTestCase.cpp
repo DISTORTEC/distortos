43270f842e34d779ca8ccaeb1ac5b91eeb9dc568
Kamil Szczygiel <distortec@distortec.com>
2014-11-19 12:37:29 +0100

    test: add PrioritizedTestCase class

---
+++ test/PrioritizedTestCase.cpp
@@ -0,0 +1,39 @@
+/**
+ * \file
+ * \brief PriorityTestCase class implementation
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-11-19
+ */
+
+#include "PrioritizedTestCase.hpp"
+
+#include "distortos/ThisThread.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| private functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+bool PrioritizedTestCase::run_() const
+{
+	const auto thisThreadPriority = ThisThread::getPriority();
+	ThisThread::setPriority(priority_);
+	const auto ret = testCase_.run();
+	ThisThread::setPriority(thisThreadPriority);
+	return ret;
+}
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/PrioritizedTestCase.hpp
@@ -0,0 +1,71 @@
+/**
+ * \file
+ * \brief PrioritizedTestCase class header
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-11-19
+ */
+
+#ifndef TEST_PRIORITIZEDTESTCASE_HPP_
+#define TEST_PRIORITIZEDTESTCASE_HPP_
+
+#include "TestCase.hpp"
+
+#include <cstdint>
+
+namespace distortos
+{
+
+namespace test
+{
+
+/**
+ * \brief PrioritizedTestCase class is a wrapper for TestCase which runs the internal TestCase at specific priority,
+ * restoring original priority afterwards.
+ */
+
+class PrioritizedTestCase : public TestCase
+{
+public:
+
+	/**
+	 * \brief PrioritizedTestCase's constructor
+	 *
+	 * \param [in] testCase is a reference to TestCase run by that object
+	 * \param [in] priority is the priority at which testCase will be executed
+	 */
+
+	constexpr PrioritizedTestCase(const TestCase& testCase, const uint8_t priority) :
+			testCase_(testCase),
+			priority_{priority}
+	{
+
+	}
+
+private:
+
+	/**
+	 * \brief Runs the test case.
+	 *
+	 * \return true if the test case succeeded, false otherwise
+	 */
+
+	virtual bool run_() const override;
+
+	/// reference to TestCase run by that object
+	const TestCase& testCase_;
+
+	/// priority at which testCase_ will be executed
+	const uint8_t priority_;
+};
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_PRIORITIZEDTESTCASE_HPP_
