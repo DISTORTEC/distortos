6acf45cbf0387018afe6ff77e92a918585badbfd
Kamil Szczygiel <distortec@distortec.com>
2014-11-20 12:00:51 +0100

    test: comment fix in PrioritizedTestCase.cpp

--- test/PrioritizedTestCase.cpp
+++ test/PrioritizedTestCase.cpp
@@ -1,6 +1,6 @@
 /**
  * \file
- * \brief PriorityTestCase class implementation
+ * \brief PrioritizedTestCase class implementation
  *
  * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-19
+ * \date 2014-11-20
  */
 
 #include "PrioritizedTestCase.hpp"
