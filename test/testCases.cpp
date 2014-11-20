63398a6fe62dff1458e62998ae82e7e30b66e459
Kamil Szczygiel <distortec@distortec.com>
2014-11-20 15:17:45 +0100

    test: add softwareTimerTestCases to executed test cases

--- test/testCases.cpp
+++ test/testCases.cpp
@@ -8,12 +8,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-19
+ * \date 2014-11-20
  */
 
 #include "testCases.hpp"
 
 #include "Thread/threadTestCases.hpp"
+#include "SoftwareTimer/softwareTimerTestCases.hpp"
 #include "Semaphore/semaphoreTestCases.hpp"
 #include "Mutex/mutexTestCases.hpp"
 
@@ -34,6 +35,7 @@ namespace
 const TestCaseRangeRange::value_type testCases_[]
 {
 		TestCaseRangeRange::value_type{threadTestCases},
+		TestCaseRangeRange::value_type{softwareTimerTestCases},
 		TestCaseRangeRange::value_type{semaphoreTestCases},
 		TestCaseRangeRange::value_type{mutexTestCases},
 };
