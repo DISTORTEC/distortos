e94b1ec3d03cf15bb5bfda0a1620e923e4c41efb
Kamil Szczygiel <distortec@distortec.com>
2014-11-20 15:17:21 +0100

    test: add softwareTimerTestCases module, currently with SoftwareTimerOrderingTestCase only

---
+++ test/SoftwareTimer/softwareTimerTestCases.cpp
@@ -0,0 +1,50 @@
+/**
+ * \file
+ * \brief softwareTimerTestCases object definition
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-11-20
+ */
+
+#include "softwareTimerTestCases.hpp"
+
+#include "SoftwareTimerOrderingTestCase.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// SoftwareTimerOrderingTestCase instance
+const SoftwareTimerOrderingTestCase orderingTestCase;
+
+/// array with references to TestCase objects related to software timers
+const TestCaseRange::value_type softwareTimerTestCases_[]
+{
+		TestCaseRange::value_type{orderingTestCase},
+};
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const TestCaseRange softwareTimerTestCases {softwareTimerTestCases_};
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/SoftwareTimer/softwareTimerTestCases.hpp
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \brief softwareTimerTestCases object declaration
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-11-20
+ */
+
+#ifndef TEST_SOFTWARETIMER_SOFTWARETIMERTESTCASES_HPP_
+#define TEST_SOFTWARETIMER_SOFTWARETIMERTESTCASES_HPP_
+
+#include "TestCaseRange.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// range of references to TestCase objects related to software timers
+extern const TestCaseRange softwareTimerTestCases;
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_SOFTWARETIMER_SOFTWARETIMERTESTCASES_HPP_
