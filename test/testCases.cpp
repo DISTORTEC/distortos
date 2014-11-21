df5c60224a8d4355199c1d09c7c97277187950ff
Kamil Szczygiel <distortec@distortec.com>
2014-11-21 21:03:47 +0100

    test: add conditionVariableTestCases to executed test cases

--- test/testCases.cpp
+++ test/testCases.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-20
+ * \date 2014-11-21
  */
 
 #include "testCases.hpp"
@@ -17,6 +17,7 @@
 #include "SoftwareTimer/softwareTimerTestCases.hpp"
 #include "Semaphore/semaphoreTestCases.hpp"
 #include "Mutex/mutexTestCases.hpp"
+#include "ConditionVariable/conditionVariableTestCases.hpp"
 
 namespace distortos
 {
@@ -38,6 +39,7 @@ const TestCaseRangeRange::value_type testCases_[]
 		TestCaseRangeRange::value_type{softwareTimerTestCases},
 		TestCaseRangeRange::value_type{semaphoreTestCases},
 		TestCaseRangeRange::value_type{mutexTestCases},
+		TestCaseRangeRange::value_type{conditionVariableTestCases},
 };
 
 }	// namespace
