3d41a85186a7b482f5603aea3316df091ecd39ff
Kamil Szczygiel <distortec@distortec.com>
2014-11-24 16:36:46 +0100

    test: add ConditionVariableOperationsTestCase to executed test cases

--- test/ConditionVariable/conditionVariableTestCases.cpp
+++ test/ConditionVariable/conditionVariableTestCases.cpp
@@ -8,12 +8,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-21
+ * \date 2014-11-24
  */
 
 #include "conditionVariableTestCases.hpp"
 
 #include "ConditionVariablePriorityTestCase.hpp"
+#include "ConditionVariableOperationsTestCase.hpp"
 
 namespace distortos
 {
@@ -34,10 +35,14 @@ const ConditionVariablePriorityTestCase::Implementation priorityTestCaseImplemen
 /// ConditionVariablePriorityTestCase instance
 const ConditionVariablePriorityTestCase priorityTestCase {priorityTestCaseImplementation};
 
+/// ConditionVariableOperationsTestCase instance
+const ConditionVariableOperationsTestCase operationsTestCase;
+
 /// array with references to TestCase objects related to condition variables
 const TestCaseRange::value_type conditionVariableTestCases_[]
 {
 		TestCaseRange::value_type{priorityTestCase},
+		TestCaseRange::value_type{operationsTestCase},
 };
 
 }	// namespace
