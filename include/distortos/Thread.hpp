c4bbe9eab6576b3d618156de76ab0d065303857b
Kamil Szczygiel <distortec@distortec.com>
2014-11-27 23:45:31 +0100

    Thread.hpp: remove unnecessary #include

--- include/distortos/Thread.hpp
+++ include/distortos/Thread.hpp
@@ -8,16 +8,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-16
+ * \date 2014-11-27
  */
 
 #ifndef INCLUDE_DISTORTOS_THREAD_HPP_
 #define INCLUDE_DISTORTOS_THREAD_HPP_
 
 #include "distortos/scheduler/ThreadBase.hpp"
-
-#include <type_traits>
-#include <functional>
 
 namespace distortos
 {
