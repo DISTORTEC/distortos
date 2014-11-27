3fbd1b09a8f82398359aeef614b5a3c6816af35f
Kamil Szczygiel <distortec@distortec.com>
2014-11-27 22:48:37 +0100

    Mutex.hpp: remove unnecessary #include

--- include/distortos/Mutex.hpp
+++ include/distortos/Mutex.hpp
@@ -8,14 +8,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-03
+ * \date 2014-11-27
  */
 
 #ifndef INCLUDE_DISTORTOS_MUTEX_HPP_
 #define INCLUDE_DISTORTOS_MUTEX_HPP_
 
 #include "distortos/scheduler/MutexControlBlock.hpp"
-#include "distortos/TickClock.hpp"
 
 namespace distortos
 {
