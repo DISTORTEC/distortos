1d7955344dfec17d3e46cecd724452b9b3f9283f
Kamil Szczygiel <distortec@distortec.com>
2014-11-29 17:35:24 +0100

    ThreadControlBlockList.hpp: remove unnecessary #include

--- include/distortos/scheduler/ThreadControlBlockList.hpp
+++ include/distortos/scheduler/ThreadControlBlockList.hpp
@@ -8,13 +8,11 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-01
+ * \date 2014-11-29
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCKLIST_HPP_
 #define INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCKLIST_HPP_
-
-#include "distortos/scheduler/ThreadControlBlockList-types.hpp"
 
 #include "distortos/scheduler/ThreadControlBlock.hpp"
 
