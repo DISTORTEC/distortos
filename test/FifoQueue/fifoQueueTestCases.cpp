d6a87dfe3ee958e6140a61e7e2f7a4e174081802
Kamil Szczygiel <distortec@distortec.com>
2014-12-06 20:53:46 +0100

    test: add fifoQueueTestCases object

---
+++ test/FifoQueue/fifoQueueTestCases.cpp
@@ -0,0 +1,53 @@
+/**
+ * \file
+ * \brief fifoQueueTestCases object definition
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-12-06
+ */
+
+#include "fifoQueueTestCases.hpp"
+
+#include "FifoQueuePriorityTestCase.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// FifoQueuePriorityTestCase::Implementation instance
+const FifoQueuePriorityTestCase::Implementation priorityTestCaseImplementation;
+
+/// FifoQueuePriorityTestCase instance
+const FifoQueuePriorityTestCase priorityTestCase {priorityTestCaseImplementation};
+
+/// array with references to TestCase objects related to FIFO queue
+const TestCaseRange::value_type fifoQueueTestCases_[]
+{
+		TestCaseRange::value_type{priorityTestCase},
+};
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const TestCaseRange fifoQueueTestCases {fifoQueueTestCases_};
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/FifoQueue/fifoQueueTestCases.hpp
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \brief fifoQueueTestCases object declaration
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-12-06
+ */
+
+#ifndef TEST_FIFOQUEUE_FIFOQUEUETESTCASES_HPP_
+#define TEST_FIFOQUEUE_FIFOQUEUETESTCASES_HPP_
+
+#include "TestCaseRange.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// range of references to TestCase objects related to FIFO queue
+extern const TestCaseRange fifoQueueTestCases;
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_FIFOQUEUE_FIFOQUEUETESTCASES_HPP_
