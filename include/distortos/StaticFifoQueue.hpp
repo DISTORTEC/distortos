80041d3393a8d5c4a1f1a9d21fd4ed903f0103ba
Kamil Szczygiel <distortec@distortec.com>
2014-12-06 20:17:19 +0100

    add StaticFifoQueue class

---
+++ include/distortos/StaticFifoQueue.hpp
@@ -0,0 +1,52 @@
+/**
+ * \file
+ * \brief StaticFifoQueue class header
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2014-12-06
+ */
+
+#ifndef INCLUDE_DISTORTOS_STATICFIFOQUEUE_HPP_
+#define INCLUDE_DISTORTOS_STATICFIFOQUEUE_HPP_
+
+#include "FifoQueue.hpp"
+
+namespace distortos
+{
+
+/**
+ * \brief StaticFifoQueue class is a variant of FifoQueue that has automatic storage for queue's contents.
+ *
+ * \param T is the type of data in queue
+ * \param QueueSize is the maximum number of elements in queue
+ */
+
+template<typename T, size_t QueueSize>
+class StaticFifoQueue : public FifoQueue<T>
+{
+public:
+
+	/**
+	 * \brief StaticFifoQueue's constructor
+	 */
+
+	explicit StaticFifoQueue() :
+			FifoQueue<T>{storage_, sizeof(storage_) / sizeof(*storage_)}
+	{
+
+	}
+
+private:
+
+	/// storage for queue's contents
+	typename FifoQueue<T>::Storage storage_[QueueSize];
+};
+
+}	// namespace distortos
+
+#endif	// INCLUDE_DISTORTOS_STATICFIFOQUEUE_HPP_
