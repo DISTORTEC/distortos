5703354c4df5e183c0ffecf311a58b59df6f038e
Kamil Szczygiel <distortec@distortec.com>
2014-12-12 19:49:55 +0100

    test: add FifoQueueOperationsTestCase to executed test cases

--- test/FifoQueue/fifoQueueTestCases.cpp
+++ test/FifoQueue/fifoQueueTestCases.cpp
@@ -8,12 +8,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-12-06
+ * \date 2014-12-12
  */
 
 #include "fifoQueueTestCases.hpp"
 
 #include "FifoQueuePriorityTestCase.hpp"
+#include "FifoQueueOperationsTestCase.hpp"
 
 namespace distortos
 {
@@ -34,10 +35,14 @@ const FifoQueuePriorityTestCase::Implementation priorityTestCaseImplementation;
 /// FifoQueuePriorityTestCase instance
 const FifoQueuePriorityTestCase priorityTestCase {priorityTestCaseImplementation};
 
+/// FifoQueueOperationsTestCase instance
+const FifoQueueOperationsTestCase operationsTestCase;
+
 /// array with references to TestCase objects related to FIFO queue
 const TestCaseRange::value_type fifoQueueTestCases_[]
 {
 		TestCaseRange::value_type{priorityTestCase},
+		TestCaseRange::value_type{operationsTestCase},
 };
 
 }	// namespace
