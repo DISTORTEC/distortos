b84b1e26304693e9102d52dbd7968635b05aa8f7
Kamil Szczygiel <distortec@distortec.com>
2014-12-20 22:49:51 +0100

    test: fix errors picked by GCC 4.9 - use of incomplete type std::reference_wrapper<>

--- include/distortos/scheduler/SoftwareTimerControlBlockList.hpp
+++ include/distortos/scheduler/SoftwareTimerControlBlockList.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-29
+ * \date 2014-12-20
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_SOFTWARETIMERCONTROLBLOCKLIST_HPP_
@@ -17,6 +17,8 @@
 #include "distortos/scheduler/SoftwareTimerControlBlock.hpp"
 
 #include "distortos/containers/SortedContainer.hpp"
+
+#include <functional>
 
 namespace distortos
 {
