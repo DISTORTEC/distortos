6cf4dd10fb7bc349359273b46da69cf6e71882ea
Kamil Szczygiel <distortec@distortec.com>
2014-12-31 10:17:02 +0100

    RawFifoQueue: add local PushFunctor class

--- source/synchronization/RawFifoQueue.cpp
+++ source/synchronization/RawFifoQueue.cpp
@@ -8,13 +8,65 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-12-30
+ * \date 2014-12-31
  */
 
 #include "distortos/RawFifoQueue.hpp"
 
+#include <cstring>
+
 namespace distortos
 {
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local types
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// PushFunctor is a functor used for pushing of data to the RawFifoQueue
+class PushFunctor : public scheduler::FifoQueueBase::Functor
+{
+public:
+
+	/**
+	 * \brief PushFunctor's constructor
+	 *
+	 * \param [in] data is a pointer to data that will be pushed to RawFifoQueue
+	 * \param [in] size is the size of \a data, bytes
+	 */
+
+	constexpr PushFunctor(const void* const data, const size_t size) :
+			data_{data},
+			size_{size}
+	{
+
+	}
+
+	/**
+	 * \brief Copies the data to RawFifoQueue's storage (with memcpy()) and increments the storage pointer to next
+	 * position (using the size of element).
+	 *
+	 * \param [in,out] storage is a reference to pointer to queue's storage
+	 */
+
+	virtual void operator()(void*& storage) const override
+	{
+		memcpy(storage, data_, size_);
+		storage = static_cast<uint8_t*>(storage) + size_;
+	}
+
+private:
+
+	/// pointer to data that will be pushed to RawFifoQueue
+	const void* const data_;
+
+	/// size of \a data_, bytes
+	const size_t size_;
+};
+
+}	// namespace
 
 /*---------------------------------------------------------------------------------------------------------------------+
 | public functions
