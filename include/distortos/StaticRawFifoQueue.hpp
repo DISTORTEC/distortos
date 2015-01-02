ce519c76a27c97df343668477ca214d5c455f4c2
Kamil Szczygiel <distortec@distortec.com>
2015-01-02 17:53:31 +0100

    add StaticRawFifoQueue class and StaticRawFifoQueueFromSize type alias

---
+++ include/distortos/StaticRawFifoQueue.hpp
@@ -0,0 +1,64 @@
+/**
+ * \file
+ * \brief StaticRawFifoQueue class header
+ *
+ * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-01-02
+ */
+
+#ifndef INCLUDE_DISTORTOS_STATICRAWFIFOQUEUE_HPP_
+#define INCLUDE_DISTORTOS_STATICRAWFIFOQUEUE_HPP_
+
+#include "RawFifoQueue.hpp"
+
+namespace distortos
+{
+
+/**
+ * \brief StaticRawFifoQueue class is a variant of RawFifoQueue that has automatic storage for queue's contents.
+ *
+ * \param T is the type of data in queue
+ * \param QueueSize is the maximum number of elements in queue
+ */
+
+template<typename T, size_t QueueSize>
+class StaticRawFifoQueue : public RawFifoQueue
+{
+public:
+
+	/**
+	 * \brief StaticRawFifoQueue's constructor
+	 */
+
+	explicit StaticRawFifoQueue() :
+			RawFifoQueue{storage_}
+	{
+
+	}
+
+private:
+
+	/// storage for queue's contents
+	std::array<typename std::aligned_storage<sizeof(T), alignof(T)>::type, QueueSize> storage_;
+};
+
+/**
+ * \brief StaticRawFifoQueueFromSize type alias is a variant of StaticRawFifoQueue which uses size of element (instead
+ * of type) as template argument.
+ *
+ * \param ElementSize is the size of single queue element, bytes
+ * \param QueueSize is the maximum number of elements in queue
+ */
+
+template<size_t ElementSize, size_t QueueSize>
+using StaticRawFifoQueueFromSize =
+		StaticRawFifoQueue<typename std::aligned_storage<ElementSize, ElementSize>::type, QueueSize>;
+
+}	// namespace distortos
+
+#endif	// INCLUDE_DISTORTOS_STATICRAWFIFOQUEUE_HPP_
