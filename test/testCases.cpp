e64753c0f515bf86e6ff3560db762331ea021045
Kamil Szczygiel <distortec@distortec.com>
2015-01-02 18:58:33 +0100

    test: add rawFifoQueueTestCases to executed test cases

--- test/testCases.cpp
+++ test/testCases.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-12-06
+ * \date 2015-01-02
  */
 
 #include "testCases.hpp"
@@ -19,6 +19,7 @@
 #include "Mutex/mutexTestCases.hpp"
 #include "ConditionVariable/conditionVariableTestCases.hpp"
 #include "FifoQueue/fifoQueueTestCases.hpp"
+#include "RawFifoQueue/rawFifoQueueTestCases.hpp"
 
 namespace distortos
 {
@@ -42,6 +43,7 @@ const TestCaseRangeRange::value_type testCases_[]
 		TestCaseRangeRange::value_type{mutexTestCases},
 		TestCaseRangeRange::value_type{conditionVariableTestCases},
 		TestCaseRangeRange::value_type{fifoQueueTestCases},
+		TestCaseRangeRange::value_type{rawFifoQueueTestCases},
 };
 
 }	// namespace
