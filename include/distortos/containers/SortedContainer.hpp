c17777b7152486a3a225ad95381c9310d7a816ac
Kamil Szczygiel <distortec@distortec.com>
2015-01-09 10:09:53 +0100

    SortedContainer: improve the implementation of forwarding erase()

--- include/distortos/containers/SortedContainer.hpp
+++ include/distortos/containers/SortedContainer.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief SortedContainer class header
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-16
+ * \date 2015-01-09
  */
 
 #ifndef INCLUDE_DISTORTOS_CONTAINERS_SORTEDCONTAINER_HPP_
@@ -121,7 +121,7 @@ public:
 	 */
 
 	template<typename... Args>
-	decltype(std::declval<Container>().erase(std::declval<Args>()...)) erase(Args&&... args)
+	auto erase(Args&&... args) -> decltype(std::declval<Container>().erase(std::forward<Args>(args)...))
 	{
 		return container_.erase(std::forward<Args>(args)...);
 	}
