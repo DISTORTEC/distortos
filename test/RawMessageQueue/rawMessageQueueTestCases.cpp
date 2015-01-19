ac3e38be37f997eb0169d4485bfdd1ada9294325
Kamil Szczygiel <distortec@distortec.com>
2015-01-19 16:55:07 +0100

    test: add rawMessageQueueTestCases object

---
+++ test/RawMessageQueue/rawMessageQueueTestCases.cpp
@@ -0,0 +1,53 @@
+/**
+ * \file
+ * \brief rawMessageQueueTestCases object definition
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-01-19
+ */
+
+#include "rawMessageQueueTestCases.hpp"
+
+#include "RawMessageQueuePriorityTestCase.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// RawMessageQueuePriorityTestCase::Implementation instance
+const RawMessageQueuePriorityTestCase::Implementation priorityTestCaseImplementation;
+
+/// RawMessageQueuePriorityTestCase instance
+const RawMessageQueuePriorityTestCase priorityTestCase {priorityTestCaseImplementation};
+
+/// array with references to TestCase objects related to raw message queue
+const TestCaseRange::value_type messageQueueTestCases_[]
+{
+		TestCaseRange::value_type{priorityTestCase},
+};
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const TestCaseRange rawMessageQueueTestCases {messageQueueTestCases_};
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/RawMessageQueue/rawMessageQueueTestCases.hpp
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \brief rawMessageQueueTestCases object declaration
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-01-19
+ */
+
+#ifndef TEST_RAWMESSAGEQUEUE_RAWMESSAGEQUEUETESTCASES_HPP_
+#define TEST_RAWMESSAGEQUEUE_RAWMESSAGEQUEUETESTCASES_HPP_
+
+#include "TestCaseRange.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// range of references to TestCase objects related to raw message queue
+extern const TestCaseRange rawMessageQueueTestCases;
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_RAWMESSAGEQUEUE_RAWMESSAGEQUEUETESTCASES_HPP_
