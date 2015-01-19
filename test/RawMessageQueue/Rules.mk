64cd622bc142e8462f32f3342a1fc6f3b9bd4cfa
Kamil Szczygiel <distortec@distortec.com>
2015-01-19 16:54:12 +0100

    test: add RawMessageQueue test module to compilation

--- test/RawMessageQueue/Rules.mk
+++ test/RawMessageQueue/Rules.mk
@@ -6,30 +6,16 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# date: 2015-01-19
+# date: 2015-01-07
 #
-
-#-----------------------------------------------------------------------------------------------------------------------
-# subdirectories
-#-----------------------------------------------------------------------------------------------------------------------
-
-SUBDIRECTORIES += ConditionVariable
-SUBDIRECTORIES += FifoQueue
-SUBDIRECTORIES += MessageQueue
-SUBDIRECTORIES += Mutex
-SUBDIRECTORIES += RawFifoQueue
-SUBDIRECTORIES += RawMessageQueue
-SUBDIRECTORIES += Semaphore
-SUBDIRECTORIES += SoftwareTimer
-SUBDIRECTORIES += Thread
 
 #-----------------------------------------------------------------------------------------------------------------------
 # compilation flags
 #-----------------------------------------------------------------------------------------------------------------------
 
 CXXFLAGS_$(d) := -I$(d)
+CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) -Itest
 CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) -Iinclude
-CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) -Isource/architecture/ARM/ARMv7-M/include
 
 #-----------------------------------------------------------------------------------------------------------------------
 # standard footer
