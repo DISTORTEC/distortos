0699a1e933f5664455783f93bd3b7a8f539ddca4
Kamil Szczygiel <distortec@distortec.com>
2015-02-12 22:51:38 +0100

    ThreadControlBlockList-types.hpp: add #include of <functional> header - for std::reference_wrapper

--- include/distortos/scheduler/ThreadControlBlockList-types.hpp
+++ include/distortos/scheduler/ThreadControlBlockList-types.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief Types used by ThreadControlBlockList
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-29
+ * \date 2015-02-12
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCKLIST_TYPES_HPP_
@@ -18,6 +18,7 @@
 #include "distortos/allocators/SimpleFeedablePool.hpp"
 
 #include <list>
+#include <functional>
 
 namespace distortos
 {
