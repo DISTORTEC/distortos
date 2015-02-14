ed56e73e1f2c2c69085f89f48f7e6ff5089b0a4a
Kamil Szczygiel <distortec@distortec.com>
2015-02-14 21:44:39 +0100

    Stack: delete copy constructor, copy assignment and move assignment, force generation of move constructor

--- include/distortos/architecture/Stack.hpp
+++ include/distortos/architecture/Stack.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief Stack class header
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-09-18
+ * \date 2015-02-14
  */
 
 #ifndef INCLUDE_DISTORTOS_ARCHITECTURE_STACK_HPP_
@@ -83,6 +83,11 @@ public:
 	 */
 
 	void setStackPointer(void* const stackPointer) { stackPointer_ = stackPointer; }
+
+	Stack(const Stack&) = delete;
+	Stack(Stack&&) = default;
+	const Stack& operator=(const Stack&) = delete;
+	Stack& operator=(Stack&&) = delete;
 
 private:
 
