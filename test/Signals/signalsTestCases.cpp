39275ce6a195dcde518cf95d74f2741afbabdac2
Kamil Szczygiel <distortec@distortec.com>
2015-02-18 21:35:33 +0100

    test: add signalsTestCases object, currently with single SignalsWaitTestCase instance

---
+++ test/Signals/signalsTestCases.cpp
@@ -0,0 +1,53 @@
+/**
+ * \file
+ * \brief signalsTestCases object definition
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-02-18
+ */
+
+#include "signalsTestCases.hpp"
+
+#include "SignalsWaitTestCase.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// SignalsWaitTestCase::Implementation instance
+const SignalsWaitTestCase::Implementation waitTestCaseImplementation;
+
+/// SignalsWaitTestCase instance
+const SignalsWaitTestCase waitTestCase {waitTestCaseImplementation};
+
+/// array with references to TestCase objects related to signals
+const TestCaseRange::value_type messageQueueTestCases_[]
+{
+		TestCaseRange::value_type{waitTestCase},
+};
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const TestCaseRange signalsTestCases {messageQueueTestCases_};
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/Signals/signalsTestCases.hpp
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \brief signalsTestCases object declaration
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-02-18
+ */
+
+#ifndef TEST_SIGNALS_SIGNALSTESTCASES_HPP_
+#define TEST_SIGNALS_SIGNALSTESTCASES_HPP_
+
+#include "TestCaseRange.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// range of references to TestCase objects related to signals
+extern const TestCaseRange signalsTestCases;
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_SIGNALS_SIGNALSTESTCASES_HPP_
