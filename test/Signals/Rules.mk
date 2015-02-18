dcdac0ab31edc1c811c77218adfbd3c13d96a105
Kamil Szczygiel <distortec@distortec.com>
2015-02-18 21:34:10 +0100

    test: add Signals test module to compilation

--- test/Signals/Rules.mk
+++ test/Signals/Rules.mk
@@ -6,31 +6,16 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# date: 2015-02-18
+# date: 2015-01-07
 #
-
-#-----------------------------------------------------------------------------------------------------------------------
-# subdirectories
-#-----------------------------------------------------------------------------------------------------------------------
-
-SUBDIRECTORIES += ConditionVariable
-SUBDIRECTORIES += FifoQueue
-SUBDIRECTORIES += MessageQueue
-SUBDIRECTORIES += Mutex
-SUBDIRECTORIES += RawFifoQueue
-SUBDIRECTORIES += RawMessageQueue
-SUBDIRECTORIES += Semaphore
-SUBDIRECTORIES += Signals
-SUBDIRECTORIES += SoftwareTimer
-SUBDIRECTORIES += Thread
 
 #-----------------------------------------------------------------------------------------------------------------------
 # compilation flags
 #-----------------------------------------------------------------------------------------------------------------------
 
 CXXFLAGS_$(d) := -I$(d)
+CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) -Itest
 CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) -Iinclude
-CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) -Isource/architecture/ARM/ARMv7-M/include
 
 #-----------------------------------------------------------------------------------------------------------------------
 # standard footer
