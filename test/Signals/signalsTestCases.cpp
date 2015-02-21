51dbd9150c14c137dc1c8852053fcdbcfe7689f3
Kamil Szczygiel <distortec@distortec.com>
2015-02-21 17:33:47 +0100

    test: add SignalsWaitOperationsTestCase to executed test cases

--- test/Signals/signalsTestCases.cpp
+++ test/Signals/signalsTestCases.cpp
@@ -8,12 +8,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-02-18
+ * \date 2015-02-21
  */
 
 #include "signalsTestCases.hpp"
 
 #include "SignalsWaitTestCase.hpp"
+#include "SignalsWaitOperationsTestCase.hpp"
 
 namespace distortos
 {
@@ -34,10 +35,14 @@ const SignalsWaitTestCase::Implementation waitTestCaseImplementation;
 /// SignalsWaitTestCase instance
 const SignalsWaitTestCase waitTestCase {waitTestCaseImplementation};
 
+/// SignalsWaitOperationsTestCase instance
+const SignalsWaitOperationsTestCase waitOperationsTestCase;
+
 /// array with references to TestCase objects related to signals
 const TestCaseRange::value_type messageQueueTestCases_[]
 {
 		TestCaseRange::value_type{waitTestCase},
+		TestCaseRange::value_type{waitOperationsTestCase},
 };
 
 }	// namespace
