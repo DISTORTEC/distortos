2ca5f41cadc1e4a0a0f6bc3827113920041cb1a3
Kamil Szczygiel <distortec@distortec.com>
2015-02-23 18:46:25 +0100

    add include/sys/signal.h - override for newlib's sys/signal.h header

---
+++ include/sys/signal.h
@@ -0,0 +1,33 @@
+/**
+ * \file
+ * \brief Override for newlib's sys/signal.h
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-02-23
+ */
+
+#ifndef INCLUDE_SYS_SIGNAL_H_
+#define INCLUDE_SYS_SIGNAL_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif	/* def __cplusplus */
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global types
++---------------------------------------------------------------------------------------------------------------------*/
+
+/** pointer to function taking an int argument (required by newlib's signal.h) */
+typedef void (*_sig_func_ptr)(int);
+
+#ifdef __cplusplus
+}	/* extern "C" */
+#endif	/* def __cplusplus */
+
+#endif /* INCLUDE_SYS_SIGNAL_H_ */
