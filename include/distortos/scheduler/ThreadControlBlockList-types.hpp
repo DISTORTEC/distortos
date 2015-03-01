82a10b7b1ba1bffa5d9ed9ea6ac2f0a510c89cc5
Kamil Szczygiel <distortec@distortec.com>
2015-03-01 11:06:56 +0100

    ThreadControlBlockList: rename ThreadControlBlockListContainer type alias to ThreadControlBlockUnsortedList

--- include/distortos/scheduler/ThreadControlBlockList-types.hpp
+++ include/distortos/scheduler/ThreadControlBlockList-types.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-02-12
+ * \date 2015-03-01
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCKLIST_TYPES_HPP_
@@ -36,10 +36,10 @@ using ThreadControlBlockListAllocator =
 		allocators::PoolAllocator<ThreadControlBlockListValueType, allocators::SimpleFeedablePool>;
 
 /// underlying unsorted container of ThreadControlBlockList
-using ThreadControlBlockListContainer = std::list<ThreadControlBlockListValueType, ThreadControlBlockListAllocator>;
+using ThreadControlBlockUnsortedList = std::list<ThreadControlBlockListValueType, ThreadControlBlockListAllocator>;
 
 /// generic iterator for ThreadControlBlockList
-using ThreadControlBlockListIterator = ThreadControlBlockListContainer::iterator;
+using ThreadControlBlockListIterator = ThreadControlBlockUnsortedList::iterator;
 
 }	// namespace scheduler
 
--- include/distortos/scheduler/ThreadControlBlockList.hpp
+++ include/distortos/scheduler/ThreadControlBlockList.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief ThreadControlBlockList class header
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-29
+ * \date 2015-03-01
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_THREADCONTROLBLOCKLIST_HPP_
@@ -45,7 +45,7 @@ struct ThreadControlBlockDescendingEffectivePriority
 /// base of ThreadControlBlockList
 using ThreadControlBlockListBase = containers::SortedContainer
 		<
-				ThreadControlBlockListContainer,
+				ThreadControlBlockUnsortedList,
 				ThreadControlBlockDescendingEffectivePriority
 		>;
 
