4ced524c12b9d3fe7217a10a151ae6e2d594ed12
Kamil Szczygiel <distortec@distortec.com>
2015-03-01 11:03:34 +0100

    SoftwareTimerControlBlockList.hpp: move #include of <functional> to SoftwareTimerControlBlockList-types.hpp

--- include/distortos/scheduler/SoftwareTimerControlBlockList.hpp
+++ include/distortos/scheduler/SoftwareTimerControlBlockList.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief SoftwareTimerControlBlockList class header
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-12-20
+ * \date 2015-03-01
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_SOFTWARETIMERCONTROLBLOCKLIST_HPP_
@@ -17,8 +17,6 @@
 #include "distortos/scheduler/SoftwareTimerControlBlock.hpp"
 
 #include "distortos/containers/SortedContainer.hpp"
-
-#include <functional>
 
 namespace distortos
 {
