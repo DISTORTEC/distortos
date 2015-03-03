c63f4b19ab194c5ef9ed37fdd433cee6ff909c16
Kamil Szczygiel <distortec@distortec.com>
2015-03-03 23:46:44 +0100

    ThisThread::get(): implement with Scheduler::getCurrentThreadControlBlock() and ThreadControlBlock::getOwner() instead of Scheduler::getCurrentThreadBase()

--- source/threads/ThisThread.cpp
+++ source/threads/ThisThread.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-02-16
+ * \date 2015-03-03
  */
 
 #include "distortos/ThisThread.hpp"
@@ -28,7 +28,7 @@ namespace ThisThread
 
 ThreadBase& get()
 {
-	return scheduler::getScheduler().getCurrentThreadBase();
+	return scheduler::getScheduler().getCurrentThreadControlBlock().getOwner();
 }
 
 uint8_t getEffectivePriority()
