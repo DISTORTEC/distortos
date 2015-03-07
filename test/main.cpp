4f42ee606f58475affcf5b3d5d9b04f832dc964c
Kamil Szczygiel <distortec@distortec.com>
2015-03-07 23:53:14 +0100

    main: use bigger stack for test thread if _REENT_SMALL is _NOT_ defined
    When _REENT_SMALL is not defined, large _reent struct is used. This
    variant of _reent is almost 1kB bigger than small variant, so RAM usage
    is increased dramatically.

--- test/main.cpp
+++ test/main.cpp
@@ -2,13 +2,13 @@
  * \file
  * \brief Main code block.
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-10-27
+ * \date 2015-03-07
  */
 
 #include "testThreadFunction.hpp"
@@ -23,7 +23,11 @@ namespace
 +---------------------------------------------------------------------------------------------------------------------*/
 
 /// size of stack for test thread, bytes
+#ifdef _REENT_SMALL
 constexpr size_t testThreadStackSize {10240};
+#else
+constexpr size_t testThreadStackSize {20480};
+#endif	// def _REENT_SMALL
 
 /// test thread
 auto testThread = distortos::makeStaticThread<testThreadStackSize>(UINT8_MAX / 2, distortos::test::testThreadFunction);
