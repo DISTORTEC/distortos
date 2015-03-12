5a655f79543728d4934f7ab33b388d3a009b1162
Kamil Szczygiel <distortec@distortec.com>
2015-03-12 21:29:05 +0100

    enableInterruptMasking() for ARMv7-M: fix bug in shifting of priority

--- source/architecture/ARM/ARMv7-M/ARMv7-M-enableInterruptMasking.cpp
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-enableInterruptMasking.cpp
@@ -2,13 +2,13 @@
  * \file
  * \brief enableInterruptMasking() implementation for ARMv7-M (Cortex-M3 / Cortex-M4)
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-10-27
+ * \date 2015-03-12
  */
 
 #include "distortos/architecture/architecture.hpp"
@@ -30,7 +30,7 @@ namespace architecture
 InterruptMask enableInterruptMasking()
 {
 	const auto interruptMask = __get_BASEPRI();
-	__set_BASEPRI(CONFIG_ARCHITECTURE_ARMV7_M_KERNEL_BASEPRI << __NVIC_PRIO_BITS);
+	__set_BASEPRI(CONFIG_ARCHITECTURE_ARMV7_M_KERNEL_BASEPRI << (8 - __NVIC_PRIO_BITS));
 	return interruptMask;
 }
 
