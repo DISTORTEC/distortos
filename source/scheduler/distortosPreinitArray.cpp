fb67476686dce887c1970898d5b901cd1acbf9c8
Kamil Szczygiel <distortec@distortec.com>
2015-03-12 22:02:50 +0100

    distortosPreinitArray[]: add address of mallocLockingInitialization()

--- source/scheduler/distortosPreinitArray.cpp
+++ source/scheduler/distortosPreinitArray.cpp
@@ -8,11 +8,12 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-03-10
+ * \date 2015-03-12
  */
 
 #include "distortos/scheduler/lowLevelSchedulerInitialization.hpp"
 
+#include "distortos/syscalls/mallocLockingInitialization.hpp"
 #include "distortos/architecture/architecture.hpp"
 
 namespace distortos
@@ -39,6 +40,7 @@ using FunctionPointer = void(*)();
 const FunctionPointer distortosPreinitArray[] __attribute__ ((section(".preinit_array"), used))
 {
 		lowLevelSchedulerInitialization,
+		syscalls::mallocLockingInitialization,
 		architecture::startScheduling,
 };
 
