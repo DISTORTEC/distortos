e2cd11daa04662ed209a336b996dd2a3a2395ec0
Kamil Szczygiel <distortec@distortec.com>
2015-03-12 22:00:36 +0100

    add mallocLockingInitialization() in new source/syscalls/mallocLocking.cpp module, add declaration in include/distortos/syscalls/mallocLockingInitialization.hpp

---
+++ include/distortos/syscalls/mallocLockingInitialization.hpp
@@ -0,0 +1,40 @@
+/**
+ * \file
+ * \brief mallocLockingInitialization() declaration
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-03-12
+ */
+
+#ifndef INCLUDE_DISTORTOS_SYSCALLS_MALLOCLOCKINGINITIALIZATION_HPP_
+#define INCLUDE_DISTORTOS_SYSCALLS_MALLOCLOCKINGINITIALIZATION_HPP_
+
+namespace distortos
+{
+
+namespace syscalls
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions' declarations
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Initializes newlib's malloc() locking.
+ *
+ * This function is called before constructors for global and static objects from __libc_init_array() via address in
+ * distortosPreinitArray[].
+ */
+
+void mallocLockingInitialization();
+
+}	// namespace syscalls
+
+}	// namespace distortos
+
+#endif	// INCLUDE_DISTORTOS_SYSCALLS_MALLOCLOCKINGINITIALIZATION_HPP_
---
+++ source/syscalls/mallocLocking.cpp
@@ -0,0 +1,47 @@
+/**
+ * \file
+ * \brief Implementation of newlib's malloc() locking functions
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-03-12
+ */
+
+#include "distortos/syscalls/mallocLockingInitialization.hpp"
+
+#include "distortos/Mutex.hpp"
+
+namespace distortos
+{
+
+namespace syscalls
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// storage for mutex used by malloc() locking functions
+std::aligned_storage<sizeof(Mutex), alignof(Mutex)>::type mallocLockingMutexStorage;
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void mallocLockingInitialization()
+{
+	new (&mallocLockingMutexStorage) Mutex {Mutex::Type::Recursive, Mutex::Protocol::PriorityInheritance};
+}
+
+}	// namespace syscalls
+
+}	// namespace distortos
