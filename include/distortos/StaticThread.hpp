49d7511ca040d4b7e610a6b03426b70f15d36b24
Kamil Szczygiel <distortec@distortec.com>
2015-03-14 18:31:27 +0100

    StaticThread: add specialization for threads with enabled reception of signals (CanReceiveSignals == true)

--- include/distortos/StaticThread.hpp
+++ include/distortos/StaticThread.hpp
@@ -15,6 +15,7 @@
 #define INCLUDE_DISTORTOS_STATICTHREAD_HPP_
 
 #include "distortos/Thread.hpp"
+#include "distortos/SignalsReceiver.hpp"
 
 namespace distortos
 {
@@ -76,6 +77,70 @@ private:
 
 	/// stack buffer
 	typename std::aligned_storage<StackSize>::type stack_;
+};
+
+/**
+ * \brief StaticThread class is a templated interface for thread that has automatic storage for stack and internal
+ * SignalsReceiver object.
+ *
+ * Specialization for threads with enabled reception of signals (CanReceiveSignals == true)
+ *
+ * \param StackSize is the size of stack, bytes
+ * \param Function is the function that will be executed in separate thread
+ * \param Args are the arguments for Function
+ */
+
+template<size_t StackSize, typename Function, typename... Args>
+class StaticThread<StackSize, true, Function, Args...> : public Thread<Function, Args...>
+{
+public:
+
+	/// base of StaticThread
+	using Base = Thread<Function, Args...>;
+
+	/**
+	 * \brief StaticThread's constructor
+	 *
+	 * \param [in] priority is the thread's priority, 0 - lowest, UINT8_MAX - highest
+	 * \param [in] schedulingPolicy is the scheduling policy of the thread
+	 * \param [in] function is a function that will be executed in separate thread
+	 * \param [in] args are arguments for function
+	 */
+
+	StaticThread(const uint8_t priority, const SchedulingPolicy schedulingPolicy, Function&& function, Args&&... args) :
+			Base{&stack_, sizeof(stack_), priority, schedulingPolicy, &signalsReceiver_,
+					std::forward<Function>(function), std::forward<Args>(args)...}
+	{
+
+	}
+
+	/**
+	 * \brief StaticThread's constructor
+	 *
+	 * \param [in] priority is the thread's priority, 0 - lowest, UINT8_MAX - highest
+	 * \param [in] function is a function that will be executed in separate thread
+	 * \param [in] args are arguments for function
+	 */
+
+	StaticThread(const uint8_t priority, Function&& function, Args&&... args) :
+			StaticThread{priority, SchedulingPolicy::RoundRobin, std::forward<Function>(function),
+					std::forward<Args>(args)...}
+	{
+
+	}
+
+	StaticThread(const StaticThread&) = delete;
+	StaticThread(StaticThread&&) = default;
+	const StaticThread& operator=(const StaticThread&) = delete;
+	StaticThread& operator=(StaticThread&&) = delete;
+
+private:
+
+	/// stack buffer
+	typename std::aligned_storage<StackSize>::type stack_;
+
+	/// internal SignalsReceiver object
+	SignalsReceiver signalsReceiver_;
 };
 
 /**
