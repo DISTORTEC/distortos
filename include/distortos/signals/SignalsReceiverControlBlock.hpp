4601e78d818856604b1ab160225fd9af21e1049e
Kamil Szczygiel <distortec@distortec.com>
2015-03-14 16:35:09 +0100

    add empty SignalsReceiverControlBlock class

---
+++ include/distortos/signals/SignalsReceiverControlBlock.hpp
@@ -0,0 +1,33 @@
+/**
+ * \file
+ * \brief SignalsReceiverControlBlock class header
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-03-14
+ */
+
+#ifndef INCLUDE_DISTORTOS_SIGNALS_SIGNALSRECEIVERCONTROLBLOCK_HPP_
+#define INCLUDE_DISTORTOS_SIGNALS_SIGNALSRECEIVERCONTROLBLOCK_HPP_
+
+namespace distortos
+{
+
+namespace synchronization
+{
+
+/// SignalsReceiverControlBlock class is a structure required by threads for "receiving" of signals
+class SignalsReceiverControlBlock
+{
+
+};
+
+}	// namespace synchronization
+
+}	// namespace distortos
+
+#endif	// INCLUDE_DISTORTOS_SIGNALS_SIGNALSRECEIVERCONTROLBLOCK_HPP_
