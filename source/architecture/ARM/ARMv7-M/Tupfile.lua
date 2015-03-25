f86b3d9ba772d0f96cab1fb55f78aa33fee41e0a
Kamil Szczygiel <distortec@distortec.com>
2015-03-25 19:48:03 +0100

    move CMSIS for architecture from source/architecture/ARM/external to top-level external folder

--- source/architecture/ARM/ARMv7-M/Tupfile.lua
+++ source/architecture/ARM/ARMv7-M/Tupfile.lua
@@ -1,12 +1,12 @@
 --
 -- file: Tupfile.lua
 --
--- author: Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+-- author: Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 --
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- date: 2014-07-29
+-- date: 2015-03-25
 --
 
 ASFLAGS["ARMv7-M-startup.S"] = "-D__USES_TWO_STACKS -D__USES_CXX"
@@ -17,7 +17,7 @@ CXXFLAGS += "-I" .. TOP .. "/include"
 
 CXXFLAGS += "-I" .. TOP .. "/source/chip/STMicroelectronics/STM32F4/include"
 CXXFLAGS += "-I" .. TOP .. "/source/chip/STMicroelectronics/STM32F4/external/CMSIS"
-CXXFLAGS += "-I" .. TOP .. "/source/architecture/ARM/external/CMSIS"
+CXXFLAGS += "-I" .. TOP .. "/external/CMSIS"
 
 CXXFLAGS += "-I" .. TOP .. "/source/architecture/ARM/ARMv7-M/include"
 
