e4dc85d6d3d2ac7063e24f08fd6c205417a4d672
Kamil Szczygiel <distortec@distortec.com>
2015-04-07 23:08:53 +0200

    tup build: the flag "-x assembler-with-cpp" is not necessary for assembly files as long as they have .S suffix

--- Tuprules.lua
+++ Tuprules.lua
@@ -6,7 +6,7 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- date: 2015-03-18
+-- date: 2015-04-07
 --
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -22,7 +22,7 @@ OUTPUT = TOP .. "/output/"
 
 TOOLCHAIN = "arm-none-eabi-"
 
-AS = TOOLCHAIN .. "gcc -x assembler-with-cpp"
+AS = TOOLCHAIN .. "gcc"
 CC = TOOLCHAIN .. "gcc"
 CXX = TOOLCHAIN .. "g++"
 LD = TOOLCHAIN .. "g++"
