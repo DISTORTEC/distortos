234e01aeb4d61ac53d68cda5d32fa59399251e63
Kamil Szczygiel <distortec@distortec.com>
2015-04-10 21:04:23 +0200

    estd::ContiguousRange: add constructor using single value

--- include/distortos/estd/ContiguousRange.hpp
+++ include/distortos/estd/ContiguousRange.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief ContiguousRange template class header.
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-29
+ * \date 2015-04-10
  */
 
 #ifndef ESTD_CONTIGUOUSRANGE_HPP_
@@ -98,6 +98,18 @@ public:
 	template<size_t N>
 	constexpr explicit ContiguousRange(T (&array)[N]) noexcept :
 			ContiguousRange{array, array + N}
+	{
+
+	}
+
+	/**
+	 * \brief ContiguousRange's constructor using single value
+	 *
+	 * \param [in] value is a reference to variable used to initialize the range
+	 */
+
+	constexpr explicit ContiguousRange(T& value) noexcept :
+			ContiguousRange{&value, &value + 1}
 	{
 
 	}
