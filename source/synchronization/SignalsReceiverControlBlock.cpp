5fc1181b069bcc6f67c55452bdb5dce1ae24cbc6
Kamil Szczygiel <distortec@distortec.com>
2015-04-23 19:35:50 +0200

    SignalsReceiverControlBlock::postGenerate(): remove the check for validity of signalNumber argument - it is checked by all callers of this function

--- source/synchronization/SignalsReceiverControlBlock.cpp
+++ source/synchronization/SignalsReceiverControlBlock.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-04-20
+ * \date 2015-04-23
  */
 
 #include "distortos/synchronization/SignalsReceiverControlBlock.hpp"
@@ -126,12 +126,12 @@ int SignalsReceiverControlBlock::setSignalMask(const SignalSet signalMask)
 int SignalsReceiverControlBlock::postGenerate(const uint8_t signalNumber,
 		const scheduler::ThreadControlBlock& threadControlBlock) const
 {
+	/// \todo add some form of assertion for validity of \a signalNumber
+
 	if (waitingSignalSet_ == nullptr)
 		return 0;
 
 	const auto testResult = waitingSignalSet_->test(signalNumber);
-	if (testResult.first != 0)
-		return testResult.first;
 	if (testResult.second == false)	// signalNumber is not "waited for"?
 		return 0;
 
