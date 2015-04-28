3c18cf1f0360703da8949f488df6eca7d284e183
Kamil Szczygiel <distortec@distortec.com>
2015-04-29 00:04:03 +0200

    STM32F4: rename STM32F4-vectors.c to STM32F4-chipVectors.c

--- source/chip/STMicroelectronics/STM32F4/STM32F4-chipVectors.c
+++ source/chip/STMicroelectronics/STM32F4/STM32F4-chipVectors.c
@@ -1,14 +1,14 @@
 /**
  * \file
- * \brief STM32F4 vector table and default weak handlers.
+ * \brief STM32F4 chip vector table and default weak handlers.
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-07-22
+ * \date 2015-04-28
  */
 
 #include "distortos/distortosConfiguration.h"
