a1de91148264a0ffbd3f11f49b9e84cd3848ca74
Kamil Szczygiel <distortec@distortec.com>
2015-04-29 00:00:05 +0200

    ARMv7-M: rename ARMv7-M-vectors.c to ARMv7-M-coreVectors.c

--- source/architecture/ARM/ARMv7-M/ARMv7-M-coreVectors.c
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-coreVectors.c
@@ -1,14 +1,14 @@
 /**
  * \file
- * \brief ARMv7-M (Cortex-M3 / Cortex-M4) vector table and default weak handlers.
+ * \brief ARMv7-M (Cortex-M3 / Cortex-M4) core vector table and default weak handlers.
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-07-21
+ * \date 2014-04-28
  */
 
 /*---------------------------------------------------------------------------------------------------------------------+
