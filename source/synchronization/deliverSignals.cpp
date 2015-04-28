437d7263213b3a93e1722dc231b441d22ed26fbd
Kamil Szczygiel <distortec@distortec.com>
2015-04-28 21:38:27 +0200

    add stub of synchronization::deliverSignals()

---
+++ source/synchronization/deliverSignals.cpp
@@ -0,0 +1,33 @@
+/**
+ * \file
+ * \brief deliverSignals() definition
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-04-28
+ */
+
+#include "distortos/synchronization/deliverSignals.hpp"
+
+namespace distortos
+{
+
+namespace synchronization
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void deliverSignals()
+{
+
+}
+
+}	// namespace synchronization
+
+}	// namespace distortos
