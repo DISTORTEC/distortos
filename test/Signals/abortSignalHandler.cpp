416b91dd09ca40486e5aa7914d7920d10e0bac86
Kamil Szczygiel <distortec@distortec.com>
2015-05-14 18:49:31 +0200

    test: add abortSignalHandler()

---
+++ test/Signals/abortSignalHandler.cpp
@@ -0,0 +1,35 @@
+/**
+ * \file
+ * \brief abortSignalHandler() definition
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-05-14
+ */
+
+#include "abortSignalHandler.hpp"
+
+#include <cstdlib>
+
+namespace distortos
+{
+
+namespace test
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void abortSignalHandler(const SignalInformation&)
+{
+	abort();
+}
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/Signals/abortSignalHandler.hpp
@@ -0,0 +1,37 @@
+/**
+ * \file
+ * \brief abortSignalHandler() declaration
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-05-14
+ */
+
+#ifndef TEST_SIGNALS_ABORTSIGNALHANDLER_HPP_
+#define TEST_SIGNALS_ABORTSIGNALHANDLER_HPP_
+
+namespace distortos
+{
+
+class SignalInformation;
+
+namespace test
+{
+
+/**
+ * \brief Signal handler that is never supposed to be executed - it calls abort().
+ *
+ * \param [in] signalInformation is a reference to received SignalInformation object
+ */
+
+void abortSignalHandler(const SignalInformation& signalInformation);
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_SIGNALS_ABORTSIGNALHANDLER_HPP_
