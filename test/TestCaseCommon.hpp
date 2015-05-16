705598a6a2db5e9f7ca37eae3fc28270d8225597
Kamil Szczygiel <distortec@distortec.com>
2015-05-16 10:04:02 +0200

    test: add TestCaseCommon class - currently empty

---
+++ test/TestCaseCommon.hpp
@@ -0,0 +1,35 @@
+/**
+ * \file
+ * \brief TestCaseCommon class header
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-05-16
+ */
+
+#ifndef TEST_TESTCASECOMMON_HPP_
+#define TEST_TESTCASECOMMON_HPP_
+
+#include "TestCase.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+/// TestCaseCommon class implements common parts of TestCase
+class TestCaseCommon : public TestCase
+{
+
+};
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_TESTCASECOMMON_HPP_
