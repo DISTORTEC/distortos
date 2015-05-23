531c3b88590ed2829937c919f1166093540b5600
Kamil Szczygiel <distortec@distortec.com>
2015-05-23 17:35:32 +0200

    architecture/ARM/ARMv7-M: use StackFrame struct in initializeStack()

--- source/architecture/ARM/ARMv7-M/ARMv7-M-initializeStack.cpp
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-initializeStack.cpp
@@ -8,14 +8,12 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-04-28
+ * \date 2015-05-23
  */
 
 #include "distortos/architecture/initializeStack.hpp"
 
-#include "distortos/chip/CMSIS-proxy.h"
-
-#include <type_traits>
+#include "StackFrame.hpp"
 
 namespace distortos
 {
@@ -29,31 +27,30 @@ namespace architecture
 
 void* initializeStack(void* const buffer, const size_t size, void (& function)(ThreadBase&), ThreadBase& threadBase)
 {
-	auto stackPointer = reinterpret_cast<uint32_t*>(static_cast<uint8_t*>(buffer) + size);
+	const auto stackFrame = reinterpret_cast<StackFrame*>(static_cast<uint8_t*>(buffer) + size) - 1;
 
-	using StackElement = std::decay<decltype(*stackPointer)>::type;
-
-	*--stackPointer = 0x01000000;									// xPSR
-	*--stackPointer = reinterpret_cast<StackElement>(&function);	// pc
-	*--stackPointer = 0;											// lr
-	*--stackPointer = 0xcccccccc;									// r12
-	*--stackPointer = 0x33333333;									// r3
-	*--stackPointer = 0x22222222;									// r2
-	*--stackPointer = 0x11111111;									// r1
-	*--stackPointer = reinterpret_cast<StackElement>(&threadBase);	// r0
+	stackFrame->softwareStackFrame.r4 = reinterpret_cast<void*>(0x44444444);
+	stackFrame->softwareStackFrame.r5 = reinterpret_cast<void*>(0x55555555);
+	stackFrame->softwareStackFrame.r6 = reinterpret_cast<void*>(0x66666666);
+	stackFrame->softwareStackFrame.r7 = reinterpret_cast<void*>(0x77777777);
+	stackFrame->softwareStackFrame.r8 = reinterpret_cast<void*>(0x88888888);
+	stackFrame->softwareStackFrame.r9 = reinterpret_cast<void*>(0x99999999);
+	stackFrame->softwareStackFrame.r10 = reinterpret_cast<void*>(0xaaaaaaaa);
+	stackFrame->softwareStackFrame.r11 = reinterpret_cast<void*>(0xbbbbbbbb);
 #if __FPU_PRESENT == 1 && __FPU_USED == 1
-	*--stackPointer = 0xfffffffd;									// lr
+	stackFrame->softwareStackFrame.exceptionReturn = SoftwareStackFrame::defaultExceptionReturn;
 #endif	// __FPU_PRESENT == 1 && __FPU_USED == 1
-	*--stackPointer = 0xbbbbbbbb;									// r11
-	*--stackPointer = 0xaaaaaaaa;									// r10
-	*--stackPointer = 0x99999999;									// r9
-	*--stackPointer = 0x88888888;									// r8
-	*--stackPointer = 0x77777777;									// r7
-	*--stackPointer = 0x66666666;									// r6
-	*--stackPointer = 0x55555555;									// r5
-	*--stackPointer = 0x44444444;									// r4
 
-	return stackPointer;
+	stackFrame->exceptionStackFrame.r0 = &threadBase;
+	stackFrame->exceptionStackFrame.r1 = reinterpret_cast<void*>(0x11111111);
+	stackFrame->exceptionStackFrame.r2 = reinterpret_cast<void*>(0x22222222);
+	stackFrame->exceptionStackFrame.r3 = reinterpret_cast<void*>(0x33333333);
+	stackFrame->exceptionStackFrame.r12 = reinterpret_cast<void*>(0xcccccccc);
+	stackFrame->exceptionStackFrame.lr = nullptr;
+	stackFrame->exceptionStackFrame.pc = reinterpret_cast<void*>(&function);
+	stackFrame->exceptionStackFrame.xpsr = ExceptionStackFrame::defaultXpsr;
+
+	return stackFrame;
 }
 
 }	// namespace architecture
