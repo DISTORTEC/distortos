ce13f7af5f443fd0a6ce1a834392e344936c71a5
Kamil Szczygiel <distortec@distortec.com>
2015-05-24 19:54:36 +0200

    SoftwareTimer: improve formatting

--- include/distortos/SoftwareTimer.hpp
+++ include/distortos/SoftwareTimer.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief SoftwareTimer class header
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-29
+ * \date 2015-05-24
  */
 
 #ifndef INCLUDE_DISTORTOS_SOFTWARETIMER_HPP_
@@ -61,7 +61,10 @@ private:
 	 * Executes bound function object.
 	 */
 
-	virtual void execute_() const override { boundFunction_(); }
+	virtual void execute_() const override
+	{
+		boundFunction_();
+	}
 
 	/// bound function object
 	decltype(std::bind(std::declval<Function>(), std::declval<Args>()...)) boundFunction_;
