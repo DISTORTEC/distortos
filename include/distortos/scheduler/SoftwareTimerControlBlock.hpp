8fdc5731d78b1725d6658e723650e58b93fc5650
Kamil Szczygiel <distortec@distortec.com>
2015-05-24 19:56:55 +0200

    SoftwareTimerControlBlock: improve formatting

--- include/distortos/scheduler/SoftwareTimerControlBlock.hpp
+++ include/distortos/scheduler/SoftwareTimerControlBlock.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief SoftwareTimerControlBlock class header
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-29
+ * \date 2015-05-24
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_SOFTWARETIMERCONTROLBLOCK_HPP_
@@ -50,25 +50,37 @@ public:
 	 * \note this should only be called by SoftwareTimerSupervisor::tickInterruptHandler()
 	 */
 
-	void execute() const { execute_(); }
+	void execute() const
+	{
+		execute_();
+	}
 
 	/**
 	 * \return reference to internal storage for list link
 	 */
 
-	Link& getLink() { return link_; }
+	Link& getLink()
+	{
+		return link_;
+	}
 
 	/**
 	 * \return const reference to internal storage for list link
 	 */
 
-	const Link& getLink() const { return link_; }
+	const Link& getLink() const
+	{
+		return link_;
+	}
 
 	/**
 	 * \return const reference to expiration time point
 	 */
 
-	const TickClock::time_point& getTimePoint() const { return timePoint_; }
+	const TickClock::time_point& getTimePoint() const
+	{
+		return timePoint_;
+	}
 
 	/**
 	 * \return true if the timer is running, false otherwise
