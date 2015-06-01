6bd3bde352b3a95efdbe0e6b51fec875a9a7f9cc
Kamil Szczygiel <distortec@distortec.com>
2015-06-01 23:14:23 +0200

    Scheduler::switchContext(): don't mask interrupts - this is done by the caller now

--- source/scheduler/Scheduler.cpp
+++ source/scheduler/Scheduler.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-04-28
+ * \date 2015-06-01
  */
 
 #include "distortos/scheduler/Scheduler.hpp"
@@ -194,7 +194,6 @@ int Scheduler::suspend(const ThreadControlBlockListIterator iterator)
 
 void* Scheduler::switchContext(void* const stackPointer)
 {
-	architecture::InterruptMaskingLock interruptMaskingLock;
 	++contextSwitchCount_;
 	getCurrentThreadControlBlock().getStack().setStackPointer(stackPointer);
 	currentThreadControlBlock_ = runnableList_.begin();
