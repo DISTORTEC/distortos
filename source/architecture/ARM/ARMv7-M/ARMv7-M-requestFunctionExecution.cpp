43d38ed1954177dfa81f4b056598170a633678fc
Kamil Szczygiel <distortec@distortec.com>
2015-06-03 23:32:47 +0200

    architecture/ARM/ARMv7-M, requestFunctionExecution(): add support for requests to non-current thread

--- source/architecture/ARM/ARMv7-M/ARMv7-M-requestFunctionExecution.cpp
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-requestFunctionExecution.cpp
@@ -14,6 +14,7 @@
 #include "distortos/architecture/requestFunctionExecution.hpp"
 
 #include "ExceptionFpuStackFrame.hpp"
+#include "StackFrame.hpp"
 #include "supervisorCall.hpp"
 
 #include "distortos/scheduler/Scheduler.hpp"
@@ -185,6 +186,44 @@ void fromInterruptToCurrentThread(void (& function)())
 	__set_PSP(reinterpret_cast<uint32_t>(exceptionFpuStackFrame));
 }
 
+/**
+ * \brief Handles request to execute provided function in non-current thread.
+ *
+ * \param [in] threadControlBlock is a reference to scheduler::ThreadControlBlock of thread in which \a function should
+ * be executed
+ * \param [in] function is a reference to function that should be executed in thread associated with
+ * \a threadControlBlock
+ */
+
+void toNonCurrentThread(scheduler::ThreadControlBlock& threadControlBlock, void (& function)())
+{
+	const auto stackPointer = threadControlBlock.getStack().getStackPointer();
+	const auto stackFrame = reinterpret_cast<StackFrame*>(stackPointer) - 1;
+
+	stackFrame->softwareStackFrame.r4 = reinterpret_cast<void*>(0x44444444);
+	stackFrame->softwareStackFrame.r5 = reinterpret_cast<void*>(0x55555555);
+	stackFrame->softwareStackFrame.r6 = reinterpret_cast<void*>(0x66666666);
+	stackFrame->softwareStackFrame.r7 = reinterpret_cast<void*>(0x77777777);
+	stackFrame->softwareStackFrame.r8 = reinterpret_cast<void*>(0x88888888);
+	stackFrame->softwareStackFrame.r9 = reinterpret_cast<void*>(0x99999999);
+	stackFrame->softwareStackFrame.r10 = reinterpret_cast<void*>(0xaaaaaaaa);
+	stackFrame->softwareStackFrame.r11 = reinterpret_cast<void*>(0xbbbbbbbb);
+#if __FPU_PRESENT == 1 && __FPU_USED == 1
+	stackFrame->softwareStackFrame.exceptionReturn = SoftwareStackFrame::defaultExceptionReturn;
+#endif	// __FPU_PRESENT == 1 && __FPU_USED == 1
+
+	stackFrame->exceptionStackFrame.r0 = reinterpret_cast<void*>(&function);
+	stackFrame->exceptionStackFrame.r1 = reinterpret_cast<void*>(stackPointer);
+	stackFrame->exceptionStackFrame.r2 = reinterpret_cast<void*>(true);
+	stackFrame->exceptionStackFrame.r3 = reinterpret_cast<void*>(0x33333333);
+	stackFrame->exceptionStackFrame.r12 = reinterpret_cast<void*>(0xcccccccc);
+	stackFrame->exceptionStackFrame.lr = nullptr;
+	stackFrame->exceptionStackFrame.pc = reinterpret_cast<void*>(&functionTrampoline);
+	stackFrame->exceptionStackFrame.xpsr = ExceptionStackFrame::defaultXpsr;
+
+	threadControlBlock.getStack().setStackPointer(stackFrame);
+}
+
 }	// namespace
 
 /*---------------------------------------------------------------------------------------------------------------------+
@@ -202,6 +241,8 @@ void requestFunctionExecution(scheduler::ThreadControlBlock& threadControlBlock,
 		else						// interrupt is sending the request to current thread?
 			fromInterruptToCurrentThread(function);
 	}
+	else	// request to non-current thread
+		toNonCurrentThread(threadControlBlock, function);
 }
 
 }	// namespace architecture
