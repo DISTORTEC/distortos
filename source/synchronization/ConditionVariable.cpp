51b1c95d781fe91d9c35f5fe4b98c965e3478ffb
Kamil Szczygiel <distortec@distortec.com>
2015-06-22 22:18:40 +0200

    ConditionVariable::waitUntil(): don't return EINTR in case of spurious wakeup

--- source/synchronization/ConditionVariable.cpp
+++ source/synchronization/ConditionVariable.cpp
@@ -2,13 +2,13 @@
  * \file
  * \brief ConditionVariable class implementation
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-20
+ * \date 2015-06-22
  */
 
 #include "distortos/ConditionVariable.hpp"
@@ -18,6 +18,8 @@
 #include "distortos/scheduler/Scheduler.hpp"
 
 #include "distortos/architecture/InterruptMaskingLock.hpp"
+
+#include <cerrno>
 
 namespace distortos
 {
@@ -84,7 +86,7 @@ int ConditionVariable::waitUntil(Mutex& mutex, const TickClock::time_point timeP
 	}
 
 	const auto ret = mutex.lock();
-	return ret != 0 ? ret : blockUntilRet;
+	return ret != 0 ? ret : blockUntilRet != EINTR ? blockUntilRet : 0;	// don't return EINTR in case of spurious wakeup
 }
 
 }	// namespace distortos
