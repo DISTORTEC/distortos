63fdc1247c63711069a4d4d805f3133dbf078bcc
Kamil Szczygiel <distortec@distortec.com>
2015-06-25 21:07:41 +0200

    rename ARMv7-M-CONTROL.h to ARMv7-M-CONTROL-bits.h

--- source/architecture/ARM/ARMv7-M/ARMv7-M-CONTROL-bits.h
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-CONTROL-bits.h
@@ -8,11 +8,11 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-05-23
+ * \date 2015-06-25
  */
 
-#ifndef SOURCE_ARCHITECTURE_ARM_ARMV7_M_ARMV7_M_CONTROL_H_
-#define SOURCE_ARCHITECTURE_ARM_ARMV7_M_ARMV7_M_CONTROL_H_
+#ifndef SOURCE_ARCHITECTURE_ARM_ARMV7_M_ARMV7_M_CONTROL_BITS_H_
+#define SOURCE_ARCHITECTURE_ARM_ARMV7_M_ARMV7_M_CONTROL_BITS_H_
 
 /*---------------------------------------------------------------------------------------------------------------------+
 | CONTROL - The special-purpose control register
@@ -24,4 +24,4 @@
 #define CONTROL_nPRIV						(1 << CONTROL_nPRIV_bit)
 #define CONTROL_SPSEL						(1 << CONTROL_SPSEL_bit)
 
-#endif	// SOURCE_ARCHITECTURE_ARM_ARMV7_M_ARMV7_M_CONTROL_H_
+#endif	// SOURCE_ARCHITECTURE_ARM_ARMV7_M_ARMV7_M_CONTROL_BITS_H_
