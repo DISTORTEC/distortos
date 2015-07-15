c63915b84048d88f7bf03170f54f69be410ac9f1
Kamil Szczygiel <distortec@distortec.com>
2015-07-15 22:32:39 +0200

    test: add callOnceTestCases

---
+++ test/CallOnce/callOnceTestCases.cpp
@@ -0,0 +1,52 @@
+/**
+ * \file
+ * \brief callOnceTestCases object definition
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-07-15
+ */
+
+#include "callOnceTestCases.hpp"
+
+#include "CallOnceFunctionTypesTestCase.hpp"
+
+#include "TestCaseGroup.hpp"
+
+namespace distortos
+{
+
+namespace test
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// CallOnceFunctionTypesTestCase instance
+const CallOnceFunctionTypesTestCase functionTypesTestCase;
+
+/// array with references to TestCase objects related to callOnce()
+const TestCaseGroup::Range::value_type callOnceTestCases_[]
+{
+		TestCaseGroup::Range::value_type{functionTypesTestCase},
+};
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const TestCaseGroup callOnceTestCases {TestCaseGroup::Range{callOnceTestCases_}};
+
+}	// namespace test
+
+}	// namespace distortos
---
+++ test/CallOnce/callOnceTestCases.hpp
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \brief callOnceTestCases object declaration
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-07-15
+ */
+
+#ifndef TEST_CALLONCE_CALLONCETESTCASES_HPP_
+#define TEST_CALLONCE_CALLONCETESTCASES_HPP_
+
+namespace distortos
+{
+
+namespace test
+{
+
+class TestCaseGroup;
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// group of test cases related to callOnce()
+extern const TestCaseGroup callOnceTestCases;
+
+}	// namespace test
+
+}	// namespace distortos
+
+#endif	// TEST_CALLONCE_CALLONCETESTCASES_HPP_
