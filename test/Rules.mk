c3b84ba1624a4e3da2a696b68725a9b7682fc63d
Kamil Szczygiel <distortec@distortec.com>
2015-07-17 22:07:46 +0200

    test: add CallOnce test module to "make" based compilation

--- test/Rules.mk
+++ test/Rules.mk
@@ -6,7 +6,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# date: 2015-05-28
+# date: 2015-07-17
 #
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -14,6 +14,7 @@
 #-----------------------------------------------------------------------------------------------------------------------
 
 SUBDIRECTORIES += architecture/ARM/ARMv7-M
+SUBDIRECTORIES += CallOnce
 SUBDIRECTORIES += ConditionVariable
 SUBDIRECTORIES += FifoQueue
 SUBDIRECTORIES += MessageQueue
