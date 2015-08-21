e1a7b98a8da0b32fcb522aebbcbd48548cb31449
Kamil Szczygiel <distortec@distortec.com>
2015-08-21 15:12:45 +0200

    include/distortos/Tupfile.lua: use $(CONFIG_SELECTED_CONFIGURATION)
    Use $(CONFIG_SELECTED_CONFIGURATION) variable to get
    distortosConfiguration.mk file selected by user.

--- include/distortos/Tupfile.lua
+++ include/distortos/Tupfile.lua
@@ -6,9 +6,9 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- date: 2015-08-20
+-- date: 2015-08-21
 --
 
-distortosConfigurationSource = "$(TOP)/configurations/STM32F4DISCOVERY/test/distortosConfiguration.mk"
+distortosConfigurationSource = "$(TOP)/$(CONFIG_SELECTED_CONFIGURATION)/distortosConfiguration.mk"
 
 tup.rule(distortosConfigurationSource, "$(TOP)/scripts/makeDistortosConfiguration.awk %f > %o", "%B.h")
