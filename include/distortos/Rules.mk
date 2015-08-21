f204e77c94c44b47080006308cc410e1e266bd79
Kamil Szczygiel <distortec@distortec.com>
2015-08-21 15:12:11 +0200

    include/distortos/Rules.mk: use $(CONFIG_SELECTED_CONFIGURATION)
    Use $(CONFIG_SELECTED_CONFIGURATION) variable to get
    distortosConfiguration.mk file selected by user.

--- include/distortos/Rules.mk
+++ include/distortos/Rules.mk
@@ -6,14 +6,14 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# date: 2015-08-20
+# date: 2015-08-21
 #
 
 #-----------------------------------------------------------------------------------------------------------------------
 # generated headers
 #-----------------------------------------------------------------------------------------------------------------------
 
-$(d)/distortosConfiguration.h: configurations/STM32F4DISCOVERY/test/distortosConfiguration.mk
+$(d)/distortosConfiguration.h: ./$(subst ",,$(CONFIG_SELECTED_CONFIGURATION))/distortosConfiguration.mk
 	./scripts/makeDistortosConfiguration.awk $< > $@
 
 #-----------------------------------------------------------------------------------------------------------------------
