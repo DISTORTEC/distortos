a4124b4ceaffd6f8c640b43f5b258046727e89dd
Kamil Szczygiel <distortec@distortec.com>
2015-08-24 15:47:57 +0200

    include/distortos/Tupfile.lua: small improvement
    Mark local variable with "local", rename it from "...Source" to
    "...Mk" and use lua syntax instead of make syntax.

--- include/distortos/Tupfile.lua
+++ include/distortos/Tupfile.lua
@@ -6,9 +6,9 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- date: 2015-08-21
+-- date: 2015-08-24
 --
 
-distortosConfigurationSource = "$(TOP)/$(CONFIG_SELECTED_CONFIGURATION)/distortosConfiguration.mk"
+local distortosConfigurationMk = TOP .. "/" .. CONFIG_SELECTED_CONFIGURATION .. "/distortosConfiguration.mk"
 
-tup.rule(distortosConfigurationSource, "$(TOP)/scripts/makeDistortosConfiguration.awk %f > %o", "%B.h")
+tup.rule(distortosConfigurationMk, "$(TOP)/scripts/makeDistortosConfiguration.awk %f > %o", "%B.h")
