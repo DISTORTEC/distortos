8acb6723b756a11efa47076d296949239501d1b7
Kamil Szczygiel <distortec@distortec.com>
2015-08-28 23:05:54 +0200

    tup build: generate linker script for STM32F4 chip
    Generated file is currently unused.

--- source/chip/STMicroelectronics/STM32F4/Tupfile.lua
+++ source/chip/STMicroelectronics/STM32F4/Tupfile.lua
@@ -6,8 +6,10 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- date: 2015-08-24
+-- date: 2015-08-28
 --
+
+tup.rule(DISTORTOS_CONFIGURATION_MK, "./STM32F4.ld.sh \"%f\" > \"%o\"", OUTPUT .. CONFIG_CHIP .. ".ld")
 
 CFLAGS += "-I" .. OUTPUT .. "include"
 CFLAGS += "-I" .. TOP .. "/include"
