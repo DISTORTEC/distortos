68b90dd79d329f89512c67e44ff78e5d9d699ea1
Kamil Szczygiel <distortec@distortec.com>
2015-09-10 16:17:25 +0200

    PrioritizedTestCase: use global CONFIG_MAIN_THREAD_PRIORITY value
    Use CONFIG_MAIN_THREAD_PRIORITY instead of mainTestThreadPriority value
    from test/mainTestThreadParameters.hpp in
    PrioritizedTestCase::finalize().

--- test/PrioritizedTestCase.cpp
+++ test/PrioritizedTestCase.cpp
@@ -8,12 +8,10 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-05-16
+ * \date 2015-09-10
  */
 
 #include "PrioritizedTestCase.hpp"
-
-#include "mainTestThreadParameters.hpp"
 
 #include "distortos/ThisThread.hpp"
 
@@ -29,7 +27,7 @@ namespace test
 
 bool PrioritizedTestCase::finalize() const
 {
-	ThisThread::setPriority(mainTestThreadPriority);
+	ThisThread::setPriority(CONFIG_MAIN_THREAD_PRIORITY);
 	return true;
 }
 
