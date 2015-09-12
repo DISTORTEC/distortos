bbfa0ccc1a875dc8e0c40538d4c594af1bac9856
Kamil Szczygiel <distortec@distortec.com>
2015-09-12 10:13:12 +0200

    test: run the test cases in main() thread
    Remove mainTestThread object and mainTestThreadFunction() and move
    contents of the removed function to main().

--- test/main.cpp
+++ test/main.cpp
@@ -8,23 +8,29 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-05-16
+ * \date 2015-09-12
  */
 
-#include "mainTestThread.hpp"
+#include "TestCaseGroup.hpp"
+#include "testCases.hpp"
 
 /*---------------------------------------------------------------------------------------------------------------------+
 | global functions
 +---------------------------------------------------------------------------------------------------------------------*/
 
 /**
- * \brief main code block
+ * \brief Main code block of test application
+ *
+ * Runs all the test cases. If any test case fails (TestCase::run() returns false) infinite loop is entered so the
+ * situation can be noticed in debugging.
+ *
+ * \return 0 on success, doesn't return otherwise
  */
 
 int main()
 {
-	distortos::test::mainTestThread.start();
-	distortos::test::mainTestThread.join();
+	if (distortos::test::testCases.run() == false)
+		while (1);
 
 	return 0;
 }
