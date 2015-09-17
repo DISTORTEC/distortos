b114a643fc0a202fbe25b9395668c59aa7fe4440
Kamil Szczygiel <distortec@distortec.com>
2015-09-17 22:21:25 +0200

    chip: add disableDataCache() for STM32F4

---
+++ source/chip/STMicroelectronics/STM32F4/include/distortos/chip/STM32F4-FLASH.hpp
@@ -0,0 +1,33 @@
+/**
+ * \file
+ * \brief Header for FLASH-related functions for STM32F4
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-09-17
+ */
+
+#ifndef SOURCE_CHIP_STMICROELECTRONICS_STM32F4_INCLUDE_DISTORTOS_CHIP_STM32F4_FLASH_HPP_
+#define SOURCE_CHIP_STMICROELECTRONICS_STM32F4_INCLUDE_DISTORTOS_CHIP_STM32F4_FLASH_HPP_
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/**
+ * \brief Disables data cache.
+ */
+
+void disableDataCache();
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_CHIP_STMICROELECTRONICS_STM32F4_INCLUDE_DISTORTOS_CHIP_STM32F4_FLASH_HPP_
