1a4d14da93993aa3dafa876da27081c2c866620e
Kamil Szczygiel <distortec@distortec.com>
2015-09-17 19:37:01 +0200

    chip: configure and enable PLL in lowLevelInitialization() for STM32F4

--- source/chip/STMicroelectronics/STM32F4/STM32F4-lowLevelInitialization.cpp
+++ source/chip/STMicroelectronics/STM32F4/STM32F4-lowLevelInitialization.cpp
@@ -44,6 +44,43 @@ void lowLevelInitialization()
 
 #endif	// def CONFIG_CHIP_STM32F4_RCC_HSE_ENABLE
 
+#ifdef CONFIG_CHIP_STM32F4_RCC_PLL_ENABLE
+
+#if defined(CONFIG_CHIP_STM32F4_RCC_PLLSRC_HSI)
+	constexpr bool pllClockSourceHse {};
+#elif defined(CONFIG_CHIP_STM32F4_RCC_PLLSRC_HSE)
+	constexpr bool pllClockSourceHse {true};
+#endif
+	configurePllClockSource(pllClockSourceHse);
+
+	constexpr uint32_t pllInHz {pllClockSourceHse == false ? hsiHz : CONFIG_CHIP_STM32F4_RCC_HSE_HZ};
+	constexpr uint32_t vcoInHz {pllInHz / CONFIG_CHIP_STM32F4_RCC_PLLM};
+	static_assert(minVcoInHz <= vcoInHz && vcoInHz <= maxVcoInHz, "Invalid VCO input frequency!");
+	configurePllInputClockDivider(CONFIG_CHIP_STM32F4_RCC_PLLM);
+
+	constexpr uint32_t vcoOutHz {vcoInHz * CONFIG_CHIP_STM32F4_RCC_PLLN};
+	static_assert(minVcoOutHz <= vcoOutHz && vcoOutHz <= maxVcoOutHz, "Invalid VCO output frequency!");
+
+#if defined(CONFIG_CHIP_STM32F4_RCC_PLLP_DIV2)
+	constexpr uint8_t pllp {pllpDiv2};
+#elif defined(CONFIG_CHIP_STM32F4_RCC_PLLP_DIV4)
+	constexpr uint8_t pllp {pllpDiv4};
+#elif defined(CONFIG_CHIP_STM32F4_RCC_PLLP_DIV6)
+	constexpr uint8_t pllp {pllpDiv6};
+#elif defined(CONFIG_CHIP_STM32F4_RCC_PLLP_DIV8)
+	constexpr uint8_t pllp {pllpDiv8};
+#endif
+
+	constexpr uint32_t pllOutHz {vcoOutHz / pllp};
+	static_assert(pllOutHz <= maxPllOutHz, "Invalid PLL output frequency!");
+
+	constexpr uint32_t pllqOutHz {vcoOutHz / CONFIG_CHIP_STM32F4_RCC_PLLQ};
+	static_assert(pllqOutHz <= maxPllqOutHz, "Invalid PLL \"Q\" output frequency!");
+
+	enablePll(CONFIG_CHIP_STM32F4_RCC_PLLN, pllp, CONFIG_CHIP_STM32F4_RCC_PLLQ);
+
+#endif	// def CONFIG_CHIP_STM32F4_RCC_PLL_ENABLE
+
 #endif	// def CONFIG_CHIP_STM32F4_RCC_STANDARD_CLOCK_CONFIGURATION_ENABLE
 
 	constexpr uint32_t period {hsiHz / CONFIG_TICK_RATE_HZ};
