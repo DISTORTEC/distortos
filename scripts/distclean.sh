daedda747c921eb2035b22857f7ff043c8732754
Kamil Szczygiel <distortec@distortec.com>
2015-09-25 23:52:54 +0200

    scripts: use sh instead of bash
    Modify shebang in all shell scripts to use "/bin/sh" instead of
    "/bin/bash".

--- scripts/distclean.sh
+++ scripts/distclean.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # file: distclean.sh
@@ -8,7 +8,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# date: 2015-09-20
+# date: 2015-09-25
 #
 
 set -e
