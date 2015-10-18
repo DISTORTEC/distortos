6b3f13dcb2ef3ea8744aa8b4134797f0b1d32925
Kamil Szczygiel <distortec@distortec.com>
2015-10-18 15:43:27 +0200

    ReverseAdaptor: minor style improvements

--- include/estd/ReverseAdaptor.hpp
+++ include/estd/ReverseAdaptor.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief ReverseAdaptor template class header.
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-15
+ * \date 2015-10-18
  */
 
 #ifndef ESTD_REVERSEADAPTOR_HPP_
@@ -35,7 +35,7 @@ public:
 	 */
 
 	constexpr explicit ReverseAdaptor(T& container) noexcept :
-			container_(container)
+			container_{container}
 	{
 
 	}
