2ef5f26ab795efe0519ca0388023101d872c4e37
Kamil Szczygiel <distortec@distortec.com>
2015-10-18 15:42:43 +0200

    ReferenceHolder: minor style improvements

--- include/estd/ReferenceHolder.hpp
+++ include/estd/ReferenceHolder.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief ReferenceHolder template class header.
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-11-15
+ * \date 2015-10-18
  */
 
 #ifndef ESTD_REFERENCEHOLDER_HPP_
@@ -35,7 +35,7 @@ public:
 	 */
 
 	constexpr explicit ReferenceHolder(T& reference) noexcept :
-			reference_(reference)
+			reference_{reference}
 	{
 
 	}
