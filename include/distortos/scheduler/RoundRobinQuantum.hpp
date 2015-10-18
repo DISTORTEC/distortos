06d0d6316bd9335b74c57791adcea045778cc4ac
Kamil Szczygiel <distortec@distortec.com>
2015-10-18 15:24:05 +0200

    RoundRobinQuantum: minor style improvements

--- include/distortos/scheduler/RoundRobinQuantum.hpp
+++ include/distortos/scheduler/RoundRobinQuantum.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief RoundRobinQuantum class header
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-10-25
+ * \date 2015-10-18
  */
 
 #ifndef INCLUDE_DISTORTOS_SCHEDULER_ROUNDROBINQUANTUM_HPP_
@@ -50,7 +50,9 @@ public:
 
 	constexpr RoundRobinQuantum() :
 			quantum_{getInitial()}
-	{}
+	{
+
+	}
 
 	/**
 	 * \brief Decrements round-robin's quantum.
