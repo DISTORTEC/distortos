346fd29e729d0f7fb9703d5244ee2acbf32bcdea
Kamil Szczygiel <distortec@distortec.com>
2015-10-18 15:31:46 +0200

    SwapPopQueueFunctor: minor style improvements

--- include/distortos/synchronization/SwapPopQueueFunctor.hpp
+++ include/distortos/synchronization/SwapPopQueueFunctor.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-01-19
+ * \date 2015-10-18
  */
 
 #ifndef INCLUDE_DISTORTOS_SYNCHRONIZATION_SWAPPOPQUEUEFUNCTOR_HPP_
@@ -43,7 +43,7 @@ public:
 	 */
 
 	constexpr explicit SwapPopQueueFunctor(T& value) :
-			value_(value)
+			value_{value}
 	{
 
 	}
