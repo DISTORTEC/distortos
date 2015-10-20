9601d6a85b6e946d720559e504f0aec709f6f641
Kamil Szczygiel <distortec@distortec.com>
2015-10-20 21:32:22 +0200

    add source/memory folder to compilation

--- source/Rules.mk
+++ source/Rules.mk
@@ -6,7 +6,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# date: 2015-09-27
+# date: 2015-10-20
 #
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -17,6 +17,7 @@ SUBDIRECTORIES += allocators
 SUBDIRECTORIES += architecture
 SUBDIRECTORIES += chip/STMicroelectronics/STM32F4
 SUBDIRECTORIES += clocks
+SUBDIRECTORIES += memory
 SUBDIRECTORIES += scheduler
 SUBDIRECTORIES += synchronization
 SUBDIRECTORIES += syscalls
