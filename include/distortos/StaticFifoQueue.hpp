0945207f0e7a128b88c94ebbe926413146f83fb0
Kamil Szczygiel <distortec@distortec.com>
2015-10-20 22:45:09 +0200

    StaticFifoQueue's constructor: use basic FifoQueue constructor

--- include/distortos/StaticFifoQueue.hpp
+++ include/distortos/StaticFifoQueue.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-10-14
+ * \date 2015-10-20
  */
 
 #ifndef INCLUDE_DISTORTOS_STATICFIFOQUEUE_HPP_
@@ -38,7 +38,7 @@ public:
 	 */
 
 	explicit StaticFifoQueue() :
-			fifoQueue_{storage_}
+			fifoQueue_{storage_.data(), storage_.size()}
 	{
 
 	}
