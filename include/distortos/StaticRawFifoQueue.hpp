80f9326caa9ae59cc1596bd9d5b416c59ffc8273
Kamil Szczygiel <distortec@distortec.com>
2015-10-21 21:12:55 +0200

    StaticRawFifoQueue's constructor: use basic RawFifoQueue constructor

--- include/distortos/StaticRawFifoQueue.hpp
+++ include/distortos/StaticRawFifoQueue.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-10-15
+ * \date 2015-10-21
  */
 
 #ifndef INCLUDE_DISTORTOS_STATICRAWFIFOQUEUE_HPP_
@@ -39,7 +39,7 @@ public:
 	 */
 
 	explicit StaticRawFifoQueue() :
-			rawFifoQueue_{storage_}
+			rawFifoQueue_{storage_.data(), sizeof(*storage_.data()), storage_.size()}
 	{
 
 	}
