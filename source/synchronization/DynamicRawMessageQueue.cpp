368b58faf12dd980437ef6ec4d9aa98ca74c66e2
Kamil Szczygiel <distortec@distortec.com>
2015-10-22 18:30:31 +0200

    DynamicRawMessageQueue's constructor: fix bug with invalid size
    Instead of proper elementSize, a value from helper constructor of
    RawMessageQueue - sizeof(*valueStorage.data()) - was used. This fix also
    removes usage of helper constructor, replacing its use with basic
    constructor of RawMessageQueue.

--- source/synchronization/DynamicRawMessageQueue.cpp
+++ source/synchronization/DynamicRawMessageQueue.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-10-17
+ * \date 2015-10-22
  */
 
 #include "distortos/DynamicRawMessageQueue.hpp"
@@ -23,7 +23,7 @@ namespace distortos
 DynamicRawMessageQueue::DynamicRawMessageQueue(const size_t elementSize, const size_t queueSize) :
 		entryStorageUniquePointer_{new typename EntryStorageUniquePointer::element_type[queueSize]},
 		valueStorageUniquePointer_{new uint8_t[elementSize * queueSize]},
-		rawMessageQueue_{entryStorageUniquePointer_.get(), valueStorageUniquePointer_.get(), queueSize}
+		rawMessageQueue_{entryStorageUniquePointer_.get(), valueStorageUniquePointer_.get(), elementSize, queueSize}
 {
 
 }
