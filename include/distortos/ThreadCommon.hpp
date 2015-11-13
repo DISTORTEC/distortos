3fc773013e16ed101f024498176032b183b9f403
Kamil Szczygiel <distortec@distortec.com>
2015-11-13 18:43:58 +0100

    add empty ThreadCommon class

---
+++ include/distortos/ThreadCommon.hpp
@@ -0,0 +1,30 @@
+/**
+ * \file
+ * \brief ThreadCommon class header
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-11-13
+ */
+
+#ifndef INCLUDE_DISTORTOS_THREADCOMMON_HPP_
+#define INCLUDE_DISTORTOS_THREADCOMMON_HPP_
+
+#include "distortos/Thread.hpp"
+
+namespace distortos
+{
+
+/// ThreadCommon class implements common functionality of threads
+class ThreadCommon : public Thread
+{
+
+};
+
+}	// namespace distortos
+
+#endif	// INCLUDE_DISTORTOS_THREADCOMMON_HPP_
