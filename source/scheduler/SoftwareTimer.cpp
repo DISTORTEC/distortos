8f9594a1f48f245c7267a1426adb33c37155280e
Kamil Szczygiel <distortec@distortec.com>
2015-11-15 12:08:17 +0100

    SoftwareTimer::softwareTimerRunner(): call run() instead of execute()

--- source/scheduler/SoftwareTimer.cpp
+++ source/scheduler/SoftwareTimer.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-14
+ * \date 2015-11-15
  */
 
 #include "distortos/SoftwareTimer.hpp"
@@ -22,7 +22,7 @@ namespace distortos
 
 void SoftwareTimer::softwareTimerRunner(SoftwareTimer& softwareTimer)
 {
-	softwareTimer.execute();
+	softwareTimer.run();
 }
 
 }	// namespace distortos
