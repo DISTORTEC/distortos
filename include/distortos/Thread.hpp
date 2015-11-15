02da99b0566167802fea0551df009460898d409e
Kamil Szczygiel <distortec@distortec.com>
2015-11-15 14:32:16 +0100

    Thread: remove all "... = default" and "... = delete" lines
    These are not needed now when the class is a pure abstract interface.

--- include/distortos/Thread.hpp
+++ include/distortos/Thread.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-13
+ * \date 2015-11-15
  */
 
 #ifndef INCLUDE_DISTORTOS_THREAD_HPP_
@@ -154,12 +154,6 @@ public:
 	 */
 
 	virtual int start() = 0;
-
-	Thread() = default;
-	Thread(const Thread&) = delete;
-	Thread(Thread&&) = default;
-	const Thread& operator=(const Thread&) = delete;
-	Thread& operator=(Thread&&) = delete;
 
 protected:
 
