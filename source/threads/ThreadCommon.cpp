b81de6923b8f5da67020adda94824c92ee6253b5
Kamil Szczygiel <distortec@distortec.com>
2015-11-26 22:37:20 +0100

    ThreadCommon::terminationHook(): don't clear list of control block
    Don't call ThreadControlBlock::setList() to clear the list on which the
    thread is placed - this is done by ThreadControlBlockList's destructor.

--- source/threads/ThreadCommon.cpp
+++ source/threads/ThreadCommon.cpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-13
+ * \date 2015-11-26
  */
 
 #include "distortos/ThreadCommon.hpp"
@@ -137,7 +137,6 @@ int ThreadCommon::start()
 void ThreadCommon::terminationHook()
 {
 	joinSemaphore_.post();
-	getThreadControlBlock().setList(nullptr);
 }
 
 }	// namespace distortos
