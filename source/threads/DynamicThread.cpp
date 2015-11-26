6a0c4548e72fd1312b868dc60b6e57ac7247b3de
Kamil Szczygiel <distortec@distortec.com>
2015-11-26 22:44:17 +0100

    DynamicThread: add missing comment block

--- source/threads/DynamicThread.cpp
+++ source/threads/DynamicThread.cpp
@@ -8,13 +8,17 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-09
+ * \date 2015-11-26
  */
 
 #include "distortos/DynamicThread.hpp"
 
 namespace distortos
 {
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| public functions
++---------------------------------------------------------------------------------------------------------------------*/
 
 void DynamicThread::run()
 {
