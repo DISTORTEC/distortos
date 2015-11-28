91712f979676e32ca1faa66910e9e8df7356310a
Kamil Szczygiel <distortec@distortec.com>
2015-11-28 16:07:02 +0100

    documentation: add "statistics" module
    Add definition of new module to distortos-modules.dox. Add contents of
    statistics namespace to this new module.

--- include/distortos/statistics.hpp
+++ include/distortos/statistics.hpp
@@ -2,13 +2,13 @@
  * \file
  * \brief statistics namespace header
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-27
+ * \date 2015-11-28
  */
 
 #ifndef INCLUDE_DISTORTOS_STATISTICS_HPP_
@@ -22,11 +22,16 @@ namespace distortos
 namespace statistics
 {
 
+/// \addtogroup statistics
+/// \{
+
 /**
  * \return number of context switches
  */
 
 uint64_t getContextSwitchCount();
+
+/// \}
 
 }	// namespace statistics
 
