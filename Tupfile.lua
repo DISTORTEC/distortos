fc6b1b6457e988795e37c99061bedac36da9c666
Kamil Szczygiel <distortec@distortec.com>
2015-12-02 18:06:34 +0100

    tup build: force name of generated header to be distortosConfiguration.h
    Recent tup changes allow the use of dot-files as inputs, so actually
    ".config" can be used as DISTORTOS_CONFIGURATION_MK (via manual editing
    of selectedConfiguration.mk file). In that case tup build would produce
    a ".config.h" header file instead of expected "distortosConfiguration.h"
    - fix this by forcing proper name instead of relying on the name of
    input file with "%B".

--- Tupfile.lua
+++ Tupfile.lua
@@ -6,11 +6,11 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- date: 2015-10-12
+-- date: 2015-12-02
 --
 
 local makeDistortosConfiguration_awk = TOP .. "/scripts/makeDistortosConfiguration.awk"
-local distortosConfigurationOutputs = {OUTPUT .. "include/distortos/%B.h", TOP .. "/<headers>"}
+local distortosConfigurationOutputs = {OUTPUT .. "include/distortos/distortosConfiguration.h", TOP .. "/<headers>"}
 
 tup.rule(DISTORTOS_CONFIGURATION_MK, "^ AWK " .. makeDistortosConfiguration_awk .. "^ " ..
 		makeDistortosConfiguration_awk .. " \"%f\" > \"%o\"", distortosConfigurationOutputs)
