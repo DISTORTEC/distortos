011aca3e64e9144b2a3ee5a5347aca9f2cc86759
Kamil Szczygiel <distortec@distortec.com>
2015-12-04 20:12:22 +0100

    move MutexControlBlockList.hpp to "synchronization" internal folder

--- include/distortos/internal/scheduler/ThreadControlBlock.hpp
+++ include/distortos/internal/scheduler/ThreadControlBlock.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-27
+ * \date 2015-12-04
  */
 
 #ifndef INCLUDE_DISTORTOS_INTERNAL_SCHEDULER_THREADCONTROLBLOCK_HPP_
@@ -16,7 +16,8 @@
 
 #include "distortos/internal/scheduler/RoundRobinQuantum.hpp"
 #include "distortos/internal/scheduler/ThreadControlBlockList-types.hpp"
-#include "distortos/internal/scheduler/MutexControlBlockList.hpp"
+
+#include "distortos/internal/synchronization/MutexControlBlockList.hpp"
 
 #include "distortos/architecture/Stack.hpp"
 
--- include/distortos/internal/synchronization/MutexControlBlockList.hpp
+++ include/distortos/internal/synchronization/MutexControlBlockList.hpp
@@ -8,11 +8,11 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-27
+ * \date 2015-12-04
  */
 
-#ifndef INCLUDE_DISTORTOS_INTERNAL_SCHEDULER_MUTEXCONTROLBLOCKLIST_HPP_
-#define INCLUDE_DISTORTOS_INTERNAL_SCHEDULER_MUTEXCONTROLBLOCKLIST_HPP_
+#ifndef INCLUDE_DISTORTOS_INTERNAL_SYNCHRONIZATION_MUTEXCONTROLBLOCKLIST_HPP_
+#define INCLUDE_DISTORTOS_INTERNAL_SYNCHRONIZATION_MUTEXCONTROLBLOCKLIST_HPP_
 
 #include "distortos/internal/allocators/PoolAllocator.hpp"
 #include "distortos/internal/allocators/SimpleFeedablePool.hpp"
@@ -40,4 +40,4 @@ using MutexControlBlockList = std::list<MutexControlBlockListValueType, MutexCon
 
 }	// namespace distortos
 
-#endif	// INCLUDE_DISTORTOS_INTERNAL_SCHEDULER_MUTEXCONTROLBLOCKLIST_HPP_
+#endif	// INCLUDE_DISTORTOS_INTERNAL_SYNCHRONIZATION_MUTEXCONTROLBLOCKLIST_HPP_
