db88550111cbcc33f4aef72017110c842b1901a4
Kamil Szczygiel <distortec@distortec.com>
2015-12-05 21:35:44 +0100

    rename SoftwareTimerControlBlockList-types.hpp
    Rename SoftwareTimerControlBlockList-types.hpp to
    SoftwareTimerList-types.hpp

--- include/distortos/internal/scheduler/SoftwareTimerControlBlock.hpp
+++ include/distortos/internal/scheduler/SoftwareTimerControlBlock.hpp
@@ -14,7 +14,7 @@
 #ifndef INCLUDE_DISTORTOS_INTERNAL_SCHEDULER_SOFTWARETIMERCONTROLBLOCK_HPP_
 #define INCLUDE_DISTORTOS_INTERNAL_SCHEDULER_SOFTWARETIMERCONTROLBLOCK_HPP_
 
-#include "distortos/internal/scheduler/SoftwareTimerControlBlockList-types.hpp"
+#include "distortos/internal/scheduler/SoftwareTimerList-types.hpp"
 
 #include "distortos/TickClock.hpp"
 
