96d451685aa638880b63a7de2dee8ab2851808e4
Kamil Szczygiel <distortec@distortec.com>
2015-12-23 13:23:32 +0100

    IntrusiveListNode: add private reset() function

--- include/estd/IntrusiveList.hpp
+++ include/estd/IntrusiveList.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-12-14
+ * \date 2015-12-23
  */
 
 #ifndef ESTD_INTRUSIVELIST_HPP_
@@ -144,6 +144,16 @@ public:
 	}
 
 private:
+
+	/**
+	 * \brief Resets the node to the same state as right after construction.
+	 */
+
+	void reset()
+	{
+		nextNode_ = this;
+		previousNode_ = this;
+	}
 
 	/// reference to next node on the list
 	IntrusiveListNode* nextNode_;
