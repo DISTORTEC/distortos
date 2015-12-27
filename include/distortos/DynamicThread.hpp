3517be4abc4eac45ac9efd2c0e650b497264e9ea
Kamil Szczygiel <distortec@distortec.com>
2015-12-27 11:02:12 +0100

    DynamicThread: add implementation of detachable dynamic thread
    When CONFIG_THREAD_DETACH_ENABLE option is enabled, DynamicThread
    contains an internal thread object (managed with std::unique_ptr) that
    can be detached on request. If this option is disabled, DynamicThread
    just inherits directly from internal::DynamicThreadBase.

--- include/distortos/DynamicThread.hpp
+++ include/distortos/DynamicThread.hpp
@@ -8,7 +8,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-12-24
+ * \date 2015-12-27
  */
 
 #ifndef INCLUDE_DISTORTOS_DYNAMICTHREAD_HPP_
@@ -27,12 +27,232 @@ namespace distortos
  * and internal DynamicSignalsReceiver object.
  */
 
+#ifdef CONFIG_THREAD_DETACH_ENABLE
+
+class DynamicThread : public Thread
+{
+public:
+
+	/**
+	 * \brief DynamicThread's constructor
+	 *
+	 * \tparam Function is the function that will be executed in separate thread
+	 * \tparam Args are the arguments for \a Function
+	 *
+	 * \param [in] stackSize is the size of stack, bytes
+	 * \param [in] canReceiveSignals selects whether reception of signals is enabled (true) or disabled (false) for this
+	 * thread
+	 * \param [in] queuedSignals is the max number of queued signals for this thread, relevant only if
+	 * \a canReceiveSignals == true, 0 to disable queuing of signals for this thread
+	 * \param [in] signalActions is the max number of different SignalAction objects for this thread, relevant only if
+	 * \a canReceiveSignals == true, 0 to disable catching of signals for this thread
+	 * \param [in] priority is the thread's priority, 0 - lowest, UINT8_MAX - highest
+	 * \param [in] schedulingPolicy is the scheduling policy of the thread
+	 * \param [in] function is a function that will be executed in separate thread
+	 * \param [in] args are arguments for \a function
+	 */
+
+	template<typename Function, typename... Args>
+	DynamicThread(size_t stackSize, bool canReceiveSignals, size_t queuedSignals, size_t signalActions,
+			uint8_t priority, SchedulingPolicy schedulingPolicy, Function&& function, Args&&... args);
+
+	/**
+	 * \brief DynamicThread's constructor
+	 *
+	 * \tparam Function is the function that will be executed in separate thread
+	 * \tparam Args are the arguments for \a Function
+	 *
+	 * \param [in] parameters is a DynamicThreadParameters struct with thread parameters
+	 * \param [in] function is a function that will be executed in separate thread
+	 * \param [in] args are arguments for \a function
+	 */
+
+	template<typename Function, typename... Args>
+	DynamicThread(const DynamicThreadParameters parameters, Function&& function, Args&&... args) :
+			DynamicThread{parameters.stackSize, parameters.canReceiveSignals, parameters.queuedSignals,
+					parameters.signalActions, parameters.priority, parameters.schedulingPolicy,
+					std::forward<Function>(function), std::forward<Args>(args)...}
+	{
+
+	}
+
+	/**
+	 * \brief DynamicThread's destructor
+	 */
+
+	virtual ~DynamicThread() override;
+
+	/**
+	 * \brief Generates signal for thread.
+	 *
+	 * Similar to pthread_kill() - http://pubs.opengroup.org/onlinepubs/9699919799/functions/pthread_kill.html
+	 *
+	 * Adds the signalNumber to set of pending signals. If this thread is currently waiting for this signal, it will be
+	 * unblocked.
+	 *
+	 * \param [in] signalNumber is the signal that will be generated, [0; 31]
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - EINVAL - \a signalNumber value is invalid;
+	 * - ENOTSUP - reception of signals is disabled for this thread;
+	 * - EINVAL - internal thread object was detached;
+	 *
+	 * \ingroup signals
+	 */
+
+	virtual int generateSignal(uint8_t signalNumber) override;
+
+	/**
+	 * \return effective priority of thread
+	 */
+
+	virtual uint8_t getEffectivePriority() const override;
+
+	/**
+	 * \brief Gets set of currently pending signals.
+	 *
+	 * Similar to sigpending() - http://pubs.opengroup.org/onlinepubs/9699919799/functions/sigpending.html
+	 *
+	 * This function shall return the set of signals that are blocked from delivery and are pending on the thread.
+	 *
+	 * \return set of currently pending signals
+	 *
+	 * \ingroup signals
+	 */
+
+	virtual SignalSet getPendingSignalSet() const override;
+
+	/**
+	 * \return priority of thread
+	 */
+
+	virtual uint8_t getPriority() const override;
+
+	/**
+	 * \return scheduling policy of the thread
+	 */
+
+	virtual SchedulingPolicy getSchedulingPolicy() const override;
+
+	/**
+	 * \return current state of thread
+	 */
+
+	virtual ThreadState getState() const override;
+
+	/**
+	 * \brief Waits for thread termination.
+	 *
+	 * Similar to std::thread::join() - http://en.cppreference.com/w/cpp/thread/thread/join
+	 * Similar to POSIX pthread_join() - http://pubs.opengroup.org/onlinepubs/9699919799/functions/pthread_join.html
+	 *
+	 * Blocks current thread until this thread finishes its execution. The results of multiple simultaneous calls to
+	 * join() on the same target thread are undefined.
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - EDEADLK - deadlock condition was detected,
+	 * - EINVAL - this thread is not joinable,
+	 * - EINVAL - internal thread object was detached;
+	 * - ...
+	 *
+	 * \ingroup synchronization
+	 */
+
+	virtual int join() override;
+
+	/**
+	 * \brief Queues signal for thread.
+	 *
+	 * Similar to sigqueue() - http://pubs.opengroup.org/onlinepubs/9699919799/functions/sigqueue.html
+	 *
+	 * Adds the signalNumber and signal value (sigval union) to queue of SignalInformation objects. If this thread is
+	 * currently waiting for this signal, it will be unblocked.
+	 *
+	 * \param [in] signalNumber is the signal that will be queued, [0; 31]
+	 * \param [in] value is the signal value
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - EAGAIN - no resources are available to queue the signal, maximal number of signals is already queued in
+	 * associated queue of SignalInformation objects;
+	 * - EINVAL - \a signalNumber value is invalid;
+	 * - ENOTSUP - reception or queuing of signals are disabled for this thread;
+	 * - EINVAL - internal thread object was detached;
+	 *
+	 * \ingroup signals
+	 */
+
+	virtual int queueSignal(uint8_t signalNumber, sigval value) override;
+
+	/**
+	 * \brief Changes priority of thread.
+	 *
+	 * If the priority really changes, the position in the thread list is adjusted and context switch may be requested.
+	 *
+	 * \param [in] priority is the new priority of thread
+	 * \param [in] alwaysBehind selects the method of ordering when lowering the priority
+	 * - false - the thread is moved to the head of the group of threads with the new priority (default),
+	 * - true - the thread is moved to the tail of the group of threads with the new priority.
+	 */
+
+	virtual void setPriority(uint8_t priority, bool alwaysBehind = {}) override;
+
+	/**
+	 * param [in] schedulingPolicy is the new scheduling policy of the thread
+	 */
+
+	virtual void setSchedulingPolicy(SchedulingPolicy schedulingPolicy) override;
+
+	/**
+	 * \brief Starts the thread.
+	 *
+	 * This operation can be performed on threads in "New" state only.
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - EINVAL - thread is already started;
+	 * - EINVAL - internal thread object was detached;
+	 * - error codes returned by scheduler::Scheduler::add();
+	 */
+
+	virtual int start() override;
+
+	DynamicThread(const DynamicThread&) = delete;
+	DynamicThread(DynamicThread&&) = default;
+	const DynamicThread& operator=(const DynamicThread&) = delete;
+	DynamicThread& operator=(DynamicThread&&) = delete;
+
+private:
+
+	/**
+	 * \brief "Run" function of thread
+	 *
+	 * This is never used, as only internal thread is executed.
+	 */
+
+	virtual void run() override;
+
+	/**
+	 * \brief Termination hook function of thread
+	 *
+	 * This function is called after run() completes, from Scheduler::remove(). This is never used, as only internal
+	 * thread is executed.
+	 */
+
+	virtual void terminationHook() override;
+
+	/// internal thread object
+	std::unique_ptr<internal::DynamicThreadBase> detachableThread_;
+};
+
+#else	// !def CONFIG_THREAD_DETACH_ENABLE
+
 class DynamicThread : public internal::DynamicThreadBase
 {
 public:
 
 	using internal::DynamicThreadBase::DynamicThreadBase;
 };
+
+#endif	// !def CONFIG_THREAD_DETACH_ENABLE
 
 /**
  * \brief Helper factory function to make DynamicThread object
@@ -84,6 +304,20 @@ DynamicThread makeDynamicThread(const DynamicThreadParameters parameters, Functi
 }
 
 /// \}
+
+#ifdef CONFIG_THREAD_DETACH_ENABLE
+
+template<typename Function, typename... Args>
+DynamicThread::DynamicThread(const size_t stackSize, const bool canReceiveSignals, const size_t queuedSignals,
+		const size_t signalActions, const uint8_t priority, const SchedulingPolicy schedulingPolicy,
+		Function&& function, Args&&... args) :
+		detachableThread_{new internal::DynamicThreadBase{stackSize, canReceiveSignals, queuedSignals, signalActions,
+				priority, schedulingPolicy, std::forward<Function>(function), std::forward<Args>(args)...}}
+{
+
+}
+
+#endif	// def CONFIG_THREAD_DETACH_ENABLE
 
 }	// namespace distortos
 
---
+++ source/threads/DynamicThread.cpp
@@ -0,0 +1,158 @@
+/**
+ * \file
+ * \brief DynamicThread class implementation
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-12-27
+ */
+
+#include "distortos/DynamicThread.hpp"
+
+#include "distortos/architecture/InterruptMaskingLock.hpp"
+
+namespace distortos
+{
+
+#ifdef CONFIG_THREAD_DETACH_ENABLE
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| public functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+DynamicThread::~DynamicThread()
+{
+
+}
+
+int DynamicThread::generateSignal(const uint8_t signalNumber)
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return EINVAL;
+
+	return detachableThread_->generateSignal(signalNumber);
+}
+
+uint8_t DynamicThread::getEffectivePriority() const
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return {};
+
+	return detachableThread_->getEffectivePriority();
+}
+
+SignalSet DynamicThread::getPendingSignalSet() const
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return SignalSet{SignalSet::empty};
+
+	return detachableThread_->getPendingSignalSet();
+}
+
+uint8_t DynamicThread::getPriority() const
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return {};
+
+	return detachableThread_->getPriority();
+}
+
+SchedulingPolicy DynamicThread::getSchedulingPolicy() const
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return {};
+
+	return detachableThread_->getSchedulingPolicy();
+}
+
+ThreadState DynamicThread::getState() const
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return ThreadState::Detached;
+
+	return detachableThread_->getState();
+}
+
+int DynamicThread::join()
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return EINVAL;
+
+	return detachableThread_->join();
+}
+
+int DynamicThread::queueSignal(const uint8_t signalNumber, const sigval value)
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return EINVAL;
+
+	return detachableThread_->queueSignal(signalNumber, value);
+}
+
+void DynamicThread::setPriority(const uint8_t priority, const bool alwaysBehind)
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return;
+
+	detachableThread_->setPriority(priority, alwaysBehind);
+}
+
+void DynamicThread::setSchedulingPolicy(const SchedulingPolicy schedulingPolicy)
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return;
+
+	detachableThread_->setSchedulingPolicy(schedulingPolicy);
+}
+
+int DynamicThread::start()
+{
+	architecture::InterruptMaskingLock interruptMaskingLock;
+
+	if (detachableThread_ == nullptr)
+		return EINVAL;
+
+	return detachableThread_->start();
+}
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| private functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void DynamicThread::run()
+{
+	while (1);	/// \todo add some form of assertion that this never gets called
+}
+
+void DynamicThread::terminationHook()
+{
+	while (1);	/// \todo add some form of assertion that this never gets called
+}
+
+#endif	// def CONFIG_THREAD_DETACH_ENABLE
+
+}	// namespace distortos
