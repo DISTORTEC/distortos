e469d2a7e21d2355c9ff16956b47ef7db53a040a
Kamil Szczygiel <distortec@distortec.com>
2015-12-27 15:24:01 +0100

    add getDeferredThreadDeleter()

---
+++ include/distortos/internal/memory/getDeferredThreadDeleter.hpp
@@ -0,0 +1,41 @@
+/**
+ * \file
+ * \brief getDeferredThreadDeleter() declaration
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-12-27
+ */
+
+#ifndef INCLUDE_DISTORTOS_INTERNAL_MEMORY_GETDEFERREDTHREADDELETER_HPP_
+#define INCLUDE_DISTORTOS_INTERNAL_MEMORY_GETDEFERREDTHREADDELETER_HPP_
+
+#include "distortos/distortosConfiguration.h"
+
+#ifdef CONFIG_THREAD_DETACH_ENABLE
+
+namespace distortos
+{
+
+namespace internal
+{
+
+class DeferredThreadDeleter;
+
+/**
+ * \return reference to main instance of DeferredThreadDeleter
+ */
+
+DeferredThreadDeleter& getDeferredThreadDeleter();
+
+}	// namespace internal
+
+}	// namespace distortos
+
+#endif	// def CONFIG_THREAD_DETACH_ENABLE
+
+#endif	// INCLUDE_DISTORTOS_INTERNAL_MEMORY_GETDEFERREDTHREADDELETER_HPP_
---
+++ source/memory/getDeferredThreadDeleter.cpp
@@ -0,0 +1,51 @@
+/**
+ * \file
+ * \brief getDeferredThreadDeleter() definition
+ *
+ * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \date 2015-12-27
+ */
+
+#include "distortos/internal/memory/getDeferredThreadDeleter.hpp"
+
+#ifdef CONFIG_THREAD_DETACH_ENABLE
+
+#include "distortos/internal/memory/DeferredThreadDeleter.hpp"
+
+namespace distortos
+{
+
+namespace internal
+{
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// storage for main instance of DeferredThreadDeleter
+std::aligned_storage<sizeof(DeferredThreadDeleter), alignof(DeferredThreadDeleter)>::type deferredThreadDeleterStorage;
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+DeferredThreadDeleter& getDeferredThreadDeleter()
+{
+	return reinterpret_cast<DeferredThreadDeleter&>(deferredThreadDeleterStorage);
+}
+
+}	// namespace internal
+
+}	// namespace distortos
+
+#endif	// def CONFIG_THREAD_DETACH_ENABLE
