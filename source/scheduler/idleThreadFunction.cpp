1cf237bae7469b2a29f93a10e494a39d9832ff64
Kamil Szczygiel <distortec@distortec.com>
2015-12-27 23:31:31 +0100

    idle thread: add support for deferred deletion of detached threads
    If CONFIG_THREAD_DETACH_ENABLE option is enabled, call
    DeferredThreadDeleter::tryCleanup() in the loop of idle thread.

--- source/scheduler/idleThreadFunction.cpp
+++ source/scheduler/idleThreadFunction.cpp
@@ -8,10 +8,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-27
+ * \date 2015-12-27
  */
 
 #include "distortos/internal/scheduler/idleThreadFunction.hpp"
+
+#include "distortos/internal/memory/DeferredThreadDeleter.hpp"
+#include "distortos/internal/memory/getDeferredThreadDeleter.hpp"
 
 #include <cstdint>
 
@@ -32,6 +35,12 @@ void idleThreadFunction()
 	while (1)
 	{
 		++i;
+
+#ifdef CONFIG_THREAD_DETACH_ENABLE
+
+		getDeferredThreadDeleter().tryCleanup();	/// \todo error handling?
+
+#endif	// def CONFIG_THREAD_DETACH_ENABLE
 	}
 }
 
