2cb189e0f61c1e9cb439dc66f68fb617cf4f4b92
Kamil Szczygiel <distortec@distortec.com>
2016-01-01 12:27:33 +0100

    Thread::threadRunner(): call Scheduler::remove() with masked interrupts

--- source/threads/Thread.cpp
+++ source/threads/Thread.cpp
@@ -2,19 +2,21 @@
  * \file
  * \brief Thread class implementation
  *
- * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-12-30
+ * \date 2016-01-01
  */
 
 #include "distortos/Thread.hpp"
 
 #include "distortos/internal/scheduler/getScheduler.hpp"
 #include "distortos/internal/scheduler/Scheduler.hpp"
+
+#include "distortos/architecture/InterruptMaskingLock.hpp"
 
 namespace distortos
 {
@@ -35,7 +37,12 @@ Thread::~Thread()
 void Thread::threadRunner(Thread& thread, void (& run)(Thread&))
 {
 	run(thread);
-	internal::getScheduler().remove(&Thread::terminationHook);
+
+	{
+		architecture::InterruptMaskingLock interruptMaskingLock;
+
+		internal::getScheduler().remove(&Thread::terminationHook);
+	}
 
 	while (1);
 }
