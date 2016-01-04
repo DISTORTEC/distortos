06748c10d6ce7314c2559aa4dc21913c3d55dcf2
Kamil Szczygiel <distortec@distortec.com>
2016-01-04 20:47:33 +0100

    test: extend ThreadOperationsTestCase
    Change static threads to dynamic threads in first phase. Add third phase
    which tests various scenarios involving thread detachment.

--- test/Thread/ThreadOperationsTestCase.cpp
+++ test/Thread/ThreadOperationsTestCase.cpp
@@ -2,24 +2,27 @@
  * \file
  * \brief ThreadOperationsTestCase class implementation
  *
- * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2015-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-12-02
+ * \date 2016-01-04
  */
 
 #include "ThreadOperationsTestCase.hpp"
 
+#include "waitForNextTick.hpp"
+
+#include "distortos/architecture/InterruptMaskingLock.hpp"
+
+#include "distortos/DynamicThread.hpp"
 #include "distortos/StaticThread.hpp"
 #include "distortos/statistics.hpp"
 #include "distortos/ThisThread.hpp"
 
-#include "distortos/architecture/InterruptMaskingLock.hpp"
-
-#include <cerrno>
+#include <malloc.h>
 
 namespace distortos
 {
@@ -35,7 +38,16 @@ namespace
 +---------------------------------------------------------------------------------------------------------------------*/
 
 /**
- * \brief Phase 1 of test case.
+ * \brief Empty function used for test threads
+ */
+
+void emptyFunction()
+{
+
+}
+
+/**
+ * \brief Phase 1 of test case
  *
  * Tests behavior of creating, starting and joining the test thread.
  *
@@ -44,7 +56,7 @@ namespace
 
 bool phase1()
 {
-	auto testThread = makeStaticThread<192>(1, [](){});
+	auto testThread = makeDynamicThread({192, 1}, emptyFunction);
 
 	if (testThread.getState() != ThreadState::New)	// state of created (but not yet started) thread must be "New"
 		return false;
@@ -89,7 +101,7 @@ bool phase1()
 }
 
 /**
- * \brief Phase 2 of test case.
+ * \brief Phase 2 of test case
  *
  * Tests behavior Thread::start() and Thread::join() when used with current thread.
  *
@@ -113,6 +125,129 @@ bool phase2()
 	return true;
 }
 
+/**
+ * \brief Phase 3 of test case
+ *
+ * Tests scenarios involving thread detachment.
+ *
+ * \return true if test succeeded, false otherwise
+ */
+
+bool phase3()
+{
+#ifdef CONFIG_THREAD_DETACH_ENABLE
+
+	const auto lambda =
+			[](int& sharedRet)
+			{
+				sharedRet = ThisThread::detach();
+			};
+
+	// attempt to detach static thread must fail with ENOTSUP
+	{
+		int sharedRet {};
+		auto staticThread = makeStaticThread<192>(1, lambda, std::ref(sharedRet));
+		bool result {true};
+		if (staticThread.start() != 0)
+			result = false;
+		if (staticThread.detach() != ENOTSUP)	// static thread cannot be detached
+			result = false;
+		if (staticThread.getState() != ThreadState::Runnable)
+			result = false;
+		if (staticThread.join() != 0 || result == false || sharedRet != ENOTSUP)	// self-detach must fail too
+			return false;
+	}
+
+	// detaching dynamic thread that is not yet started must succeed, but the thread is just deleted and does not run
+	{
+		int sharedRet {0x2e981e48};
+		auto dynamicThread = makeDynamicThread({192, 1}, lambda, std::ref(sharedRet));
+		bool result {true};
+		if (dynamicThread.detach() != 0)
+			result = false;
+		if (dynamicThread.start() != EINVAL)	// detached thread cannot be started
+			result = false;
+		if (dynamicThread.getState() != ThreadState::Detached)
+			result = false;
+		if (dynamicThread.join() != EINVAL || result == false || sharedRet != 0x2e981e48 ||
+				dynamicThread.detach() != EINVAL)	// detached thread cannot be joined, self-detach must fail
+			return false;
+	}
+
+	if (mallinfo().uordblks != 0)	// all dynamic memory must be deallocated after each test phase
+		return false;
+
+	// detaching dynamic thread that is started, but not yet terminated, must succeed
+	{
+		int sharedRet {0x0f0dad58};
+		auto dynamicThread = makeDynamicThread({192, 1}, lambda, std::ref(sharedRet));
+		bool result {true};
+		if (dynamicThread.start() != 0)
+			result = false;
+		if (dynamicThread.detach() != 0)
+			result = false;
+		if (dynamicThread.getState() != ThreadState::Detached)
+			result = false;
+		if (dynamicThread.join() != EINVAL)	// detached thread cannot be joined
+			result = false;
+		if (sharedRet != 0x0f0dad58)
+			result = false;
+		waitForNextTick();
+		if (result == false || sharedRet != EINVAL || dynamicThread.detach() != EINVAL)	// self-detach must fail
+			return false;
+	}
+
+	if (mallinfo().uordblks != 0)	// all dynamic memory must be deallocated after each test phase
+		return false;
+
+	// self-detach of dynamic thread must succeed
+	{
+		int sharedRet {0x5d3c799b};
+		auto dynamicThread = makeDynamicThread({192, UINT8_MAX}, lambda, std::ref(sharedRet));
+		bool result {true};
+		if (dynamicThread.start() != 0)
+			result = false;
+		if (dynamicThread.getState() != ThreadState::Detached)
+			result = false;
+		if (dynamicThread.detach() != EINVAL)	// second attempt to detach a thread must fail
+			result = false;
+		if (dynamicThread.join() != EINVAL)	// detached thread cannot be joined
+			result = false;
+		if (sharedRet != 0)	// self-detach must be successful
+			result = false;
+		waitForNextTick();
+		if (result == false)
+			return false;
+	}
+
+	if (mallinfo().uordblks != 0)	// all dynamic memory must be deallocated after each test phase
+		return false;
+
+	// detaching dynamic thread that is already terminated must succeed, the thread is just deleted
+	{
+		auto dynamicThread = makeDynamicThread({192, UINT8_MAX}, emptyFunction);
+		bool result {true};
+		if (dynamicThread.start() != 0)
+			result = false;
+		if (dynamicThread.getState() != ThreadState::Terminated)
+			result = false;
+		if (dynamicThread.detach() != 0)
+			result = false;
+		if (dynamicThread.getState() != ThreadState::Detached)
+			result = false;
+		// detached thread cannot be joined
+		if (dynamicThread.join() != EINVAL || result == false || dynamicThread.detach() != EINVAL)
+			return false;
+	}
+
+	if (mallinfo().uordblks != 0)	// all dynamic memory must be deallocated after each test phase
+		return false;
+
+#endif	// def CONFIG_THREAD_DETACH_ENABLE
+
+	return true;
+}
+
 }	// namespace
 
 /*---------------------------------------------------------------------------------------------------------------------+
@@ -123,15 +258,24 @@ bool ThreadOperationsTestCase::run_() const
 {
 	constexpr auto phase1ExpectedContextSwitchCount = 2;
 	constexpr auto phase2ExpectedContextSwitchCount = 0;
-	constexpr auto expectedContextSwitchCount = phase1ExpectedContextSwitchCount + phase2ExpectedContextSwitchCount;
+#ifdef CONFIG_THREAD_DETACH_ENABLE
+	constexpr auto phase3ExpectedContextSwitchCount = 11;
+#else	// !def CONFIG_THREAD_DETACH_ENABLE
+	constexpr auto phase3ExpectedContextSwitchCount = 0;
+#endif	// !def CONFIG_THREAD_DETACH_ENABLE
+	constexpr auto expectedContextSwitchCount = phase1ExpectedContextSwitchCount + phase2ExpectedContextSwitchCount +
+			phase3ExpectedContextSwitchCount;
 
 	const auto contextSwitchCount = statistics::getContextSwitchCount();
 
-	for (const auto& function : {phase1, phase2})
+	for (const auto& function : {phase1, phase2, phase3})
 	{
 		const auto ret = function();
 		if (ret != true)
 			return ret;
+
+		if (mallinfo().uordblks != 0)	// all dynamic memory must be deallocated after each test phase
+			return false;
 	}
 
 	if (statistics::getContextSwitchCount() - contextSwitchCount != expectedContextSwitchCount)
