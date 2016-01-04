11d7bc50b2404e2921498a35dec3e1370e56b99a
Kamil Szczygiel <distortec@distortec.com>
2016-01-04 21:10:13 +0100

    test: convert ThreadPriorityTestCase to use dynamic threads

--- test/Thread/ThreadPriorityTestCase.cpp
+++ test/Thread/ThreadPriorityTestCase.cpp
@@ -2,13 +2,13 @@
  * \file
  * \brief ThreadPriorityTestCase class implementation
  *
- * \author Copyright (C) 2014 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2014-10-27
+ * \date 2016-01-04
  */
 
 #include "ThreadPriorityTestCase.hpp"
@@ -16,9 +16,11 @@
 #include "priorityTestPhases.hpp"
 #include "SequenceAsserter.hpp"
 
-#include "distortos/StaticThread.hpp"
-
 #include "distortos/architecture/InterruptMaskingLock.hpp"
+
+#include "distortos/DynamicThread.hpp"
+
+#include <malloc.h>
 
 namespace distortos
 {
@@ -37,25 +39,11 @@ namespace
 constexpr size_t testThreadStackSize {192};
 
 /*---------------------------------------------------------------------------------------------------------------------+
-| local functions' declarations
-+---------------------------------------------------------------------------------------------------------------------*/
-
-void thread(SequenceAsserter& sequenceAsserter, unsigned int sequencePoint);
-
-/*---------------------------------------------------------------------------------------------------------------------+
-| local types
-+---------------------------------------------------------------------------------------------------------------------*/
-
-/// type of test thread
-using TestThread = decltype(makeStaticThread<testThreadStackSize>({}, thread,
-		std::ref(std::declval<SequenceAsserter&>()), std::declval<unsigned int>()));
-
-/*---------------------------------------------------------------------------------------------------------------------+
 | local functions
 +---------------------------------------------------------------------------------------------------------------------*/
 
 /**
- * \brief Test thread.
+ * \brief Test thread
  *
  * Just marks the sequence point in SequenceAsserter.
  *
@@ -69,18 +57,18 @@ void thread(SequenceAsserter& sequenceAsserter, const unsigned int sequencePoint
 }
 
 /**
- * \brief Builder of TestThread objects.
+ * \brief Builder of test threads
  *
  * \param [in] threadParameters is a reference to ThreadParameters object
  * \param [in] sequenceAsserter is a reference to SequenceAsserter shared object
  *
- * \return constructed TestThread object
+ * \return constructed DynamicThread object
  */
 
-TestThread makeTestThread(const ThreadParameters& threadParameters, SequenceAsserter& sequenceAsserter)
+DynamicThread makeTestThread(const ThreadParameters& threadParameters, SequenceAsserter& sequenceAsserter)
 {
-	return makeStaticThread<testThreadStackSize>(threadParameters.first, thread, std::ref(sequenceAsserter),
-			static_cast<unsigned int>(threadParameters.second));
+	return makeDynamicThread({testThreadStackSize, threadParameters.first}, thread, std::ref(sequenceAsserter),
+			threadParameters.second);
 }
 
 }	// namespace
@@ -93,33 +81,38 @@ bool ThreadPriorityTestCase::run_() const
 {
 	for (const auto& phase : priorityTestPhases)
 	{
-		SequenceAsserter sequenceAsserter;
-
-		std::array<TestThread, totalThreads> threads
-		{{
-				makeTestThread(phase.first[phase.second[0]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[1]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[2]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[3]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[4]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[5]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[6]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[7]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[8]], sequenceAsserter),
-				makeTestThread(phase.first[phase.second[9]], sequenceAsserter),
-		}};
-
 		{
-			architecture::InterruptMaskingLock interruptMaskingLock;
+			SequenceAsserter sequenceAsserter;
+
+			std::array<DynamicThread, totalThreads> threads
+			{{
+					makeTestThread(phase.first[phase.second[0]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[1]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[2]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[3]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[4]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[5]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[6]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[7]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[8]], sequenceAsserter),
+					makeTestThread(phase.first[phase.second[9]], sequenceAsserter),
+			}};
+
+			{
+				architecture::InterruptMaskingLock interruptMaskingLock;
+
+				for (auto& thread : threads)
+					thread.start();
+			}
 
 			for (auto& thread : threads)
-				thread.start();
+				thread.join();
+
+			if (sequenceAsserter.assertSequence(totalThreads) == false)
+				return false;
 		}
 
-		for (auto& thread : threads)
-			thread.join();
-
-		if (sequenceAsserter.assertSequence(totalThreads) == false)
+		if (mallinfo().uordblks != 0)	// all dynamic memory must be deallocated after each test phase
 			return false;
 	}
 
