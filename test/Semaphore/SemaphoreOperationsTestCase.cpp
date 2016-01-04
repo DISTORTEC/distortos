a4cd962bd8d74a91e870e3716e7961f9d3ebe6e2
Kamil Szczygiel <distortec@distortec.com>
2016-01-04 23:20:34 +0100

    test: convert SemaphoreOperationsTestCase to use dynamic threads

--- test/Semaphore/SemaphoreOperationsTestCase.cpp
+++ test/Semaphore/SemaphoreOperationsTestCase.cpp
@@ -2,25 +2,23 @@
  * \file
  * \brief SemaphoreOperationsTestCase class implementation
  *
- * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * \date 2015-11-11
+ * \date 2016-01-04
  */
 
 #include "SemaphoreOperationsTestCase.hpp"
 
 #include "waitForNextTick.hpp"
 
+#include "distortos/DynamicThread.hpp"
 #include "distortos/StaticSoftwareTimer.hpp"
-#include "distortos/StaticThread.hpp"
 #include "distortos/statistics.hpp"
 #include "distortos/ThisThread.hpp"
-
-#include <cerrno>
 
 namespace distortos
 {
@@ -248,7 +246,7 @@ bool phase3()
 
 		const auto contextSwitchCount = statistics::getContextSwitchCount();
 		const auto wakeUpTimePoint = TickClock::now() + longDuration;
-		auto thread = makeStaticThread<testThreadStackSize>(UINT8_MAX, sleepUntilFunctor, wakeUpTimePoint);
+		auto thread = makeDynamicThread({testThreadStackSize, UINT8_MAX}, sleepUntilFunctor, wakeUpTimePoint);
 
 		thread.start();
 		ThisThread::yield();
@@ -273,7 +271,7 @@ bool phase3()
 
 		const auto contextSwitchCount = statistics::getContextSwitchCount();
 		const auto wakeUpTimePoint = TickClock::now() + longDuration;
-		auto thread = makeStaticThread<testThreadStackSize>(UINT8_MAX, sleepUntilFunctor, wakeUpTimePoint);
+		auto thread = makeDynamicThread({testThreadStackSize, UINT8_MAX}, sleepUntilFunctor, wakeUpTimePoint);
 
 		thread.start();
 		ThisThread::yield();
@@ -298,7 +296,7 @@ bool phase3()
 
 		const auto contextSwitchCount = statistics::getContextSwitchCount();
 		const auto wakeUpTimePoint = TickClock::now() + longDuration;
-		auto thread = makeStaticThread<testThreadStackSize>(UINT8_MAX, sleepUntilFunctor, wakeUpTimePoint);
+		auto thread = makeDynamicThread({testThreadStackSize, UINT8_MAX}, sleepUntilFunctor, wakeUpTimePoint);
 
 		thread.start();
 		ThisThread::yield();
