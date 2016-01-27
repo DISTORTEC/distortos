40420cada25a310e1439b3abcd6446592009252b
Kamil Szczygiel <distortec@distortec.com>
2016-01-27 21:29:37 +0100

    add empty board::lowLevelInitialization() for NUCLEO-F401RE

---
+++ source/board/STM32/STM32F4/NUCLEO-F401RE/NUCLEO-F401RE-lowLevelInitialization.cpp
@@ -0,0 +1,31 @@
+/**
+ * \file
+ * \brief board::lowLevelInitialization() implementation for NUCLEO-F401RE
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/board/lowLevelInitialization.hpp"
+
+namespace distortos
+{
+
+namespace board
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void lowLevelInitialization()
+{
+
+}
+
+}	// namespace board
+
+}	// namespace distortos
