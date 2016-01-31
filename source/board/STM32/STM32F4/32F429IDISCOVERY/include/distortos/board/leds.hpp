27892644599433c7d17957517cac1f53b5c29fd2
Kamil Szczygiel <distortec@distortec.com>
2016-01-31 23:28:23 +0100

    board: add leds module for 32F429IDISCOVERY

---
+++ source/board/STM32/STM32F4/32F429IDISCOVERY/include/distortos/board/leds.hpp
@@ -0,0 +1,85 @@
+/**
+ * \file
+ * \brief Declaration of LEDs for 32F429IDISCOVERY
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOURCE_BOARD_STM32_STM32F4_32F429IDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
+#define SOURCE_BOARD_STM32_STM32F4_32F429IDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
+
+#include "distortos/chip/STM32F4-GPIO.hpp"
+
+#include <array>
+
+namespace distortos
+{
+
+#ifdef CONFIG_BOARD_LEDS_ENABLE
+
+namespace chip
+{
+
+class ChipOutputPin;
+
+}	// namespace chip
+
+#endif	// def CONFIG_BOARD_LEDS_ENABLE
+
+namespace board
+{
+
+/// total number of LEDs on the board
+constexpr size_t totalLeds {CONFIG_BOARD_TOTAL_LEDS};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| LED indexes
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// index of LD3 LED (green)
+constexpr size_t ld3LedIndex {0};
+
+/// index of LD4 LED (red)
+constexpr size_t ld4LedIndex {1};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| alternative (color-based) LED indexes
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// alternative index of LD3 LED (green)
+constexpr size_t greenLedIndex {ld3LedIndex};
+
+/// alternative index of LD4 LED (red)
+constexpr size_t redLedIndex {ld4LedIndex};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| indexed access to pin identifiers
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// array with pin identifiers of all LEDs
+constexpr std::array<chip::Pin, totalLeds> ledPins
+{
+		chip::Pin::PG13,
+		chip::Pin::PG14,
+};
+
+#ifdef CONFIG_BOARD_LEDS_ENABLE
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| indexed access to LED objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// array with all LED objects
+extern chip::ChipOutputPin leds[totalLeds];
+
+#endif	// def CONFIG_BOARD_LEDS_ENABLE
+
+}	// namespace board
+
+}	// namespace distortos
+
+#endif	// SOURCE_BOARD_STM32_STM32F4_32F429IDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
