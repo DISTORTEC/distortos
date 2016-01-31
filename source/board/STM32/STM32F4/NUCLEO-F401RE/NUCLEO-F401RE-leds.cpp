172f477f42871e2f3e608ed210b968981e2f1e13
Kamil Szczygiel <distortec@distortec.com>
2016-01-31 23:20:49 +0100

    board: add leds module for NUCLEO-F401RE

---
+++ source/board/STM32/STM32F4/NUCLEO-F401RE/NUCLEO-F401RE-leds.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file
+ * \brief Definition of LEDs for NUCLEO-F401RE
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/board/leds.hpp"
+
+#ifdef CONFIG_BOARD_LEDS_ENABLE
+
+#include "distortos/chip/ChipOutputPin.hpp"
+
+namespace distortos
+{
+
+namespace board
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+chip::ChipOutputPin leds[totalLeds]
+{
+		chip::ChipOutputPin{ledPins[0]},
+};
+
+}	// namespace board
+
+}	// namespace distortos
+
+#endif	// def CONFIG_BOARD_LEDS_ENABLE
