4655a3626fe62b396580c609ad92a95761d0357c
Kamil Szczygiel <distortec@distortec.com>
2016-02-01 21:43:55 +0100

    ReferenceHolder: make all member functions constexpr

--- include/estd/ReferenceHolder.hpp
+++ include/estd/ReferenceHolder.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief ReferenceHolder template class header.
  *
- * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -39,13 +39,13 @@ public:
 	}
 
 	/// \return reference held by the object
-	operator T&() const noexcept
+	constexpr operator T&() const noexcept
 	{
 		return reference_;
 	}
 
 	/// \return reference held by the object
-	T& get() const noexcept
+	constexpr T& get() const noexcept
 	{
 		return reference_;
 	}
