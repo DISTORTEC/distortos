cc10993815769672e3325f5a6b63fc7d749328db
Kamil Szczygiel <distortec@distortec.com>
2016-02-01 23:17:55 +0100

    test: show result of test with blinking of board's LEDs

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief Main code block.
  *
- * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -12,6 +12,18 @@
 #include "TestCaseGroup.hpp"
 #include "testCases.hpp"
 
+#include "distortos/distortosConfiguration.h"
+
+#ifdef CONFIG_BOARD_LEDS_ENABLE
+
+#include "distortos/board/leds.hpp"
+
+#include "distortos/chip/ChipOutputPin.hpp"
+
+#endif	// def CONFIG_BOARD_LEDS_ENABLE
+
+#include "distortos/ThisThread.hpp"
+
 /*---------------------------------------------------------------------------------------------------------------------+
 | global functions
 +---------------------------------------------------------------------------------------------------------------------*/
@@ -19,16 +31,32 @@
 /**
  * \brief Main code block of test application
  *
- * Runs all the test cases. If any test case fails (TestCase::run() returns false) infinite loop is entered so the
- * situation can be noticed in debugging.
- *
- * \return 0 on success, doesn't return otherwise
+ * Runs all test cases. The result is signaled with blinking of all board's LEDs:
+ * - success - slow blinking, 1Hz frequency,
+ * - failure - fast blinking, 10Hz frequency.
+ * If the board doesn't provide LEDs, the result can be examined with the debugger by checking the value of "result"
+ * variable.
  */
 
 int main()
 {
-	if (distortos::test::testCases.run() == false)
-		while (1);
+	// "volatile" to allow examination of the value with debugger - the variable will not be optimized out
+	const volatile auto result = distortos::test::testCases.run();
 
-	return 0;
+	// next line is a good place for a breakpoint that will be hit right after test cases
+	const auto duration = result == true ? std::chrono::milliseconds{500} :  std::chrono::milliseconds{50};
+	while (1)
+	{
+#ifdef CONFIG_BOARD_LEDS_ENABLE
+
+		for (size_t ledIndex {}; ledIndex < distortos::board::totalLeds; ++ledIndex)
+		{
+			auto& led = distortos::board::leds[ledIndex];
+			led.set(!led.get());
+		}
+
+#endif	// def CONFIG_BOARD_LEDS_ENABLE
+
+		distortos::ThisThread::sleepFor(duration);
+	}
 }
