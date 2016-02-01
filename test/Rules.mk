99720ba582f2a29f094f22ec7d24262a1c4ba30e
Kamil Szczygiel <distortec@distortec.com>
2016-02-01 22:32:49 +0100

    make build: add "chip" and "board" include paths for test application

--- test/Rules.mk
+++ test/Rules.mk
@@ -46,6 +46,8 @@ GENERATED := $(GENERATED) $(ELF_$(d)) $(ELF_$(d):%.elf=%.map) $(HEX_$(d)) $(BIN_
 CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) -I$(d)
 CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) $(STANDARD_INCLUDES)
 CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) $(ARCHITECTURE_INCLUDES)
+CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) $(CHIP_INCLUDES)
+CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) $(BOARD_INCLUDES)
 
 #-----------------------------------------------------------------------------------------------------------------------
 # standard footer
