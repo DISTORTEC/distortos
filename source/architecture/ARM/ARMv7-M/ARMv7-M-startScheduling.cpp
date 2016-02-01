1ac2fd320090ae85d67efdddca09532bab7ee835
Kamil Szczygiel <distortec@distortec.com>
2016-02-01 23:57:20 +0100

    ARMv7-M: reduce priority of SVCall interrupt
    Reduce priority of SVCall interrupt to max allowed value for system code
    - CONFIG_ARCHITECTURE_ARMV7_M_KERNEL_BASEPRI.

--- source/architecture/ARM/ARMv7-M/ARMv7-M-startScheduling.cpp
+++ source/architecture/ARM/ARMv7-M/ARMv7-M-startScheduling.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief startScheduling() implementation for ARMv7-M (Cortex-M3 / Cortex-M4)
  *
- * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -28,6 +28,7 @@ void startScheduling()
 	// SysTick and PendSV - lowest possible priority
 	NVIC_SetPriority(SysTick_IRQn, 0xff);
 	NVIC_SetPriority(PendSV_IRQn, 0xff);
+	NVIC_SetPriority(SVCall_IRQn, CONFIG_ARCHITECTURE_ARMV7_M_KERNEL_BASEPRI);
 
 	// enable SysTick timer as the tick timer
 	SysTick->VAL = 0;
