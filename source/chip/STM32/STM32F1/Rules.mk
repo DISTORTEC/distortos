ec8e3678001193c2f738ecfb330ae9659127cbe6
Kamil Szczygiel <distortec@distortec.com>
2016-02-13 11:25:38 +0100

    make build: add STM32F1 chip sources to compilation

---
+++ source/chip/STM32/STM32F1/Rules.mk
@@ -0,0 +1,50 @@
+#
+# file: Rules.mk
+#
+# author: Copyright (C) 2015-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+# distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+ifeq ($(CONFIG_CHIP_STM32F1),y)
+
+#-----------------------------------------------------------------------------------------------------------------------
+# linker script
+#-----------------------------------------------------------------------------------------------------------------------
+
+STM32F1_LD_SH := $(d)STM32F1.ld.sh
+
+$(LDSCRIPT): $(DISTORTOS_CONFIGURATION_MK)
+	$(call PRETTY_PRINT," SH     " $(STM32F1_LD_SH))
+	$(Q)./$(STM32F1_LD_SH) "$(dir $<)$(notdir $<)" > "$@"
+
+#-----------------------------------------------------------------------------------------------------------------------
+# generated linker script depends on this Rules.mk, the script that generates it and the selectedConfiguration.mk file
+#-----------------------------------------------------------------------------------------------------------------------
+
+$(LDSCRIPT): $(d)Rules.mk $(STM32F1_LD_SH) selectedConfiguration.mk
+
+#-----------------------------------------------------------------------------------------------------------------------
+# add generated linker script to list of generated files
+#-----------------------------------------------------------------------------------------------------------------------
+
+GENERATED := $(GENERATED) $(LDSCRIPT)
+
+#-----------------------------------------------------------------------------------------------------------------------
+# compilation flags
+#-----------------------------------------------------------------------------------------------------------------------
+
+CFLAGS_$(d) := $(CFLAGS_$(d)) $(STANDARD_INCLUDES)
+
+CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) $(STANDARD_INCLUDES)
+CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) $(ARCHITECTURE_INCLUDES)
+CXXFLAGS_$(d) := $(CXXFLAGS_$(d)) $(CHIP_INCLUDES)
+
+#-----------------------------------------------------------------------------------------------------------------------
+# standard footer
+#-----------------------------------------------------------------------------------------------------------------------
+
+include $(DISTORTOS_PATH)footer.mk
+
+endif	# eq ($(CONFIG_CHIP_STM32F1),y)
