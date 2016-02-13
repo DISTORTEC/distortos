3c19795d8484a29b6260df86668a877184daf45e
Kamil Szczygiel <distortec@distortec.com>
2016-02-13 11:25:01 +0100

    add STM32F1-lowLevelInitialization.cpp
    Currently only SysTick is configured.

---
+++ source/chip/STM32/STM32F1/STM32F1-lowLevelInitialization.cpp
@@ -0,0 +1,42 @@
+/**
+ * \file
+ * \brief chip::lowLevelInitialization() implementation for STM32F1
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/lowLevelInitialization.hpp"
+
+#include "distortos/architecture/configureSysTick.hpp"
+
+#include "distortos/distortosConfiguration.h"
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void lowLevelInitialization()
+{
+	constexpr uint32_t ahbHz {CONFIG_CHIP_STM32F1_RCC_AHB_HZ};
+	constexpr uint32_t period {ahbHz / CONFIG_TICK_RATE_HZ};
+	constexpr uint32_t periodDividedBy8 {period / 8};
+	constexpr bool divideBy8 {period > architecture::maxSysTickPeriod};
+	// at least one of the periods must be valid
+	static_assert(period <= architecture::maxSysTickPeriod || periodDividedBy8 <= architecture::maxSysTickPeriod,
+			"Invalid SysTick configuration!");
+	architecture::configureSysTick(divideBy8 == false ? period : periodDividedBy8, divideBy8);
+}
+
+}	// namespace chip
+
+}	// namespace distortos
