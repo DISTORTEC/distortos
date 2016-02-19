0dc4c6cd0c4968f6cd589280533fd251e3fff01b
Kamil Szczygiel <distortec@distortec.com>
2016-02-19 18:43:18 +0100

    STM32F4: replace "Hz" suffix with "Frequency"
    There's not point in stating the default unit.

--- source/chip/STM32/STM32F4/include/distortos/chip/STM32F4-RCC.hpp
+++ source/chip/STM32/STM32F4/include/distortos/chip/STM32F4-RCC.hpp
@@ -10,7 +10,7 @@
  * - RM0390 reference manual (STM32F446xx), Revision 1, 2015-03-17
  * - RM0386 reference manual (STM32F469xx and STM32F479xx), Revision 2, 2015-11-19
  *
- * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2015-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -101,23 +101,23 @@ constexpr uint8_t pllpDiv6 {6};
 constexpr uint8_t pllpDiv8 {8};
 
 /// HSI clock frequency, Hz
-constexpr uint32_t hsiHz {16000000};
+constexpr uint32_t hsiFrequency {16000000};
 
 /// minimum allowed value for VCO input frequency, Hz
-constexpr uint32_t minVcoInHz {1000000};
+constexpr uint32_t minVcoInFrequency {1000000};
 
 /// maximum allowed value for VCO input frequency, Hz
-constexpr uint32_t maxVcoInHz {2000000};
+constexpr uint32_t maxVcoInFrequency {2000000};
 
 /// minimum allowed value for VCO output frequency, Hz
 #if defined(CONFIG_CHIP_STM32F401)
-constexpr uint32_t minVcoOutHz {192000000};
+constexpr uint32_t minVcoOutFrequency {192000000};
 #else	// !defined(CONFIG_CHIP_STM32F401)
-constexpr uint32_t minVcoOutHz {100000000};
+constexpr uint32_t minVcoOutFrequency {100000000};
 #endif	// !defined(CONFIG_CHIP_STM32F401)
 
 /// maximum allowed value for VCO output frequency, Hz
-constexpr uint32_t maxVcoOutHz {432000000};
+constexpr uint32_t maxVcoOutFrequency {432000000};
 
 /// maximum allowed value for PLL output frequency, Hz
 /// [0] - in overdrive mode with voltage scale 1
@@ -125,33 +125,33 @@ constexpr uint32_t maxVcoOutHz {432000000};
 /// [2] - with voltage scale 2
 /// [3] - with voltage scale 3
 #if defined(CONFIG_CHIP_STM32F401)
-constexpr std::array<uint32_t, 4> maxPllOutHz {0, 0, 84000000, 60000000};
+constexpr std::array<uint32_t, 4> maxPllOutFrequency {0, 0, 84000000, 60000000};
 #elif defined(CONFIG_CHIP_STM32F405) || defined(CONFIG_CHIP_STM32F407) || defined(CONFIG_CHIP_STM32F415) || \
 		defined(CONFIG_CHIP_STM32F417)
-constexpr std::array<uint32_t, 4> maxPllOutHz {0, 168000000, 144000000, 0};
+constexpr std::array<uint32_t, 4> maxPllOutFrequency {0, 168000000, 144000000, 0};
 #elif defined(CONFIG_CHIP_STM32F410) || defined(CONFIG_CHIP_STM32F411)
-constexpr std::array<uint32_t, 4> maxPllOutHz {0, 100000000, 84000000, 64000000};
+constexpr std::array<uint32_t, 4> maxPllOutFrequency {0, 100000000, 84000000, 64000000};
 #else	// !defined(CONFIG_CHIP_STM32F401) && !defined(CONFIG_CHIP_STM32F405) && !defined(CONFIG_CHIP_STM32F407) &&
 		// !defined(CONFIG_CHIP_STM32F415) && !defined(CONFIG_CHIP_STM32F417) && !defined(CONFIG_CHIP_STM32F410) &&
 		// !defined(CONFIG_CHIP_STM32F411)
-constexpr std::array<uint32_t, 4> maxPllOutHz {180000000, 168000000, 144000000, 120000000};
+constexpr std::array<uint32_t, 4> maxPllOutFrequency {180000000, 168000000, 144000000, 120000000};
 #endif	// !defined(CONFIG_CHIP_STM32F401) && !defined(CONFIG_CHIP_STM32F405) && !defined(CONFIG_CHIP_STM32F407) &&
 		// !defined(CONFIG_CHIP_STM32F415) && !defined(CONFIG_CHIP_STM32F417) && !defined(CONFIG_CHIP_STM32F410) &&
 		// !defined(CONFIG_CHIP_STM32F411)
 
 /// maximum allowed value for PLL "Q" output frequency, Hz
-constexpr uint32_t maxPllqOutHz {48000000};
+constexpr uint32_t maxPllqOutFrequency {48000000};
 
 /// maximum allowed APB1 (low speed) frequency, Hz
 #if defined(CONFIG_CHIP_STM32F401) || defined(CONFIG_CHIP_STM32F405) || defined(CONFIG_CHIP_STM32F407) || \
 		defined(CONFIG_CHIP_STM32F415) || defined(CONFIG_CHIP_STM32F417)
-constexpr uint32_t maxApb1Hz {42000000};
+constexpr uint32_t maxApb1Frequency {42000000};
 #elif defined(CONFIG_CHIP_STM32F410) || defined(CONFIG_CHIP_STM32F411)
-constexpr uint32_t maxApb1Hz {50000000};
+constexpr uint32_t maxApb1Frequency {50000000};
 #else	// !defined(CONFIG_CHIP_STM32F401) && !defined(CONFIG_CHIP_STM32F405) && !defined(CONFIG_CHIP_STM32F407) &&
 		// !defined(CONFIG_CHIP_STM32F415) && !defined(CONFIG_CHIP_STM32F417) && !defined(CONFIG_CHIP_STM32F410) &&
 		// !defined(CONFIG_CHIP_STM32F411)
-constexpr uint32_t maxApb1Hz {45000000};
+constexpr uint32_t maxApb1Frequency {45000000};
 #endif	// !defined(CONFIG_CHIP_STM32F401) && !defined(CONFIG_CHIP_STM32F405) && !defined(CONFIG_CHIP_STM32F407) &&
 		// !defined(CONFIG_CHIP_STM32F415) && !defined(CONFIG_CHIP_STM32F417) && !defined(CONFIG_CHIP_STM32F410) &&
 		// !defined(CONFIG_CHIP_STM32F411)
@@ -159,13 +159,13 @@ constexpr uint32_t maxApb1Hz {45000000};
 /// maximum allowed APB2 (high speed) frequency, Hz
 #if defined(CONFIG_CHIP_STM32F401) || defined(CONFIG_CHIP_STM32F405) || defined(CONFIG_CHIP_STM32F407) || \
 		defined(CONFIG_CHIP_STM32F415) || defined(CONFIG_CHIP_STM32F417)
-constexpr uint32_t maxApb2Hz {84000000};
+constexpr uint32_t maxApb2Frequency {84000000};
 #elif defined(CONFIG_CHIP_STM32F410) || defined(CONFIG_CHIP_STM32F411)
-constexpr uint32_t maxApb2Hz {100000000};
+constexpr uint32_t maxApb2Frequency {100000000};
 #else	// !defined(CONFIG_CHIP_STM32F401) && !defined(CONFIG_CHIP_STM32F405) && !defined(CONFIG_CHIP_STM32F407) &&
 		// !defined(CONFIG_CHIP_STM32F415) && !defined(CONFIG_CHIP_STM32F417) && !defined(CONFIG_CHIP_STM32F410) &&
 		// !defined(CONFIG_CHIP_STM32F411)
-constexpr uint32_t maxApb2Hz {90000000};
+constexpr uint32_t maxApb2Frequency {90000000};
 #endif	// !defined(CONFIG_CHIP_STM32F401) && !defined(CONFIG_CHIP_STM32F405) && !defined(CONFIG_CHIP_STM32F407) &&
 		// !defined(CONFIG_CHIP_STM32F415) && !defined(CONFIG_CHIP_STM32F417) && !defined(CONFIG_CHIP_STM32F410) &&
 		// !defined(CONFIG_CHIP_STM32F411)
