1005fb82fecb3a935c5da84d0eac7fd4cdab967c
Kamil Szczygiel <distortec@distortec.com>
2016-03-03 21:13:16 +0100

    Configure FLASH module in low-level initialization for STM32F1
    Configuration of flash prefetch, flash half cycle access and flash
    latency is enabled only if
    CONFIG_CHIP_STM32F1_FLASH_STANDARD_CONFIGURATION_ENABLE option is
    selected.

--- source/chip/STM32/STM32F1/STM32F1-lowLevelInitialization.cpp
+++ source/chip/STM32/STM32F1/STM32F1-lowLevelInitialization.cpp
@@ -11,9 +11,9 @@
 
 #include "distortos/chip/lowLevelInitialization.hpp"
 
-#include "distortos/architecture/configureSysTick.hpp"
+#include "distortos/chip/STM32F1-FLASH.hpp"
 
-#include "distortos/distortosConfiguration.h"
+#include "distortos/architecture/configureSysTick.hpp"
 
 namespace distortos
 {
@@ -27,7 +27,36 @@ namespace chip
 
 void lowLevelInitialization()
 {
+#ifdef CONFIG_CHIP_STM32F1_FLASH_STANDARD_CONFIGURATION_ENABLE
+
+#ifndef CONFIG_CHIP_STM32F100
+
+#ifdef CONFIG_CHIP_STM32F1_FLASH_PREFETCH_ENABLE
+	configurePrefetchBuffer(true);
+#else	// !def CONFIG_CHIP_STM32F1_FLASH_PREFETCH_ENABLE
+	configurePrefetchBuffer(false);
+#endif	// !def CONFIG_CHIP_STM32F1_FLASH_PREFETCH_ENABLE
+
+#endif	// !def CONFIG_CHIP_STM32F100
+
+#ifdef CONFIG_CHIP_STM32F1_FLASH_HALF_CYCLE_ACCESS_ENABLE
+	configureFlashHalfCycleAccess(true);
+#else	// !def CONFIG_CHIP_STM32F1_FLASH_HALF_CYCLE_ACCESS_ENABLE
+	configureFlashHalfCycleAccess(false);
+#endif	// !def CONFIG_CHIP_STM32F1_FLASH_HALF_CYCLE_ACCESS_ENABLE
+
+#endif	// def CONFIG_CHIP_STM32F1_FLASH_STANDARD_CONFIGURATION_ENABLE
+
 	constexpr uint32_t ahbFrequency {CONFIG_CHIP_STM32F1_RCC_AHB_FREQUENCY};
+
+#if !defined(CONFIG_CHIP_STM32F100) && defined(CONFIG_CHIP_STM32F1_FLASH_STANDARD_CONFIGURATION_ENABLE)
+
+	constexpr uint8_t flashLatency {(ahbFrequency - 1) / 24000000};
+	static_assert(flashLatency <= maxFlashLatency, "Invalid flash latency!");
+	configureFlashLatency(flashLatency);
+
+#endif	// !defined(CONFIG_CHIP_STM32F100) && defined(CONFIG_CHIP_STM32F1_FLASH_STANDARD_CONFIGURATION_ENABLE)
+
 	constexpr uint32_t period {ahbFrequency / CONFIG_TICK_FREQUENCY};
 	constexpr uint32_t periodDividedBy8 {period / 8};
 	constexpr bool divideBy8 {period > architecture::maxSysTickPeriod};
