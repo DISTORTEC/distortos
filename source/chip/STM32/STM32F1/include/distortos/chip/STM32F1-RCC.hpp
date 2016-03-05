7e5ae4d0e6f644c11bc6bf1d10307da52c1c4ae5
Kamil Szczygiel <distortec@distortec.com>
2016-03-05 09:03:39 +0100

    Add configureApbClockDivider() for STM32F1

--- source/chip/STM32/STM32F1/include/distortos/chip/STM32F1-RCC.hpp
+++ source/chip/STM32/STM32F1/include/distortos/chip/STM32F1-RCC.hpp
@@ -100,6 +100,21 @@ constexpr uint16_t hpreDiv256 {256};
 /// ninth allowed value for AHB divider - 512
 constexpr uint16_t hpreDiv512 {512};
 
+/// first allowed value for APB1 and APB2 dividers - 1
+constexpr uint8_t ppreDiv1 {1};
+
+/// second allowed value for APB1 and APB2 dividers - 2
+constexpr uint8_t ppreDiv2 {2};
+
+/// third allowed value for APB1 and APB2 dividers - 4
+constexpr uint8_t ppreDiv4 {4};
+
+/// fourth allowed value for APB1 and APB2 dividers - 8
+constexpr uint8_t ppreDiv8 {8};
+
+/// fifth allowed value for APB1 and APB2 dividers - 16
+constexpr uint8_t ppreDiv16 {16};
+
 /*---------------------------------------------------------------------------------------------------------------------+
 | global functions' declarations
 +---------------------------------------------------------------------------------------------------------------------*/
@@ -115,6 +130,18 @@ constexpr uint16_t hpreDiv512 {512};
  */
 
 int configureAhbClockDivider(uint16_t hpre);
+
+/**
+ * \brief Configures divider of APB1 or APB2 clock (PPRE1 or PPRE2 value).
+ *
+ * \param [in] ppre2 selects whether PPRE1 (false) or PPRE2 (true) is configured
+ * \param [in] ppre is the PPRE value, {1, 2, 4, 8, 16} or {ppreDiv1, ppreDiv2, ppreDiv4, ppreDiv8, ppreDiv16}
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a ppre value is invalid;
+ */
+
+int configureApbClockDivider(bool ppre2, uint8_t ppre);
 
 /**
  * \brief Configures PREDIV1 division factor.
