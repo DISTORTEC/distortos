5293762a1a58102e38a1b7993cb78bd0e47cd308
Kamil Szczygiel <distortec@distortec.com>
2016-03-08 20:12:24 +0100

    Enable GPIOB in board::lowLevelInitialization() for NUCLEO-F429ZI
    Enable clock for GPIOB if CONFIG_BOARD_LEDS_ENABLE option is defined.

--- source/board/STM32/STM32F4/NUCLEO-F429ZI/NUCLEO-F429ZI-lowLevelInitialization.cpp
+++ source/board/STM32/STM32F4/NUCLEO-F429ZI/NUCLEO-F429ZI-lowLevelInitialization.cpp
@@ -11,6 +11,8 @@
 
 #include "distortos/board/lowLevelInitialization.hpp"
 
+#include "distortos/chip/CMSIS-proxy.h"
+
 namespace distortos
 {
 
@@ -23,7 +25,11 @@ namespace board
 
 void lowLevelInitialization()
 {
-
+	RCC->AHB1ENR |=
+#ifdef CONFIG_BOARD_LEDS_ENABLE
+			RCC_AHB1ENR_GPIOBEN |
+#endif	// def CONFIG_BOARD_LEDS_ENABLE
+			0;
 }
 
 }	// namespace board
