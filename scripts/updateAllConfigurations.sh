7901e53aacd2299a4cbd0735324b31502c17240e
Kamil Szczygiel <distortec@distortec.com>
2016-03-08 21:00:47 +0100

    Add updateAllConfigurations.sh scripts
    This script can be used to run "make olddefconfig" for all
    configurations found in provided (optional) search path.

---
+++ scripts/updateAllConfigurations.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#
+# file: updateAllConfigurations.sh
+#
+# author: Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+# distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+set -e
+set -u
+
+searchPath=.
+
+# If any argument was given, then use it as the search path, otherwise search in current directory.
+if [ $# -ge 1 ]; then
+	searchPath=$1
+fi
+
+make distclean
+
+for configurationPath in `find $searchPath -name "distortosConfiguration.mk" -printf "%h "`
+do
+
+	make configure CONFIG_PATH=$configurationPath
+	make olddefconfig
+	make distclean
+
+done
