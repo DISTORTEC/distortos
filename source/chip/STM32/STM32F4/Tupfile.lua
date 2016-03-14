03aba6adb0dfff162d292af75702b932a55c75ba
Kamil Szczygiel <distortec@distortec.com>
2016-03-14 20:10:52 +0100

    Use ARMv7-M.ld.sh linker script generator for STM32F4 in tup build

--- source/chip/STM32/STM32F4/Tupfile.lua
+++ source/chip/STM32/STM32F4/Tupfile.lua
@@ -9,10 +9,52 @@
 
 if CONFIG_CHIP_STM32F4 == "y" then
 
-	local stm32f4LdSh = "STM32F4.ld.sh"
-	local ldscriptOutputs = {LDSCRIPT, filenameToGroup(LDSCRIPT)}
+	local unifiedRamSize = 0
 
-	tup.rule(DISTORTOS_CONFIGURATION_MK, "^ SH " .. stm32f4LdSh .. "^ ./" .. stm32f4LdSh .. " \"%f\" > \"%o\"",
+	if CONFIG_CHIP_STM32F4_UNIFY_SRAM1_SRAM2 == "y" then
+		unifiedRamSize = CONFIG_CHIP_STM32F4_SRAM1_SIZE + CONFIG_CHIP_STM32F4_SRAM2_SIZE
+	elseif CONFIG_CHIP_STM32F4_UNIFY_SRAM1_SRAM2_SRAM3 == "y" then
+		unifiedRamSize = CONFIG_CHIP_STM32F4_SRAM1_SIZE + CONFIG_CHIP_STM32F4_SRAM2_SIZE +
+				CONFIG_CHIP_STM32F4_SRAM3_SIZE
+	else
+		unifiedRamSize = CONFIG_CHIP_STM32F4_SRAM1_SIZE
+	end
+
+	local ldScriptGenerator = TOP .. "source/architecture/ARM/ARMv7-M/ARMv7-M.ld.sh"
+	local ldScriptGeneratorArguments = " \"" .. CONFIG_CHIP .. "\" \"" ..
+			CONFIG_CHIP_STM32F4_FLASH_ADDRESS .. "," .. CONFIG_CHIP_STM32F4_FLASH_SIZE .. "\" \"" ..
+			CONFIG_CHIP_STM32F4_SRAM1_ADDRESS .. "," .. unifiedRamSize .. "\" \"" ..
+			CONFIG_ARCHITECTURE_ARMV7_M_MAIN_STACK_SIZE .. "\" \"" .. CONFIG_MAIN_THREAD_STACK_SIZE .. "\""
+
+	if CONFIG_CHIP_STM32F4_BKPSRAM_ADDRESS ~= nil then
+		ldScriptGeneratorArguments = ldScriptGeneratorArguments ..
+				" \"bkpsram," .. CONFIG_CHIP_STM32F4_BKPSRAM_ADDRESS .. "," .. CONFIG_CHIP_STM32F4_BKPSRAM_SIZE .. "\""
+	end
+
+	if CONFIG_CHIP_STM32F4_CCM_ADDRESS ~= nil then
+		ldScriptGeneratorArguments = ldScriptGeneratorArguments ..
+				" \"ccm," .. CONFIG_CHIP_STM32F4_CCM_ADDRESS .. "," .. CONFIG_CHIP_STM32F4_CCM_SIZE .. "\""
+	end
+
+	if CONFIG_CHIP_STM32F4_SRAM2_ADDRESS ~= nil and CONFIG_CHIP_STM32F4_UNIFY_NONE == "y" then
+		ldScriptGeneratorArguments = ldScriptGeneratorArguments ..
+				" \"sram2," .. CONFIG_CHIP_STM32F4_SRAM2_ADDRESS .. "," .. CONFIG_CHIP_STM32F4_SRAM2_SIZE .. "\""
+	end
+
+	if CONFIG_CHIP_STM32F4_SRAM3_ADDRESS ~= nil and CONFIG_CHIP_STM32F4_UNIFY_SRAM1_SRAM2_SRAM3 == "n" and
+			CONFIG_CHIP_STM32F4_UNIFY_SRAM2_SRAM3 == "n" then
+		ldScriptGeneratorArguments = ldScriptGeneratorArguments ..
+				" \"sram3," .. CONFIG_CHIP_STM32F4_SRAM3_ADDRESS .. "," .. CONFIG_CHIP_STM32F4_SRAM3_SIZE .. "\""
+	end
+
+	if CONFIG_CHIP_STM32F4_UNIFY_SRAM2_SRAM3 == "y" then
+		local sram23Size = CONFIG_CHIP_STM32F4_SRAM2_SIZE + CONFIG_CHIP_STM32F4_SRAM3_SIZE
+		ldScriptGeneratorArguments = ldScriptGeneratorArguments ..
+				" \"sram23," .. CONFIG_CHIP_STM32F4_SRAM2_ADDRESS .. "," .. sram23Size .. "\""
+	end
+
+	local ldscriptOutputs = {LDSCRIPT, filenameToGroup(LDSCRIPT)}
+	tup.rule("^ SH " .. ldScriptGenerator .. "^ ./" .. ldScriptGenerator .. ldScriptGeneratorArguments .. " > \"%o\"",
 			ldscriptOutputs)
 
 	CFLAGS += STANDARD_INCLUDES
