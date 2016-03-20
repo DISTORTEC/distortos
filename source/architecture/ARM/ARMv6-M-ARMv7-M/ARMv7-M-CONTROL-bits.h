16289caeb449efb3ece48ccfd5c97ce2470da3d0
Kamil Szczygiel <distortec@distortec.com>
2016-03-20 18:00:29 +0100

    Add FPCA bit to ARMv7-M-CONTROL-bits.h
    This bit is defined only if __ARM_ARCH_7M__ or __ARM_ARCH_7EM__ are also
    defined.

--- source/architecture/ARM/ARMv6-M-ARMv7-M/ARMv7-M-CONTROL-bits.h
+++ source/architecture/ARM/ARMv6-M-ARMv7-M/ARMv7-M-CONTROL-bits.h
@@ -2,7 +2,7 @@
  * \file
  * \brief Header with definition of bits in CONTROL register in ARMv7-M core (Cortex-M3 / Cortex-M4).
  *
- * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -19,7 +19,19 @@
 #define CONTROL_nPRIV_bit					0
 #define CONTROL_SPSEL_bit					1
 
+#if defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
+
+#define CONTROL_FPCA_bit					2
+
+#endif	/* defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__) */
+
 #define CONTROL_nPRIV						(1 << CONTROL_nPRIV_bit)
 #define CONTROL_SPSEL						(1 << CONTROL_SPSEL_bit)
+
+#if defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
+
+#define CONTROL_FPCA						(1 << CONTROL_FPCA_bit)
+
+#endif	/* defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__) */
 
 #endif	/* SOURCE_ARCHITECTURE_ARM_ARMV6_M_ARMV7_M_ARMV7_M_CONTROL_BITS_H_ */
