2ea039715e57249550fc19cacd524f566accc27b
Kamil Szczygiel <distortec@distortec.com>
2016-03-20 18:18:27 +0100

    Rename ARMv7-M-requestContextSwitch.cpp
    Rename ARMv7-M-requestContextSwitch.cpp to
    ARMv6-M-ARMv7-M-requestContextSwitch.cpp - this file is common for
    ARMv6-M and ARMv7-M.

--- source/architecture/ARM/ARMv6-M-ARMv7-M/ARMv6-M-ARMv7-M-requestContextSwitch.cpp
+++ source/architecture/ARM/ARMv6-M-ARMv7-M/ARMv6-M-ARMv7-M-requestContextSwitch.cpp
@@ -1,6 +1,6 @@
 /**
  * \file
- * \brief requestContextSwitch() implementation for ARMv7-M (Cortex-M3 / Cortex-M4)
+ * \brief requestContextSwitch() implementation for ARMv6-M and ARMv7-M
  *
  * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
