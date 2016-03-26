29281b3aaa03b53a60f3cfd5ceec691d5a647f26
Kamil Szczygiel <distortec@distortec.com>
2016-03-26 19:42:38 +0100

    Add buttons for NUCLEO-F429ZI board

---
+++ source/board/STM32/STM32F4/NUCLEO-F429ZI/include/distortos/board/buttons.hpp
@@ -0,0 +1,71 @@
+/**
+ * \file
+ * \brief Declaration of buttons for NUCLEO-F429ZI
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOURCE_BOARD_STM32_STM32F4_NUCLEO_F429ZI_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
+#define SOURCE_BOARD_STM32_STM32F4_NUCLEO_F429ZI_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
+
+#include "distortos/chip/STM32F4-GPIO.hpp"
+
+#include <array>
+
+namespace distortos
+{
+
+#ifdef CONFIG_BOARD_BUTTONS_ENABLE
+
+namespace chip
+{
+
+class ChipInputPin;
+
+}	// namespace chip
+
+#endif	// def CONFIG_BOARD_BUTTONS_ENABLE
+
+namespace board
+{
+
+/// total number of buttons on the board
+constexpr size_t totalButtons {CONFIG_BOARD_TOTAL_BUTTONS};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| button indexes
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// index of B1 button
+constexpr size_t b1ButtonIndex {0};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| indexed access to pin identifiers
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// array with pin identifiers of all buttons
+constexpr std::array<chip::Pin, totalButtons> buttonPins
+{
+		chip::Pin::pc13,
+};
+
+#ifdef CONFIG_BOARD_BUTTONS_ENABLE
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| indexed access to button objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// array with all button objects
+extern const chip::ChipInputPin buttons[totalButtons];
+
+#endif	// def CONFIG_BOARD_BUTTONS_ENABLE
+
+}	// namespace board
+
+}	// namespace distortos
+
+#endif	// SOURCE_BOARD_STM32_STM32F4_NUCLEO_F429ZI_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
