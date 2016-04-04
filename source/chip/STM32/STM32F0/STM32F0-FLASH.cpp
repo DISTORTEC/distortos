3b0bcfc1b9aafe4c5149d1540948dbed81a7924e
Kamil Szczygiel <distortec@distortec.com>
2016-04-04 23:07:15 +0200

    Add STM32F0-FLASH module

---
+++ source/chip/STM32/STM32F0/STM32F0-FLASH.cpp
@@ -0,0 +1,45 @@
+/**
+ * \file
+ * \brief Implementation of FLASH-related functions for STM32F0
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/STM32F0-FLASH.hpp"
+
+#include "distortos/chip/CMSIS-proxy.h"
+#include "distortos/chip/STM32F0-FLASH-bits.h"
+
+#include <cerrno>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+int configureFlashLatency(const uint8_t latency)
+{
+	if (latency > maxFlashLatency)
+		return EINVAL;
+
+	FLASH->ACR = (FLASH->ACR & ~FLASH_ACR_LATENCY) | (latency << FLASH_ACR_LATENCY_bit);
+	return 0;
+}
+
+void configurePrefetchBuffer(const bool enable)
+{
+	FLASH->ACR = (FLASH->ACR & ~FLASH_ACR_PRFTBE) | (enable << FLASH_ACR_PRFTBE_bit);
+}
+
+}	// namespace chip
+
+}	// namespace distortos
