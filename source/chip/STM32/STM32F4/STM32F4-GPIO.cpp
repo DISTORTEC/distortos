96803b8fd88b85de71be7949bdde9589bf37a994
Kamil Szczygiel <distortec@distortec.com>
2016-04-14 22:07:10 +0200

    Optimize configurePin() for STM32F4
    Make the critical section as short as possible by precomputing most of
    the values before masking the interrupts.

--- source/chip/STM32/STM32F4/STM32F4-GPIO.cpp
+++ source/chip/STM32/STM32F4/STM32F4-GPIO.cpp
@@ -22,91 +22,6 @@ namespace chip
 {
 
 /*---------------------------------------------------------------------------------------------------------------------+
-| local functions
-+---------------------------------------------------------------------------------------------------------------------*/
-
-/**
- * \brief Configures pin's alternate function.
- *
- * \param [in] port is a reference to GPIO port
- * \param [in] pinNumber is the pin number, [0; 15]
- * \param [in] alternateFunction is the desired alternate function of pin
- */
-
-void configureAlternateFunction(GPIO_TypeDef& port, const uint8_t pinNumber,
-		const PinAlternateFunction alternateFunction)
-{
-	auto afr = port.AFR[pinNumber / 8];
-	afr &= ~(GPIO_AFRx_mask << ((pinNumber % 8) * 4));
-	afr |= static_cast<uint32_t>(alternateFunction) << ((pinNumber % 8) * 4);
-	port.AFR[pinNumber / 8] = afr;
-}
-
-/**
- * \brief Configures pin's mode.
- *
- * \param [in] port is a reference to GPIO port
- * \param [in] pinNumber is the pin number, [0; 15]
- * \param [in] mode is the desired mode of pin
- */
-
-void configureMode(GPIO_TypeDef& port, const uint8_t pinNumber, const PinMode mode)
-{
-	auto moder = port.MODER;
-	moder &= ~(GPIO_MODER_mask << (pinNumber * 2));
-	moder |= static_cast<uint32_t>(mode) << (pinNumber * 2);
-	port.MODER = moder;
-}
-
-/**
- * \brief Configures pin's output speed.
- *
- * \param [in] port is a reference to GPIO port
- * \param [in] pinNumber is the pin number, [0; 15]
- * \param [in] outputSpeed is the desired output speed of pin
- */
-
-void configureOutputSpeed(GPIO_TypeDef& port, const uint8_t pinNumber, const PinOutputSpeed outputSpeed)
-{
-	auto ospeedr = port.OSPEEDR;
-	ospeedr &= ~(GPIO_OSPEEDR_mask << (pinNumber * 2));
-	ospeedr |= static_cast<uint32_t>(outputSpeed) << (pinNumber * 2);
-	port.OSPEEDR = ospeedr;
-}
-
-/**
- * \brief Configures pin's output type.
- *
- * \param [in] port is a reference to GPIO port
- * \param [in] pinNumber is the pin number, [0; 15]
- * \param [in] openDrain is the desired output type of pin: push-pull (false) or open-drain (true)
- */
-
-void configureOutputType(GPIO_TypeDef& port, const uint8_t pinNumber, const bool openDrain)
-{
-	auto otyper = port.OTYPER;
-	otyper &= ~(GPIO_OTYPER_mask << pinNumber);
-	otyper |= static_cast<uint32_t>(openDrain) << pinNumber;
-	port.OTYPER = otyper;
-}
-
-/**
- * \brief Configures pin's pull-up/pull-down configuration.
- *
- * \param [in] port is a reference to GPIO port
- * \param [in] pinNumber is the pin number, [0; 15]
- * \param [in] pull is the desired pull-up/pull-down configuration of pin
- */
-
-void configurePull(GPIO_TypeDef& port, const uint8_t pinNumber, const PinPull pull)
-{
-	auto pupdr = port.PUPDR;
-	pupdr &= ~(GPIO_PUPDR_mask << (pinNumber * 2));
-	pupdr |= static_cast<uint32_t>(pull) << (pinNumber * 2);
-	port.PUPDR = pupdr;
-}
-
-/*---------------------------------------------------------------------------------------------------------------------+
 | global functions
 +---------------------------------------------------------------------------------------------------------------------*/
 
@@ -116,16 +31,27 @@ void configurePin(const Pin pin, const PinMode mode, const bool openDrain, const
 	const auto decodedPin = decodePin(pin);
 	auto& port = *decodedPin.first;
 	const auto pinNumber = decodedPin.second;
+	auto& afr = port.AFR[pinNumber / 8];
+	const auto moderInvertedMask = ~(GPIO_MODER_mask << (pinNumber * 2));
+	const auto shiftedMode = static_cast<uint32_t>(mode) << (pinNumber * 2);
+	const auto otyperInvertedMask = ~(GPIO_OTYPER_mask << pinNumber);
+	const auto shiftedOpenDrain = static_cast<uint32_t>(openDrain) << pinNumber;
+	const auto ospeedrInvertedMask = ~(GPIO_OSPEEDR_mask << (pinNumber * 2));
+	const auto shiftedOutputSpeed = static_cast<uint32_t>(outputSpeed) << (pinNumber * 2);
+	const auto pupdrInvertedMask = ~(GPIO_PUPDR_mask << (pinNumber * 2));
+	const auto shiftedPull = static_cast<uint32_t>(pull) << (pinNumber * 2);
+	const auto afrInvertedMask = ~(GPIO_AFRx_mask << ((pinNumber * 4) % 32));
+	const auto shiftedAlternateFunction = static_cast<uint32_t>(alternateFunction) << ((pinNumber * 4) % 32);
 
 	port.BSRR = 1 << (pinNumber + (initialState == false ? 16 : 0));
 
 	architecture::InterruptMaskingLock interruptMaskingLock;
 
-	configureMode(port, pinNumber, mode);
-	configureOutputType(port, pinNumber, openDrain);
-	configureOutputSpeed(port, pinNumber, outputSpeed);
-	configurePull(port, pinNumber, pull);
-	configureAlternateFunction(port, pinNumber, alternateFunction);
+	port.MODER = (port.MODER & moderInvertedMask) | shiftedMode;
+	port.OTYPER = (port.OTYPER & otyperInvertedMask) | shiftedOpenDrain;
+	port.OSPEEDR = (port.OSPEEDR & ospeedrInvertedMask) | shiftedOutputSpeed;
+	port.PUPDR = (port.PUPDR & pupdrInvertedMask) | shiftedPull;
+	afr = (afr & afrInvertedMask) | shiftedAlternateFunction;
 }
 
 }	// namespace chip
