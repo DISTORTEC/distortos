9cf8bddf8a2de61021d29c03055a321e9c6614ca
Kamil Szczygiel <distortec@distortec.com>
2016-04-27 22:14:11 +0200

    Add forAllConfigurations.sh
    This script is a generalized version of buildAllConfigurations.sh and
    updateAllConfigurations.sh. The first - mandatory - argument is the
    command that will be executed for each configuration. The second -
    optional - argument is the search path for "distortosConfiguration.mk"
    files, default - ".".

---
+++ scripts/forAllConfigurations.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#
+# file: forAllConfigurations.sh
+#
+# author: Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+# distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+set -e
+set -u
+
+if [ $# -lt 1 ]; then
+	echo "Error - invalid number of arguments!"
+	exit 1
+fi
+
+command=$1
+
+searchPath=.
+
+# If any additional argument was given, then use it as the search path, otherwise search in current directory.
+if [ $# -ge 2 ]; then
+	searchPath=$2
+fi
+
+make distclean
+
+for configurationPath in `find $searchPath -name "distortosConfiguration.mk" -printf "%h "`
+do
+
+	make configure CONFIG_PATH=$configurationPath
+	$command
+	make distclean
+
+done
