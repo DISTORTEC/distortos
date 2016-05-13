49f3c67b743d56b7bd7ff9e6c8cd92bf4e3c9c63
Kamil Szczygiel <distortec@distortec.com>
2016-05-13 13:13:28 +0200

    Add UartLowLevel class

---
+++ source/devices/communication/UartLowLevel.cpp
@@ -0,0 +1,31 @@
+/**
+ * \file
+ * \brief UartLowLevel class implementation
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/internal/devices/UartLowLevel.hpp"
+
+namespace distortos
+{
+
+namespace internal
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| public functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+UartLowLevel::~UartLowLevel()
+{
+
+}
+
+}	// namespace internal
+
+}	// namespace distortos
