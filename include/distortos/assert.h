0b5649709ea93006f2e7b811077eb88ec90dd52a
Kamil Szczygiel <distortec@distortec.com>
2016-06-10 18:12:21 +0200

    Add assert.h with replacement of newlib's assert() macro

---
+++ include/distortos/assert.h
@@ -0,0 +1,30 @@
+/**
+ * \file
+ * \brief assert() macro
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDE_DISTORTOS_ASSERT_H_
+#define INCLUDE_DISTORTOS_ASSERT_H_
+
+#include <assert.h>
+
+#ifdef NDEBUG
+
+#undef assert
+
+/**
+ * \brief Replacement of newlib's assert() macro for "asserts disabled" configuration which does not produce "unused
+ * variable" warning
+ */
+
+#define assert(expression)		(void)sizeof(expression)
+
+#endif	/* def NDEBUG */
+
+#endif /* INCLUDE_DISTORTOS_ASSERT_H_ */
