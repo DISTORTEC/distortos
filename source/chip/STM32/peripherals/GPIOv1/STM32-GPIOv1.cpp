8854b2eb096bb61e0bc66d5e096d251a6c0ad7c5
Kamil Szczygiel <distortec@distortec.com>
2016-09-16 00:00:32 +0200

    Simplify code of configurePin() for GPIOv1 in STM32

--- source/chip/STM32/peripherals/GPIOv1/STM32-GPIOv1.cpp
+++ source/chip/STM32/peripherals/GPIOv1/STM32-GPIOv1.cpp
@@ -32,7 +32,7 @@ void configurePin(const Pin pin, const PinConfiguration configuration, const boo
 	const auto invertedMask = ~((GPIO_CRL_CNF0 | GPIO_CRL_MODE0) << ((pinNumber * 4) % 32));
 	const auto configurationValue = static_cast<uint32_t>(configuration) << ((pinNumber * 4) % 32);
 
-	(initialState == false ? port.BRR : port.BSRR) = 1 << pinNumber;
+	port.BSRR = 1 << (pinNumber + (initialState == false ? 16 : 0));
 
 	architecture::InterruptMaskingLock interruptMaskingLock;
 
