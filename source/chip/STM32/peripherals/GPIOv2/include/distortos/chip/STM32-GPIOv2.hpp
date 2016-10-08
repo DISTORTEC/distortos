4523bc3b611a507c9df5683a055782b9f3452236
Kamil Szczygiel <distortec@distortec.com>
2016-10-08 15:57:57 +0200

    Use GPIO._BASE instead of GPIO. in STM32-GPIOv2.hpp
    GCC 6.2.0 doesn't allow reinterpret_cast<>() within constant
    expressions.

--- source/chip/STM32/peripherals/GPIOv2/include/distortos/chip/STM32-GPIOv2.hpp
+++ source/chip/STM32/peripherals/GPIOv2/include/distortos/chip/STM32-GPIOv2.hpp
@@ -27,47 +27,47 @@ namespace chip
 +---------------------------------------------------------------------------------------------------------------------*/
 
 #ifdef GPIOA
-static_assert((reinterpret_cast<uint32_t>(GPIOA) & 15) == 0, "Cannot combine pin number with GPIOA address!");
+static_assert((GPIOA_BASE & 15) == 0, "Cannot combine pin number with GPIOA address!");
 #endif	// def GPIOA
 
 #ifdef GPIOB
-static_assert((reinterpret_cast<uint32_t>(GPIOB) & 15) == 0, "Cannot combine pin number with GPIOB address!");
+static_assert((GPIOB_BASE & 15) == 0, "Cannot combine pin number with GPIOB address!");
 #endif	// def GPIOB
 
 #ifdef GPIOC
-static_assert((reinterpret_cast<uint32_t>(GPIOC) & 15) == 0, "Cannot combine pin number with GPIOC address!");
+static_assert((GPIOC_BASE & 15) == 0, "Cannot combine pin number with GPIOC address!");
 #endif	// def GPIOC
 
 #ifdef GPIOD
-static_assert((reinterpret_cast<uint32_t>(GPIOD) & 15) == 0, "Cannot combine pin number with GPIOD address!");
+static_assert((GPIOD_BASE & 15) == 0, "Cannot combine pin number with GPIOD address!");
 #endif	// def GPIOD
 
 #ifdef GPIOE
-static_assert((reinterpret_cast<uint32_t>(GPIOE) & 15) == 0, "Cannot combine pin number with GPIOE address!");
+static_assert((GPIOE_BASE & 15) == 0, "Cannot combine pin number with GPIOE address!");
 #endif	// def GPIOE
 
 #ifdef GPIOF
-static_assert((reinterpret_cast<uint32_t>(GPIOF) & 15) == 0, "Cannot combine pin number with GPIOF address!");
+static_assert((GPIOF_BASE & 15) == 0, "Cannot combine pin number with GPIOF address!");
 #endif	// def GPIOF
 
 #ifdef GPIOG
-static_assert((reinterpret_cast<uint32_t>(GPIOG) & 15) == 0, "Cannot combine pin number with GPIOG address!");
+static_assert((GPIOG_BASE & 15) == 0, "Cannot combine pin number with GPIOG address!");
 #endif	// def GPIOG
 
 #ifdef GPIOH
-static_assert((reinterpret_cast<uint32_t>(GPIOH) & 15) == 0, "Cannot combine pin number with GPIOH address!");
+static_assert((GPIOH_BASE & 15) == 0, "Cannot combine pin number with GPIOH address!");
 #endif	// def GPIOH
 
 #ifdef GPIOI
-static_assert((reinterpret_cast<uint32_t>(GPIOI) & 15) == 0, "Cannot combine pin number with GPIOI address!");
+static_assert((GPIOI_BASE & 15) == 0, "Cannot combine pin number with GPIOI address!");
 #endif	// def GPIOI
 
 #ifdef GPIOJ
-static_assert((reinterpret_cast<uint32_t>(GPIOJ) & 15) == 0, "Cannot combine pin number with GPIOJ address!");
+static_assert((GPIOJ_BASE & 15) == 0, "Cannot combine pin number with GPIOJ address!");
 #endif	// def GPIOJ
 
 #ifdef GPIOK
-static_assert((reinterpret_cast<uint32_t>(GPIOK) & 15) == 0, "Cannot combine pin number with GPIOK address!");
+static_assert((GPIOK_BASE & 15) == 0, "Cannot combine pin number with GPIOK address!");
 #endif	// def GPIOK
 
 /// identifier of single pin - combined address of GPIO port with pin number
@@ -76,407 +76,407 @@ enum class Pin : uint32_t
 #ifdef GPIOA
 
 		/// PA0 pin
-		pa0 = reinterpret_cast<uint32_t>(GPIOA) | 0,
+		pa0 = GPIOA_BASE | 0,
 		/// PA1 pin
-		pa1 = reinterpret_cast<uint32_t>(GPIOA) | 1,
+		pa1 = GPIOA_BASE | 1,
 		/// PA2 pin
-		pa2 = reinterpret_cast<uint32_t>(GPIOA) | 2,
+		pa2 = GPIOA_BASE | 2,
 		/// PA3 pin
-		pa3 = reinterpret_cast<uint32_t>(GPIOA) | 3,
+		pa3 = GPIOA_BASE | 3,
 		/// PA4 pin
-		pa4 = reinterpret_cast<uint32_t>(GPIOA) | 4,
+		pa4 = GPIOA_BASE | 4,
 		/// PA5 pin
-		pa5 = reinterpret_cast<uint32_t>(GPIOA) | 5,
+		pa5 = GPIOA_BASE | 5,
 		/// PA6 pin
-		pa6 = reinterpret_cast<uint32_t>(GPIOA) | 6,
+		pa6 = GPIOA_BASE | 6,
 		/// PA7 pin
-		pa7 = reinterpret_cast<uint32_t>(GPIOA) | 7,
+		pa7 = GPIOA_BASE | 7,
 		/// PA8 pin
-		pa8 = reinterpret_cast<uint32_t>(GPIOA) | 8,
+		pa8 = GPIOA_BASE | 8,
 		/// PA9 pin
-		pa9 = reinterpret_cast<uint32_t>(GPIOA) | 9,
+		pa9 = GPIOA_BASE | 9,
 		/// PA10 pin
-		pa10 = reinterpret_cast<uint32_t>(GPIOA) | 10,
+		pa10 = GPIOA_BASE | 10,
 		/// PA11 pin
-		pa11 = reinterpret_cast<uint32_t>(GPIOA) | 11,
+		pa11 = GPIOA_BASE | 11,
 		/// PA12 pin
-		pa12 = reinterpret_cast<uint32_t>(GPIOA) | 12,
+		pa12 = GPIOA_BASE | 12,
 		/// PA13 pin
-		pa13 = reinterpret_cast<uint32_t>(GPIOA) | 13,
+		pa13 = GPIOA_BASE | 13,
 		/// PA14 pin
-		pa14 = reinterpret_cast<uint32_t>(GPIOA) | 14,
+		pa14 = GPIOA_BASE | 14,
 		/// PA15 pin
-		pa15 = reinterpret_cast<uint32_t>(GPIOA) | 15,
+		pa15 = GPIOA_BASE | 15,
 
 #endif	// def GPIOA
 
 #ifdef GPIOB
 
 		/// PB0 pin
-		pb0 = reinterpret_cast<uint32_t>(GPIOB) | 0,
+		pb0 = GPIOB_BASE | 0,
 		/// PB1 pin
-		pb1 = reinterpret_cast<uint32_t>(GPIOB) | 1,
+		pb1 = GPIOB_BASE | 1,
 		/// PB2 pin
-		pb2 = reinterpret_cast<uint32_t>(GPIOB) | 2,
+		pb2 = GPIOB_BASE | 2,
 		/// PB3 pin
-		pb3 = reinterpret_cast<uint32_t>(GPIOB) | 3,
+		pb3 = GPIOB_BASE | 3,
 		/// PB4 pin
-		pb4 = reinterpret_cast<uint32_t>(GPIOB) | 4,
+		pb4 = GPIOB_BASE | 4,
 		/// PB5 pin
-		pb5 = reinterpret_cast<uint32_t>(GPIOB) | 5,
+		pb5 = GPIOB_BASE | 5,
 		/// PB6 pin
-		pb6 = reinterpret_cast<uint32_t>(GPIOB) | 6,
+		pb6 = GPIOB_BASE | 6,
 		/// PB7 pin
-		pb7 = reinterpret_cast<uint32_t>(GPIOB) | 7,
+		pb7 = GPIOB_BASE | 7,
 		/// PB8 pin
-		pb8 = reinterpret_cast<uint32_t>(GPIOB) | 8,
+		pb8 = GPIOB_BASE | 8,
 		/// PB9 pin
-		pb9 = reinterpret_cast<uint32_t>(GPIOB) | 9,
+		pb9 = GPIOB_BASE | 9,
 		/// PB10 pin
-		pb10 = reinterpret_cast<uint32_t>(GPIOB) | 10,
+		pb10 = GPIOB_BASE | 10,
 		/// PB11 pin
-		pb11 = reinterpret_cast<uint32_t>(GPIOB) | 11,
+		pb11 = GPIOB_BASE | 11,
 		/// PB12 pin
-		pb12 = reinterpret_cast<uint32_t>(GPIOB) | 12,
+		pb12 = GPIOB_BASE | 12,
 		/// PB13 pin
-		pb13 = reinterpret_cast<uint32_t>(GPIOB) | 13,
+		pb13 = GPIOB_BASE | 13,
 		/// PB14 pin
-		pb14 = reinterpret_cast<uint32_t>(GPIOB) | 14,
+		pb14 = GPIOB_BASE | 14,
 		/// PB15 pin
-		pb15 = reinterpret_cast<uint32_t>(GPIOB) | 15,
+		pb15 = GPIOB_BASE | 15,
 
 #endif	// def GPIOB
 
 #ifdef GPIOC
 
 		/// PC0 pin
-		pc0 = reinterpret_cast<uint32_t>(GPIOC) | 0,
+		pc0 = GPIOC_BASE | 0,
 		/// PC1 pin
-		pc1 = reinterpret_cast<uint32_t>(GPIOC) | 1,
+		pc1 = GPIOC_BASE | 1,
 		/// PC2 pin
-		pc2 = reinterpret_cast<uint32_t>(GPIOC) | 2,
+		pc2 = GPIOC_BASE | 2,
 		/// PC3 pin
-		pc3 = reinterpret_cast<uint32_t>(GPIOC) | 3,
+		pc3 = GPIOC_BASE | 3,
 		/// PC4 pin
-		pc4 = reinterpret_cast<uint32_t>(GPIOC) | 4,
+		pc4 = GPIOC_BASE | 4,
 		/// PC5 pin
-		pc5 = reinterpret_cast<uint32_t>(GPIOC) | 5,
+		pc5 = GPIOC_BASE | 5,
 		/// PC6 pin
-		pc6 = reinterpret_cast<uint32_t>(GPIOC) | 6,
+		pc6 = GPIOC_BASE | 6,
 		/// PC7 pin
-		pc7 = reinterpret_cast<uint32_t>(GPIOC) | 7,
+		pc7 = GPIOC_BASE | 7,
 		/// PC8 pin
-		pc8 = reinterpret_cast<uint32_t>(GPIOC) | 8,
+		pc8 = GPIOC_BASE | 8,
 		/// PC9 pin
-		pc9 = reinterpret_cast<uint32_t>(GPIOC) | 9,
+		pc9 = GPIOC_BASE | 9,
 		/// PC10 pin
-		pc10 = reinterpret_cast<uint32_t>(GPIOC) | 10,
+		pc10 = GPIOC_BASE | 10,
 		/// PC11 pin
-		pc11 = reinterpret_cast<uint32_t>(GPIOC) | 11,
+		pc11 = GPIOC_BASE | 11,
 		/// PC12 pin
-		pc12 = reinterpret_cast<uint32_t>(GPIOC) | 12,
+		pc12 = GPIOC_BASE | 12,
 		/// PC13 pin
-		pc13 = reinterpret_cast<uint32_t>(GPIOC) | 13,
+		pc13 = GPIOC_BASE | 13,
 		/// PC14 pin
-		pc14 = reinterpret_cast<uint32_t>(GPIOC) | 14,
+		pc14 = GPIOC_BASE | 14,
 		/// PC15 pin
-		pc15 = reinterpret_cast<uint32_t>(GPIOC) | 15,
+		pc15 = GPIOC_BASE | 15,
 
 #endif	// def GPIOC
 
 #ifdef GPIOD
 
 		/// PD0 pin
-		pd0 = reinterpret_cast<uint32_t>(GPIOD) | 0,
+		pd0 = GPIOD_BASE | 0,
 		/// PD1 pin
-		pd1 = reinterpret_cast<uint32_t>(GPIOD) | 1,
+		pd1 = GPIOD_BASE | 1,
 		/// PD2 pin
-		pd2 = reinterpret_cast<uint32_t>(GPIOD) | 2,
+		pd2 = GPIOD_BASE | 2,
 		/// PD3 pin
-		pd3 = reinterpret_cast<uint32_t>(GPIOD) | 3,
+		pd3 = GPIOD_BASE | 3,
 		/// PD4 pin
-		pd4 = reinterpret_cast<uint32_t>(GPIOD) | 4,
+		pd4 = GPIOD_BASE | 4,
 		/// PD5 pin
-		pd5 = reinterpret_cast<uint32_t>(GPIOD) | 5,
+		pd5 = GPIOD_BASE | 5,
 		/// PD6 pin
-		pd6 = reinterpret_cast<uint32_t>(GPIOD) | 6,
+		pd6 = GPIOD_BASE | 6,
 		/// PD7 pin
-		pd7 = reinterpret_cast<uint32_t>(GPIOD) | 7,
+		pd7 = GPIOD_BASE | 7,
 		/// PD8 pin
-		pd8 = reinterpret_cast<uint32_t>(GPIOD) | 8,
+		pd8 = GPIOD_BASE | 8,
 		/// PD9 pin
-		pd9 = reinterpret_cast<uint32_t>(GPIOD) | 9,
+		pd9 = GPIOD_BASE | 9,
 		/// PD10 pin
-		pd10 = reinterpret_cast<uint32_t>(GPIOD) | 10,
+		pd10 = GPIOD_BASE | 10,
 		/// PD11 pin
-		pd11 = reinterpret_cast<uint32_t>(GPIOD) | 11,
+		pd11 = GPIOD_BASE | 11,
 		/// PD12 pin
-		pd12 = reinterpret_cast<uint32_t>(GPIOD) | 12,
+		pd12 = GPIOD_BASE | 12,
 		/// PD13 pin
-		pd13 = reinterpret_cast<uint32_t>(GPIOD) | 13,
+		pd13 = GPIOD_BASE | 13,
 		/// PD14 pin
-		pd14 = reinterpret_cast<uint32_t>(GPIOD) | 14,
+		pd14 = GPIOD_BASE | 14,
 		/// PD15 pin
-		pd15 = reinterpret_cast<uint32_t>(GPIOD) | 15,
+		pd15 = GPIOD_BASE | 15,
 
 #endif	// def GPIOD
 
 #ifdef GPIOE
 
 		/// PE0 pin
-		pe0 = reinterpret_cast<uint32_t>(GPIOE) | 0,
+		pe0 = GPIOE_BASE | 0,
 		/// PE1 pin
-		pe1 = reinterpret_cast<uint32_t>(GPIOE) | 1,
+		pe1 = GPIOE_BASE | 1,
 		/// PE2 pin
-		pe2 = reinterpret_cast<uint32_t>(GPIOE) | 2,
+		pe2 = GPIOE_BASE | 2,
 		/// PE3 pin
-		pe3 = reinterpret_cast<uint32_t>(GPIOE) | 3,
+		pe3 = GPIOE_BASE | 3,
 		/// PE4 pin
-		pe4 = reinterpret_cast<uint32_t>(GPIOE) | 4,
+		pe4 = GPIOE_BASE | 4,
 		/// PE5 pin
-		pe5 = reinterpret_cast<uint32_t>(GPIOE) | 5,
+		pe5 = GPIOE_BASE | 5,
 		/// PE6 pin
-		pe6 = reinterpret_cast<uint32_t>(GPIOE) | 6,
+		pe6 = GPIOE_BASE | 6,
 		/// PE7 pin
-		pe7 = reinterpret_cast<uint32_t>(GPIOE) | 7,
+		pe7 = GPIOE_BASE | 7,
 		/// PE8 pin
-		pe8 = reinterpret_cast<uint32_t>(GPIOE) | 8,
+		pe8 = GPIOE_BASE | 8,
 		/// PE9 pin
-		pe9 = reinterpret_cast<uint32_t>(GPIOE) | 9,
+		pe9 = GPIOE_BASE | 9,
 		/// PE10 pin
-		pe10 = reinterpret_cast<uint32_t>(GPIOE) | 10,
+		pe10 = GPIOE_BASE | 10,
 		/// PE11 pin
-		pe11 = reinterpret_cast<uint32_t>(GPIOE) | 11,
+		pe11 = GPIOE_BASE | 11,
 		/// PE12 pin
-		pe12 = reinterpret_cast<uint32_t>(GPIOE) | 12,
+		pe12 = GPIOE_BASE | 12,
 		/// PE13 pin
-		pe13 = reinterpret_cast<uint32_t>(GPIOE) | 13,
+		pe13 = GPIOE_BASE | 13,
 		/// PE14 pin
-		pe14 = reinterpret_cast<uint32_t>(GPIOE) | 14,
+		pe14 = GPIOE_BASE | 14,
 		/// PE15 pin
-		pe15 = reinterpret_cast<uint32_t>(GPIOE) | 15,
+		pe15 = GPIOE_BASE | 15,
 
 #endif	// def GPIOE
 
 #ifdef GPIOF
 
 		/// PF0 pin
-		pf0 = reinterpret_cast<uint32_t>(GPIOF) | 0,
+		pf0 = GPIOF_BASE | 0,
 		/// PF1 pin
-		pf1 = reinterpret_cast<uint32_t>(GPIOF) | 1,
+		pf1 = GPIOF_BASE | 1,
 		/// PF2 pin
-		pf2 = reinterpret_cast<uint32_t>(GPIOF) | 2,
+		pf2 = GPIOF_BASE | 2,
 		/// PF3 pin
-		pf3 = reinterpret_cast<uint32_t>(GPIOF) | 3,
+		pf3 = GPIOF_BASE | 3,
 		/// PF4 pin
-		pf4 = reinterpret_cast<uint32_t>(GPIOF) | 4,
+		pf4 = GPIOF_BASE | 4,
 		/// PF5 pin
-		pf5 = reinterpret_cast<uint32_t>(GPIOF) | 5,
+		pf5 = GPIOF_BASE | 5,
 		/// PF6 pin
-		pf6 = reinterpret_cast<uint32_t>(GPIOF) | 6,
+		pf6 = GPIOF_BASE | 6,
 		/// PF7 pin
-		pf7 = reinterpret_cast<uint32_t>(GPIOF) | 7,
+		pf7 = GPIOF_BASE | 7,
 		/// PF8 pin
-		pf8 = reinterpret_cast<uint32_t>(GPIOF) | 8,
+		pf8 = GPIOF_BASE | 8,
 		/// PF9 pin
-		pf9 = reinterpret_cast<uint32_t>(GPIOF) | 9,
+		pf9 = GPIOF_BASE | 9,
 		/// PF10 pin
-		pf10 = reinterpret_cast<uint32_t>(GPIOF) | 10,
+		pf10 = GPIOF_BASE | 10,
 		/// PF11 pin
-		pf11 = reinterpret_cast<uint32_t>(GPIOF) | 11,
+		pf11 = GPIOF_BASE | 11,
 		/// PF12 pin
-		pf12 = reinterpret_cast<uint32_t>(GPIOF) | 12,
+		pf12 = GPIOF_BASE | 12,
 		/// PF13 pin
-		pf13 = reinterpret_cast<uint32_t>(GPIOF) | 13,
+		pf13 = GPIOF_BASE | 13,
 		/// PF14 pin
-		pf14 = reinterpret_cast<uint32_t>(GPIOF) | 14,
+		pf14 = GPIOF_BASE | 14,
 		/// PF15 pin
-		pf15 = reinterpret_cast<uint32_t>(GPIOF) | 15,
+		pf15 = GPIOF_BASE | 15,
 
 #endif	// def GPIOF
 
 #ifdef GPIOG
 
 		/// PG0 pin
-		pg0 = reinterpret_cast<uint32_t>(GPIOG) | 0,
+		pg0 = GPIOG_BASE | 0,
 		/// PG1 pin
-		pg1 = reinterpret_cast<uint32_t>(GPIOG) | 1,
+		pg1 = GPIOG_BASE | 1,
 		/// PG2 pin
-		pg2 = reinterpret_cast<uint32_t>(GPIOG) | 2,
+		pg2 = GPIOG_BASE | 2,
 		/// PG3 pin
-		pg3 = reinterpret_cast<uint32_t>(GPIOG) | 3,
+		pg3 = GPIOG_BASE | 3,
 		/// PG4 pin
-		pg4 = reinterpret_cast<uint32_t>(GPIOG) | 4,
+		pg4 = GPIOG_BASE | 4,
 		/// PG5 pin
-		pg5 = reinterpret_cast<uint32_t>(GPIOG) | 5,
+		pg5 = GPIOG_BASE | 5,
 		/// PG6 pin
-		pg6 = reinterpret_cast<uint32_t>(GPIOG) | 6,
+		pg6 = GPIOG_BASE | 6,
 		/// PG7 pin
-		pg7 = reinterpret_cast<uint32_t>(GPIOG) | 7,
+		pg7 = GPIOG_BASE | 7,
 		/// PG8 pin
-		pg8 = reinterpret_cast<uint32_t>(GPIOG) | 8,
+		pg8 = GPIOG_BASE | 8,
 		/// PG9 pin
-		pg9 = reinterpret_cast<uint32_t>(GPIOG) | 9,
+		pg9 = GPIOG_BASE | 9,
 		/// PG10 pin
-		pg10 = reinterpret_cast<uint32_t>(GPIOG) | 10,
+		pg10 = GPIOG_BASE | 10,
 		/// PG11 pin
-		pg11 = reinterpret_cast<uint32_t>(GPIOG) | 11,
+		pg11 = GPIOG_BASE | 11,
 		/// PG12 pin
-		pg12 = reinterpret_cast<uint32_t>(GPIOG) | 12,
+		pg12 = GPIOG_BASE | 12,
 		/// PG13 pin
-		pg13 = reinterpret_cast<uint32_t>(GPIOG) | 13,
+		pg13 = GPIOG_BASE | 13,
 		/// PG14 pin
-		pg14 = reinterpret_cast<uint32_t>(GPIOG) | 14,
+		pg14 = GPIOG_BASE | 14,
 		/// PG15 pin
-		pg15 = reinterpret_cast<uint32_t>(GPIOG) | 15,
+		pg15 = GPIOG_BASE | 15,
 
 #endif	// def GPIOG
 
 #ifdef GPIOH
 
 		/// PH0 pin
-		ph0 = reinterpret_cast<uint32_t>(GPIOH) | 0,
+		ph0 = GPIOH_BASE | 0,
 		/// PH1 pin
-		ph1 = reinterpret_cast<uint32_t>(GPIOH) | 1,
+		ph1 = GPIOH_BASE | 1,
 		/// PH2 pin
-		ph2 = reinterpret_cast<uint32_t>(GPIOH) | 2,
+		ph2 = GPIOH_BASE | 2,
 		/// PH3 pin
-		ph3 = reinterpret_cast<uint32_t>(GPIOH) | 3,
+		ph3 = GPIOH_BASE | 3,
 		/// PH4 pin
-		ph4 = reinterpret_cast<uint32_t>(GPIOH) | 4,
+		ph4 = GPIOH_BASE | 4,
 		/// PH5 pin
-		ph5 = reinterpret_cast<uint32_t>(GPIOH) | 5,
+		ph5 = GPIOH_BASE | 5,
 		/// PH6 pin
-		ph6 = reinterpret_cast<uint32_t>(GPIOH) | 6,
+		ph6 = GPIOH_BASE | 6,
 		/// PH7 pin
-		ph7 = reinterpret_cast<uint32_t>(GPIOH) | 7,
+		ph7 = GPIOH_BASE | 7,
 		/// PH8 pin
-		ph8 = reinterpret_cast<uint32_t>(GPIOH) | 8,
+		ph8 = GPIOH_BASE | 8,
 		/// PH9 pin
-		ph9 = reinterpret_cast<uint32_t>(GPIOH) | 9,
+		ph9 = GPIOH_BASE | 9,
 		/// PH10 pin
-		ph10 = reinterpret_cast<uint32_t>(GPIOH) | 10,
+		ph10 = GPIOH_BASE | 10,
 		/// PH11 pin
-		ph11 = reinterpret_cast<uint32_t>(GPIOH) | 11,
+		ph11 = GPIOH_BASE | 11,
 		/// PH12 pin
-		ph12 = reinterpret_cast<uint32_t>(GPIOH) | 12,
+		ph12 = GPIOH_BASE | 12,
 		/// PH13 pin
-		ph13 = reinterpret_cast<uint32_t>(GPIOH) | 13,
+		ph13 = GPIOH_BASE | 13,
 		/// PH14 pin
-		ph14 = reinterpret_cast<uint32_t>(GPIOH) | 14,
+		ph14 = GPIOH_BASE | 14,
 		/// PH15 pin
-		ph15 = reinterpret_cast<uint32_t>(GPIOH) | 15,
+		ph15 = GPIOH_BASE | 15,
 
 #endif	// def GPIOH
 
 #ifdef GPIOI
 
 		/// PI0 pin
-		pi0 = reinterpret_cast<uint32_t>(GPIOI) | 0,
+		pi0 = GPIOI_BASE | 0,
 		/// PI1 pin
-		pi1 = reinterpret_cast<uint32_t>(GPIOI) | 1,
+		pi1 = GPIOI_BASE | 1,
 		/// PI2 pin
-		pi2 = reinterpret_cast<uint32_t>(GPIOI) | 2,
+		pi2 = GPIOI_BASE | 2,
 		/// PI3 pin
-		pi3 = reinterpret_cast<uint32_t>(GPIOI) | 3,
+		pi3 = GPIOI_BASE | 3,
 		/// PI4 pin
-		pi4 = reinterpret_cast<uint32_t>(GPIOI) | 4,
+		pi4 = GPIOI_BASE | 4,
 		/// PI5 pin
-		pi5 = reinterpret_cast<uint32_t>(GPIOI) | 5,
+		pi5 = GPIOI_BASE | 5,
 		/// PI6 pin
-		pi6 = reinterpret_cast<uint32_t>(GPIOI) | 6,
+		pi6 = GPIOI_BASE | 6,
 		/// PI7 pin
-		pi7 = reinterpret_cast<uint32_t>(GPIOI) | 7,
+		pi7 = GPIOI_BASE | 7,
 		/// PI8 pin
-		pi8 = reinterpret_cast<uint32_t>(GPIOI) | 8,
+		pi8 = GPIOI_BASE | 8,
 		/// PI9 pin
-		pi9 = reinterpret_cast<uint32_t>(GPIOI) | 9,
+		pi9 = GPIOI_BASE | 9,
 		/// PI10 pin
-		pi10 = reinterpret_cast<uint32_t>(GPIOI) | 10,
+		pi10 = GPIOI_BASE | 10,
 		/// PI11 pin
-		pi11 = reinterpret_cast<uint32_t>(GPIOI) | 11,
+		pi11 = GPIOI_BASE | 11,
 		/// PI12 pin
-		pi12 = reinterpret_cast<uint32_t>(GPIOI) | 12,
+		pi12 = GPIOI_BASE | 12,
 		/// PI13 pin
-		pi13 = reinterpret_cast<uint32_t>(GPIOI) | 13,
+		pi13 = GPIOI_BASE | 13,
 		/// PI14 pin
-		pi14 = reinterpret_cast<uint32_t>(GPIOI) | 14,
+		pi14 = GPIOI_BASE | 14,
 		/// PI15 pin
-		pi15 = reinterpret_cast<uint32_t>(GPIOI) | 15,
+		pi15 = GPIOI_BASE | 15,
 
 #endif	// def GPIOI
 
 #ifdef GPIOJ
 
 		/// PJ0 pin
-		pj0 = reinterpret_cast<uint32_t>(GPIOJ) | 0,
+		pj0 = GPIOJ_BASE | 0,
 		/// PJ1 pin
-		pj1 = reinterpret_cast<uint32_t>(GPIOJ) | 1,
+		pj1 = GPIOJ_BASE | 1,
 		/// PJ2 pin
-		pj2 = reinterpret_cast<uint32_t>(GPIOJ) | 2,
+		pj2 = GPIOJ_BASE | 2,
 		/// PJ3 pin
-		pj3 = reinterpret_cast<uint32_t>(GPIOJ) | 3,
+		pj3 = GPIOJ_BASE | 3,
 		/// PJ4 pin
-		pj4 = reinterpret_cast<uint32_t>(GPIOJ) | 4,
+		pj4 = GPIOJ_BASE | 4,
 		/// PJ5 pin
-		pj5 = reinterpret_cast<uint32_t>(GPIOJ) | 5,
+		pj5 = GPIOJ_BASE | 5,
 		/// PJ6 pin
-		pj6 = reinterpret_cast<uint32_t>(GPIOJ) | 6,
+		pj6 = GPIOJ_BASE | 6,
 		/// PJ7 pin
-		pj7 = reinterpret_cast<uint32_t>(GPIOJ) | 7,
+		pj7 = GPIOJ_BASE | 7,
 		/// PJ8 pin
-		pj8 = reinterpret_cast<uint32_t>(GPIOJ) | 8,
+		pj8 = GPIOJ_BASE | 8,
 		/// PJ9 pin
-		pj9 = reinterpret_cast<uint32_t>(GPIOJ) | 9,
+		pj9 = GPIOJ_BASE | 9,
 		/// PJ10 pin
-		pj10 = reinterpret_cast<uint32_t>(GPIOJ) | 10,
+		pj10 = GPIOJ_BASE | 10,
 		/// PJ11 pin
-		pj11 = reinterpret_cast<uint32_t>(GPIOJ) | 11,
+		pj11 = GPIOJ_BASE | 11,
 		/// PJ12 pin
-		pj12 = reinterpret_cast<uint32_t>(GPIOJ) | 12,
+		pj12 = GPIOJ_BASE | 12,
 		/// PJ13 pin
-		pj13 = reinterpret_cast<uint32_t>(GPIOJ) | 13,
+		pj13 = GPIOJ_BASE | 13,
 		/// PJ14 pin
-		pj14 = reinterpret_cast<uint32_t>(GPIOJ) | 14,
+		pj14 = GPIOJ_BASE | 14,
 		/// PJ15 pin
-		pj15 = reinterpret_cast<uint32_t>(GPIOJ) | 15,
+		pj15 = GPIOJ_BASE | 15,
 
 #endif	// def GPIOJ
 
 #ifdef GPIOK
 
 		/// PK0 pin
-		pk0 = reinterpret_cast<uint32_t>(GPIOK) | 0,
+		pk0 = GPIOK_BASE | 0,
 		/// PK1 pin
-		pk1 = reinterpret_cast<uint32_t>(GPIOK) | 1,
+		pk1 = GPIOK_BASE | 1,
 		/// PK2 pin
-		pk2 = reinterpret_cast<uint32_t>(GPIOK) | 2,
+		pk2 = GPIOK_BASE | 2,
 		/// PK3 pin
-		pk3 = reinterpret_cast<uint32_t>(GPIOK) | 3,
+		pk3 = GPIOK_BASE | 3,
 		/// PK4 pin
-		pk4 = reinterpret_cast<uint32_t>(GPIOK) | 4,
+		pk4 = GPIOK_BASE | 4,
 		/// PK5 pin
-		pk5 = reinterpret_cast<uint32_t>(GPIOK) | 5,
+		pk5 = GPIOK_BASE | 5,
 		/// PK6 pin
-		pk6 = reinterpret_cast<uint32_t>(GPIOK) | 6,
+		pk6 = GPIOK_BASE | 6,
 		/// PK7 pin
-		pk7 = reinterpret_cast<uint32_t>(GPIOK) | 7,
+		pk7 = GPIOK_BASE | 7,
 		/// PK8 pin
-		pk8 = reinterpret_cast<uint32_t>(GPIOK) | 8,
+		pk8 = GPIOK_BASE | 8,
 		/// PK9 pin
-		pk9 = reinterpret_cast<uint32_t>(GPIOK) | 9,
+		pk9 = GPIOK_BASE | 9,
 		/// PK10 pin
-		pk10 = reinterpret_cast<uint32_t>(GPIOK) | 10,
+		pk10 = GPIOK_BASE | 10,
 		/// PK11 pin
-		pk11 = reinterpret_cast<uint32_t>(GPIOK) | 11,
+		pk11 = GPIOK_BASE | 11,
 		/// PK12 pin
-		pk12 = reinterpret_cast<uint32_t>(GPIOK) | 12,
+		pk12 = GPIOK_BASE | 12,
 		/// PK13 pin
-		pk13 = reinterpret_cast<uint32_t>(GPIOK) | 13,
+		pk13 = GPIOK_BASE | 13,
 		/// PK14 pin
-		pk14 = reinterpret_cast<uint32_t>(GPIOK) | 14,
+		pk14 = GPIOK_BASE | 14,
 		/// PK15 pin
-		pk15 = reinterpret_cast<uint32_t>(GPIOK) | 15,
+		pk15 = GPIOK_BASE | 15,
 
 #endif	// def GPIOK
 };
