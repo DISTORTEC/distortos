c1c60838b0fa2ef673c2aeee922852162c25a9bb
Kamil Szczygiel <distortec@distortec.com>
2016-10-08 20:14:19 +0200

    Workaround for removeStackFrame() problem with ARMv6-M and GCC 6.2.0
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=77904
    
    If compiler decides to add a frame pointer to this function (as in case
    of ARMv6-M and GCC 6.2.0) then this frame pointer will clobber one of
    thread's registers, because r7 is not stacked. Making this function
    "naked" and reimplementing it in assembly solves this particular
    problem.

--- source/architecture/ARM/ARMv6-M-ARMv7-M/ARMv6-M-ARMv7-M-requestFunctionExecution.cpp
+++ source/architecture/ARM/ARMv6-M-ARMv7-M/ARMv6-M-ARMv7-M-requestFunctionExecution.cpp
@@ -48,6 +48,7 @@ namespace
  * (false)
  */
 
+__attribute__ ((naked))
 #if __FPU_PRESENT == 1 && __FPU_USED == 1
 void removeStackFrame(const void* const savedStackPointer, const bool fpuContextActive)
 #else	// __FPU_PRESENT != 1 || __FPU_USED != 1
@@ -56,12 +57,24 @@ void removeStackFrame(const void* const savedStackPointer)
 {
 #if __FPU_PRESENT == 1 && __FPU_USED == 1
 
-	if (fpuContextActive == true)
-		asm volatile ("vmov s0, s0");	// force stacking of FPU context
+	asm volatile
+	(
+			"	cbz		%[fpuContextActive], 1f		\n"	// if (fpuContextActive == true) {
+			"	vmov	s0, s0						\n"	// force stacking of FPU context
+			"1:										\n"	// }
+
+			::	[fpuContextActive] "r" (fpuContextActive)
+	);
 
 #endif	// __FPU_PRESENT == 1 && __FPU_USED == 1
 
-	__set_PSP(reinterpret_cast<uint32_t>(savedStackPointer));
+	asm volatile
+	(
+			"	msr		psp, %[savedStackPointer]	\n"	// restore stack pointer for current thread
+			"	bx		lr							\n"	// return
+
+			::	[savedStackPointer] "r" (savedStackPointer)
+	);
 }
 
 /**
