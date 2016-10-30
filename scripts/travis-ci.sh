c8deacfe0b286dd842b64ccc1f7ec9c3e2c48437
Kamil Szczygiel <distortec@distortec.com>
2016-10-30 15:21:14 +0100

    Load environment variables from files generated for each toolchain

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -26,6 +26,9 @@ install() {
 	wget http://www.freddiechopin.info/en/download/category/11-bleeding-edge-toolchain?download=143%3Ableeding-edge-toolchain-160412-64-bit-linux -O gcc-arm-none-eabi-5_3-160412-linux-x64.tar.xz
 	echo "Extracting gcc-arm-none-eabi-5_3-160412-linux-x64.tar.xz..."
 	tar -xf gcc-arm-none-eabi-5_3-160412-linux-x64.tar.xz
+	cat > gcc-arm-none-eabi-5_3-160412.sh <<- EOF
+	export PATH="$(pwd)/gcc-arm-none-eabi-5_3-160412/bin:\${PATH-}"
+	EOF
 
 	echo "Downloading gcc-arm-none-eabi-4_9-150928-linux-x64.tar.xz..."
 	wget http://www.freddiechopin.info/en/download/category/11-bleeding-edge-toolchain?download=122%3Ableeding-edge-toolchain-150928-64-bit-linux -O gcc-arm-none-eabi-4_9-150928-linux-x64.tar.xz
@@ -33,20 +36,23 @@ install() {
 	tar -xf gcc-arm-none-eabi-4_9-150928-linux-x64.tar.xz
 	mkdir gcc-arm-none-eabi-4_9-150928/bin/lib
 	gcc -shared -o gcc-arm-none-eabi-4_9-150928/bin/lib/libfl.so.2 -lfl
+	cat > gcc-arm-none-eabi-4_9-150928.sh <<- EOF
+	export LD_LIBRARY_PATH="$(pwd)/gcc-arm-none-eabi-4_9-150928/bin/lib"
+	export PATH="$(pwd)/gcc-arm-none-eabi-4_9-150928/bin:\${PATH-}"
+	EOF
 }
 
 # "script" phase
 script() {
 	(
 	echo "Testing with gcc-arm-none-eabi-5_3-160412..."
-	export PATH="${HOME}/toolchains/gcc-arm-none-eabi-5_3-160412/bin:${PATH-}"
+	. "${HOME}/toolchains/gcc-arm-none-eabi-5_3-160412.sh"
 	./scripts/buildAllConfigurations.sh
 	)
 
 	(
 	echo "Testing with gcc-arm-none-eabi-4_9-150928..."
-	export PATH="${HOME}/toolchains/gcc-arm-none-eabi-4_9-150928/bin:${PATH-}"
-	export LD_LIBRARY_PATH="${HOME}/toolchains/gcc-arm-none-eabi-4_9-150928/bin/lib"
+	. "${HOME}/toolchains/gcc-arm-none-eabi-4_9-150928.sh"
 	./scripts/buildAllConfigurations.sh
 	)
 }
