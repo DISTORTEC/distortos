1d33f93ab3c8950e6d43f1639ceb3c6595abb72a
Kamil Szczygiel <distortec@distortec.com>
2016-11-04 20:17:28 +0100

    Add empty chip::usartLowLevelInitialization() for USARTv1 in STM32

---
+++ source/chip/STM32/peripherals/USARTv1/STM32-USARTv1-usartLowLevelInitialization.cpp
@@ -0,0 +1,31 @@
+/**
+ * \file
+ * \brief chip::usartLowLevelInitialization() definition for USARTv1 in STM32
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "STM32-USARTv1-usartLowLevelInitialization.hpp"
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void usartLowLevelInitialization()
+{
+
+}
+
+}	// namespace chip
+
+}	// namespace distortos
