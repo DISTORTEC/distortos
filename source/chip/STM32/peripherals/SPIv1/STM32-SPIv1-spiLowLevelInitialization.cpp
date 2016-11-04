a295f413aaf82827c399a2675f6d915fff528f8b
Kamil Szczygiel <distortec@distortec.com>
2016-11-04 19:58:19 +0100

    Add empty chip::spiLowLevelInitialization() for SPIv1 in STM32

---
+++ source/chip/STM32/peripherals/SPIv1/STM32-SPIv1-spiLowLevelInitialization.cpp
@@ -0,0 +1,31 @@
+/**
+ * \file
+ * \brief chip::spiLowLevelInitialization() definition for SPIv1 in STM32
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "STM32-SPIv1-spiLowLevelInitialization.hpp"
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void spiLowLevelInitialization()
+{
+
+}
+
+}	// namespace chip
+
+}	// namespace distortos
