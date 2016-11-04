b88f17c2cc3d75b7489dabfcaa1810d67885c82a
Kamil Szczygiel <distortec@distortec.com>
2016-11-04 17:40:57 +0100

    Add empty chip::peripheralsLowLevelInitialization() for STM32

---
+++ source/chip/STM32/peripherals/STM32-peripheralsLowLevelInitialization.cpp
@@ -0,0 +1,31 @@
+/**
+ * \file
+ * \brief chip::peripheralsLowLevelInitialization() implementation for STM32
+ *
+ * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/peripheralsLowLevelInitialization.hpp"
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void peripheralsLowLevelInitialization()
+{
+
+}
+
+}	// namespace chip
+
+}	// namespace distortos
