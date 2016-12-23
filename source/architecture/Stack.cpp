780cfa8d013e01df6a085408fbf5da81a533b167
Kamil Szczygiel <distortec@distortec.com>
2016-12-23 18:27:26 +0100

    Use stack alignment instead of divisibility in Stack class

--- source/architecture/Stack.cpp
+++ source/architecture/Stack.cpp
@@ -48,20 +48,20 @@ void* adjustStorage(void* const storage, const size_t stackAlignment)
 }
 
 /**
- * \brief Adjusts storage's size to suit architecture's divisibility requirements,
+ * \brief Adjusts storage's size to suit architecture's alignment requirements,
  *
  * \param [in] storage is a pointer to stack's storage
  * \param [in] size is the size of stack's storage, bytes
  * \param [in] adjustedStorage is an adjusted storage's address
- * \param [in] divisibility is required stack's size divisibility
+ * \param [in] stackAlignment is required stack alignment
  *
  * \return adjusted storage's size
  */
 
-size_t adjustSize(void* const storage, const size_t size, void* const adjustedStorage, const size_t divisibility)
+size_t adjustSize(void* const storage, const size_t size, void* const adjustedStorage, const size_t stackAlignment)
 {
 	const auto offset = static_cast<uint8_t*>(adjustedStorage) - static_cast<uint8_t*>(storage);
-	return ((size - offset) / divisibility) * divisibility;
+	return ((size - offset) / stackAlignment) * stackAlignment;
 }
 
 /**
@@ -94,7 +94,7 @@ Stack::Stack(StorageUniquePointer&& storageUniquePointer, const size_t size, Thr
 		void (* preTerminationHook)(Thread&), void (& terminationHook)(Thread&)) :
 				storageUniquePointer_{std::move(storageUniquePointer)},
 				adjustedStorage_{adjustStorage(storageUniquePointer_.get(), stackAlignment)},
-				adjustedSize_{adjustSize(storageUniquePointer_.get(), size, adjustedStorage_, stackSizeDivisibility)},
+				adjustedSize_{adjustSize(storageUniquePointer_.get(), size, adjustedStorage_, stackAlignment)},
 				stackPointer_{initializeStackProxy(adjustedStorage_, adjustedSize_, thread, run, preTerminationHook,
 						terminationHook)}
 {
