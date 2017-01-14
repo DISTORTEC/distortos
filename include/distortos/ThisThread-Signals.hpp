31a175f368659984365c61cde4840b69ec87dd6b
Kamil Szczygiel <distortec@distortec.com>
2017-01-14 16:58:55 +0100

    ThisThread::Signals::generateSignal() may return ENOMEM
    This function will fail with ENOMEM if handling the request to deliver
    signals would result in stack overflow.

--- include/distortos/ThisThread-Signals.hpp
+++ include/distortos/ThisThread-Signals.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief ThisThread::Signals namespace header
  *
- * \author Copyright (C) 2015-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2015-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -47,6 +47,7 @@ namespace Signals
  *
  * \return 0 on success, error code otherwise:
  * - EINVAL - \a signalNumber value is invalid;
+ * - ENOMEM - amount of free stack is too small to request delivery of signals;
  * - ENOTSUP - reception of signals is disabled for current thread;
  */
 
