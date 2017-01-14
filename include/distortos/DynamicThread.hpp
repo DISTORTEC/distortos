c8e85674ff54fd8cc236b1a7fe5cfa89fea3fa0e
Kamil Szczygiel <distortec@distortec.com>
2017-01-14 16:55:15 +0100

    DynamicThread::generateSignal() may return ENOMEM
    This function will fail with ENOMEM if handling the request to deliver
    signals would result in stack overflow.

--- include/distortos/DynamicThread.hpp
+++ include/distortos/DynamicThread.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief DynamicThread class header
  *
- * \author Copyright (C) 2015-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2015-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -111,6 +111,7 @@ public:
 	 *
 	 * \return 0 on success, error code otherwise:
 	 * - EINVAL - \a signalNumber value is invalid;
+	 * - ENOMEM - amount of free stack is too small to request delivery of signals;
 	 * - ENOTSUP - reception of signals is disabled for this thread;
 	 * - EINVAL - internal thread object was detached;
 	 *
