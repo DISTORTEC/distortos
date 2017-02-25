8db582b3b8731eb5b33fc2c9bb37e9a1461f5f10
Kamil Szczygiel <distortec@distortec.com>
2017-02-25 23:59:52 +0100

    Implement workaround for ARM Cortex-M7 r0p1 bug ID 837070
    Writes to BASEPRI which increase current execution priority don't take
    effect immediatelly on ARM Cortex-M7 r0p1. ARM's suggested workaround is
    to surround such operations with a critical section which uses PRIMASK.

--- source/architecture/ARM/ARMv6-M-ARMv7-M/ARMv6-M-ARMv7-M-restoreInterruptMasking.cpp
+++ source/architecture/ARM/ARMv6-M-ARMv7-M/ARMv6-M-ARMv7-M-restoreInterruptMasking.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief restoreInterruptMasking() implementation for ARMv6-M and ARMv7-M
  *
- * \author Copyright (C) 2014-2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -27,7 +27,15 @@ void restoreInterruptMasking(const InterruptMask interruptMask)
 {
 #if CONFIG_ARCHITECTURE_ARMV7_M_KERNEL_BASEPRI != 0
 
+#ifdef CONFIG_ARCHITECTURE_ARM_CORTEX_M7_R0P1
+	__disable_irq();	// ARM Cortex-M7 r0p1 bug ID 837070
+#endif	// def CONFIG_ARCHITECTURE_ARM_CORTEX_M7_R0P1
+
 	__set_BASEPRI(interruptMask);
+
+#ifdef CONFIG_ARCHITECTURE_ARM_CORTEX_M7_R0P1
+	__enable_irq();	// ARM Cortex-M7 r0p1 bug ID 837070
+#endif	// def CONFIG_ARCHITECTURE_ARM_CORTEX_M7_R0P1
 
 #else	// CONFIG_ARCHITECTURE_ARMV7_M_KERNEL_BASEPRI == 0
 
