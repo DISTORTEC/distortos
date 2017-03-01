9840086b04d4282e29dd9b1a2a3f15ab6c93a53c
Kamil Szczygiel <distortec@distortec.com>
2017-03-01 23:06:00 +0100

    Configure SPI3-6 in spiLowLevelInitialization() for STM32's SPIv2

--- source/chip/STM32/peripherals/SPIv2/STM32-SPIv2-spiLowLevelInitialization.cpp
+++ source/chip/STM32/peripherals/SPIv2/STM32-SPIv2-spiLowLevelInitialization.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief chip::spiLowLevelInitialization() definition for SPIv2 in STM32
  *
- * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2016-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -33,6 +33,22 @@ void spiLowLevelInitialization()
 	NVIC_SetPriority(SPI2_IRQn, {});
 	NVIC_EnableIRQ(SPI2_IRQn);
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI2_ENABLE
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
+	NVIC_SetPriority(SPI3_IRQn, {});
+	NVIC_EnableIRQ(SPI3_IRQn);
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
+	NVIC_SetPriority(SPI4_IRQn, {});
+	NVIC_EnableIRQ(SPI4_IRQn);
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
+	NVIC_SetPriority(SPI5_IRQn, {});
+	NVIC_EnableIRQ(SPI5_IRQn);
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI6_ENABLE
+	NVIC_SetPriority(SPI6_IRQn, {});
+	NVIC_EnableIRQ(SPI6_IRQn);
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI6_ENABLE
 }
 
 }	// namespace chip
