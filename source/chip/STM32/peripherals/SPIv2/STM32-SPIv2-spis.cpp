ae6f7278521b7af7e6d161f1904ca0485832d00c
Kamil Szczygiel <distortec@distortec.com>
2017-03-02 21:39:36 +0100

    Rearrange things in STM32-SPIv2-spis.cpp

--- source/chip/STM32/peripherals/SPIv2/STM32-SPIv2-spis.cpp
+++ source/chip/STM32/peripherals/SPIv2/STM32-SPIv2-spis.cpp
@@ -20,12 +20,50 @@ namespace chip
 {
 
 /*---------------------------------------------------------------------------------------------------------------------+
-| SPI1 global objects and functions
+| global objects
 +---------------------------------------------------------------------------------------------------------------------*/
 
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI1_ENABLE
 
 ChipSpiMasterLowLevel spi1 {ChipSpiMasterLowLevel::spi1Parameters};
+
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI1_ENABLE
+
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI2_ENABLE
+
+ChipSpiMasterLowLevel spi2 {ChipSpiMasterLowLevel::spi2Parameters};
+
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI2_ENABLE
+
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
+
+ChipSpiMasterLowLevel spi3 {ChipSpiMasterLowLevel::spi3Parameters};
+
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
+
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
+
+ChipSpiMasterLowLevel spi4 {ChipSpiMasterLowLevel::spi4Parameters};
+
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
+
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
+
+ChipSpiMasterLowLevel spi5 {ChipSpiMasterLowLevel::spi5Parameters};
+
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
+
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI6_ENABLE
+
+ChipSpiMasterLowLevel spi6 {ChipSpiMasterLowLevel::spi6Parameters};
+
+#endif	// def CONFIG_CHIP_STM32_SPIV2_SPI6_ENABLE
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI1_ENABLE
 
 /**
  * \brief SPI1 interrupt handler
@@ -38,13 +76,7 @@ extern "C" void SPI1_IRQHandler()
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI1_ENABLE
 
-/*---------------------------------------------------------------------------------------------------------------------+
-| SPI2 global objects and functions
-+---------------------------------------------------------------------------------------------------------------------*/
-
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI2_ENABLE
-
-ChipSpiMasterLowLevel spi2 {ChipSpiMasterLowLevel::spi2Parameters};
 
 /**
  * \brief SPI2 interrupt handler
@@ -57,13 +89,7 @@ extern "C" void SPI2_IRQHandler()
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI2_ENABLE
 
-/*---------------------------------------------------------------------------------------------------------------------+
-| SPI3 global objects and functions
-+---------------------------------------------------------------------------------------------------------------------*/
-
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
-
-ChipSpiMasterLowLevel spi3 {ChipSpiMasterLowLevel::spi3Parameters};
 
 /**
  * \brief SPI3 interrupt handler
@@ -76,13 +102,7 @@ extern "C" void SPI3_IRQHandler()
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
 
-/*---------------------------------------------------------------------------------------------------------------------+
-| SPI4 global objects and functions
-+---------------------------------------------------------------------------------------------------------------------*/
-
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
-
-ChipSpiMasterLowLevel spi4 {ChipSpiMasterLowLevel::spi4Parameters};
 
 /**
  * \brief SPI4 interrupt handler
@@ -95,13 +115,7 @@ extern "C" void SPI4_IRQHandler()
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
 
-/*---------------------------------------------------------------------------------------------------------------------+
-| SPI5 global objects and functions
-+---------------------------------------------------------------------------------------------------------------------*/
-
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
-
-ChipSpiMasterLowLevel spi5 {ChipSpiMasterLowLevel::spi5Parameters};
 
 /**
  * \brief SPI5 interrupt handler
@@ -114,13 +128,7 @@ extern "C" void SPI5_IRQHandler()
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
 
-/*---------------------------------------------------------------------------------------------------------------------+
-| SPI6 global objects and functions
-+---------------------------------------------------------------------------------------------------------------------*/
-
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI6_ENABLE
-
-ChipSpiMasterLowLevel spi6 {ChipSpiMasterLowLevel::spi6Parameters};
 
 /**
  * \brief SPI6 interrupt handler
