24cb4a33cc9b3961a0b87019961364dc8f241f85
Kamil Szczygiel <distortec@distortec.com>
2017-03-04 22:18:47 +0100

    Add STM32F7-PWR.{c,h}pp

---
+++ source/chip/STM32/STM32F7/STM32F7-PWR.cpp
@@ -0,0 +1,53 @@
+/**
+ * \file
+ * \brief Implementation of PWR-related functions for STM32F7
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/STM32F7-PWR.hpp"
+
+#include "distortos/chip/CMSIS-proxy.h"
+
+#include <cerrno>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+int configureVoltageScaling(const uint8_t voltageScale)
+{
+	if (voltageScale < minVoltageScale || voltageScale > maxVoltageScale)
+		return EINVAL;
+
+	PWR->CR1 = (PWR->CR1 & ~PWR_CR1_VOS) | (((4 - voltageScale) << PWR_CR1_VOS_Pos) & PWR_CR1_VOS);
+	return 0;
+}
+
+void disableOverDriveMode()
+{
+	PWR->CR1 &= ~(PWR_CR1_ODEN | PWR_CR1_ODSWEN);
+	while ((PWR->CSR1 & PWR_CSR1_ODSWRDY) != 0);
+}
+
+void enableOverDriveMode()
+{
+	PWR->CR1 |= PWR_CR1_ODEN;
+	while ((PWR-> CSR1 & PWR_CSR1_ODRDY) == 0);
+	PWR->CR1 |= PWR_CR1_ODSWEN;
+	while ((PWR-> CSR1 & PWR_CSR1_ODSWRDY) == 0);
+}
+
+}	// namespace chip
+
+}	// namespace distortos
---
+++ source/chip/STM32/STM32F7/include/distortos/chip/STM32F7-PWR.hpp
@@ -0,0 +1,73 @@
+/**
+ * \file
+ * \brief Header for PWR-related functions for STM32F7
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOURCE_CHIP_STM32_STM32F7_INCLUDE_DISTORTOS_CHIP_STM32F7_PWR_HPP_
+#define SOURCE_CHIP_STM32_STM32F7_INCLUDE_DISTORTOS_CHIP_STM32F7_PWR_HPP_
+
+#include <cstdint>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global constants
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// minimum allowed value of voltage scale mode
+constexpr uint8_t minVoltageScale {1};
+
+/// maximum allowed value of voltage scale mode
+constexpr uint8_t maxVoltageScale {3};
+
+/// default voltage scale mode after reset
+constexpr uint8_t defaultVoltageScale {1};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions' declarations
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Configures voltage scaling.
+ *
+ * \warning Voltage scaling can be configured only when PLL is disabled.
+ *
+ * \param [in] voltageScale is the new voltage scale mode, [1; 3] or [minVoltageScale; maxVoltageScale]
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a voltageScale value is invalid;
+ */
+
+int configureVoltageScaling(uint8_t voltageScale);
+
+/**
+ * \brief Disables over-drive mode.
+ *
+ * \warning Over-drive mode can be enabied/disabled only when HSI or HSE is selected as source of system clock
+ */
+
+void disableOverDriveMode();
+
+/**
+ * \brief Enables over-drive mode.
+ *
+ * \warning Over-drive mode can be enabied/disabled only when HSI or HSE is selected as source of system clock
+ */
+
+void enableOverDriveMode();
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_CHIP_STM32_STM32F7_INCLUDE_DISTORTOS_CHIP_STM32F7_PWR_HPP_
