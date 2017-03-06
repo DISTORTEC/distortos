1d692e118fabb187123bd24bc64c0f070f53dbc5
Kamil Szczygiel <distortec@distortec.com>
2017-03-06 21:56:31 +0100

    Add STM32F7-RCC.{c,h}pp

---
+++ source/chip/STM32/STM32F7/STM32F7-RCC.cpp
@@ -0,0 +1,149 @@
+/**
+ * \file
+ * \brief Implementation of RCC-related functions for STM32F7
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/STM32F7-RCC.hpp"
+
+#include "distortos/chip/CMSIS-proxy.h"
+
+#include <array>
+
+#include <cerrno>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+int configureAhbClockDivider(const uint16_t hpre)
+{
+	static const std::pair<decltype(hpre), decltype(RCC_CFGR_HPRE_DIV1)> associations[]
+	{
+			{hpreDiv1, RCC_CFGR_HPRE_DIV1},
+			{hpreDiv2, RCC_CFGR_HPRE_DIV2},
+			{hpreDiv4, RCC_CFGR_HPRE_DIV4},
+			{hpreDiv8, RCC_CFGR_HPRE_DIV8},
+			{hpreDiv16, RCC_CFGR_HPRE_DIV16},
+			{hpreDiv64, RCC_CFGR_HPRE_DIV64},
+			{hpreDiv128, RCC_CFGR_HPRE_DIV128},
+			{hpreDiv256, RCC_CFGR_HPRE_DIV256},
+			{hpreDiv512, RCC_CFGR_HPRE_DIV512},
+	};
+
+	for (auto& association : associations)
+		if (association.first == hpre)
+		{
+			RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_HPRE) | association.second;
+			return 0;
+		}
+
+	return EINVAL;
+}
+
+int configureApbClockDivider(const bool ppre2, const uint8_t ppre)
+{
+	static const std::pair<decltype(ppre), std::array<decltype(RCC_CFGR_PPRE1_DIV1), 2>> associations[]
+	{
+			{ppreDiv1, {RCC_CFGR_PPRE1_DIV1, RCC_CFGR_PPRE2_DIV1}},
+			{ppreDiv2, {RCC_CFGR_PPRE1_DIV2, RCC_CFGR_PPRE2_DIV2}},
+			{ppreDiv4, {RCC_CFGR_PPRE1_DIV4, RCC_CFGR_PPRE2_DIV4}},
+			{ppreDiv8, {RCC_CFGR_PPRE1_DIV8, RCC_CFGR_PPRE2_DIV8}},
+			{ppreDiv16, {RCC_CFGR_PPRE1_DIV16, RCC_CFGR_PPRE2_DIV16}},
+	};
+
+	for (auto& association : associations)
+		if (association.first == ppre)
+		{
+			static const decltype(RCC_CFGR_PPRE1) masks[] {RCC_CFGR_PPRE1, RCC_CFGR_PPRE2};
+			RCC->CFGR = (RCC->CFGR & ~masks[ppre2]) | association.second[ppre2];
+			return 0;
+		}
+
+	return EINVAL;
+}
+
+void configurePllClockSource(const bool hse)
+{
+	RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLSRC) | hse << RCC_PLLCFGR_PLLSRC_Pos;
+}
+
+int configurePllInputClockDivider(const uint8_t pllm)
+{
+	if (pllm < minPllm || pllm > maxPllm)
+		return EINVAL;
+
+	RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLM) | pllm << RCC_PLLCFGR_PLLM_Pos;
+	return 0;
+}
+
+void disableHse()
+{
+	RCC->CR &= ~RCC_CR_HSEON;
+}
+
+void disablePll()
+{
+	RCC->CR &= ~RCC_CR_PLLON;
+}
+
+void enableHse(const bool bypass)
+{
+	RCC->CR = (RCC->CR & ~RCC_CR_HSEBYP) | bypass << RCC_CR_HSEBYP_Pos;
+	RCC->CR |= RCC_CR_HSEON;
+	while ((RCC->CR & RCC_CR_HSERDY) == 0);	// wait until HSE oscillator is stable
+}
+
+#if defined(CONFIG_CHIP_STM32F76) || defined(CONFIG_CHIP_STM32F77)
+int enablePll(const uint16_t plln, const uint8_t pllp, const uint8_t pllq, const uint8_t pllr)
+#else	// !defined(CONFIG_CHIP_STM32F76) && !defined(CONFIG_CHIP_STM32F77)
+int enablePll(const uint16_t plln, const uint8_t pllp, const uint8_t pllq)
+#endif	// !defined(CONFIG_CHIP_STM32F76) && !defined(CONFIG_CHIP_STM32F77)
+{
+	if (plln < minPlln || plln > maxPlln ||
+			(pllp != pllpDiv2 && pllp != pllpDiv4 && pllp != pllpDiv6 && pllp != pllpDiv8) ||
+			pllq < minPllq || pllq > maxPllq)
+		return EINVAL;
+
+#if defined(CONFIG_CHIP_STM32F76) || defined(CONFIG_CHIP_STM32F77)
+
+	if (pllr < minPllr || pllr > maxPllr)
+		return EINVAL;
+
+	RCC->PLLCFGR = (RCC->PLLCFGR & ~(RCC_PLLCFGR_PLLN | RCC_PLLCFGR_PLLP | RCC_PLLCFGR_PLLQ | RCC_PLLCFGR_PLLR)) |
+			plln << RCC_PLLCFGR_PLLN_Pos | (pllp / 2 - 1) << RCC_PLLCFGR_PLLP_Pos | pllq << RCC_PLLCFGR_PLLQ_Pos |
+			pllr << RCC_PLLCFGR_PLLR_Pos;
+
+#else	// !defined(CONFIG_CHIP_STM32F76) && !defined(CONFIG_CHIP_STM32F77)
+
+	RCC->PLLCFGR = (RCC->PLLCFGR & ~(RCC_PLLCFGR_PLLN | RCC_PLLCFGR_PLLP | RCC_PLLCFGR_PLLQ)) |
+			plln << RCC_PLLCFGR_PLLN_Pos | (pllp / 2 - 1) << RCC_PLLCFGR_PLLP_Pos | pllq << RCC_PLLCFGR_PLLQ_Pos;
+
+#endif	// !defined(CONFIG_CHIP_STM32F76) && !defined(CONFIG_CHIP_STM32F77)
+
+	RCC->CR |= RCC_CR_PLLON;
+	while ((RCC->CR & RCC_CR_PLLRDY) == 0);	// wait until PLL is stable
+	return 0;
+}
+
+void switchSystemClock(const SystemClockSource source)
+{
+	const auto sourceValue = static_cast<uint32_t>(source);
+	RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_SW) | sourceValue << RCC_CFGR_SW_Pos;
+	while ((RCC->CFGR & RCC_CFGR_SWS) != sourceValue << RCC_CFGR_SWS_Pos);
+}
+
+}	// namespace chip
+
+}	// namespace distortos
---
+++ source/chip/STM32/STM32F7/include/distortos/chip/STM32F7-RCC.hpp
@@ -0,0 +1,266 @@
+/**
+ * \file
+ * \brief Header for RCC-related functions for STM32F7
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOURCE_CHIP_STM32_STM32F7_INCLUDE_DISTORTOS_CHIP_STM32F7_RCC_HPP_
+#define SOURCE_CHIP_STM32_STM32F7_INCLUDE_DISTORTOS_CHIP_STM32F7_RCC_HPP_
+
+#include "distortos/distortosConfiguration.h"
+
+#include <cstdint>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global types
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// system clock source
+enum class SystemClockSource : uint8_t
+{
+	/// HSI oscillator selected as system clock
+	hsi,
+	/// HSE oscillator selected as system clock
+	hse,
+	/// main PLL selected as system clock
+	pll,
+};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global constants
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// minimum allowed value for PLLM
+constexpr uint8_t minPllm {2};
+
+/// maximum allowed value for PLLM
+constexpr uint8_t maxPllm {63};
+
+/// minimum allowed value for PLLN
+constexpr uint16_t minPlln {50};
+
+/// maximum allowed value for PLLN
+constexpr uint16_t maxPlln {432};
+
+/// minimum allowed value for PLLQ
+constexpr uint8_t minPllq {2};
+
+/// maximum allowed value for PLLQ
+constexpr uint8_t maxPllq {15};
+
+#if defined(CONFIG_CHIP_STM32F76) || defined(CONFIG_CHIP_STM32F77)
+
+/// minimum allowed value for PLLR
+constexpr uint8_t minPllr {2};
+
+/// maximum allowed value for PLLR
+constexpr uint8_t maxPllr {7};
+
+#endif	// defined(CONFIG_CHIP_STM32F76) || defined(CONFIG_CHIP_STM32F77)
+
+/// first allowed value for PLLP - 2
+constexpr uint8_t pllpDiv2 {2};
+
+/// second allowed value for PLLP - 4
+constexpr uint8_t pllpDiv4 {4};
+
+/// third allowed value for PLLP - 6
+constexpr uint8_t pllpDiv6 {6};
+
+/// fourth allowed value for PLLP - 8
+constexpr uint8_t pllpDiv8 {8};
+
+/// first allowed value for AHB divider - 1
+constexpr uint16_t hpreDiv1 {1};
+
+/// second allowed value for AHB divider - 2
+constexpr uint16_t hpreDiv2 {2};
+
+/// third allowed value for AHB divider - 4
+constexpr uint16_t hpreDiv4 {4};
+
+/// fourth allowed value for AHB divider - 8
+constexpr uint16_t hpreDiv8 {8};
+
+/// fifth allowed value for AHB divider - 16
+constexpr uint16_t hpreDiv16 {16};
+
+/// sixth allowed value for AHB divider - 64
+constexpr uint16_t hpreDiv64 {64};
+
+/// seventh allowed value for AHB divider - 128
+constexpr uint16_t hpreDiv128 {128};
+
+/// eighth allowed value for AHB divider - 256
+constexpr uint16_t hpreDiv256 {256};
+
+/// ninth allowed value for AHB divider - 512
+constexpr uint16_t hpreDiv512 {512};
+
+/// first allowed value for APB1 and APB2 dividers - 1
+constexpr uint8_t ppreDiv1 {1};
+
+/// second allowed value for APB1 and APB2 dividers - 2
+constexpr uint8_t ppreDiv2 {2};
+
+/// third allowed value for APB1 and APB2 dividers - 4
+constexpr uint8_t ppreDiv4 {4};
+
+/// fourth allowed value for APB1 and APB2 dividers - 8
+constexpr uint8_t ppreDiv8 {8};
+
+/// fifth allowed value for APB1 and APB2 dividers - 16
+constexpr uint8_t ppreDiv16 {16};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions' declarations
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Configures divider of AHB clock (HPRE value).
+ *
+ * \param [in] hpre is the HPRE value, {1, 2, 4, 8, 16, 64, 128, 256, 512} or {hpreDiv1, hpreDiv2, hpreDiv4, hpreDiv8,
+ * hpreDiv16, hpreDiv64, hpreDiv128, hpreDiv256, hpreDiv512}
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a hpre value is invalid;
+ */
+
+int configureAhbClockDivider(uint16_t hpre);
+
+/**
+ * \brief Configures divider of APB1 or APB2 clock (PPRE1 or PPRE2 value).
+ *
+ * \param [in] ppre2 selects whether PPRE1 (false) or PPRE2 (true) is configured
+ * \param [in] ppre is the PPRE value, {1, 2, 4, 8, 16} or {ppreDiv1, ppreDiv2, ppreDiv4, ppreDiv8, ppreDiv16}
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a ppre value is invalid;
+ */
+
+int configureApbClockDivider(bool ppre2, uint8_t ppre);
+
+/**
+ * \brief Configures clock source of main and audio PLLs.
+ *
+ * \warning Before changing configuration of any PLL make sure that they are not used in any way (as core clock or as
+ * source of peripheral clocks) and that they are disabled.
+ *
+ * \param [in] hse selects whether HSI (false) or HSE (true) is used as clock source of main and audio PLLs
+ */
+
+void configurePllClockSource(bool hse);
+
+/**
+ * \brief Configures divider of PLL input clock (PLLM value) for main and audio PLLs.
+ *
+ * \warning Before changing configuration of any PLL make sure that they are not used in any way (as core clock or as
+ * source of peripheral clocks) and that they are disabled.
+ *
+ * \param [in] pllm is the PLLM value for main PLL and audio PLLI2S, [2; 63] or [minPllm; maxPllm]
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a pllm value is invalid;
+ */
+
+int configurePllInputClockDivider(uint8_t pllm);
+
+/**
+ * \brief Disables HSE clock.
+ *
+ * \warning Before changing configuration of HSE clock make sure that it is not used in any way (as core clock, as
+ * source for any PLL or as source of RTC clock).
+ */
+
+void disableHse();
+
+/**
+ * \brief Disables main PLL.
+ *
+ * \warning Before changing configuration of main PLL make sure that it is not used in any way (as core clock or as
+ * source of peripheral clocks).
+ */
+
+void disablePll();
+
+/**
+ * \brief Enables HSE clock.
+ *
+ * Enables HSE clock using crystal/ceramic resonator (bypass disabled) or external user clock (bypass enabled). This
+ * function waits until the HSE oscillator is stable after enabling the clock.
+ *
+ * \warning Before changing configuration of HSE clock make sure that it is not used in any way (as core clock, as
+ * source for any PLL or as source of RTC clock).
+ *
+ * \param [in] bypass selects whether crystal/ceramic resonator (false) or external user clock (true) is used
+ */
+
+void enableHse(bool bypass);
+
+#if defined(CONFIG_CHIP_STM32F76) || defined(CONFIG_CHIP_STM32F77)
+
+/**
+ * \brief Enables main PLL.
+ *
+ * Enables main PLL using selected parameters and waits until it is stable.
+ *
+ * \warning Before changing configuration of main PLL make sure that it is not used in any way (as core clock or as
+ * source of peripheral clocks) and that it is disabled.
+ *
+ * \param [in] plln is the PLLN value for main PLL, [50; 432] or [minPlln; maxPlln]
+ * \param [in] pllp is the PLLP value for main PLL, {2, 4, 6, 8} or {pllpDiv2, pllpDiv4, pllpDiv6, pllpDiv8}
+ * \param [in] pllq is the PLLQ value for main PLL, [2; 15] or [minPllq; maxPllq]
+ * \param [in] pllr is the PLLR value for main PLL, [2; 7] or [minPllr; maxPllr]
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a plln or \a pllp or \a pllq or \a pllr value is invalid;
+ */
+
+int enablePll(uint16_t plln, uint8_t pllp, uint8_t pllq, uint8_t pllr);
+
+#else	// !defined(CONFIG_CHIP_STM32F76) && !defined(CONFIG_CHIP_STM32F77)
+
+/**
+ * \brief Enables main PLL.
+ *
+ * Enables main PLL using selected parameters and waits until it is stable.
+ *
+ * \warning Before changing configuration of main PLL make sure that it is not used in any way (as core clock or as
+ * source of peripheral clocks) and that it is disabled.
+ *
+ * \param [in] plln is the PLLN value for main PLL, [50; 432] or [minPlln; maxPlln]
+ * \param [in] pllp is the PLLP value for main PLL, {2, 4, 6, 8} or {pllpDiv2, pllpDiv4, pllpDiv6, pllpDiv8}
+ * \param [in] pllq is the PLLQ value for main PLL, [2; 15] or [minPllq; maxPllq]
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a plln or \a pllp or \a pllq value is invalid;
+ */
+
+int enablePll(uint16_t plln, uint8_t pllp, uint8_t pllq);
+
+#endif	// !defined(CONFIG_CHIP_STM32F76) && !defined(CONFIG_CHIP_STM32F77)
+
+/**
+ * \brief Switches system clock.
+ *
+ * \param [in] source is the new source of system clock
+ */
+
+void switchSystemClock(SystemClockSource source);
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_CHIP_STM32_STM32F7_INCLUDE_DISTORTOS_CHIP_STM32F7_RCC_HPP_
