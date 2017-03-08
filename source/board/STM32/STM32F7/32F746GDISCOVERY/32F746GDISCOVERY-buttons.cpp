91ba39cfee9c6c9ea8d4aa009cb327e57c533034
Kamil Szczygiel <distortec@distortec.com>
2017-03-08 22:33:20 +0100

    Add buttons module for 32F746GDISCOVERY board

---
+++ source/board/STM32/STM32F7/32F746GDISCOVERY/32F746GDISCOVERY-buttons.cpp
@@ -0,0 +1,39 @@
+/**
+ * \file
+ * \brief Definition of buttons for 32F746GDISCOVERY
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/board/buttons.hpp"
+
+#ifdef CONFIG_BOARD_BUTTONS_ENABLE
+
+#include "distortos/chip/ChipInputPin.hpp"
+
+namespace distortos
+{
+
+namespace board
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const chip::ChipInputPin buttons[totalButtons]
+{
+#if DISTORTOS_BOARD_B1_BUTTON_ENABLE == 1
+		chip::ChipInputPin{chip::Pin::pi11, chip::PinPull::none, false},
+#endif	// DISTORTOS_BOARD_B1_BUTTON_ENABLE == 1
+};
+
+}	// namespace board
+
+}	// namespace distortos
+
+#endif	// def CONFIG_BOARD_BUTTONS_ENABLE
