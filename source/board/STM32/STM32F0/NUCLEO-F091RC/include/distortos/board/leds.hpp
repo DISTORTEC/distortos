2270d9321732464e107066320715b97436e4d073
CezaryGapinski <gapa1986@gmail.com>
2017-03-16 22:51:00 +0100

    Update files for all boards
    Generate sources, headers, Kconfigs, make and tup files for
    NUCLEO-F091RC, NUCLEO-F103RB, 32F429IDISCOVERY, NUCLEO-F401RE,
    NUCLEO-F429ZI, STM32F4DISCOVERY, 32F746GDISCOVERY

--- source/board/STM32/STM32F0/NUCLEO-F091RC/include/distortos/board/leds.hpp
+++ source/board/STM32/STM32F0/NUCLEO-F091RC/include/distortos/board/leds.hpp
@@ -2,11 +2,17 @@
  * \file
  * \brief Declaration of LEDs for NUCLEO-F091RC
  *
- * \author Copyright (C) 2016-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \warning
+ * Automatically generated file - do not edit!
+ *
+ * \date 2017-03-15 17:37:28
  */
 
 #ifndef SOURCE_BOARD_STM32_STM32F0_NUCLEO_F091RC_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
@@ -23,8 +29,10 @@
 #define DISTORTOS_BOARD_LD2_LED_ENABLE		0
 #endif	// !def CONFIG_CHIP_STM32_GPIOV2_GPIOA_ENABLE
 
-/// total number of LEDs on the board
-#define DISTORTOS_BOARD_TOTAL_LEDS		DISTORTOS_BOARD_LD2_LED_ENABLE
+/// total number of leds on the board
+#define DISTORTOS_BOARD_TOTAL_LEDS		( \
+		DISTORTOS_BOARD_LD2_LED_ENABLE + \
+		0)
 
 namespace distortos
 {
