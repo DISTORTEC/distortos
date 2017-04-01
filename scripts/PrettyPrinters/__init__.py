b99d2f5fe303a679df7298d21e1ef0b40c12bf21
Kamil Szczygiel <distortec@distortec.com>
2017-04-01 21:36:25 +0200

    Add PrettyPrinters.registerPrettyPrinters()

---
+++ scripts/PrettyPrinters/__init__.py
@@ -0,0 +1,20 @@
+#
+# file: __init__.py
+#
+# author: Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+# distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+########################################################################################################################
+# registerPrettyPrinters()
+########################################################################################################################
+
+def registerPrettyPrinters(obj):
+	'Register pretty-printers'
+
+	import PrettyPrinters.estd
+	PrettyPrinters.estd.registerPrettyPrinters(obj)
+	import PrettyPrinters.distortos
+	PrettyPrinters.distortos.registerPrettyPrinters(obj)
