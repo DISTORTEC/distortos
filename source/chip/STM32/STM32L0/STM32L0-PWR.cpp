e71f271f1f85cde5c03d91101babb36ad86e4b10
CezaryGapinski <gapa1986@gmail.com>
2017-04-09 17:40:49 +0200

    Add STM32L0-PWR.{c,h}pp

---
+++ source/chip/STM32/STM32L0/STM32L0-PWR.cpp
@@ -0,0 +1,39 @@
+/**
+ * \file
+ * \brief Implementation of PWR-related functions for STM32L0
+ *
+ * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/STM32L0-PWR.hpp"
+
+#include "distortos/chip/CMSIS-proxy.h"
+
+#include <cerrno>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+int configureVoltageScaling(const uint8_t voltageScale)
+{
+	if (voltageScale < minVoltageScale || voltageScale > maxVoltageScale)
+		return EINVAL;
+
+	PWR->CR = (PWR->CR & ~PWR_CR_VOS) | (((4 - voltageScale) << PWR_CR_VOS_Pos) & PWR_CR_VOS);
+	return 0;
+}
+
+}	// namespace chip
+
+}	// namespace distortos
---
+++ source/chip/STM32/STM32L0/include/distortos/chip/STM32L0-PWR.hpp
@@ -0,0 +1,57 @@
+/**
+ * \file
+ * \brief Header for PWR-related functions for STM32L0
+ *
+ * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOURCE_CHIP_STM32_STM32L0_INCLUDE_DISTORTOS_CHIP_STM32L0_PWR_HPP_
+#define SOURCE_CHIP_STM32_STM32L0_INCLUDE_DISTORTOS_CHIP_STM32L0_PWR_HPP_
+
+#include <cstdint>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global constants
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// minimum allowed value of voltage scale mode
+constexpr uint8_t minVoltageScale {1};
+
+/// maximum allowed value of voltage scale mode
+constexpr uint8_t maxVoltageScale {3};
+
+/// default voltage scale mode after reset
+constexpr uint8_t defaultVoltageScale {1};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions' declarations
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Configures voltage scaling.
+ *
+ * \warning Voltage scaling can be configured only when PLL is disabled.
+ *
+ * \param [in] voltageScale is the new voltage scale mode, [1; 3] or [minVoltageScale; maxVoltageScale]
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a voltageScale value is invalid;
+ */
+
+int configureVoltageScaling(uint8_t voltageScale);
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_CHIP_STM32_STM32L0_INCLUDE_DISTORTOS_CHIP_STM32L0_PWR_HPP_
