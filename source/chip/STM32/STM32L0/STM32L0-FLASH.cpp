5cae2e30e99f477924972fee2dfcda112d06275b
CezaryGapinski <gapa1986@gmail.com>
2017-04-10 19:02:28 +0200

    Add STM32L0-FLASH.{c,h}pp

---
+++ source/chip/STM32/STM32L0/STM32L0-FLASH.cpp
@@ -0,0 +1,44 @@
+/**
+ * \file
+ * \brief Implementation of FLASH-related functions for STM32L0
+ *
+ * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/STM32L0-FLASH.hpp"
+
+#include "distortos/chip/CMSIS-proxy.h"
+
+#include <cerrno>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+int configureFlashLatency(const uint8_t latency)
+{
+	if (latency > maxFlashLatency)
+		return EINVAL;
+
+	FLASH->ACR = (FLASH->ACR & ~FLASH_ACR_LATENCY) | latency << FLASH_ACR_LATENCY_Pos;
+	return 0;
+}
+
+void configurePrefetchBuffer(const bool enable)
+{
+	FLASH->ACR = (FLASH->ACR & ~FLASH_ACR_PRFTEN) | enable << FLASH_ACR_PRFTEN_Pos;
+}
+
+}	// namespace chip
+
+}	// namespace distortos
---
+++ source/chip/STM32/STM32L0/include/distortos/chip/STM32L0-FLASH.hpp
@@ -0,0 +1,57 @@
+/**
+ * \file
+ * \brief Header for FLASH-related functions for STM32L0
+ *
+ * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOURCE_CHIP_STM32_STM32L0_INCLUDE_DISTORTOS_CHIP_STM32L0_FLASH_HPP_
+#define SOURCE_CHIP_STM32_STM32L0_INCLUDE_DISTORTOS_CHIP_STM32L0_FLASH_HPP_
+
+#include <cstdint>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global constants
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// maximum allowed value of flash latency
+constexpr uint8_t maxFlashLatency {1};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions' declarations
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Configures flash latency.
+ *
+ * \param [in] latency is the new flash latency, [0; maxFlashLatency]
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a latency value is invalid;
+ */
+
+int configureFlashLatency(uint8_t latency);
+
+/**
+ * \brief Configures prefetch buffer.
+ *
+ * \param [in] enable selects whether prefetch buffer will be disabled (false) or enabled (true)
+ */
+
+void configurePrefetchBuffer(bool enable);
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_CHIP_STM32_STM32L0_INCLUDE_DISTORTOS_CHIP_STM32L0_FLASH_HPP_
