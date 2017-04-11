762aa72d123dd6b032f98e28edafd796a30095f8
CezaryGapinski <gapa1986@gmail.com>
2017-04-11 22:29:15 +0200

    Update STM32L0's clocks.hpp in preparation for standard clock config

--- source/chip/STM32/STM32L0/include/distortos/chip/clocks.hpp
+++ source/chip/STM32/STM32L0/include/distortos/chip/clocks.hpp
@@ -26,6 +26,25 @@ namespace chip
 | global constants
 +---------------------------------------------------------------------------------------------------------------------*/
 
+/// allowed values for MSI frequency, Hz
+/// [0] - range 0 around 65.536 kHz
+/// [1] - range 1 around 131.072 kHz
+/// [2] - range 2 around 262.144 kHz
+/// [3] - range 3 around 524.288 kHz
+/// [4] - range 4 around 1.048 MHz
+/// [5] - range 5 around 2.097 MHz
+/// [6] - range 6 around 4.194 MHz
+constexpr uint32_t msiFrequencies[7]
+{
+		65536,
+		131072,
+		262144,
+		524288,
+		1048000,
+		2097000,
+		4194000,
+};
+
 /// HSI16 clock frequency, Hz
 constexpr uint32_t hsi16Frequency {16000000};
 
@@ -38,17 +57,77 @@ constexpr uint32_t maxPllInFrequency {24000000};
 /// minimum allowed value for PLL output frequency, Hz
 constexpr uint32_t minPllOutFrequency {1500000};
 
-/// maximum allowed value for PLL output frequency, Hz
-constexpr uint32_t maxPllOutFrequency {96000000};
-
-/// SYSCLK frequency, Hz
-constexpr uint32_t sysclkFrequency {CONFIG_CHIP_STM32L0_RCC_SYSCLK_FREQUENCY};
+/// maximum allowed values for PLL output frequency, Hz
+/// [0] - voltage scale 1
+/// [1] - voltage scale 2
+/// [2] - voltage scale 3
+constexpr uint32_t maxPllOutFrequencies[3]
+{
+		960000000,
+		480000000,
+		240000000,
+};
 
 /// maximum allowed APB1 (low speed) frequency, Hz
 constexpr uint32_t maxApb1Frequency {32000000};
 
 /// maximum allowed APB2 (high speed) frequency, Hz
 constexpr uint32_t maxApb2Frequency {32000000};
+
+#ifdef CONFIG_CHIP_STM32L0_STANDARD_CLOCK_CONFIGURATION_ENABLE
+
+/// voltage scale index for \a maxPllOutFrequencies array (maxPllOutFrequencies[voltageScaleIndex])
+constexpr uint8_t voltageScaleIndex {CONFIG_CHIP_STM32L0_PWR_VOLTAGE_SCALE_MODE - 1};
+
+/// maximum allowed value for PLL output frequency, Hz
+constexpr uint32_t maxPllOutFrequency {maxPllOutFrequencies[voltageScaleIndex]};
+
+#if defined(CONFIG_CHIP_STM32L0_RCC_MSI_ENABLE)
+/// voltage scale index for \a msiFrequencies array (maxPllOutFrequencies[msiRangeIndex])
+constexpr uint8_t msiRangeIndex {CONFIG_CHIP_STM32L0_RCC_MSIRANGE};
+
+/// allowed value for MSI frequency, Hz
+constexpr uint32_t msiFrequency {msiFrequencies[msiRangeIndex]};
+
+#endif	// defined(CONFIG_CHIP_STM32L0_RCC_MSI_ENABLE)
+
+#ifdef CONFIG_CHIP_STM32L0_RCC_PLL_ENABLE
+
+/// PLL input frequency, Hz
+#if defined(CONFIG_CHIP_STM32L0_RCC_PLLSRC_HSI16)
+constexpr uint32_t pllInFrequency {hsi16Frequency};
+#elif defined(CONFIG_CHIP_STM32L0_RCC_PLLSRC_HSE)
+constexpr uint32_t pllInFrequency {CONFIG_CHIP_STM32L0_RCC_HSE_FREQUENCY};
+#endif	// defined(CONFIG_CHIP_STM32L0_RCC_PLLSRC_HSE)
+
+static_assert(minPllInFrequency <= pllInFrequency && pllInFrequency <= maxPllInFrequency,
+		"Invalid PLL input frequency!");
+
+/// PLL output frequency, Hz
+constexpr uint32_t pllOutFrequency {(pllInFrequency * CONFIG_CHIP_STM32L0_RCC_PLLMUL) / CONFIG_CHIP_STM32L0_RCC_PLLDIV};
+
+static_assert(minPllOutFrequency <= pllOutFrequency && pllOutFrequency <= maxPllOutFrequency,
+		"Invalid PLL output frequency!");
+
+#endif	// def CONFIG_CHIP_STM32L0_RCC_PLL_ENABLE
+
+/// SYSCLK frequency, Hz
+#if defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_MSI)
+constexpr uint32_t sysclkFrequency {msiFrequency};
+#elif defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_HSI16)
+constexpr uint32_t sysclkFrequency {hsi16Frequency};
+#elif defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_HSE)
+constexpr uint32_t sysclkFrequency {CONFIG_CHIP_STM32L0_RCC_HSE_FREQUENCY};
+#elif defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_PLL)
+constexpr uint32_t sysclkFrequency {pllOutFrequency};
+#endif	// defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_PLL)
+
+#else	// !def CONFIG_CHIP_STM32L0_STANDARD_CLOCK_CONFIGURATION_ENABLE
+
+/// SYSCLK frequency, Hz
+constexpr uint32_t sysclkFrequency {CONFIG_CHIP_STM32L0_RCC_SYSCLK_FREQUENCY};
+
+#endif	// !def CONFIG_CHIP_STM32L0_STANDARD_CLOCK_CONFIGURATION_ENABLE
 
 /// AHB frequency, Hz
 constexpr uint32_t ahbFrequency {sysclkFrequency / CONFIG_CHIP_STM32L0_RCC_HPRE};
