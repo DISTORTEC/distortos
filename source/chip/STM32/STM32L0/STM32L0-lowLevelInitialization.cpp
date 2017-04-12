c8449f334d74623f9bf229defbbead5f226bf0c4
CezaryGapinski <gapa1986@gmail.com>
2017-04-12 19:57:01 +0200

    Update STM32L0's low-level init in preparation for standard clock config

--- source/chip/STM32/STM32L0/STM32L0-lowLevelInitialization.cpp
+++ source/chip/STM32/STM32L0/STM32L0-lowLevelInitialization.cpp
@@ -11,7 +11,10 @@
 
 #include "distortos/chip/lowLevelInitialization.hpp"
 
+#include "distortos/chip/clocks.hpp"
 #include "distortos/chip/STM32L0-FLASH.hpp"
+#include "distortos/chip/STM32L0-PWR.hpp"
+#include "distortos/chip/STM32L0-RCC.hpp"
 
 namespace distortos
 {
@@ -30,6 +33,72 @@ void lowLevelInitialization()
 #else	// !def CONFIG_CHIP_STM32L0_FLASH_PREFETCH_ENABLE
 	configurePrefetchBuffer(false);
 #endif	// !def CONFIG_CHIP_STM32L0_FLASH_PREFETCH_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L0_STANDARD_CLOCK_CONFIGURATION_ENABLE
+
+	configureVoltageScaling(CONFIG_CHIP_STM32L0_PWR_VOLTAGE_SCALE_MODE);
+
+#ifdef CONFIG_CHIP_STM32L0_RCC_MSI_ENABLE
+	enableMsi(CONFIG_CHIP_STM32L0_RCC_MSIRANGE);
+#endif
+
+#ifdef CONFIG_CHIP_STM32L0_RCC_HSE_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L0_RCC_HSE_CLOCK_BYPASS
+	enableHse(true);
+#else	// !def CONFIG_CHIP_STM32L0_RCC_HSE_CLOCK_BYPASS
+	enableHse(false);
+#endif	// !def CONFIG_CHIP_STM32L0_RCC_HSE_CLOCK_BYPASS
+
+#endif	// def CONFIG_CHIP_STM32L0_RCC_HSE_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L0_RCC_HSI16_ENABLE
+	enableHsi16();
+#endif	// def CONFIG_CHIP_STM32L0_RCC_HSI16_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L0_RCC_HSI48_ENABLE
+	enableHsi48();
+#endif	// def CONFIG_CHIP_STM32L0_RCC_HSI48_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L0_RCC_PLL_ENABLE
+
+#if defined(CONFIG_CHIP_STM32L0_RCC_PLLSRC_HSI16)
+	configurePllClockSource(false);
+#elif defined(CONFIG_CHIP_STM32L0_RCC_PLLSRC_HSE)
+	configurePllClockSource(true);
+#endif
+
+	enablePll(CONFIG_CHIP_STM32L0_RCC_PLLMUL, CONFIG_CHIP_STM32L0_RCC_PLLDIV);
+
+#endif	// def CONFIG_CHIP_STM32L0_RCC_PLL_ENABLE
+
+	configureAhbClockDivider(CONFIG_CHIP_STM32L0_RCC_HPRE);
+	configureApbClockDivider(false, CONFIG_CHIP_STM32L0_RCC_PPRE1);
+	configureApbClockDivider(true, CONFIG_CHIP_STM32L0_RCC_PPRE2);
+
+#if CONFIG_CHIP_STM32L0_PWR_VOLTAGE_SCALE_MODE == 3
+	constexpr uint32_t frequencyThreshold {4200000};
+#elif CONFIG_CHIP_STM32L0_PWR_VOLTAGE_SCALE_MODE == 2
+	constexpr uint32_t frequencyThreshold {8000000};
+#else
+	constexpr uint32_t frequencyThreshold {16000000};
+#endif
+
+	constexpr uint8_t flashLatency {(sysclkFrequency - 1) / frequencyThreshold};
+	static_assert(flashLatency <= maxFlashLatency, "Invalid flash latency!");
+	configureFlashLatency(flashLatency);
+
+#if defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_MSI)
+	switchSystemClock(SystemClockSource::msi);
+#elif defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_HSI16)
+	switchSystemClock(SystemClockSource::hsi16);
+#elif defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_HSE)
+	switchSystemClock(SystemClockSource::hse);
+#elif defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_PLL)
+	switchSystemClock(SystemClockSource::pll);
+#endif	// defined(CONFIG_CHIP_STM32L0_RCC_SYSCLK_PLL)
+
+#endif	// def CONFIG_CHIP_STM32L0_STANDARD_CLOCK_CONFIGURATION_ENABLE
 }
 
 }	// namespace chip
