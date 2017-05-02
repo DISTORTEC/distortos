92f40f4ca46d0ec7c60eee1be08e312d8879abd9
CezaryGapinski <gapa1986@gmail.com>
2017-05-02 08:57:28 +0200

    Add generated files for NUCLEO-L073RZ board

---
+++ source/board/STM32/STM32L0/NUCLEO-L073RZ/Tupfile.lua
@@ -0,0 +1,25 @@
+-- File generated with scripts/generateBoard.py:
+-- Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+--
+-- file: Tupfile.lua
+--
+-- author: Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+-- author: Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+-- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Automatically generated file - do not edit!
+--
+-- date: 2017-05-02 08:52:55
+--
+
+if CONFIG_BOARD_NUCLEO_L073RZ == "y" then
+
+	CXXFLAGS += STANDARD_INCLUDES
+	CXXFLAGS += CHIP_INCLUDES
+	CXXFLAGS += BOARD_INCLUDES
+
+	tup.include(DISTORTOS_TOP .. "compile.lua")
+
+end	-- if CONFIG_BOARD_NUCLEO_L073RZ == "y" then
