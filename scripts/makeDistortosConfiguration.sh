4afad84b4385b276e415e098c1b4ca5b52a2f640
Kamil Szczygiel <distortec@distortec.com>
2017-05-05 22:39:10 +0200

    Don't "undefine" kconfig symbols which are not set
    This serves no purpose and just makes the generated header longer.

--- scripts/makeDistortosConfiguration.sh
+++ scripts/makeDistortosConfiguration.sh
@@ -3,7 +3,7 @@
 #
 # file: makeDistortosConfiguration.sh
 #
-# author: Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+# author: Copyright (C) 2016-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -34,7 +34,6 @@ cat << EOF
 $(sed -n \
 	-e 's/^\(CONFIG_[A-Za-z0-9_]\{1,\}\)=y$/#define \1/p' \
 	-e 's/^\(CONFIG_[A-Za-z0-9_]\{1,\}\)=\(.*\)$/#define \1 \2/p' \
-	-e 's/^# \(CONFIG_[A-Za-z0-9_]\{1,\}\) is not set$/#undef \1/p' \
 	"${1}")
 
 #endif	/* INCLUDE_DISTORTOS_DISTORTOSCONFIGURATION_H_ */
