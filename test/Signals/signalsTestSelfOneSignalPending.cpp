9d3af74bbe8db79db0d92af3e373bc467d16daab
Kamil Szczygiel <distortec@distortec.com>
2017-05-06 16:28:33 +0200

    Disable signalsTestSelfOneSignalPending() if signals are disabled

--- test/Signals/signalsTestSelfOneSignalPending.cpp
+++ test/Signals/signalsTestSelfOneSignalPending.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief signalsTestSelfOneSignalPending() definition
  *
- * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2015-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -11,8 +11,14 @@
 
 #include "signalsTestSelfOneSignalPending.hpp"
 
+#include "distortos/distortosConfiguration.h"
+
+#if CONFIG_SIGNALS_ENABLE == 1
+
 #include "distortos/ThisThread-Signals.hpp"
 #include "distortos/SignalSet.hpp"
+
+#endif	// CONFIG_SIGNALS_ENABLE == 1
 
 namespace distortos
 {
@@ -26,11 +32,20 @@ namespace test
 
 bool signalsTestSelfOneSignalPending(const uint8_t signalNumber)
 {
+#if CONFIG_SIGNALS_ENABLE == 1
+
 	SignalSet expectedSignalSet {SignalSet::empty};
 	if (expectedSignalSet.add(signalNumber) != 0)
 		return false;
 
 	return ThisThread::Signals::getPendingSignalSet().getBitset() == expectedSignalSet.getBitset();
+
+#else	// CONFIG_SIGNALS_ENABLE != 1
+
+	static_cast<void>(signalNumber);	// suppress warning
+	return false;
+
+#endif	// CONFIG_SIGNALS_ENABLE != 1
 }
 
 }	// namespace test
