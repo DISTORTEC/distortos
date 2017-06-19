8e2b3f281fd2fbb23b040ca8501eb8f3383442c7
Kamil Szczygiel <distortec@distortec.com>
2017-06-19 23:09:12 +0200

    Move fatalErrorHandler.cpp from source/syscalls/ to source/

--- source/Tupfile.lua
+++ source/Tupfile.lua
@@ -1,10 +1,15 @@
 --
 -- file: Tupfile.lua
 --
--- author: Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+-- author: Copyright (C) 2015-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 --
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
+
+CXXFLAGS += STANDARD_INCLUDES
+CXXFLAGS += ARCHITECTURE_INCLUDES
+
+tup.include(DISTORTOS_TOP .. "compile.lua")
 
 ar(OUTPUT .. "libdistortos.a", "<objects>")
