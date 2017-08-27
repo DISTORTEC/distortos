9e93e70f6a6a7ada5e4a712451810b394e460a6b
Titus von Boxberg <titus@elbe-informatik.de>
2017-08-27 07:56:24 +0200

    Initial board files for STM32F769I-DISCO

---
+++ source/board/STM32/STM32F7/STM32F769IDISCO/README.md
@@ -0,0 +1,24 @@
+STM32F769IDISCO
+================
+
+This folder provides support for [STM32F769IDISCO](http://www.st.com/content/st_com/en/products/evaluation-tools/product-evaluation-tools/mcu-eval-tools/stm32-mcu-eval-tools/stm32-mcu-discovery-kits/32f769idiscovery.html) board,
+also known as *32F769IDISCOVERY*. This board features a *STM32F769I* chip in *BGA216* package:
+- *ARM Cortex-M7* (*ARMv7-M*) core;
+- single precision FPU;
+- 2 MB of flash;
+- 512 kB of RAM;
+- 4 kB of BKPSRAM;
+- 216 MHz max clock;
+
+Buttons
+-------
+
+This board has following buttons:
+- *B1* ("Blue USER & WAKE-UP Button") at *PA0*;
+
+LEDs
+----
+
+This board has following LEDs:
+- *LD1* (red) at *PJ13*;
+- *LD2* (green) at *PJ5*;
---
+++ source/board/STM32/STM32F7/STM32F769IDISCO/STM32F769IDISCO-leds.cpp
@@ -0,0 +1,47 @@
+/**
+ * \file
+ * \brief Definition of LEDs for STM32F769IDISCO
+ *
+ * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+ * \author Copyright (C) 2017 elbe informatik GmbH
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \warning
+ * Automatically generated file - do not edit!
+ */
+
+#include "distortos/board/leds.hpp"
+
+#ifdef CONFIG_BOARD_LEDS_ENABLE
+
+#include "distortos/chip/ChipOutputPin.hpp"
+
+namespace distortos
+{
+
+namespace board
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+chip::ChipOutputPin leds[totalLeds]
+{
+#if DISTORTOS_BOARD_LD1_LED_ENABLE == 1
+		chip::ChipOutputPin{chip::Pin::pj13, false, chip::PinOutputSpeed::low, chip::PinPull::none, false, false},
+#endif	// DISTORTOS_BOARD_LD1_LED_ENABLE == 1
+#if DISTORTOS_BOARD_LD2_LED_ENABLE == 1
+		chip::ChipOutputPin{chip::Pin::pj5, false, chip::PinOutputSpeed::low, chip::PinPull::none, false, false},
+#endif	// DISTORTOS_BOARD_LD2_LED_ENABLE == 1
+};
+
+}	// namespace board
+
+}	// namespace distortos
+
+#endif	// def CONFIG_BOARD_LEDS_ENABLE
