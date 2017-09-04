9ca82ddf76dc9fa9c784a5f0aa0b990c64bf8780
Kamil Szczygiel <distortec@distortec.com>
2017-09-04 21:02:26 +0200

    Regenerate ST_32F746GDISCOVERY from .dts, update config
    The folders were also renamed from 32F746GDISCOVERY to
    ST_32F746GDISCOVERY to be in sync with filenames generated from .dts.

--- source/board/STM32/STM32F7/ST_32F746GDISCOVERY/include/distortos/board/leds.hpp
+++ source/board/STM32/STM32F7/ST_32F746GDISCOVERY/include/distortos/board/leds.hpp
@@ -1,9 +1,8 @@
 /**
  * \file
- * \brief Declaration of LEDs for 32F746GDISCOVERY
+ * \brief Declaration of leds for ST,32F746GDISCOVERY
  *
  * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
- * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -13,29 +12,16 @@
  * Automatically generated file - do not edit!
  */
 
-#ifndef SOURCE_BOARD_STM32_STM32F7_32F746GDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
-#define SOURCE_BOARD_STM32_STM32F7_32F746GDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
-
-#include "distortos/distortosConfiguration.h"
+#ifndef SOURCE_BOARD_STM32_STM32F7_ST_32F746GDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
+#define SOURCE_BOARD_STM32_STM32F7_ST_32F746GDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
 
 #include <cstddef>
 
-/// indicates whether LD1 LED (green) is enabled (1) or not (0)
-#ifdef CONFIG_CHIP_STM32_GPIOV2_GPIOI_ENABLE
-#define DISTORTOS_BOARD_LD1_LED_ENABLE		1
-#else	// !def CONFIG_CHIP_STM32_GPIOV2_GPIOI_ENABLE
-#define DISTORTOS_BOARD_LD1_LED_ENABLE		0
-#endif	// !def CONFIG_CHIP_STM32_GPIOV2_GPIOI_ENABLE
-
 /// total number of leds on the board
-#define DISTORTOS_BOARD_TOTAL_LEDS		( \
-		DISTORTOS_BOARD_LD1_LED_ENABLE + \
-		0)
+#define DISTORTOS_BOARD_TOTAL_LEDS		(1)
 
 namespace distortos
 {
-
-#ifdef CONFIG_BOARD_LEDS_ENABLE
 
 namespace chip
 {
@@ -44,48 +30,38 @@ class ChipOutputPin;
 
 }	// namespace chip
 
-#endif	// def CONFIG_BOARD_LEDS_ENABLE
-
 namespace board
 {
 
-/// total number of LEDs on the board
+/// total number of leds on the board
 constexpr size_t totalLeds {DISTORTOS_BOARD_TOTAL_LEDS};
 
 /*---------------------------------------------------------------------------------------------------------------------+
-| LED indexes
+| indexes of leds
 +---------------------------------------------------------------------------------------------------------------------*/
 
 enum
 {
-#if DISTORTOS_BOARD_LD1_LED_ENABLE == 1
-		/// index of LD1 LED (green)
+		/// index of ld1 LED (green)
 		ld1LedIndex,
-#endif	// DISTORTOS_BOARD_LD1_LED_ENABLE == 1
 };
 
-#ifdef CONFIG_BOARD_LEDS_ENABLE
-
 /*---------------------------------------------------------------------------------------------------------------------+
-| alternative (color-based) LED indexes
+| alternative (label-based) indexes of leds
 +---------------------------------------------------------------------------------------------------------------------*/
 
-#if DISTORTOS_BOARD_LD1_LED_ENABLE == 1
-/// alternative index of LD1 LED (green)
+/// alternative index of ld1 LED (green)
 constexpr size_t greenLedIndex {ld1LedIndex};
-#endif	// DISTORTOS_BOARD_LD1_LED_ENABLE == 1
 
 /*---------------------------------------------------------------------------------------------------------------------+
-| indexed access to LED objects
+| indexed access to leds objects
 +---------------------------------------------------------------------------------------------------------------------*/
 
-/// array with all LED objects
+/// array with all leds objects
 extern chip::ChipOutputPin leds[totalLeds];
-
-#endif	// def CONFIG_BOARD_LEDS_ENABLE
 
 }	// namespace board
 
 }	// namespace distortos
 
-#endif	// SOURCE_BOARD_STM32_STM32F7_32F746GDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
+#endif	// SOURCE_BOARD_STM32_STM32F7_ST_32F746GDISCOVERY_INCLUDE_DISTORTOS_BOARD_LEDS_HPP_
