2d951b9934b6d526ad994a45b394578618aa1590
Kamil Szczygiel <distortec@distortec.com>
2017-09-04 20:13:40 +0200

    Regenerate ST_NUCLEO-F091RC from .dts, update config
    The folders were also renamed from NUCLEO-F091RC to ST_NUCLEO-F091RC to
    be in sync with filenames generated from .dts.

--- source/board/STM32/STM32F0/ST_NUCLEO-F091RC/include/distortos/board/buttons.hpp
+++ source/board/STM32/STM32F0/ST_NUCLEO-F091RC/include/distortos/board/buttons.hpp
@@ -1,9 +1,8 @@
 /**
  * \file
- * \brief Declaration of buttons for NUCLEO-F091RC
+ * \brief Declaration of buttons for ST,NUCLEO-F091RC
  *
  * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
- * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -13,29 +12,16 @@
  * Automatically generated file - do not edit!
  */
 
-#ifndef SOURCE_BOARD_STM32_STM32F0_NUCLEO_F091RC_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
-#define SOURCE_BOARD_STM32_STM32F0_NUCLEO_F091RC_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
-
-#include "distortos/distortosConfiguration.h"
+#ifndef SOURCE_BOARD_STM32_STM32F0_ST_NUCLEO_F091RC_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
+#define SOURCE_BOARD_STM32_STM32F0_ST_NUCLEO_F091RC_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
 
 #include <cstddef>
 
-/// indicates whether B1 button is enabled (1) or not (0)
-#ifdef CONFIG_CHIP_STM32_GPIOV2_GPIOC_ENABLE
-#define DISTORTOS_BOARD_B1_BUTTON_ENABLE	1
-#else	// !def CONFIG_CHIP_STM32_GPIOV2_GPIOC_ENABLE
-#define DISTORTOS_BOARD_B1_BUTTON_ENABLE	0
-#endif	// !def CONFIG_CHIP_STM32_GPIOV2_GPIOC_ENABLE
-
 /// total number of buttons on the board
-#define DISTORTOS_BOARD_TOTAL_BUTTONS		( \
-		DISTORTOS_BOARD_B1_BUTTON_ENABLE + \
-		0)
+#define DISTORTOS_BOARD_TOTAL_BUTTONS		(1)
 
 namespace distortos
 {
-
-#ifdef CONFIG_BOARD_BUTTONS_ENABLE
 
 namespace chip
 {
@@ -44,8 +30,6 @@ class ChipInputPin;
 
 }	// namespace chip
 
-#endif	// def CONFIG_BOARD_BUTTONS_ENABLE
-
 namespace board
 {
 
@@ -53,30 +37,31 @@ namespace board
 constexpr size_t totalButtons {DISTORTOS_BOARD_TOTAL_BUTTONS};
 
 /*---------------------------------------------------------------------------------------------------------------------+
-| button indexes
+| indexes of buttons
 +---------------------------------------------------------------------------------------------------------------------*/
 
 enum
 {
-#if DISTORTOS_BOARD_B1_BUTTON_ENABLE == 1
-		/// index of B1 button
+		/// index of b1 button (user)
 		b1ButtonIndex,
-#endif	// DISTORTOS_BOARD_B1_BUTTON_ENABLE == 1
 };
 
-#ifdef CONFIG_BOARD_BUTTONS_ENABLE
-
 /*---------------------------------------------------------------------------------------------------------------------+
-| indexed access to button objects
+| alternative (label-based) indexes of buttons
 +---------------------------------------------------------------------------------------------------------------------*/
 
-/// array with all button objects
-extern const chip::ChipInputPin buttons[totalButtons];
+/// alternative index of b1 button (user)
+constexpr size_t userButtonIndex {b1ButtonIndex};
 
-#endif	// def CONFIG_BOARD_BUTTONS_ENABLE
+/*---------------------------------------------------------------------------------------------------------------------+
+| indexed access to buttons objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// array with all buttons objects
+extern const chip::ChipInputPin buttons[totalButtons];
 
 }	// namespace board
 
 }	// namespace distortos
 
-#endif	// SOURCE_BOARD_STM32_STM32F0_NUCLEO_F091RC_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
+#endif	// SOURCE_BOARD_STM32_STM32F0_ST_NUCLEO_F091RC_INCLUDE_DISTORTOS_BOARD_BUTTONS_HPP_
