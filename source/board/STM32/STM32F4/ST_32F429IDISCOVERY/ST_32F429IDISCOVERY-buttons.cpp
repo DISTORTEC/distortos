90ef9b62d547cb5f6e6757f977199fbb5b649c9b
Kamil Szczygiel <distortec@distortec.com>
2017-09-04 20:25:07 +0200

    Regenerate ST_32F429IDISCOVERY from .dts, update config
    The folders were also renamed from 32F429IDISCOVERY to
    ST_32F429IDISCOVERY to be in sync with filenames generated from .dts.

--- source/board/STM32/STM32F4/ST_32F429IDISCOVERY/ST_32F429IDISCOVERY-buttons.cpp
+++ source/board/STM32/STM32F4/ST_32F429IDISCOVERY/ST_32F429IDISCOVERY-buttons.cpp
@@ -1,9 +1,8 @@
 /**
  * \file
- * \brief Definition of LEDs for 32F429IDISCOVERY
+ * \brief Definition of buttons for ST,32F429IDISCOVERY
  *
  * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
- * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -13,11 +12,9 @@
  * Automatically generated file - do not edit!
  */
 
-#include "distortos/board/leds.hpp"
+#include "distortos/board/buttons.hpp"
 
-#ifdef CONFIG_BOARD_LEDS_ENABLE
-
-#include "distortos/chip/ChipOutputPin.hpp"
+#include "distortos/chip/ChipInputPin.hpp"
 
 namespace distortos
 {
@@ -29,18 +26,11 @@ namespace board
 | global objects
 +---------------------------------------------------------------------------------------------------------------------*/
 
-chip::ChipOutputPin leds[totalLeds]
+const chip::ChipInputPin buttons[totalButtons]
 {
-#if DISTORTOS_BOARD_LD3_LED_ENABLE == 1
-		chip::ChipOutputPin{chip::Pin::pg13, false, chip::PinOutputSpeed::low, chip::PinPull::none, false, false},
-#endif	// DISTORTOS_BOARD_LD3_LED_ENABLE == 1
-#if DISTORTOS_BOARD_LD4_LED_ENABLE == 1
-		chip::ChipOutputPin{chip::Pin::pg14, false, chip::PinOutputSpeed::low, chip::PinPull::none, false, false},
-#endif	// DISTORTOS_BOARD_LD4_LED_ENABLE == 1
+		chip::ChipInputPin{chip::Pin::pa0, chip::PinPull::none, false},
 };
 
 }	// namespace board
 
 }	// namespace distortos
-
-#endif	// def CONFIG_BOARD_LEDS_ENABLE
