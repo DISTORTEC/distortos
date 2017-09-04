2ad432a2860e5d254492bbe0fe3b4c9c29419a1b
Kamil Szczygiel <distortec@distortec.com>
2017-09-04 20:46:19 +0200

    Regenerate ST_NUCLEO-F429ZI from .dts, update config
    The folders were also renamed from NUCLEO-F429ZI to ST_NUCLEO-F429ZI to
    be in sync with filenames generated from .dts.

--- source/board/STM32/STM32F4/ST_NUCLEO-F429ZI/ST_NUCLEO-F429ZI-buttons.cpp
+++ source/board/STM32/STM32F4/ST_NUCLEO-F429ZI/ST_NUCLEO-F429ZI-buttons.cpp
@@ -1,9 +1,8 @@
 /**
  * \file
- * \brief Definition of buttons for NUCLEO-F429ZI
+ * \brief Definition of buttons for ST,NUCLEO-F429ZI
  *
  * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
- * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -14,8 +13,6 @@
  */
 
 #include "distortos/board/buttons.hpp"
-
-#ifdef CONFIG_BOARD_BUTTONS_ENABLE
 
 #include "distortos/chip/ChipInputPin.hpp"
 
@@ -31,13 +28,9 @@ namespace board
 
 const chip::ChipInputPin buttons[totalButtons]
 {
-#if DISTORTOS_BOARD_B1_BUTTON_ENABLE == 1
 		chip::ChipInputPin{chip::Pin::pc13, chip::PinPull::none, false},
-#endif	// DISTORTOS_BOARD_B1_BUTTON_ENABLE == 1
 };
 
 }	// namespace board
 
 }	// namespace distortos
-
-#endif	// def CONFIG_BOARD_BUTTONS_ENABLE
--- source/board/STM32/STM32F4/ST_NUCLEO-F429ZI/ST_NUCLEO-F429ZI-leds.cpp
+++ source/board/STM32/STM32F4/ST_NUCLEO-F429ZI/ST_NUCLEO-F429ZI-leds.cpp
@@ -1,9 +1,8 @@
 /**
  * \file
- * \brief Definition of LEDs for NUCLEO-F429ZI
+ * \brief Definition of leds for ST,NUCLEO-F429ZI
  *
  * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
- * \author Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -14,8 +13,6 @@
  */
 
 #include "distortos/board/leds.hpp"
-
-#ifdef CONFIG_BOARD_LEDS_ENABLE
 
 #include "distortos/chip/ChipOutputPin.hpp"
 
@@ -31,19 +28,11 @@ namespace board
 
 chip::ChipOutputPin leds[totalLeds]
 {
-#if DISTORTOS_BOARD_LD1_LED_ENABLE == 1
 		chip::ChipOutputPin{chip::Pin::pb0, false, chip::PinOutputSpeed::low, chip::PinPull::none, false, false},
-#endif	// DISTORTOS_BOARD_LD1_LED_ENABLE == 1
-#if DISTORTOS_BOARD_LD2_LED_ENABLE == 1
 		chip::ChipOutputPin{chip::Pin::pb7, false, chip::PinOutputSpeed::low, chip::PinPull::none, false, false},
-#endif	// DISTORTOS_BOARD_LD2_LED_ENABLE == 1
-#if DISTORTOS_BOARD_LD3_LED_ENABLE == 1
 		chip::ChipOutputPin{chip::Pin::pb14, false, chip::PinOutputSpeed::low, chip::PinPull::none, false, false},
-#endif	// DISTORTOS_BOARD_LD3_LED_ENABLE == 1
 };
 
 }	// namespace board
 
 }	// namespace distortos
-
-#endif	// def CONFIG_BOARD_LEDS_ENABLE
