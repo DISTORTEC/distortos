591efb6035906c2427ff6e0354dbd0066f3b9329
Kamil Szczygiel <distortec@distortec.com>
2017-09-04 23:18:15 +0200

    Use `find -L ...` to search for configurations
    distortos subfolder can be a symbolic link, in which case `find`
    withouth `-L` will find nothing.

--- scripts/forAllConfigurations.sh
+++ scripts/forAllConfigurations.sh
@@ -3,7 +3,7 @@
 #
 # file: forAllConfigurations.sh
 #
-# author: Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+# author: Copyright (C) 2016-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -28,7 +28,7 @@ fi
 
 make distclean
 
-for configurationPath in $(find "${searchPath}" -name 'distortosConfiguration.mk' -printf '%h ')
+for configurationPath in $(find -L "${searchPath}" -name 'distortosConfiguration.mk' -printf '%h ')
 do
 
 	make configure "CONFIG_PATH=${configurationPath}"
