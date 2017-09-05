52fccbd0b3a4911595845d6e75df6e04bdc79f7e
Kamil Szczygiel <distortec@distortec.com>
2017-09-05 18:33:14 +0200

    Make ThreadListNode's constructor explicit
    Fixes codacy issue "Class 'ThreadListNode' has a constructor with 1
    argument that is not explicit.".

--- include/distortos/internal/scheduler/ThreadListNode.hpp
+++ include/distortos/internal/scheduler/ThreadListNode.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief ThreadListNode class header
  *
- * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2015-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -37,7 +37,7 @@ public:
 	 * \param [in] priority is the thread's priority, 0 - lowest, UINT8_MAX - highest
 	 */
 
-	constexpr ThreadListNode(const uint8_t priority) :
+	constexpr explicit ThreadListNode(const uint8_t priority) :
 			threadListNode{},
 			threadGroupNode{},
 			priority_{priority},
