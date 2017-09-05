5d03f49a19876710f3449664220a1a5394352354
Kamil Szczygiel <distortec@distortec.com>
2017-09-05 18:38:44 +0200

    Make SortedIntrusiveForwardList's constructor explicit
    Fixes codacy issue "Class 'SortedIntrusiveForwardList's' has a
    constructor with 1 argument that is not explicit.".

--- include/estd/SortedIntrusiveForwardList.hpp
+++ include/estd/SortedIntrusiveForwardList.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief SortedIntrusiveForwardList template class header
  *
- * \author Copyright (C) 2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2015-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -71,7 +71,7 @@ public:
 	 * \param [in] compare is a reference to Compare object used to copy-construct internal comparison functor
 	 */
 
-	constexpr SortedIntrusiveForwardList(const Compare& compare = Compare{}) :
+	constexpr explicit SortedIntrusiveForwardList(const Compare& compare = Compare{}) :
 			implementation_{compare}
 	{
 
