4020e2b9cbc4c97f5f1a4e406cb2ed6965281cb1
Kamil Szczygiel <distortec@distortec.com>
2017-09-26 20:58:07 +0200

    Add mock distortosConfiguration.h

---
+++ unit-test/include-mocks/distortos/distortosConfiguration.h
@@ -0,0 +1,17 @@
+/**
+ * \file
+ * \brief Mock distortos configuration
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef UNIT_TEST_INCLUDE_MOCKS_DISTORTOS_DISTORTOSCONFIGURATION_H_
+#define UNIT_TEST_INCLUDE_MOCKS_DISTORTOS_DISTORTOSCONFIGURATION_H_
+
+#define CONFIG_TICK_FREQUENCY 1000
+
+#endif	/* UNIT_TEST_INCLUDE_MOCKS_DISTORTOS_DISTORTOSCONFIGURATION_H_ */
