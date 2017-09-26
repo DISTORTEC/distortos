c913c5ec3b75441c7312c6ba40870499636d1b66
Kamil Szczygiel <distortec@distortec.com>
2017-09-26 20:57:41 +0200

    Add unit-test-common.hpp

---
+++ unit-test/unit-test-common.hpp
@@ -0,0 +1,20 @@
+/**
+ * \file
+ * \brief Common header for unit test
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef UNIT_TEST_UNIT_TEST_COMMON_HPP_
+#define UNIT_TEST_UNIT_TEST_COMMON_HPP_
+
+#include "catch.hpp"
+#include "trompeloeil.hpp"
+
+extern template struct trompeloeil::reporter<trompeloeil::specialized>;
+
+#endif	// UNIT_TEST_UNIT_TEST_COMMON_HPP_
