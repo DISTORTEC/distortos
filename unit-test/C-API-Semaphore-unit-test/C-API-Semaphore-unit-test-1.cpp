1f1f1b95a2d6276d171a3d4e404d5e5b7f4d03cc
Kamil Szczygiel <distortec@distortec.com>
2017-09-27 23:59:49 +0200

    Add C-API-Semaphore-unit-test-1
    This unit test focuses only on comparing whether distortos_Semaphore
    initialized by various C-API functions/macros is binary identical to
    distortos::Semaphore which would be propertly constructed.

---
+++ unit-test/C-API-Semaphore-unit-test/C-API-Semaphore-unit-test-1.cpp
@@ -0,0 +1,113 @@
+/**
+ * \file
+ * \brief Semaphore C-API test cases
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/Semaphore.hpp"
+#include "distortos/C-API/Semaphore.h"
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void testCommon(distortos_Semaphore& semaphore, const unsigned int value, const unsigned int maxValue = UINT_MAX)
+{
+	REQUIRE(semaphore.value == std::min(value, maxValue));
+	REQUIRE(semaphore.maxValue == maxValue);
+	distortos_Semaphore constructed;
+	memcpy(&constructed, &semaphore, sizeof(semaphore));
+	REQUIRE(distortos_Semaphore_destruct(&semaphore) == 0);
+	struct distortos_Semaphore destructed;
+	memcpy(&destructed, &semaphore, sizeof(semaphore));
+
+	memset(&semaphore, 0, sizeof(semaphore));
+
+	const auto realSemaphore = new (&semaphore) distortos::Semaphore {value, maxValue};
+	REQUIRE(realSemaphore->getValue() == std::min(value, maxValue));
+	REQUIRE(memcmp(&constructed, &semaphore, sizeof(semaphore)) == 0);
+	realSemaphore->~Semaphore();
+	REQUIRE(memcmp(&destructed, &semaphore, sizeof(semaphore)) == 0);
+}
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global test cases
++---------------------------------------------------------------------------------------------------------------------*/
+
+TEST_CASE("Testing DISTORTOS_SEMAPHORE_INITIALIZER()", "[initializer]")
+{
+	constexpr unsigned int lowRandomValue {0x2c380555};
+	constexpr unsigned int highRandomValue {0x8d87de63};
+
+	{
+		distortos_Semaphore semaphore = DISTORTOS_SEMAPHORE_INITIALIZER(semaphore, lowRandomValue, highRandomValue);
+		testCommon(semaphore, lowRandomValue, highRandomValue);
+	}
+	{
+		distortos_Semaphore semaphore = DISTORTOS_SEMAPHORE_INITIALIZER(semaphore, highRandomValue, lowRandomValue);
+		testCommon(semaphore, highRandomValue, lowRandomValue);
+	}
+}
+
+TEST_CASE("Testing DISTORTOS_SEMAPHORE_CONSTRUCT_1()", "[construct]")
+{
+	constexpr unsigned int lowRandomValue {0x0dc449b6};
+	constexpr unsigned int highRandomValue {0x726030cd};
+
+	{
+		DISTORTOS_SEMAPHORE_CONSTRUCT_1(semaphore, lowRandomValue, highRandomValue);
+		testCommon(semaphore, lowRandomValue, highRandomValue);
+	}
+	{
+		DISTORTOS_SEMAPHORE_CONSTRUCT_1(semaphore, highRandomValue, lowRandomValue);
+		testCommon(semaphore, highRandomValue, lowRandomValue);
+	}
+}
+
+TEST_CASE("Testing DISTORTOS_SEMAPHORE_CONSTRUCT()", "[construct]")
+{
+	constexpr unsigned int randomValue {0x5d0051dc};
+
+	{
+		DISTORTOS_SEMAPHORE_CONSTRUCT(semaphore, randomValue);
+		testCommon(semaphore, randomValue);
+	}
+}
+
+TEST_CASE("Testing distortos_Semaphore_construct_1()", "[construct]")
+{
+	constexpr unsigned int lowRandomValue {0x7a78032b};
+	constexpr unsigned int highRandomValue {0xb24e4367};
+
+	{
+		distortos_Semaphore semaphore;
+		REQUIRE(distortos_Semaphore_construct_1(&semaphore, lowRandomValue, highRandomValue) == 0);
+		testCommon(semaphore, lowRandomValue, highRandomValue);
+	}
+	{
+		distortos_Semaphore semaphore;
+		REQUIRE(distortos_Semaphore_construct_1(&semaphore, highRandomValue, lowRandomValue) == 0);
+		testCommon(semaphore, highRandomValue, lowRandomValue);
+	}
+}
+
+TEST_CASE("Testing distortos_Semaphore_construct()", "[construct]")
+{
+	constexpr unsigned int randomValue {0xb86c251b};
+
+	{
+		distortos_Semaphore semaphore;
+		REQUIRE(distortos_Semaphore_construct(&semaphore, randomValue) == 0);
+		testCommon(semaphore, randomValue);
+	}
+}
