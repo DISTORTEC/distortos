a439ffd5b2b6cc6c498bebe8f1c5f8390c524059
Kamil Szczygiel <distortec@distortec.com>
2017-10-09 22:37:56 +0200

    Check deallocation in SoftwareTimerOrderingTestCase

--- test/SoftwareTimer/SoftwareTimerOrderingTestCase.cpp
+++ test/SoftwareTimer/SoftwareTimerOrderingTestCase.cpp
@@ -17,6 +17,8 @@
 
 #include "distortos/DynamicSoftwareTimer.hpp"
 
+#include <malloc.h>
+
 namespace distortos
 {
 
@@ -31,36 +33,43 @@ bool SoftwareTimerOrderingTestCase::run_() const
 {
 	constexpr auto totalSoftwareTimers = totalThreads;
 
+	const auto allocatedMemory = mallinfo().uordblks;
+
 	for (const auto& phase : priorityTestPhases)
 	{
-		SequenceAsserter sequenceAsserter;
-		std::array<DynamicSoftwareTimer, totalSoftwareTimers> softwareTimers
-		{{
-				{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[0]].second},
-				{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[1]].second},
-				{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[2]].second},
-				{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[3]].second},
-				{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[4]].second},
-				{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[5]].second},
-				{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[6]].second},
-				{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[7]].second},
-		}};
+		{
+			SequenceAsserter sequenceAsserter;
+			std::array<DynamicSoftwareTimer, totalSoftwareTimers> softwareTimers
+			{{
+					{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[0]].second},
+					{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[1]].second},
+					{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[2]].second},
+					{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[3]].second},
+					{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[4]].second},
+					{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[5]].second},
+					{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[6]].second},
+					{&SequenceAsserter::sequencePoint, std::ref(sequenceAsserter), phase.first[phase.second[7]].second},
+			}};
 
-		waitForNextTick();
+			waitForNextTick();
 
-		for (size_t i = 0; i < softwareTimers.size(); ++i)
-			softwareTimers[i].start(TickClock::duration{maxPhasePriority + 1 - phase.first[phase.second[i]].first});
+			for (size_t i = 0; i < softwareTimers.size(); ++i)
+				softwareTimers[i].start(TickClock::duration{maxPhasePriority + 1 - phase.first[phase.second[i]].first});
 
-		if (sequenceAsserter.assertSequence(0) == false)
-			return false;
+			if (sequenceAsserter.assertSequence(0) == false)
+				return false;
 
-		for (const auto& softwareTimer : softwareTimers)
-			while(softwareTimer.isRunning())
-			{
+			for (const auto& softwareTimer : softwareTimers)
+				while(softwareTimer.isRunning())
+				{
 
-			}
+				}
 
-		if (sequenceAsserter.assertSequence(totalSoftwareTimers) == false)
+			if (sequenceAsserter.assertSequence(totalSoftwareTimers) == false)
+				return false;
+		}
+
+		if (mallinfo().uordblks != allocatedMemory)	// dynamic memory must be deallocated after each test phase
 			return false;
 	}
 
