b4567f3bfb475b58fccc7977196b16e120945298
Kamil Szczygiel <distortec@distortec.com>
2017-10-22 19:02:57 +0200

    Add ThreadIdentifier class

---
+++ source/threads/ThreadIdentifier.cpp
@@ -0,0 +1,32 @@
+/**
+ * \file
+ * \brief ThreadIdentifier class implementation
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/ThreadIdentifier.hpp"
+
+#include "distortos/internal/scheduler/RunnableThread.hpp"
+#include "distortos/internal/scheduler/ThreadControlBlock.hpp"
+
+namespace distortos
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| public functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+Thread* ThreadIdentifier::getThread() const
+{
+	if (threadControlBlock_ == nullptr || threadControlBlock_->getSequenceNumber() != sequenceNumber_)
+		return nullptr;
+
+	return &threadControlBlock_->getOwner();
+}
+
+}	// namespace distortos
