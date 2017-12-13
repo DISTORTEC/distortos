7ff8d705539ce7f7b8de8950db7cf8b279a8ef02
Kamil Szczygiel <distortec@distortec.com>
2017-12-13 18:24:21 +0100

    Add basic clocks.hpp for STM32L4

---
+++ source/chip/STM32/STM32L4/include/distortos/chip/clocks.hpp
@@ -0,0 +1,55 @@
+/**
+ * \file
+ * \brief Definitions of clocks for STM32L4
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOURCE_CHIP_STM32_STM32L4_INCLUDE_DISTORTOS_CHIP_CLOCKS_HPP_
+#define SOURCE_CHIP_STM32_STM32L4_INCLUDE_DISTORTOS_CHIP_CLOCKS_HPP_
+
+#include "distortos/distortosConfiguration.h"
+
+#include <cstdint>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global constants
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// maximum allowed APB1 (low speed) frequency, Hz
+constexpr uint32_t maxApb1Frequency {80000000};
+
+/// maximum allowed APB2 (high speed) frequency, Hz
+constexpr uint32_t maxApb2Frequency {80000000};
+
+/// SYSCLK frequency, Hz
+constexpr uint32_t sysclkFrequency {CONFIG_CHIP_STM32L4_RCC_SYSCLK_FREQUENCY};
+
+/// AHB frequency, Hz
+constexpr uint32_t ahbFrequency {sysclkFrequency / CONFIG_CHIP_STM32L4_RCC_HPRE};
+
+/// APB1 frequency, Hz
+constexpr uint32_t apb1Frequency {ahbFrequency / CONFIG_CHIP_STM32L4_RCC_PPRE1};
+
+static_assert(apb1Frequency <= maxApb1Frequency, "Invalid APB1 (low speed) frequency!");
+
+/// APB2 frequency, Hz
+constexpr uint32_t apb2Frequency {ahbFrequency / CONFIG_CHIP_STM32L4_RCC_PPRE2};
+
+static_assert(apb2Frequency <= maxApb2Frequency, "Invalid APB2 (high speed) frequency!");
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_CHIP_STM32_STM32L4_INCLUDE_DISTORTOS_CHIP_CLOCKS_HPP_
