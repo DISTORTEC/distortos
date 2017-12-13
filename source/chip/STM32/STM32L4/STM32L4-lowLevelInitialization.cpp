78cd9badc11250cae5a1e1ba2ffae345d2eb8cf8
Kamil Szczygiel <distortec@distortec.com>
2017-12-13 22:13:20 +0100

    Add empty chip::lowLevelInitialization() for STM32L4

---
+++ source/chip/STM32/STM32L4/STM32L4-lowLevelInitialization.cpp
@@ -0,0 +1,31 @@
+/**
+ * \file
+ * \brief chip::lowLevelInitialization() implementation for STM32L4
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/lowLevelInitialization.hpp"
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+void lowLevelInitialization()
+{
+
+}
+
+}	// namespace chip
+
+}	// namespace distortos
