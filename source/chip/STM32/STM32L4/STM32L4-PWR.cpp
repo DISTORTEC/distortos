38207c20380f6391830ef15b3120fe94d3662652
Kamil Szczygiel <distortec@distortec.com>
2017-12-23 19:10:19 +0100

    Add STM32L4-PWR module

---
+++ source/chip/STM32/STM32L4/STM32L4-PWR.cpp
@@ -0,0 +1,41 @@
+/**
+ * \file
+ * \brief Implementation of PWR-related functions for STM32L4
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/STM32L4-PWR.hpp"
+
+#include "distortos/chip/CMSIS-proxy.h"
+#include "distortos/chip/STM32-bit-banding.h"
+
+#include <cerrno>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+int configureVoltageScaling(const uint8_t voltageScale)
+{
+	if (voltageScale < minVoltageScale || voltageScale > maxVoltageScale)
+		return EINVAL;
+
+	PWR->CR1 = (PWR->CR1 & ~PWR_CR1_VOS) | ((voltageScale << PWR_CR1_VOS_Pos) & PWR_CR1_VOS);
+	while (STM32_BITBAND(PWR, SR2, VOSF) != 0);
+	return 0;
+}
+
+}	// namespace chip
+
+}	// namespace distortos
---
+++ source/chip/STM32/STM32L4/include/distortos/chip/STM32L4-PWR.hpp
@@ -0,0 +1,57 @@
+/**
+ * \file
+ * \brief Header for PWR-related functions for STM32L4
+ *
+ * \author Copyright (C) 2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOURCE_CHIP_STM32_STM32L4_INCLUDE_DISTORTOS_CHIP_STM32L4_PWR_HPP_
+#define SOURCE_CHIP_STM32_STM32L4_INCLUDE_DISTORTOS_CHIP_STM32L4_PWR_HPP_
+
+#include <cstdint>
+
+namespace distortos
+{
+
+namespace chip
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global constants
++---------------------------------------------------------------------------------------------------------------------*/
+
+/// minimum allowed value of voltage scale mode
+constexpr uint8_t minVoltageScale {1};
+
+/// maximum allowed value of voltage scale mode
+constexpr uint8_t maxVoltageScale {2};
+
+/// default voltage scale mode after reset
+constexpr uint8_t defaultVoltageScale {1};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions' declarations
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Configures voltage scaling.
+ *
+ * This function waits until selected voltage level is reached.
+ *
+ * \param [in] voltageScale is the new voltage scale mode, [minVoltageScale; maxVoltageScale]
+ *
+ * \return 0 on success, error code otherwise:
+ * - EINVAL - \a voltageScale value is invalid;
+ */
+
+int configureVoltageScaling(uint8_t voltageScale);
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_CHIP_STM32_STM32L4_INCLUDE_DISTORTOS_CHIP_STM32L4_PWR_HPP_
