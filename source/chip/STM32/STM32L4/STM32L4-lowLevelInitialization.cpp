b1b1f0de16865ab5f445e7835eb7255916d034ef
Kamil Szczygiel <distortec@distortec.com>
2018-01-05 23:27:29 +0100

    Extend STM32L4's lowLevelInitialization() with clock configuration

--- source/chip/STM32/STM32L4/STM32L4-lowLevelInitialization.cpp
+++ source/chip/STM32/STM32L4/STM32L4-lowLevelInitialization.cpp
@@ -11,9 +11,14 @@
 
 #include "distortos/chip/lowLevelInitialization.hpp"
 
+#include "distortos/chip/clocks.hpp"
+#include "distortos/chip/CMSIS-proxy.h"
+#include "distortos/chip/STM32-bit-banding.h"
 #include "distortos/chip/STM32L4-FLASH.hpp"
+#include "distortos/chip/STM32L4-PWR.hpp"
+#include "distortos/chip/STM32L4-RCC.hpp"
 
-#include "distortos/distortosConfiguration.h"
+#include <algorithm>
 
 namespace distortos
 {
@@ -44,6 +49,78 @@ void lowLevelInitialization()
 #else	// !def CONFIG_CHIP_STM32L4_FLASH_INSTRUCTION_CACHE_ENABLE
 	disableInstructionCache();
 #endif	// !def CONFIG_CHIP_STM32L4_FLASH_INSTRUCTION_CACHE_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L4_STANDARD_CLOCK_CONFIGURATION_ENABLE
+
+	STM32_BITBAND(RCC, APB1ENR1, PWREN) = 1;
+
+	configureVoltageScaling(CONFIG_CHIP_STM32L4_PWR_VOLTAGE_SCALING_RANGE);
+
+#ifdef CONFIG_CHIP_STM32L4_RCC_HSE_ENABLE
+	enableHse(hseClockBypass);
+#endif	// def CONFIG_CHIP_STM32L4_RCC_HSE_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L4_RCC_HSI16_ENABLE
+	enableHsi16();
+#endif	// def CONFIG_CHIP_STM32L4_RCC_HSI16_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L4_RCC_MSI_ENABLE
+	enableMsi(CONFIG_CHIP_STM32L4_RCC_MSI_RANGE);
+#endif	// def CONFIG_CHIP_STM32L4_RCC_MSI_ENABLE
+
+#ifdef CONFIG_CHIP_STM32L4_RCC_PLL_ENABLE
+
+#if defined(CONFIG_CHIP_STM32L4_RCC_PLLSRC_HSE)
+	configurePllClockSource(PllClockSource::hse);
+#elif defined(CONFIG_CHIP_STM32L4_RCC_PLLSRC_HSI16)
+	configurePllClockSource(PllClockSource::hsi16);
+#elif defined(CONFIG_CHIP_STM32L4_RCC_PLLSRC_MSI)
+	configurePllClockSource(PllClockSource::msi);
+#endif	// defined(CONFIG_CHIP_STM32L4_RCC_PLLSRC_MSI)
+
+	configurePllInputClockDivider(CONFIG_CHIP_STM32L4_RCC_PLLM);
+	enablePll(CONFIG_CHIP_STM32L4_RCC_PLLN, CONFIG_CHIP_STM32L4_RCC_PLLP, CONFIG_CHIP_STM32L4_RCC_PLLQ,
+			CONFIG_CHIP_STM32L4_RCC_PLLR);
+
+#ifdef CONFIG_CHIP_STM32L4_RCC_PLLP_ENABLE
+	enablePllOutput(PllOutput::p, true);
+#endif	// def CONFIG_CHIP_STM32L4_RCC_PLLP_ENABLE
+#ifdef CONFIG_CHIP_STM32L4_RCC_PLLQ_ENABLE
+	enablePllOutput(PllOutput::q, true);
+#endif	// def CONFIG_CHIP_STM32L4_RCC_PLLQ_ENABLE
+#ifdef CONFIG_CHIP_STM32L4_RCC_PLLR_ENABLE
+	enablePllOutput(PllOutput::r, true);
+#endif	// def CONFIG_CHIP_STM32L4_RCC_PLLR_ENABLE
+
+#endif	// def CONFIG_CHIP_STM32L4_RCC_PLL_ENABLE
+
+	configureAhbClockDivider(CONFIG_CHIP_STM32L4_RCC_HPRE);
+	configureApbClockDivider(false, CONFIG_CHIP_STM32L4_RCC_PPRE1);
+	configureApbClockDivider(true, CONFIG_CHIP_STM32L4_RCC_PPRE2);
+
+#if CONFIG_CHIP_STM32L4_PWR_VOLTAGE_SCALING_RANGE == 1
+	constexpr uint32_t frequencyThreshold {16000000};
+	constexpr uint8_t flashLatencyUpperBound {4};
+#elif CONFIG_CHIP_STM32L4_PWR_VOLTAGE_SCALING_RANGE == 2
+	constexpr uint32_t frequencyThreshold {6000000};
+	constexpr uint8_t flashLatencyUpperBound {3};
+#endif
+
+	constexpr uint8_t flashLatency {(ahbFrequency - 1) / frequencyThreshold};
+	static_assert(flashLatency <= maxFlashLatency, "Invalid flash latency!");
+	configureFlashLatency(std::min(flashLatency, flashLatencyUpperBound));
+
+#if defined(CONFIG_CHIP_STM32L4_RCC_SYSCLK_HSE)
+	switchSystemClock(SystemClockSource::hse);
+#elif defined(CONFIG_CHIP_STM32L4_RCC_SYSCLK_HSI16)
+	switchSystemClock(SystemClockSource::hsi16);
+#elif defined(CONFIG_CHIP_STM32L4_RCC_SYSCLK_MSI)
+	switchSystemClock(SystemClockSource::msi);
+#elif defined(CONFIG_CHIP_STM32L4_RCC_SYSCLK_PLL)
+	switchSystemClock(SystemClockSource::pll);
+#endif	// defined(CONFIG_CHIP_STM32L4_RCC_SYSCLK_PLL)
+
+#endif	// def CONFIG_CHIP_STM32L4_STANDARD_CLOCK_CONFIGURATION_ENABLE
 }
 
 }	// namespace chip
