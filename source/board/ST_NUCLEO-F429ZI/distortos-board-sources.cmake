4719120f2612e2f7d20db1c6361ee553f06a9cc3
Kamil Szczygiel <distortec@distortec.com>
2018-02-03 11:40:47 +0100

    Regenerate all boards
    $ find source/board/*/ -name '*.yaml' -print0 | xargs -0 -I{} -t make
    board CONFIG_FILE={}

---
+++ source/board/ST_NUCLEO-F429ZI/distortos-board-sources.cmake
@@ -0,0 +1,18 @@
+#
+# file: distortos-board-sources.cmake
+#
+# author: Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+# distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Automatically generated file - do not edit!
+#
+
+if(CONFIG_BOARD_SOURCE_BOARD_ST_NUCLEO_F429ZI_ST_NUCLEO_F429ZI)
+
+	target_sources(distortos PRIVATE
+			${CMAKE_CURRENT_LIST_DIR}/ST_NUCLEO-F429ZI-leds.cpp
+			${CMAKE_CURRENT_LIST_DIR}/ST_NUCLEO-F429ZI-buttons.cpp)
+
+endif()
---
+++ source/board/ST_STM32F4DISCOVERY/distortos-board-sources.cmake
@@ -0,0 +1,18 @@
+#
+# file: distortos-board-sources.cmake
+#
+# author: Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+# distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Automatically generated file - do not edit!
+#
+
+if(CONFIG_BOARD_SOURCE_BOARD_ST_STM32F4DISCOVERY_ST_STM32F4DISCOVERY)
+
+	target_sources(distortos PRIVATE
+			${CMAKE_CURRENT_LIST_DIR}/ST_STM32F4DISCOVERY-leds.cpp
+			${CMAKE_CURRENT_LIST_DIR}/ST_STM32F4DISCOVERY-buttons.cpp)
+
+endif()
