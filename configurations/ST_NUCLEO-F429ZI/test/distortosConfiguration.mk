b584c4533558ace8453f6953ffdf3b5346121a6b
Kamil Szczygiel <distortec@distortec.com>
2018-02-27 19:33:11 +0100

    Move external/CMSIS-STM32F4 to source/chip/STM32/STM32F4/external/...
    Update all configurations:
    $ ./scripts/forAllConfigurations.sh "make olddefconfig"

--- configurations/ST_NUCLEO-F429ZI/test/distortosConfiguration.mk
+++ configurations/ST_NUCLEO-F429ZI/test/distortosConfiguration.mk
@@ -67,7 +67,7 @@ CONFIG_BOARD_HAS_YAML=y
 CONFIG_CHIP_ROM_SIZE=2097152
 CONFIG_CHIP_ROM_ADDRESS=0x08000000
 CONFIG_CHIP="STM32F429ZI"
-CONFIG_CHIP_INCLUDES="source/chip/STM32/include source/chip/STM32/STM32F4/include source/chip/STM32/peripherals/GPIOv2/include source/chip/STM32/peripherals/SPIv1/include source/chip/STM32/peripherals/USARTv1/include external/CMSIS-STM32F4"
+CONFIG_CHIP_INCLUDES="source/chip/STM32/include source/chip/STM32/STM32F4/include source/chip/STM32/peripherals/GPIOv2/include source/chip/STM32/peripherals/SPIv1/include source/chip/STM32/peripherals/USARTv1/include source/chip/STM32/STM32F4/external/CMSIS-STM32F4"
 
 #
 # STM32F4 chip options
