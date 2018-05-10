736b354bec28d63bdadb2697b753817e7c0cc785
Kamil Szczygiel <distortec@distortec.com>
2018-05-10 23:10:22 +0200

    Add constructor to ThreadList
    Without it, code cannot be compiled in C++17 standard. Compilation with
    this standard doesn't work anyway because of
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=85739
    but this is a step in good direction.

--- include/distortos/internal/scheduler/ThreadList.hpp
+++ include/distortos/internal/scheduler/ThreadList.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief ThreadList class header
  *
- * \author Copyright (C) 2014-2015 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -55,7 +55,17 @@ struct ThreadDescendingEffectivePriority
 class ThreadList : public estd::SortedIntrusiveList<ThreadDescendingEffectivePriority, ThreadListNode,
 		&ThreadListNode::threadListNode, ThreadControlBlock>
 {
+public:
 
+	/**
+	 * \brief ThreadList's constructor
+	 */
+
+	constexpr ThreadList() :
+			SortedIntrusiveList{}
+	{
+
+	}
 };
 
 }	// namespace internal
