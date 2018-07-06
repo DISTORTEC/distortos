4fa56231b778949e3f3a3130c05b849f0365a739
Kamil Szczygiel <distortec@distortec.com>
2018-07-06 17:54:56 +0200

    Add SpiSdMmcCard and SpiSdMmcCardProxy classes
    This is the first version of code to support SD or MMC card connected
    via SPI. At this moment it has following limitations:
    - handles only SD version 2.0 cards;
    - tested only with 32 GB SDXC card;
    - no support for run-time detection of card insertion/removal;
    - no support for detecting whether card is write-protected;

---
+++ include/distortos/devices/memory/SpiSdMmcCard.hpp
@@ -0,0 +1,152 @@
+/**
+ * \file
+ * \brief SpiSdMmcCard class header
+ *
+ * \author Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDE_DISTORTOS_DEVICES_MEMORY_SPISDMMCCARD_HPP_
+#define INCLUDE_DISTORTOS_DEVICES_MEMORY_SPISDMMCCARD_HPP_
+
+#include "distortos/devices/communication/SpiDevice.hpp"
+
+namespace distortos
+{
+
+namespace devices
+{
+
+class SpiSdMmcCardProxy;
+
+/**
+ * SpiSdMmcCard class is a SD or MMC card connected via SPI.
+ *
+ * \ingroup devices
+ */
+
+class SpiSdMmcCard
+{
+	friend class SpiSdMmcCardProxy;
+
+public:
+
+	/// import SpiSdMmcCardProxy as SpiSdMmcCard::Proxy
+	using Proxy = SpiSdMmcCardProxy;
+
+	/// type of card connected via SPI
+	enum class Type : uint8_t
+	{
+		/// unknown type
+		unknown,
+
+		/// MMC card
+		mmc,
+		/// SD version 1.0 card
+		sdVersion1,
+		/// SD version 2.0 card
+		sdVersion2,
+	};
+
+	/// size of block, bytes
+	constexpr static size_t blockSize {512};
+
+	/**
+	 * \brief SpiSdMmcCard's constructor
+	 *
+	 * \param [in] spiMaster is a reference to SPI master to which this SD or MMC card is connected
+	 * \param [in] slaveSelectPin is a reference to slave select pin of this SD or MMC card
+	 * \param [in] clockFrequency is the desired clock frequency of SD or MMC card, Hz, default - 5 MHz
+	 */
+
+	constexpr SpiSdMmcCard(SpiMaster& spiMaster, OutputPin& slaveSelectPin, const uint32_t clockFrequency = 5000000) :
+			spiDevice_{spiMaster, slaveSelectPin},
+			blocksCount_{},
+			clockFrequency_{clockFrequency},
+			blockAddressing_{},
+			type_{}
+	{
+
+	}
+
+	/**
+	 * \brief Closes SD or MMC card connected via SPI.
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - error codes returned by SpiDevice::close();
+	 */
+
+	int close();
+
+	/**
+	 * \brief Opens SD or MMC card connected via SPI.
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - error codes returned by Proxy::initialize();
+	 * - error codes returned by SpiDevice::open();
+	 */
+
+	int open();
+
+	/**
+	 * \brief Wrapper for Proxy::read()
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \param [in] address is the address of data that will be read, must be a multiple of block size
+	 * \param [out] buffer is the buffer to which the data will be written
+	 * \param [in] size is the size of \a buffer, bytes, must be a multiple of block size
+	 *
+	 * \return pair with return code (0 on success, error code otherwise) and number of read bytes (valid even when
+	 * error code is returned); error codes:
+	 * - error codes returned by Proxy::read();
+	 */
+
+	std::pair<int, size_t> read(uint64_t address, void* buffer, size_t size);
+
+	/**
+	 * \brief Wrapper for Proxy::write()
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \param [in] address is the address of data that will be written, must be a multiple of block size
+	 * \param [in] buffer is the buffer with data that will be written
+	 * \param [in] size is the size of \a buffer, bytes, must be a multiple of block size
+	 *
+	 * \return pair with return code (0 on success, error code otherwise) and number of written bytes (valid even when
+	 * error code is returned); error codes:
+	 * - error codes returned by Proxy::write();
+	 */
+
+	std::pair<int, size_t> write(uint64_t address, const void* buffer, size_t size);
+
+private:
+
+	/// internal SPI slave device
+	SpiDevice spiDevice_;
+
+	/// number of blocks available on SD or MMC card
+	size_t blocksCount_;
+
+	/// desired clock frequency of SD or MMC card, Hz
+	uint32_t clockFrequency_;
+
+	/// selects whether card uses byte (false) or block (true) addressing
+	bool blockAddressing_;
+
+	/// type of card connected via SPI
+	Type type_;
+};
+
+}	// namespace devices
+
+}	// namespace distortos
+
+#endif	// INCLUDE_DISTORTOS_DEVICES_MEMORY_SPISDMMCCARD_HPP_
---
+++ include/distortos/devices/memory/SpiSdMmcCardProxy.hpp
@@ -0,0 +1,154 @@
+/**
+ * \file
+ * \brief SpiSdMmcCardProxy class header
+ *
+ * \author Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDE_DISTORTOS_DEVICES_MEMORY_SPISDMMCCARDPROXY_HPP_
+#define INCLUDE_DISTORTOS_DEVICES_MEMORY_SPISDMMCCARDPROXY_HPP_
+
+#include "distortos/devices/communication/SpiDeviceProxy.hpp"
+
+#include <utility>
+
+#include <cstddef>
+#include <cstdint>
+
+namespace distortos
+{
+
+namespace devices
+{
+
+class SpiSdMmcCard;
+
+/**
+ * SpiSdMmcCardProxy is a [std::lock_guard](https://en.cppreference.com/w/cpp/thread/lock_guard)-like class for locking
+ * SpiSdMmcCard and for accessing its core functionality.
+ *
+ * \note Locks are recursive.
+ *
+ * \ingroup devices
+ */
+
+class SpiSdMmcCardProxy
+{
+	friend class SpiSdMmcCard;
+
+public:
+
+	/**
+	 * \brief SpiSdMmcCardProxy's constructor
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \param [in] spiSdMmcCard is a reference to SpiSdMmcCard associated with this proxy
+	 */
+
+	explicit SpiSdMmcCardProxy(SpiSdMmcCard& spiSdMmcCard);
+
+	/**
+	 * \return true if associated SPI device is opened, false otherwise
+	 */
+
+	bool isOpened() const
+	{
+		return spiDeviceProxy_.isOpened();
+	}
+
+	/**
+	 * \brief Reads data from SD or MMC card connected via SPI.
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \param [in] address is the address of data that will be read, must be a multiple of block size
+	 * \param [out] buffer is the buffer to which the data will be written
+	 * \param [in] size is the size of \a buffer, bytes, must be a multiple of block size
+	 *
+	 * \return pair with return code (0 on success, error code otherwise) and number of read bytes (valid even when
+	 * error code is returned); error codes:
+	 * - EBADF - associated SD or MMC card is not opened;
+	 * - EINVAL - \a address and/or \a buffer and/or \a size are not valid;
+	 * - EIO - error during communication with SD or MMC card;
+	 * - ENOSPC - selected range is invalid;
+	 * - error codes returned by executeCmd12();
+	 * - error codes returned by executeCmd17();
+	 * - error codes returned by executeCmd18();
+	 * - error codes returned by readDataBlock();
+	 */
+
+	std::pair<int, size_t> read(uint64_t address, void* buffer, size_t size) const;
+
+	/**
+	 * \brief Writes data to SD or MMC card connected via SPI.
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \param [in] address is the address of data that will be written, must be a multiple of block size
+	 * \param [in] buffer is the buffer with data that will be written
+	 * \param [in] size is the size of \a buffer, bytes, must be a multiple of block size
+	 *
+	 * \return pair with return code (0 on success, error code otherwise) and number of written bytes (valid even when
+	 * error code is returned); error codes:
+	 * - EBADF - associated SD or MMC card is not opened;
+	 * - EINVAL - \a address and/or \a buffer and/or \a size are not valid;
+	 * - EIO - error during communication with SD or MMC card;
+	 * - ENOSPC - selected range is invalid;
+	 * - error codes returned by executeAcmd23();
+	 * - error codes returned by executeCmd24();
+	 * - error codes returned by executeCmd25();
+	 * - error codes returned by waitWhileBusy();
+	 * - error codes returned by writeDataBlock();
+	 * - error codes returned by SpiMasterProxy::executeTransaction();
+	 */
+
+	std::pair<int, size_t> write(uint64_t address, const void* buffer, size_t size) const;
+
+private:
+
+	/**
+	 * \brief Deinitializes SD or MMC card connected via SPI.
+	 */
+
+	void deinitialize() const;
+
+	/**
+	 * \brief Initializes SD or MMC card connected via SPI.
+	 *
+	 * Algorithm is based on ChaN's
+	 * [How to Use MMC/SDC: Initialization Procedure for SPI Mode](http://elm-chan.org/docs/mmc/mmc_e.html#spiinit).
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - EBADF - associated SD or MMC card is already initialized;
+	 * - EIO - error during communication with SD or MMC card;
+	 * - ETIMEDOUT - timed-out while waiting for SD or MMC card to respond;
+	 * - error codes returned by executeAcmd41();
+	 * - error codes returned by executeCmd0();
+	 * - error codes returned by executeCmd1();
+	 * - error codes returned by executeCmd8();
+	 * - error codes returned by executeCmd9();
+	 * - error codes returned by executeCmd16();
+	 * - error codes returned by executeCmd58();
+	 * - error codes returned by SpiMasterProxy::configure();
+	 * - error codes returned by SpiMasterProxy::executeTransaction();
+	 */
+
+	int initialize() const;
+
+	/// internal proxy for SPI device
+	SpiDeviceProxy spiDeviceProxy_;
+
+	/// reference to SpiSdMmcCard associated with this proxy
+	SpiSdMmcCard& spiSdMmcCard_;
+};
+
+}	// namespace devices
+
+}	// namespace distortos
+
+#endif	// INCLUDE_DISTORTOS_DEVICES_MEMORY_SPISDMMCCARDPROXY_HPP_
