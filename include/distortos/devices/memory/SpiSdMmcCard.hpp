1a7eb8315f45ceb80d519e71aa3d7d1c7e081521
Kamil Szczygiel <distortec@distortec.com>
2018-07-09 14:42:29 +0200

    SpiSdMmcCard inherits from BlockDevice

--- include/distortos/devices/memory/SpiSdMmcCard.hpp
+++ include/distortos/devices/memory/SpiSdMmcCard.hpp
@@ -14,6 +14,8 @@
 
 #include "distortos/devices/communication/SpiDevice.hpp"
 
+#include "distortos/devices/memory/BlockDevice.hpp"
+
 namespace distortos
 {
 
@@ -28,7 +30,7 @@ class SpiSdMmcCardProxy;
  * \ingroup devices
  */
 
-class SpiSdMmcCard
+class SpiSdMmcCard : public BlockDevice
 {
 	friend class SpiSdMmcCardProxy;
 
@@ -73,6 +75,12 @@ public:
 	}
 
 	/**
+	 * \brief SpiSdMmcCard's destructor
+	 */
+
+	~SpiSdMmcCard() override;
+
+	/**
 	 * \brief Closes SD or MMC card connected via SPI.
 	 *
 	 * \warning This function must not be called from interrupt context!
@@ -81,10 +89,10 @@ public:
 	 * - error codes returned by SpiDevice::close();
 	 */
 
-	int close();
+	int close() override;
 
 	/**
-	 * \brief Wrapper for Proxy::erase()
+	 * \brief Erases blocks on a SD or MMC card connected via SPI.
 	 *
 	 * \warning This function must not be called from interrupt context!
 	 *
@@ -95,7 +103,54 @@ public:
 	 * - error codes returned by Proxy::erase();
 	 */
 
-	int erase(uint64_t address, uint64_t size);
+	int erase(uint64_t address, uint64_t size) override;
+
+	/**
+	 * \return erase block size, bytes
+	 */
+
+	size_t getEraseBlockSize() const override;
+
+	/**
+	 * \return pair with bool telling whether erased value is defined (true) or not (false) and value of erased bytes
+	 * (valid only if defined);
+	 */
+
+	std::pair<bool, uint8_t> getErasedValue() const override;
+
+	/**
+	 * \return program block size, bytes
+	 */
+
+	size_t getProgramBlockSize() const override;
+
+	/**
+	 * \return read block size, bytes
+	 */
+
+	size_t getReadBlockSize() const override;
+
+	/**
+	 * \return size of SD or MMC card connected via SPI, bytes
+	 */
+
+	uint64_t getSize() const override;
+
+ 	/**
+	 * \brief Locks the device for exclusive use by current thread.
+	 *
+	 * When the object is locked, any call to any member function from other thread will be blocked until the object is
+	 * unlocked. Locking is optional, but may be useful when more than one transaction must be done atomically.
+	 *
+	 * \note Locks are recursive.
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - error codes returned by SpiDevice::lock();
+	 */
+
+	int lock() override;
 
 	/**
 	 * \brief Opens SD or MMC card connected via SPI.
@@ -107,39 +162,75 @@ public:
 	 * - error codes returned by SpiDevice::open();
 	 */
 
-	int open();
+	int open() override;
 
 	/**
-	 * \brief Wrapper for Proxy::program()
+	 * \brief Programs data to SD or MMC card connected via SPI.
+	 *
+	 * Selected range of blocks must have been erased prior to being programmed.
 	 *
 	 * \warning This function must not be called from interrupt context!
 	 *
-	 * \param [in] address is the address of data that will be programmed, must be a multiple of block size
+	 * \param [in] address is the address of data that will be programmed, must be a multiple of program block size
 	 * \param [in] buffer is the buffer with data that will be programmed
-	 * \param [in] size is the size of \a buffer, bytes, must be a multiple of block size
+	 * \param [in] size is the size of \a buffer, bytes, must be a multiple of program block size
 	 *
-	 * \return pair with return code (0 on success, error code otherwise) and number of programmed bytes (valid even when
-	 * error code is returned); error codes:
+	 * \return pair with return code (0 on success, error code otherwise) and number of programmed bytes (valid even
+	 * when error code is returned); error codes:
 	 * - error codes returned by Proxy::program();
 	 */
 
-	std::pair<int, size_t> program(uint64_t address, const void* buffer, size_t size);
+	std::pair<int, size_t> program(uint64_t address, const void* buffer, size_t size) override;
 
 	/**
-	 * \brief Wrapper for Proxy::read()
+	 * \brief Reads data from SD or MMC card connected via SPI.
 	 *
 	 * \warning This function must not be called from interrupt context!
 	 *
-	 * \param [in] address is the address of data that will be read, must be a multiple of block size
-	 * \param [out] buffer is the buffer to which the data will be written
-	 * \param [in] size is the size of \a buffer, bytes, must be a multiple of block size
+	 * \param [in] address is the address of data that will be read, must be a multiple of read block size
+	 * \param [out] buffer is the buffer into which the data will be read
+	 * \param [in] size is the size of \a buffer, bytes, must be a multiple of read block size
 	 *
 	 * \return pair with return code (0 on success, error code otherwise) and number of read bytes (valid even when
 	 * error code is returned); error codes:
 	 * - error codes returned by Proxy::read();
 	 */
 
-	std::pair<int, size_t> read(uint64_t address, void* buffer, size_t size);
+	std::pair<int, size_t> read(uint64_t address, void* buffer, size_t size) override;
+
+	/**
+	 * \brief Synchronizes state of SD or MMC card connected via SPI, ensuring all cached writes are finished.
+	 *
+	 * \return always 0
+	 */
+
+	int synchronize() override;
+
+	/**
+	 * \brief Trims unused blocks on SD or MMC card connected via SPI.
+	 *
+	 * Selected range of blocks is no longer used and SD or MMC card connected via SPI may erase it when convenient.
+	 *
+	 * \param [in] address is the address of range that will be trimmed, must be a multiple of erase block size
+	 * \param [in] size is the size of trimmed range, bytes, must be a multiple of erase block size
+	 *
+	 * \return always 0
+	 */
+
+	int trim(uint64_t address, uint64_t size) override;
+
+	/**
+	 * \brief Unlocks the device which was previously locked by current thread.
+	 *
+	 * \note Locks are recursive.
+	 *
+	 * \warning This function must not be called from interrupt context!
+	 *
+	 * \return 0 on success, error code otherwise:
+	 * - error codes returned by SpiDevice::unlock();
+	 */
+
+	int unlock() override;
 
 private:
 
--- source/devices/memory/SpiSdMmcCard.cpp
+++ source/devices/memory/SpiSdMmcCard.cpp
@@ -25,6 +25,11 @@ namespace devices
 | public functions
 +---------------------------------------------------------------------------------------------------------------------*/
 
+SpiSdMmcCard::~SpiSdMmcCard()
+{
+
+}
+
 int SpiSdMmcCard::close()
 {
 	const Proxy proxy {*this};
@@ -40,6 +45,37 @@ int SpiSdMmcCard::close()
 int SpiSdMmcCard::erase(const uint64_t address, const uint64_t size)
 {
 	return Proxy{*this}.erase(address, size);
+}
+
+size_t SpiSdMmcCard::getEraseBlockSize() const
+{
+	return blockSize;
+}
+
+std::pair<bool, uint8_t> SpiSdMmcCard::getErasedValue() const
+{
+	/// \todo implement by reading DATA_STAT_AFTER_ERASE from SCR register
+	return {};
+}
+
+size_t SpiSdMmcCard::getProgramBlockSize() const
+{
+	return blockSize;
+}
+
+size_t SpiSdMmcCard::getReadBlockSize() const
+{
+	return blockSize;
+}
+
+uint64_t SpiSdMmcCard::getSize() const
+{
+	return blockSize * blocksCount_;
+}
+
+int SpiSdMmcCard::lock()
+{
+	return spiDevice_.lock();
 }
 
 int SpiSdMmcCard::open()
@@ -79,6 +115,21 @@ std::pair<int, size_t> SpiSdMmcCard::program(const uint64_t address, const void*
 std::pair<int, size_t> SpiSdMmcCard::read(const uint64_t address, void* const buffer, const size_t size)
 {
 	return Proxy{*this}.read(address, buffer, size);
+}
+
+int SpiSdMmcCard::synchronize()
+{
+	return {};
+}
+
+int SpiSdMmcCard::trim(uint64_t, uint64_t)
+{
+	return {};
+}
+
+int SpiSdMmcCard::unlock()
+{
+	return spiDevice_.unlock();
 }
 
 }	// namespace devices
