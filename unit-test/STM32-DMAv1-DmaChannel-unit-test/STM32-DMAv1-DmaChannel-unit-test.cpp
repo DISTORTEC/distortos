a6f53a328749c6b45f8cfcc7f5e0a8ebdbbd6741
Kamil Szczygiel <distortec@distortec.com>
2018-12-18 14:24:31 +0100

    Add unit test of DmaChannel for STM32 DMAv1

---
+++ unit-test/STM32-DMAv1-DmaChannel-unit-test/STM32-DMAv1-DmaChannel-unit-test.cpp
@@ -0,0 +1,412 @@
+/**
+ * \file
+ * \brief STM32 DMAv1's DmaChannel test cases
+ *
+ * This test checks whether STM32 DMAv1's DmaChannel performs all h/w operations properly and in correct order.
+ *
+ * \author Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/chip/DmaChannel.hpp"
+#include "distortos/chip/DmaChannelFunctor.hpp"
+#include "distortos/chip/STM32-DMAv1-DmaChannelPeripheral.hpp"
+#include "distortos/chip/STM32-DMAv1-DmaPeripheral.hpp"
+
+#include "distortos/InterruptMaskingLock.hpp"
+
+using trompeloeil::_;
+
+namespace
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local types
++---------------------------------------------------------------------------------------------------------------------*/
+
+class DmaChannelFunctor : public distortos::chip::DmaChannelFunctor
+{
+public:
+
+	MAKE_MOCK0(transferComplete, void());
+	MAKE_MOCK1(transferError, void(size_t));
+};
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+uint8_t getChannelShift(const uint8_t channelId)
+{
+	return (channelId - 1) * 4;
+}
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| local objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+constexpr uint8_t maxRequest {(1 << DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS) - 1};
+constexpr uint8_t request1 {maxRequest};
+constexpr uint8_t request2 {};
+constexpr uint32_t initialCselr {0x55555555};
+
+}	// namespace
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global test cases
++---------------------------------------------------------------------------------------------------------------------*/
+
+TEST_CASE("Testing reserve() & release() interactions", "[reserve/release]")
+{
+	DmaChannelFunctor functorMock {};
+	distortos::chip::DmaPeripheral peripheralMock {};
+	distortos::chip::DmaChannelPeripheral channelPeripheralMock {};
+	distortos::InterruptMaskingLock::Proxy interruptMaskingLockProxyMock {};
+	trompeloeil::sequence sequence {};
+	std::vector<std::unique_ptr<trompeloeil::expectation>> expectations {};
+
+	constexpr uint8_t channelId {6};
+	const auto channelShift = getChannelShift(channelId);
+
+	distortos::chip::DmaChannel channel {peripheralMock, channelPeripheralMock};
+
+	ALLOW_CALL(channelPeripheralMock, getChannelId()).RETURN(channelId);
+
+	SECTION("Configuring released driver should fail with EBADF")
+	{
+		distortos::chip::DmaChannel::UniqueHandle handle;
+		REQUIRE(handle.configureTransfer({}, {}, {}, {}, {}, {}, {}, {}) == EBADF);
+	}
+	SECTION("Starting a transfer with released driver should fail with EBADF")
+	{
+		distortos::chip::DmaChannel::UniqueHandle handle;
+		REQUIRE(handle.startTransfer() == EBADF);
+	}
+	SECTION("Stopping a transfer with released driver should fail with EBADF")
+	{
+		distortos::chip::DmaChannel::UniqueHandle handle;
+		REQUIRE(handle.stopTransfer() == EBADF);
+	}
+	SECTION("Reserving released driver with wrong request number should fail with EINVAL")
+	{
+		distortos::chip::DmaChannel::UniqueHandle handle;
+		REQUIRE(handle.reserve(channel, maxRequest + 1, functorMock) == EINVAL);
+	}
+	SECTION("Releasing released driver results in hardware accesses")
+	{
+		distortos::chip::DmaChannel::UniqueHandle handle;
+		handle.release();
+	}
+	SECTION("Reserving released driver should succeed")
+	{
+		distortos::chip::DmaChannel::UniqueHandle handle;
+
+		{
+			REQUIRE_CALL(interruptMaskingLockProxyMock, construct()).IN_SEQUENCE(sequence);
+
+#if DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS != 0
+
+			const auto newCselr = (initialCselr & ~(DMA_CSELR_C1S << channelShift)) | request1 << channelShift;
+			const auto oldCselr = newCselr ^ DMA_CSELR_C1S << channelShift;
+			REQUIRE_CALL(peripheralMock, readCselr()).IN_SEQUENCE(sequence).RETURN(oldCselr);
+			REQUIRE_CALL(peripheralMock, writeCselr(newCselr)).IN_SEQUENCE(sequence);
+
+#endif	// DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS != 0
+
+			REQUIRE_CALL(interruptMaskingLockProxyMock, destruct()).IN_SEQUENCE(sequence);
+			REQUIRE(handle.reserve(channel, request1, functorMock) == 0);
+		}
+
+		SECTION("Reserving reserved driver with the same handle should succeed")
+		{
+			const auto oldCcr = UINT32_MAX;
+			REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(oldCcr);
+			const auto newCcr = oldCcr & ~(DMA_CCR_TEIE | DMA_CCR_HTIE | DMA_CCR_TCIE | DMA_CCR_EN);
+			REQUIRE_CALL(channelPeripheralMock, writeCcr(newCcr)).IN_SEQUENCE(sequence);
+			REQUIRE_CALL(peripheralMock, writeIfcr(DMA_IFCR_CGIF1 << channelShift)).IN_SEQUENCE(sequence);
+
+			REQUIRE_CALL(interruptMaskingLockProxyMock, construct()).IN_SEQUENCE(sequence);
+
+#if DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS != 0
+
+			const auto newCselr = (initialCselr & ~(DMA_CSELR_C1S << channelShift)) | request2 << channelShift;
+			const auto oldCselr = newCselr ^ DMA_CSELR_C1S << channelShift;
+			REQUIRE_CALL(peripheralMock, readCselr()).IN_SEQUENCE(sequence).RETURN(oldCselr);
+			REQUIRE_CALL(peripheralMock, writeCselr(newCselr)).IN_SEQUENCE(sequence);
+
+#endif	// DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS != 0
+
+			REQUIRE_CALL(interruptMaskingLockProxyMock, destruct()).IN_SEQUENCE(sequence);
+			REQUIRE(handle.reserve(channel, request2, functorMock) == 0);
+		}
+		SECTION("Reserving reserved driver with a different handle should fail with EBADF")
+		{
+			distortos::chip::DmaChannel::UniqueHandle anotherHandle;
+			REQUIRE_CALL(interruptMaskingLockProxyMock, construct()).IN_SEQUENCE(sequence);
+			REQUIRE_CALL(interruptMaskingLockProxyMock, destruct()).IN_SEQUENCE(sequence);
+			REQUIRE(anotherHandle.reserve(channel, 0, functorMock) == EBADF);
+		}
+
+		const auto oldCcr = UINT32_MAX;
+		expectations.emplace_back(NAMED_REQUIRE_CALL(channelPeripheralMock,
+				readCcr()).IN_SEQUENCE(sequence).RETURN(oldCcr));
+		const auto newCcr = oldCcr & ~(DMA_CCR_TEIE | DMA_CCR_HTIE | DMA_CCR_TCIE | DMA_CCR_EN);
+		expectations.emplace_back(NAMED_REQUIRE_CALL(channelPeripheralMock, writeCcr(newCcr)).IN_SEQUENCE(sequence));
+		expectations.emplace_back(NAMED_REQUIRE_CALL(peripheralMock,
+				writeIfcr(DMA_IFCR_CGIF1 << channelShift)).IN_SEQUENCE(sequence));
+	}
+}
+
+TEST_CASE("Testing configureTransfer()", "[configureTransfer]")
+{
+	DmaChannelFunctor functorMock {};
+	distortos::chip::DmaPeripheral peripheralMock {};
+	distortos::chip::DmaChannelPeripheral channelPeripheralMock {};
+	distortos::InterruptMaskingLock::Proxy interruptMaskingLockProxyMock {};
+	trompeloeil::sequence sequence {};
+
+	constexpr uint8_t channelId {5};
+	const auto channelShift = getChannelShift(channelId);
+
+	distortos::chip::DmaChannel channel {peripheralMock, channelPeripheralMock};
+
+	distortos::chip::DmaChannel::UniqueHandle handle;
+
+	ALLOW_CALL(channelPeripheralMock, getChannelId()).RETURN(channelId);
+
+	{
+		REQUIRE_CALL(interruptMaskingLockProxyMock, construct()).IN_SEQUENCE(sequence);
+
+#if DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS != 0
+
+		const auto newCselr = (initialCselr & ~(DMA_CSELR_C1S << channelShift)) | request1 << channelShift;
+		const auto oldCselr = newCselr ^ DMA_CSELR_C1S << channelShift;
+		REQUIRE_CALL(peripheralMock, readCselr()).IN_SEQUENCE(sequence).RETURN(oldCselr);
+		REQUIRE_CALL(peripheralMock, writeCselr(newCselr)).IN_SEQUENCE(sequence);
+
+#endif	// DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS != 0
+
+		REQUIRE_CALL(interruptMaskingLockProxyMock, destruct()).IN_SEQUENCE(sequence);
+		REQUIRE(handle.reserve(channel, request1, functorMock) == 0);
+	}
+
+	for (uint8_t dataSize {}; dataSize <= 8; ++dataSize)
+	{
+		if (dataSize == 1 || dataSize == 2 || dataSize == 4)
+			continue;
+
+		DYNAMIC_SECTION("Trying to use data size " << static_cast<int>(dataSize) << " should fail with EINVAL")
+		{
+			REQUIRE(handle.configureTransfer({}, dataSize, {}, {}, 1, {}, 1, {}) == EINVAL);
+			REQUIRE(handle.configureTransfer({}, 1, {}, {}, dataSize, {}, 1, {}) == EINVAL);
+			REQUIRE(handle.configureTransfer({}, dataSize, {}, {}, dataSize, {}, 1, {}) == EINVAL);
+		}
+	}
+
+	const uint8_t dataSizes[]
+	{
+			1,
+			2,
+			4,
+	};
+	for (const auto dataSize : dataSizes)
+		for (uintptr_t address {1}; address < dataSize; ++address)
+		{
+			DYNAMIC_SECTION("Trying to use address " << static_cast<int>(address) << " when data size is " <<
+					static_cast<int>(dataSize) << " should fail with EINVAL")
+			{
+				REQUIRE(handle.configureTransfer(address, dataSize, {}, {}, 1, {}, 1, {}) == EINVAL);
+				REQUIRE(handle.configureTransfer({}, 1, {}, address, dataSize, {}, 1, {}) == EINVAL);
+			}
+		}
+
+	SECTION("Trying to use 0 transactions should fail with EINVAL")
+	{
+		REQUIRE(handle.configureTransfer({}, 1, {}, {}, 1, {}, 0, {}) == EINVAL);
+	}
+
+	SECTION("Trying to configure the driver when transfer is ongoing should fail with EBUSY")
+	{
+		REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(DMA_CCR_EN);
+		REQUIRE(handle.configureTransfer({}, 1, {}, {}, 1, {}, 1, {}) == EBUSY);
+	}
+
+	SECTION("Trying to use valid configuration should succeed")
+	{
+		const bool falseTrue[]
+		{
+				false,
+				true
+		};
+		for (const auto memoryDataSize : dataSizes)
+			for (const auto memoryIncrement : falseTrue)
+				for (const auto peripheralDataSize : dataSizes)
+					for (const auto peripheralIncrement : falseTrue)
+						for (const auto memoryToPeripheral : falseTrue)
+						{
+							constexpr uintptr_t memoryAddress {0xdf5d76a8};
+							constexpr uintptr_t peripheralAddress {0x1b932358};
+							constexpr uint16_t transactions {0xd353};
+
+							REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(0);
+							const auto ccr = memoryDataSize / 2 << DMA_CCR_MSIZE_Pos |
+									peripheralDataSize / 2 << DMA_CCR_PSIZE_Pos |
+									memoryIncrement << DMA_CCR_MINC_Pos |
+									peripheralIncrement << DMA_CCR_PINC_Pos |
+									memoryToPeripheral << DMA_CCR_DIR_Pos |
+									DMA_CCR_TEIE |
+									DMA_CCR_TCIE;
+							REQUIRE_CALL(channelPeripheralMock, writeCcr(ccr)).IN_SEQUENCE(sequence);
+							REQUIRE_CALL(channelPeripheralMock, writeCndtr(transactions)).IN_SEQUENCE(sequence);
+							REQUIRE_CALL(channelPeripheralMock, writeCpar(peripheralAddress)).IN_SEQUENCE(sequence);
+							REQUIRE_CALL(channelPeripheralMock, writeCmar(memoryAddress)).IN_SEQUENCE(sequence);
+							REQUIRE(handle.configureTransfer(memoryAddress, memoryDataSize, memoryIncrement,
+									peripheralAddress, peripheralDataSize, peripheralIncrement, transactions,
+									memoryToPeripheral) == 0);
+						}
+	}
+
+	const auto oldCcr = UINT32_MAX;
+	REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(oldCcr);
+	const auto newCcr = oldCcr & ~(DMA_CCR_TEIE | DMA_CCR_HTIE | DMA_CCR_TCIE | DMA_CCR_EN);
+	REQUIRE_CALL(channelPeripheralMock, writeCcr(newCcr)).IN_SEQUENCE(sequence);
+	REQUIRE_CALL(peripheralMock, writeIfcr(DMA_IFCR_CGIF1 << channelShift)).IN_SEQUENCE(sequence);
+	handle.release();
+}
+
+TEST_CASE("Testing transfers", "[transfers]")
+{
+	DmaChannelFunctor functorMock {};
+	distortos::chip::DmaPeripheral peripheralMock {};
+	distortos::chip::DmaChannelPeripheral channelPeripheralMock {};
+	distortos::InterruptMaskingLock::Proxy interruptMaskingLockProxyMock {};
+	trompeloeil::sequence sequence {};
+	std::vector<std::unique_ptr<trompeloeil::expectation>> expectations {};
+
+	constexpr uint8_t channelId {0};
+	const auto channelShift = getChannelShift(channelId);
+
+	distortos::chip::DmaChannel channel {peripheralMock, channelPeripheralMock};
+
+	distortos::chip::DmaChannel::UniqueHandle handle;
+
+	ALLOW_CALL(channelPeripheralMock, getChannelId()).RETURN(channelId);
+
+	{
+		REQUIRE_CALL(interruptMaskingLockProxyMock, construct()).IN_SEQUENCE(sequence);
+
+#if DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS != 0
+
+		const auto newCselr = (initialCselr & ~(DMA_CSELR_C1S << channelShift)) | request1 << channelShift;
+		const auto oldCselr = newCselr ^ DMA_CSELR_C1S << channelShift;
+		REQUIRE_CALL(peripheralMock, readCselr()).IN_SEQUENCE(sequence).RETURN(oldCselr);
+		REQUIRE_CALL(peripheralMock, writeCselr(newCselr)).IN_SEQUENCE(sequence);
+
+#endif	// DISTORTOS_CHIP_DMA_CHANNEL_REQUEST_BITS != 0
+
+		REQUIRE_CALL(interruptMaskingLockProxyMock, destruct()).IN_SEQUENCE(sequence);
+		REQUIRE(handle.reserve(channel, request1, functorMock) == 0);
+	}
+
+	SECTION("Starting another transfer when the previous one is ongoing should fail with EBUSY")
+	{
+		REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(DMA_CCR_EN);
+		REQUIRE(handle.startTransfer() == EBUSY);
+	}
+
+	SECTION("Testing startTransfer()")
+	{
+		const auto newCcr = 0xaaaaaaaa | DMA_CCR_EN;
+		const auto oldCcr = newCcr ^ DMA_CCR_EN;
+		REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(oldCcr);
+		REQUIRE_CALL(channelPeripheralMock, writeCcr(newCcr)).IN_SEQUENCE(sequence);
+		REQUIRE(handle.startTransfer() == 0);
+	}
+	SECTION("Testing stopTransfer()")
+	{
+		const auto oldCcr = UINT32_MAX;
+		REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(oldCcr);
+		const auto newCcr = oldCcr & ~(DMA_CCR_TEIE | DMA_CCR_HTIE | DMA_CCR_TCIE | DMA_CCR_EN);
+		REQUIRE_CALL(channelPeripheralMock, writeCcr(newCcr)).IN_SEQUENCE(sequence);
+		REQUIRE_CALL(peripheralMock, writeIfcr(DMA_IFCR_CGIF1 << channelShift)).IN_SEQUENCE(sequence);
+		handle.stopTransfer();
+	}
+	SECTION("Testing interruptHandler()")
+	{
+		{
+			const auto isr = UINT32_MAX & ~((DMA_ISR_TEIF1 | DMA_ISR_TCIF1) << channelShift);
+			REQUIRE_CALL(peripheralMock, readIsr()).IN_SEQUENCE(sequence).RETURN(isr);
+			channel.interruptHandler();
+		}
+		{
+			const bool falseTrue[]
+			{
+					false,
+					true,
+			};
+			for (const auto teif : falseTrue)
+				for (const auto tcif : falseTrue)
+				{
+					if (teif == false && tcif == false)
+						continue;
+
+					const auto isr = UINT32_MAX & ~((DMA_ISR_TEIF1 | DMA_ISR_TCIF1) << channelShift) |
+							teif << (DMA_ISR_TEIF1_Pos + channelShift) |
+							tcif << (DMA_ISR_TCIF1_Pos + channelShift);
+					REQUIRE_CALL(peripheralMock, readIsr()).IN_SEQUENCE(sequence).RETURN(isr);
+					const auto ccr = UINT32_MAX & ~(DMA_CCR_TEIE | DMA_CCR_TCIE) |
+							!teif << DMA_CCR_TEIE_Pos |
+							!tcif << DMA_CCR_TCIE_Pos;
+					REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(ccr);
+					channel.interruptHandler();
+				}
+
+			for (const auto teif : falseTrue)
+				for (const auto tcif : falseTrue)
+					for (const auto teie : falseTrue)
+						for (const auto tcie : falseTrue)
+						{
+							if ((teif && teie) == false && (tcif && tcie) == false)
+								continue;
+
+							const auto isr = UINT32_MAX & ~((DMA_ISR_TEIF1 | DMA_ISR_TCIF1) << channelShift) |
+									teif << (DMA_ISR_TEIF1_Pos + channelShift) |
+									tcif << (DMA_ISR_TCIF1_Pos + channelShift);
+							REQUIRE_CALL(peripheralMock, readIsr()).IN_SEQUENCE(sequence).RETURN(isr);
+							const auto ccr = UINT32_MAX & ~(DMA_CCR_TEIE | DMA_CCR_TCIE) |
+									teie << DMA_CCR_TEIE_Pos |
+									tcie << DMA_CCR_TCIE_Pos;
+							REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(ccr);
+							const uint32_t ifcr = (teif && teie) << (DMA_IFCR_CTEIF1_Pos + channelShift) |
+									(tcif && tcie) << (DMA_IFCR_CTCIF1_Pos + channelShift);
+							REQUIRE_CALL(peripheralMock, writeIfcr(ifcr)).IN_SEQUENCE(sequence);
+
+							if ((teif && teie) == true)
+							{
+								constexpr uint16_t transactionsLeft {0x6e95};
+								expectations.emplace_back(NAMED_REQUIRE_CALL(channelPeripheralMock,
+										readCndtr()).IN_SEQUENCE(sequence).RETURN(transactionsLeft));
+								expectations.emplace_back(NAMED_REQUIRE_CALL(functorMock,
+										transferError(transactionsLeft)).IN_SEQUENCE(sequence));
+							}
+							if ((tcif && tcie) == true)
+							{
+								expectations.emplace_back(NAMED_REQUIRE_CALL(functorMock,
+										transferComplete()).IN_SEQUENCE(sequence));
+							}
+
+							channel.interruptHandler();
+						}
+		}
+	}
+
+	const auto oldCcr = UINT32_MAX;
+	REQUIRE_CALL(channelPeripheralMock, readCcr()).IN_SEQUENCE(sequence).RETURN(oldCcr);
+	const auto newCcr = oldCcr & ~(DMA_CCR_TEIE | DMA_CCR_HTIE | DMA_CCR_TCIE | DMA_CCR_EN);
+	REQUIRE_CALL(channelPeripheralMock, writeCcr(newCcr)).IN_SEQUENCE(sequence);
+	REQUIRE_CALL(peripheralMock, writeIfcr(DMA_IFCR_CGIF1 << channelShift)).IN_SEQUENCE(sequence);
+	handle.release();
+}
---
+++ unit-test/include-mocks/chip/STM32-DMAv1-DmaChannelPeripheral.hpp/distortos/chip/STM32-DMAv1-DmaChannelPeripheral.hpp
@@ -0,0 +1,107 @@
+/**
+ * \file
+ * \brief Mock of DmaChannelPeripheral class for DMAv1 in STM32
+ *
+ * \author Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef UNIT_TEST_INCLUDE_MOCKS_CHIP_STM32_DMAV1_DMACHANNELPERIPHERAL_HPP_DISTORTOS_CHIP_STM32_DMAV1_DMACHANNELPERIPHERAL_HPP_
+#define UNIT_TEST_INCLUDE_MOCKS_CHIP_STM32_DMAV1_DMACHANNELPERIPHERAL_HPP_DISTORTOS_CHIP_STM32_DMAV1_DMACHANNELPERIPHERAL_HPP_
+
+#include "unit-test-common.hpp"
+
+namespace distortos
+{
+
+namespace chip
+{
+
+class DmaChannelPeripheral
+{
+public:
+
+	MAKE_CONST_MOCK0(getChannelId, uint8_t());
+	MAKE_CONST_MOCK0(readCcr, uint32_t());
+	MAKE_CONST_MOCK0(readCmar, uint32_t());
+	MAKE_CONST_MOCK0(readCndtr, uint32_t());
+	MAKE_CONST_MOCK0(readCpar, uint32_t());
+	MAKE_CONST_MOCK1(writeCcr, void(uint32_t));
+	MAKE_CONST_MOCK1(writeCmar, void(uint32_t));
+	MAKE_CONST_MOCK1(writeCndtr, void(uint32_t));
+	MAKE_CONST_MOCK1(writeCpar, void(uint32_t));
+};
+
+}	// namespace chip
+
+}	// namespace distortos
+
+// following definitions were copied from CMSIS-STM32F0/stm32f091xc.h
+
+/*******************  Bit definition for DMA_CCR register  ********************/
+#define DMA_CCR_EN_Pos         (0U)
+#define DMA_CCR_EN_Msk         (0x1U << DMA_CCR_EN_Pos)                        /*!< 0x00000001 */
+#define DMA_CCR_EN             DMA_CCR_EN_Msk                                  /*!< Channel enable                      */
+#define DMA_CCR_TCIE_Pos       (1U)
+#define DMA_CCR_TCIE_Msk       (0x1U << DMA_CCR_TCIE_Pos)                      /*!< 0x00000002 */
+#define DMA_CCR_TCIE           DMA_CCR_TCIE_Msk                                /*!< Transfer complete interrupt enable  */
+#define DMA_CCR_HTIE_Pos       (2U)
+#define DMA_CCR_HTIE_Msk       (0x1U << DMA_CCR_HTIE_Pos)                      /*!< 0x00000004 */
+#define DMA_CCR_HTIE           DMA_CCR_HTIE_Msk                                /*!< Half Transfer interrupt enable      */
+#define DMA_CCR_TEIE_Pos       (3U)
+#define DMA_CCR_TEIE_Msk       (0x1U << DMA_CCR_TEIE_Pos)                      /*!< 0x00000008 */
+#define DMA_CCR_TEIE           DMA_CCR_TEIE_Msk                                /*!< Transfer error interrupt enable     */
+#define DMA_CCR_DIR_Pos        (4U)
+#define DMA_CCR_DIR_Msk        (0x1U << DMA_CCR_DIR_Pos)                       /*!< 0x00000010 */
+#define DMA_CCR_DIR            DMA_CCR_DIR_Msk                                 /*!< Data transfer direction             */
+#define DMA_CCR_CIRC_Pos       (5U)
+#define DMA_CCR_CIRC_Msk       (0x1U << DMA_CCR_CIRC_Pos)                      /*!< 0x00000020 */
+#define DMA_CCR_CIRC           DMA_CCR_CIRC_Msk                                /*!< Circular mode                       */
+#define DMA_CCR_PINC_Pos       (6U)
+#define DMA_CCR_PINC_Msk       (0x1U << DMA_CCR_PINC_Pos)                      /*!< 0x00000040 */
+#define DMA_CCR_PINC           DMA_CCR_PINC_Msk                                /*!< Peripheral increment mode           */
+#define DMA_CCR_MINC_Pos       (7U)
+#define DMA_CCR_MINC_Msk       (0x1U << DMA_CCR_MINC_Pos)                      /*!< 0x00000080 */
+#define DMA_CCR_MINC           DMA_CCR_MINC_Msk                                /*!< Memory increment mode               */
+
+#define DMA_CCR_PSIZE_Pos      (8U)
+#define DMA_CCR_PSIZE_Msk      (0x3U << DMA_CCR_PSIZE_Pos)                     /*!< 0x00000300 */
+#define DMA_CCR_PSIZE          DMA_CCR_PSIZE_Msk                               /*!< PSIZE[1:0] bits (Peripheral size)   */
+#define DMA_CCR_PSIZE_0        (0x1U << DMA_CCR_PSIZE_Pos)                     /*!< 0x00000100 */
+#define DMA_CCR_PSIZE_1        (0x2U << DMA_CCR_PSIZE_Pos)                     /*!< 0x00000200 */
+
+#define DMA_CCR_MSIZE_Pos      (10U)
+#define DMA_CCR_MSIZE_Msk      (0x3U << DMA_CCR_MSIZE_Pos)                     /*!< 0x00000C00 */
+#define DMA_CCR_MSIZE          DMA_CCR_MSIZE_Msk                               /*!< MSIZE[1:0] bits (Memory size)       */
+#define DMA_CCR_MSIZE_0        (0x1U << DMA_CCR_MSIZE_Pos)                     /*!< 0x00000400 */
+#define DMA_CCR_MSIZE_1        (0x2U << DMA_CCR_MSIZE_Pos)                     /*!< 0x00000800 */
+
+#define DMA_CCR_PL_Pos         (12U)
+#define DMA_CCR_PL_Msk         (0x3U << DMA_CCR_PL_Pos)                        /*!< 0x00003000 */
+#define DMA_CCR_PL             DMA_CCR_PL_Msk                                  /*!< PL[1:0] bits(Channel Priority level)*/
+#define DMA_CCR_PL_0           (0x1U << DMA_CCR_PL_Pos)                        /*!< 0x00001000 */
+#define DMA_CCR_PL_1           (0x2U << DMA_CCR_PL_Pos)                        /*!< 0x00002000 */
+
+#define DMA_CCR_MEM2MEM_Pos    (14U)
+#define DMA_CCR_MEM2MEM_Msk    (0x1U << DMA_CCR_MEM2MEM_Pos)                   /*!< 0x00004000 */
+#define DMA_CCR_MEM2MEM        DMA_CCR_MEM2MEM_Msk                             /*!< Memory to memory mode               */
+
+/******************  Bit definition for DMA_CNDTR register  *******************/
+#define DMA_CNDTR_NDT_Pos      (0U)
+#define DMA_CNDTR_NDT_Msk      (0xFFFFU << DMA_CNDTR_NDT_Pos)                  /*!< 0x0000FFFF */
+#define DMA_CNDTR_NDT          DMA_CNDTR_NDT_Msk                               /*!< Number of data to Transfer          */
+
+/******************  Bit definition for DMA_CPAR register  ********************/
+#define DMA_CPAR_PA_Pos        (0U)
+#define DMA_CPAR_PA_Msk        (0xFFFFFFFFU << DMA_CPAR_PA_Pos)                /*!< 0xFFFFFFFF */
+#define DMA_CPAR_PA            DMA_CPAR_PA_Msk                                 /*!< Peripheral Address                  */
+
+/******************  Bit definition for DMA_CMAR register  ********************/
+#define DMA_CMAR_MA_Pos        (0U)
+#define DMA_CMAR_MA_Msk        (0xFFFFFFFFU << DMA_CMAR_MA_Pos)                /*!< 0xFFFFFFFF */
+#define DMA_CMAR_MA            DMA_CMAR_MA_Msk                                 /*!< Memory Address                      */
+
+#endif	// UNIT_TEST_INCLUDE_MOCKS_CHIP_STM32_DMAV1_DMACHANNELPERIPHERAL_HPP_DISTORTOS_CHIP_STM32_DMAV1_DMACHANNELPERIPHERAL_HPP_
