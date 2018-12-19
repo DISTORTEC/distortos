63874d02df4b3ee49897fcfd125b4d2decac1c2c
Kamil Szczygiel <distortec@distortec.com>
2018-12-19 20:24:41 +0100

    STM32-SPIv2-ChipSpiMasterLowLevel -> ...-SpiMasterLowLevelInterruptBased

--- source/chip/STM32/peripherals/SPIv2/STM32-SPIv2-SpiMasterLowLevelInterruptBased.cpp
+++ source/chip/STM32/peripherals/SPIv2/STM32-SPIv2-SpiMasterLowLevelInterruptBased.cpp
@@ -9,7 +9,7 @@
  * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "distortos/chip/ChipSpiMasterLowLevel.hpp"
+#include "distortos/chip/SpiMasterLowLevelInterruptBased.hpp"
 
 #include "distortos/chip/STM32-SPIv2-SpiPeripheral.hpp"
 
