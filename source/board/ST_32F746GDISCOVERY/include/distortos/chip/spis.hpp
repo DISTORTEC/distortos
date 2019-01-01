41a6dd1b48a60ccd1b5fab38b6708f2adee68c0e
Kamil Szczygiel <distortec@distortec.com>
2019-01-01 18:44:18 +0100

    Add support for DMA to STM32 SPIv2 board templates
    Regenerate all boards:
    $ find source/board/ -name '*.yaml' -print0 | xargs -0 -I{} -t
    ./scripts/generateBoard.py {}

--- source/board/ST_32F746GDISCOVERY/include/distortos/chip/spis.hpp
+++ source/board/ST_32F746GDISCOVERY/include/distortos/chip/spis.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief Declarations of low-level SPI master drivers for SPIv2 in ST,32F746GDISCOVERY (ST,STM32F746NG chip)
  *
- * \author Copyright (C) 2016-2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2016-2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -23,6 +23,7 @@ namespace distortos
 namespace chip
 {
 
+class SpiMasterLowLevelDmaBased;
 class SpiMasterLowLevelInterruptBased;
 
 /*---------------------------------------------------------------------------------------------------------------------+
@@ -32,42 +33,66 @@ class SpiMasterLowLevelInterruptBased;
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI1_ENABLE
 
 /// SPI master low-level driver for SPI1
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI1_DMA_BASED
+extern SpiMasterLowLevelDmaBased spi1;
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI1_DMA_BASED
 extern SpiMasterLowLevelInterruptBased spi1;
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI1_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI1_ENABLE
 
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI2_ENABLE
 
 /// SPI master low-level driver for SPI2
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI2_DMA_BASED
+extern SpiMasterLowLevelDmaBased spi2;
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI2_DMA_BASED
 extern SpiMasterLowLevelInterruptBased spi2;
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI2_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI2_ENABLE
 
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
 
 /// SPI master low-level driver for SPI3
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI3_DMA_BASED
+extern SpiMasterLowLevelDmaBased spi3;
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI3_DMA_BASED
 extern SpiMasterLowLevelInterruptBased spi3;
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI3_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
 
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
 
 /// SPI master low-level driver for SPI4
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI4_DMA_BASED
+extern SpiMasterLowLevelDmaBased spi4;
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI4_DMA_BASED
 extern SpiMasterLowLevelInterruptBased spi4;
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI4_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
 
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
 
 /// SPI master low-level driver for SPI5
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI5_DMA_BASED
+extern SpiMasterLowLevelDmaBased spi5;
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI5_DMA_BASED
 extern SpiMasterLowLevelInterruptBased spi5;
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI5_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
 
 #ifdef CONFIG_CHIP_STM32_SPIV2_SPI6_ENABLE
 
 /// SPI master low-level driver for SPI6
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI6_DMA_BASED
+extern SpiMasterLowLevelDmaBased spi6;
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI6_DMA_BASED
 extern SpiMasterLowLevelInterruptBased spi6;
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI6_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI6_ENABLE
 
--- source/board/ST_32F769IDISCOVERY/ST_32F769IDISCOVERY-spis.cpp
+++ source/board/ST_32F769IDISCOVERY/ST_32F769IDISCOVERY-spis.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief Definitions of low-level SPI master drivers for SPIv2 in ST,32F769IDISCOVERY (ST,STM32F769NI chip)
  *
- * \author Copyright (C) 2016-2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2016-2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -14,10 +14,21 @@
 
 #include "distortos/chip/spis.hpp"
 
+#include "distortos/chip/dmas.hpp"
+#include "distortos/chip/SpiMasterLowLevelDmaBased.hpp"
 #include "distortos/chip/SpiMasterLowLevelInterruptBased.hpp"
 #include "distortos/chip/STM32-SPIv2-SpiPeripheral.hpp"
 
 #include "distortos/BIND_LOW_LEVEL_INITIALIZER.h"
+
+/**
+ * \brief Generates name of DMA channel object in the form `dma<dmaId>Channel<channelId>`.
+ *
+ * \param [in] dmaId is a DMA identifier
+ * \param [in] channelId is a DMA channel identifier
+ */
+
+#define DMA_CHANNEL(dmaId, channelId)	CONCATENATE4(dma, dmaId, Channel, channelId)
 
 namespace distortos
 {
@@ -60,6 +71,19 @@ const SpiPeripheral spi1Peripheral {SPI1_BASE};
 
 }	// namespace
 
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI1_DMA_BASED
+
+SpiMasterLowLevelDmaBased spi1
+{
+		spi1Peripheral,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI1_RX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI1_RX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI1_RX_DMA_REQUEST,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI1_TX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI1_TX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI1_TX_DMA_REQUEST
+};
+
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI1_DMA_BASED
+
 SpiMasterLowLevelInterruptBased spi1 {spi1Peripheral};
 
 /**
@@ -70,6 +94,8 @@ extern "C" void SPI1_IRQHandler()
 {
 	spi1.interruptHandler();
 }
+
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI1_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI1_ENABLE
 
@@ -108,6 +134,19 @@ const SpiPeripheral spi2Peripheral {SPI2_BASE};
 
 }	// namespace
 
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI2_DMA_BASED
+
+SpiMasterLowLevelDmaBased spi2
+{
+		spi2Peripheral,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI2_RX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI2_RX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI2_RX_DMA_REQUEST,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI2_TX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI2_TX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI2_TX_DMA_REQUEST
+};
+
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI2_DMA_BASED
+
 SpiMasterLowLevelInterruptBased spi2 {spi2Peripheral};
 
 /**
@@ -118,6 +157,8 @@ extern "C" void SPI2_IRQHandler()
 {
 	spi2.interruptHandler();
 }
+
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI2_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI2_ENABLE
 
@@ -156,6 +197,19 @@ const SpiPeripheral spi3Peripheral {SPI3_BASE};
 
 }	// namespace
 
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI3_DMA_BASED
+
+SpiMasterLowLevelDmaBased spi3
+{
+		spi3Peripheral,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI3_RX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI3_RX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI3_RX_DMA_REQUEST,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI3_TX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI3_TX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI3_TX_DMA_REQUEST
+};
+
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI3_DMA_BASED
+
 SpiMasterLowLevelInterruptBased spi3 {spi3Peripheral};
 
 /**
@@ -166,6 +220,8 @@ extern "C" void SPI3_IRQHandler()
 {
 	spi3.interruptHandler();
 }
+
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI3_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI3_ENABLE
 
@@ -204,6 +260,19 @@ const SpiPeripheral spi4Peripheral {SPI4_BASE};
 
 }	// namespace
 
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI4_DMA_BASED
+
+SpiMasterLowLevelDmaBased spi4
+{
+		spi4Peripheral,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI4_RX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI4_RX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI4_RX_DMA_REQUEST,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI4_TX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI4_TX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI4_TX_DMA_REQUEST
+};
+
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI4_DMA_BASED
+
 SpiMasterLowLevelInterruptBased spi4 {spi4Peripheral};
 
 /**
@@ -214,6 +283,8 @@ extern "C" void SPI4_IRQHandler()
 {
 	spi4.interruptHandler();
 }
+
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI4_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI4_ENABLE
 
@@ -252,6 +323,19 @@ const SpiPeripheral spi5Peripheral {SPI5_BASE};
 
 }	// namespace
 
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI5_DMA_BASED
+
+SpiMasterLowLevelDmaBased spi5
+{
+		spi5Peripheral,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI5_RX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI5_RX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI5_RX_DMA_REQUEST,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI5_TX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI5_TX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI5_TX_DMA_REQUEST
+};
+
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI5_DMA_BASED
+
 SpiMasterLowLevelInterruptBased spi5 {spi5Peripheral};
 
 /**
@@ -262,6 +346,8 @@ extern "C" void SPI5_IRQHandler()
 {
 	spi5.interruptHandler();
 }
+
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI5_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI5_ENABLE
 
@@ -300,6 +386,19 @@ const SpiPeripheral spi6Peripheral {SPI6_BASE};
 
 }	// namespace
 
+#ifdef CONFIG_CHIP_STM32_SPIV2_SPI6_DMA_BASED
+
+SpiMasterLowLevelDmaBased spi6
+{
+		spi6Peripheral,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI6_RX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI6_RX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI6_RX_DMA_REQUEST,
+		DMA_CHANNEL(CONFIG_CHIP_STM32_SPIV2_SPI6_TX_DMA, CONFIG_CHIP_STM32_SPIV2_SPI6_TX_DMA_CHANNEL),
+		CONFIG_CHIP_STM32_SPIV2_SPI6_TX_DMA_REQUEST
+};
+
+#else	// !def CONFIG_CHIP_STM32_SPIV2_SPI6_DMA_BASED
+
 SpiMasterLowLevelInterruptBased spi6 {spi6Peripheral};
 
 /**
@@ -310,6 +409,8 @@ extern "C" void SPI6_IRQHandler()
 {
 	spi6.interruptHandler();
 }
+
+#endif	// !def CONFIG_CHIP_STM32_SPIV2_SPI6_DMA_BASED
 
 #endif	// def CONFIG_CHIP_STM32_SPIV2_SPI6_ENABLE
 
