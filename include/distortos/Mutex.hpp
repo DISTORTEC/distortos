3366993e3799d87fa65972185344fef53f8b0a87
Kamil Szczygiel <distortec@distortec.com>
2019-01-25 21:28:29 +0100

    Minor comment fix in Mutex.hpp

--- include/distortos/Mutex.hpp
+++ include/distortos/Mutex.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief Mutex class header
  *
- * \author Copyright (C) 2014-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2014-2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -180,7 +180,7 @@ public:
 	int tryLockFor(TickClock::duration duration);
 
 	/**
-	 * Tries to lock the mutex for given duration of time.
+	 * \brief Tries to lock the mutex for given duration of time.
 	 *
 	 * Template variant of tryLockFor(TickClock::duration duration).
 	 *
