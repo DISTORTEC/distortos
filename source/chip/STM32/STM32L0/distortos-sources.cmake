3383b2f2b3ecb42ef5d0b6c8b459d9ba3a2c5fe9
Kamil Szczygiel <distortec@distortec.com>
2019-02-03 14:36:56 +0100

    Add source/chip/STM32/include to doxygen's INPUT

--- source/chip/STM32/STM32L0/distortos-sources.cmake
+++ source/chip/STM32/STM32L0/distortos-sources.cmake
@@ -1,7 +1,7 @@
 #
 # file: distortos-sources.cmake
 #
-# author: Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+# author: Copyright (C) 2018-2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -251,6 +251,6 @@ target_sources(distortos PRIVATE
 		${CMAKE_CURRENT_LIST_DIR}/STM32L0-PWR.cpp
 		${CMAKE_CURRENT_LIST_DIR}/STM32L0-RCC.cpp)
 
-doxygen(INPUT ${CMAKE_CURRENT_LIST_DIR}
+doxygen(INPUT ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_CURRENT_LIST_DIR}/../include
 		INCLUDE_PATH ${CMAKE_CURRENT_LIST_DIR}/include ${CMAKE_CURRENT_LIST_DIR}/external/CMSIS-STM32L0
 		EXCLUDE ${CMAKE_CURRENT_LIST_DIR}/external)
