104e1be5808cb0156fa46cc9e36ce4d3a31c8020
Kamil Szczygiel <distortec@distortec.com>
2019-02-11 10:03:31 +0100

    Add board templates for STM32 SDMMCv1
    Regenerate all boards:
    $ find source/board/ -name '*.yaml' -print0 | xargs -0 -I{} -t
    ./scripts/generateBoard.py {}

---
+++ source/board/ST_32F746GDISCOVERY/include/distortos/chip/sdmmcs.hpp
@@ -0,0 +1,43 @@
+/**
+ * \file
+ * \brief Declarations of low-level SD/MMC card drivers for SDMMCv1 in ST,32F746GDISCOVERY (ST,STM32F746NG chip)
+ *
+ * \author Copyright (C) 2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \warning
+ * Automatically generated file - do not edit!
+ */
+
+#ifndef SOURCE_BOARD_ST_32F746GDISCOVERY_INCLUDE_DISTORTOS_CHIP_SDMMCS_HPP_
+#define SOURCE_BOARD_ST_32F746GDISCOVERY_INCLUDE_DISTORTOS_CHIP_SDMMCS_HPP_
+
+#include "distortos/distortosConfiguration.h"
+
+namespace distortos
+{
+
+namespace chip
+{
+
+class SdMmcCardLowLevel;
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+#ifdef CONFIG_CHIP_STM32_SDMMCV1_SDMMC1_ENABLE
+
+/// SD/MMC card low-level driver for SDMMC1
+extern SdMmcCardLowLevel sdmmc1;
+
+#endif	// def CONFIG_CHIP_STM32_SDMMCV1_SDMMC1_ENABLE
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_BOARD_ST_32F746GDISCOVERY_INCLUDE_DISTORTOS_CHIP_SDMMCS_HPP_
---
+++ source/board/ST_32F769IDISCOVERY/include/distortos/chip/sdmmcs.hpp
@@ -0,0 +1,50 @@
+/**
+ * \file
+ * \brief Declarations of low-level SD/MMC card drivers for SDMMCv1 in ST,32F769IDISCOVERY (ST,STM32F769NI chip)
+ *
+ * \author Copyright (C) 2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \warning
+ * Automatically generated file - do not edit!
+ */
+
+#ifndef SOURCE_BOARD_ST_32F769IDISCOVERY_INCLUDE_DISTORTOS_CHIP_SDMMCS_HPP_
+#define SOURCE_BOARD_ST_32F769IDISCOVERY_INCLUDE_DISTORTOS_CHIP_SDMMCS_HPP_
+
+#include "distortos/distortosConfiguration.h"
+
+namespace distortos
+{
+
+namespace chip
+{
+
+class SdMmcCardLowLevel;
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+#ifdef CONFIG_CHIP_STM32_SDMMCV1_SDMMC1_ENABLE
+
+/// SD/MMC card low-level driver for SDMMC1
+extern SdMmcCardLowLevel sdmmc1;
+
+#endif	// def CONFIG_CHIP_STM32_SDMMCV1_SDMMC1_ENABLE
+
+#ifdef CONFIG_CHIP_STM32_SDMMCV1_SDMMC2_ENABLE
+
+/// SD/MMC card low-level driver for SDMMC2
+extern SdMmcCardLowLevel sdmmc2;
+
+#endif	// def CONFIG_CHIP_STM32_SDMMCV1_SDMMC2_ENABLE
+
+}	// namespace chip
+
+}	// namespace distortos
+
+#endif	// SOURCE_BOARD_ST_32F769IDISCOVERY_INCLUDE_DISTORTOS_CHIP_SDMMCS_HPP_
