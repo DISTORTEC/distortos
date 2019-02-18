88cfb2280e49ff04a5ae05b2a0dc65733c1bc606
Kamil Szczygiel <distortec@distortec.com>
2019-02-18 20:31:13 +0100

    Add required alignments of low-level drivers to their CMake files

--- source/chip/STM32/peripherals/USARTv2/distortos-sources.cmake
+++ source/chip/STM32/peripherals/USARTv2/distortos-sources.cmake
@@ -1,11 +1,13 @@
 #
 # file: distortos-sources.cmake
 #
-# author: Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+# author: Copyright (C) 2018-2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
+
+list(APPEND DISTORTOS_UART_BUFFER_ALIGNMENTS 1)
 
 target_include_directories(distortos PUBLIC
 		${CMAKE_CURRENT_LIST_DIR}/include)
