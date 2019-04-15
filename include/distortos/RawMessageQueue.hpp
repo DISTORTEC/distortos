df19abda96044fffc6aec5f0a547684ae088fdf8
Kamil Szczygiel <distortec@distortec.com>
2019-04-15 12:05:30 +0200

    Add RawMessageQueue::getElementSize()

--- include/distortos/RawMessageQueue.hpp
+++ include/distortos/RawMessageQueue.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief RawMessageQueue class header
  *
- * \author Copyright (C) 2015-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2015-2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -64,6 +64,15 @@ public:
 
 	RawMessageQueue(EntryStorageUniquePointer&& entryStorageUniquePointer,
 			ValueStorageUniquePointer&& valueStorageUniquePointer, size_t elementSize, size_t maxElements);
+
+	/**
+	 * \return size of single queue element, bytes
+	 */
+
+	size_t getElementSize() const
+	{
+		return elementSize_;
+	}
 
 	/**
 	 * \brief Pops oldest element with highest priority from the queue.
