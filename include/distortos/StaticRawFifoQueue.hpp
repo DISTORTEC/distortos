0597d1a173e72e20a1a033238d7c9e4f61214df3
Kamil Szczygiel <distortec@distortec.com>
2019-04-19 17:31:27 +0200

    Remove deprecated StaticRawFifoQueue2 type alias

--- include/distortos/StaticRawFifoQueue.hpp
+++ include/distortos/StaticRawFifoQueue.hpp
@@ -22,20 +22,6 @@ namespace distortos
 /**
  * \brief StaticRawFifoQueue class is a variant of RawFifoQueue that has automatic storage for queue's contents.
  *
- * This class is a replacement for StaticRawFifoQueue with old API and removed StaticRawFifoQueueFromSize. To use this
- * new API modify your code in following way:
- * - old `"StaticRawFifoQueue<T, QueueSize>"` -> `"StaticRawFifoQueue<sizeof(T), QueueSize>"`;
- * - removed `"StaticRawFifoQueueFromSize<ElementSize, QueueSize>"` -> `"StaticRawFifoQueue<ElementSize, QueueSize>"`;
- *
- * Transition schedule:
- * 1. v0.5.0 - `StaticRawFifoQueue<T, QueueSize>` and `StaticRawFifoQueueFromSize<ElementSize, QueueSize>` are converted
- * to deprecated aliases to `StaticRawFifoQueue2<ElementSize, QueueSize>`;
- * 2. v0.6.0 - "old" `StaticRawFifoQueue<T, QueueSize>` and `StaticRawFifoQueueFromSize<ElementSize, QueueSize>` aliases
- * are removed, `StaticRawFifoQueue2<ElementSize, QueueSize>` is renamed to
- * `StaticRawFifoQueue<ElementSize, QueueSize>`, deprecated `StaticRawFifoQueue2<ElementSize, QueueSize>` alias is
- * added;
- * 3. v0.7.0 - deprecated `StaticRawFifoQueue2<ElementSize, QueueSize>` alias is removed;
- *
  * \tparam ElementSize is the size of single queue element, bytes
  * \tparam QueueSize is the maximum number of elements in queue
  *
@@ -80,21 +66,6 @@ private:
 	/// storage for queue's contents
 	std::array<uint8_t, ElementSize * QueueSize> storage_;
 };
-
-/**
- * \brief StaticRawFifoQueue2 class is a variant of RawFifoQueue that has automatic storage for queue's contents.
- *
- * \deprecated scheduled to be removed after v0.6.0, use `StaticRawFifoQueue<ElementSize, QueueSize>`
- *
- * \tparam ElementSize is the size of single queue element, bytes
- * \tparam QueueSize is the maximum number of elements in queue
- *
- * \ingroup queues
- */
-
-template<size_t ElementSize, size_t QueueSize>
-using StaticRawFifoQueue2 __attribute__ ((deprecated("Use StaticRawFifoQueue<ElementSize, QueueSize>"))) =
-		StaticRawFifoQueue<ElementSize, QueueSize>;
 
 }	// namespace distortos
 
