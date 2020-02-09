a36014d55806bd85e0d1b37b4b6596f4e368c47c
Kamil Szczygiel <distortec@distortec.com>
2020-02-09 20:32:05 +0100

    Mark ChipOutputPin classes and their headers as deprecated

--- source/chip/STM32/peripherals/GPIOv1/include/distortos/chip/ChipOutputPin.hpp
+++ source/chip/STM32/peripherals/GPIOv1/include/distortos/chip/ChipOutputPin.hpp
@@ -1,6 +1,7 @@
 /**
  * \file
  * \brief ChipOutputPin class header for GPIOv1 in STM32
+ * \deprecated scheduled to be removed after v0.8.0, use distortos/chip/OutputPin.hpp
  *
  * \author Copyright (C) 2016-2020 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
@@ -12,6 +13,8 @@
 #ifndef SOURCE_CHIP_STM32_PERIPHERALS_GPIOV1_INCLUDE_DISTORTOS_CHIP_CHIPOUTPUTPIN_HPP_
 #define SOURCE_CHIP_STM32_PERIPHERALS_GPIOV1_INCLUDE_DISTORTOS_CHIP_CHIPOUTPUTPIN_HPP_
 
+#warning "'distortos/chip/ChipOutputPin.hpp' is deprecated: Use OutputPin"
+
 #include "distortos/chip/OutputPin.hpp"
 
 namespace distortos
@@ -22,9 +25,11 @@ namespace chip
 
 /**
  * \brief ChipOutputPin is an alias for OutputPin.
+ *
+ * \deprecated scheduled to be removed after v0.8.0, use distortos::chip::OutputPin
  */
 
-class ChipOutputPin : public OutputPin
+class __attribute__ ((deprecated("Use OutputPin"))) ChipOutputPin : public OutputPin
 {
 public:
 
