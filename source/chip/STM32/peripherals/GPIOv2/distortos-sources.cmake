0ea4f400d1ad7d0d6079feeff5060ba698b9908a
Kamil Szczygiel <distortec@distortec.com>
2020-02-09 19:58:21 +0100

    Rename all ChipOutputPin classes to just OutputPin
    Old name is still available through an alias.

--- source/chip/STM32/peripherals/GPIOv2/distortos-sources.cmake
+++ source/chip/STM32/peripherals/GPIOv2/distortos-sources.cmake
@@ -1,7 +1,7 @@
 #
 # file: distortos-sources.cmake
 #
-# author: Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+# author: Copyright (C) 2018-2020 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -12,7 +12,7 @@ target_include_directories(distortos PUBLIC
 
 target_sources(distortos PRIVATE
 		${CMAKE_CURRENT_LIST_DIR}/STM32-GPIOv2-ChipInputPin.cpp
-		${CMAKE_CURRENT_LIST_DIR}/STM32-GPIOv2-ChipOutputPin.cpp
+		${CMAKE_CURRENT_LIST_DIR}/STM32-GPIOv2-OutputPin.cpp
 		${CMAKE_CURRENT_LIST_DIR}/STM32-GPIOv2.cpp)
 
 doxygen(INPUT ${CMAKE_CURRENT_LIST_DIR} INCLUDE_PATH ${CMAKE_CURRENT_LIST_DIR}/include)
--- source/chip/STM32/peripherals/GPIOv2/include/distortos/chip/ChipOutputPin.hpp
+++ source/chip/STM32/peripherals/GPIOv2/include/distortos/chip/ChipOutputPin.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief ChipOutputPin class header for GPIOv2 in STM32
  *
- * \author Copyright (C) 2016 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2016-2020 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -12,9 +12,7 @@
 #ifndef SOURCE_CHIP_STM32_PERIPHERALS_GPIOV2_INCLUDE_DISTORTOS_CHIP_CHIPOUTPUTPIN_HPP_
 #define SOURCE_CHIP_STM32_PERIPHERALS_GPIOV2_INCLUDE_DISTORTOS_CHIP_CHIPOUTPUTPIN_HPP_
 
-#include "distortos/chip/STM32-GPIOv2.hpp"
-
-#include "distortos/devices/io/OutputPin.hpp"
+#include "distortos/chip/OutputPin.hpp"
 
 namespace distortos
 {
@@ -23,75 +21,14 @@ namespace chip
 {
 
 /**
- * \brief ChipOutputPin class is a single output pin of GPIOv2 in STM32.
- *
- * \ingroup devices
+ * \brief ChipOutputPin is an alias for OutputPin.
  */
 
-class ChipOutputPin : public devices::OutputPin
+class ChipOutputPin : public OutputPin
 {
 public:
 
-	/**
-	 * \brief ChipOutputPin's constructor
-	 *
-	 * \param [in] pin is the identifier of pin
-	 * \param [in] openDrain is the desired output type of pin: push-pull (false) or open-drain (true),
-	 * default - push-pull (false)
-	 * \param [in] outputSpeed is the desired output speed of pin, default - PinOutputSpeed::low
-	 * \param [in] pull is the desired pull-up/pull-down configuration of pin, default - PinPull::none
-	 * \param [in] initialState is the initial state of pin, default - false
-	 * \param [in] inverted selects whether the pin is inverted (true) - get() returns true when GPIO state is low and
-	 * false when GPIO state is high, set() sets GPIO state to low when argument is true and to high when argument is
-	 * false - or not (false), default - false, not inverted
-	 */
-
-	explicit ChipOutputPin(Pin pin, bool openDrain = {}, PinOutputSpeed outputSpeed = {}, PinPull pull = {},
-			bool initialState = {}, bool inverted = {});
-
-	/**
-	 * \return current state of pin
-	 */
-
-	bool get() const override;
-
-	/**
-	 * \brief Sets state of pin.
-	 *
-	 * \param [in] state is the new state of pin
-	 */
-
-	void set(bool state) override;
-
-protected:
-
-	/**
-	 * \return true if inverted mode is enabled, false otherwise
-	 */
-
-	bool getInvertedMode() const
-	{
-		return inverted_;
-	}
-
-	/**
-	 * \brief Enables or disables inverted mode.
-	 *
-	 * \param [in] inverted selects whether the pin is inverted (true) or not (false)
-	 */
-
-	void setInvertedMode(const bool inverted)
-	{
-		inverted_ = inverted;
-	}
-
-private:
-
-	/// identifier of pin
-	Pin pin_;
-
-	/// selects whether the pin is inverted (true) or not (false)
-	bool inverted_;
+	using OutputPin::OutputPin;
 };
 
 }	// namespace chip
