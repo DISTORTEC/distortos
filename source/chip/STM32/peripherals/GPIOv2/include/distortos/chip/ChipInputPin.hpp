ac256be88b42531d93dfe4e54419711950037ff2
Kamil Szczygiel <distortec@distortec.com>
2020-02-23 11:33:02 +0100

    Mark ChipInputPin classes and their headers as deprecated

--- source/chip/STM32/peripherals/GPIOv2/include/distortos/chip/ChipInputPin.hpp
+++ source/chip/STM32/peripherals/GPIOv2/include/distortos/chip/ChipInputPin.hpp
@@ -1,6 +1,7 @@
 /**
  * \file
  * \brief ChipInputPin class header for GPIOv2 in STM32
+ * \deprecated scheduled to be removed after v0.8.0, use distortos/chip/InputPin.hpp
  *
  * \author Copyright (C) 2016-2020 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
@@ -12,6 +13,8 @@
 #ifndef SOURCE_CHIP_STM32_PERIPHERALS_GPIOV2_INCLUDE_DISTORTOS_CHIP_CHIPINPUTPIN_HPP_
 #define SOURCE_CHIP_STM32_PERIPHERALS_GPIOV2_INCLUDE_DISTORTOS_CHIP_CHIPINPUTPIN_HPP_
 
+#warning "'distortos/chip/ChipInputPin.hpp' is deprecated: Use InputPin"
+
 #include "distortos/chip/InputPin.hpp"
 
 namespace distortos
@@ -22,9 +25,11 @@ namespace chip
 
 /**
  * \brief ChipInputPin is an alias for InputPin
+ *
+ * \deprecated scheduled to be removed after v0.8.0, use distortos::chip::InputPin
  */
 
-class ChipInputPin : public InputPin
+class __attribute__ ((deprecated("Use InputPin"))) ChipInputPin : public InputPin
 {
 public:
 
