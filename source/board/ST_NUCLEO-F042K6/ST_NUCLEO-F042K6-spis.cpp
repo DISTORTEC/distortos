82899f269e8d9363c47e346664b1e1729a746395
Kamil Szczygiel <distortec@distortec.com>
2020-03-07 21:15:44 +0100

    Add support for generating pin initializers for STM32 SPIv1 & SPIv2
    Regenerate all boards:
    $ find source/board/ -name '*.yaml' -print0 | xargs -0 -I{} -t
    ./scripts/generateBoard.py {}

--- source/board/ST_NUCLEO-F042K6/ST_NUCLEO-F042K6-spis.cpp
+++ source/board/ST_NUCLEO-F042K6/ST_NUCLEO-F042K6-spis.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief Definitions of low-level SPI master drivers for SPIv2 in ST,NUCLEO-F042K6 (ST,STM32F042K6 chip)
  *
- * \author Copyright (C) 2016-2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2016-2020 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -15,6 +15,7 @@
 #include "distortos/chip/spis.hpp"
 
 #include "distortos/chip/dmas.hpp"
+#include "distortos/chip/PinInitializer.hpp"
 #include "distortos/chip/SpiMasterLowLevelDmaBased.hpp"
 #include "distortos/chip/SpiMasterLowLevelInterruptBased.hpp"
 #include "distortos/chip/STM32-SPIv2-SpiPeripheral.hpp"
