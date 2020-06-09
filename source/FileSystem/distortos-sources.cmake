5315340039d5c19e6545c71d7c591f31600f16bd
Kamil Szczygiel <distortec@distortec.com>
2020-06-09 15:41:18 +0200

    Add fileDescriptions, ...Count and ...Mutex objects

--- source/FileSystem/distortos-sources.cmake
+++ source/FileSystem/distortos-sources.cmake
@@ -27,6 +27,7 @@ distortosSetConfiguration(BOOLEAN
 		OUTPUT_NAME DISTORTOS_FILESYSTEMS_STANDARD_LIBRARY_INTEGRATION_ENABLE)
 
 target_sources(distortos PRIVATE
+		${CMAKE_CURRENT_LIST_DIR}/fileDescriptions.cpp
 		${CMAKE_CURRENT_LIST_DIR}/MountPoint.cpp
 		${CMAKE_CURRENT_LIST_DIR}/openFile.cpp
 		${CMAKE_CURRENT_LIST_DIR}/rootFileSystem.cpp
