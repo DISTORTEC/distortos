1f66028cf0d321841de5cf44ad773068de10975a
Kamil Szczygiel <distortec@distortec.com>
2020-06-09 17:47:57 +0200

    Add implementation of _open_r()

--- source/newlib/distortos-sources.cmake
+++ source/newlib/distortos-sources.cmake
@@ -1,7 +1,7 @@
 #
 # file: distortos-sources.cmake
 #
-# author: Copyright (C) 2018 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+# author: Copyright (C) 2018-2020 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -10,5 +10,6 @@
 target_sources(distortos PRIVATE
 		${CMAKE_CURRENT_LIST_DIR}/assert_func.cpp
 		${CMAKE_CURRENT_LIST_DIR}/locking.cpp
+		${CMAKE_CURRENT_LIST_DIR}/open_r.cpp
 		${CMAKE_CURRENT_LIST_DIR}/sbrk_r.cpp
 		${CMAKE_CURRENT_LIST_DIR}/syscallsStubs.cpp)
