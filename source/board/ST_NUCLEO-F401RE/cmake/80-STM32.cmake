49266f5818f08e10e4faca7298f1e6b60be733ba
Kamil Szczygiel <distortec@distortec.com>
2021-06-10 12:24:48 +0200

    Regenerate all boards
    $ find source/board/ -name '*.yaml' -print0 | xargs -0 -I{} -t
    ./scripts/generateBoard.py {}

--- source/board/ST_NUCLEO-F401RE/cmake/80-STM32.cmake
+++ source/board/ST_NUCLEO-F401RE/cmake/80-STM32.cmake
@@ -1,7 +1,7 @@
 #
 # file: cmake/80-STM32.cmake
 #
-# author: Copyright (C) 2018-2019 Kamil Szczygiel https://distortec.com https://freddiechopin.info
+# author: Copyright (C) 2018-2021 Kamil Szczygiel https://distortec.com https://freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
