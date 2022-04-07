2f346ab34b11cbf27ca5b38a3e0d89b03e53666f
Kamil Szczygiel <distortec@distortec.com>
2022-04-07 15:29:38 +0200

    Use FATAL_ERROR() instead of abort() in __cxa_pure_virtual()

--- source/gcc/cxa_pure_virtual.cpp
+++ source/gcc/cxa_pure_virtual.cpp
@@ -2,12 +2,14 @@
  * \file
  * \brief __cxa_pure_virtual() system call implementation
  *
- * \author Copyright (C) 2014 Kamil Szczygiel https://distortec.com https://freddiechopin.info
+ * \author Copyright (C) 2014-2022 Kamil Szczygiel https://distortec.com https://freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+
+#include "distortos/FATAL_ERROR.h"
 
 #include <cstdlib>
 
@@ -23,13 +25,13 @@ extern "C"
  *
  * This function is called on pure virtual function call. It does not return.
  *
- * It just calls abort().
+ * It just calls FATAL_ERROR().
  */
 
 __attribute__ ((noreturn))
 void __cxa_pure_virtual()
 {
-	abort();
+	FATAL_ERROR("Pure virtual function called!");
 }
 
 }	// extern "C"
