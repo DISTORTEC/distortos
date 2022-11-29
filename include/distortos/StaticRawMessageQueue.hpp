c50b3de6673c54352f682ac85e47df695b6bf617
Kamil Szczygiel <distortec@distortec.com>
2022-11-29 11:12:40 +0100

    Explicitly include <array> in some headers to fix instantiation errors
    Without explicit include of <array>, some objects would cause a
    compilation error when they are instantiated (and <array> include was
    not available otherwise) due to incomplete type definition.

--- include/distortos/StaticRawMessageQueue.hpp
+++ include/distortos/StaticRawMessageQueue.hpp
@@ -2,7 +2,7 @@
  * \file
  * \brief StaticRawMessageQueue class header
  *
- * \author Copyright (C) 2015-2019 Kamil Szczygiel https://distortec.com https://freddiechopin.info
+ * \author Copyright (C) 2015-2022 Kamil Szczygiel https://distortec.com https://freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -15,6 +15,8 @@
 #include "distortos/RawMessageQueue.hpp"
 
 #include "distortos/internal/memory/dummyDeleter.hpp"
+
+#include <array>
 
 namespace distortos
 {
