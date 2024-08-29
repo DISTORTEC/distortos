4d8ec953ff17badff927813b7bb79e86e7a13f2b
Kamil Szczygiel <distortec@distortec.com>
2024-08-29 14:36:52 +0200

    Implement standard clock configuration in STM32G0's initializer
    This functionality is not yet available in CMake options

--- source/chip/STM32/STM32G0/STM32G0-chipLowLevelInitializer.cpp
+++ source/chip/STM32/STM32G0/STM32G0-chipLowLevelInitializer.cpp
@@ -9,10 +9,15 @@
  * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+#include "distortos/chip/clocks.hpp"
 #include "distortos/chip/CMSIS-proxy.h"
 #include "distortos/chip/STM32G0-FLASH.hpp"
+#include "distortos/chip/STM32G0-PWR.hpp"
+#include "distortos/chip/STM32G0-RCC.hpp"
 
 #include "distortos/BIND_LOW_LEVEL_INITIALIZER.h"
+
+#include <algorithm>
 
 namespace distortos
 {
@@ -46,6 +51,101 @@ void chipLowLevelInitializer()
 #else	// !def DISTORTOS_CHIP_FLASH_INSTRUCTION_CACHE_ENABLE
 	disableInstructionCache();
 #endif	// !def DISTORTOS_CHIP_FLASH_INSTRUCTION_CACHE_ENABLE
+
+#ifdef DISTORTOS_CHIP_STANDARD_CLOCK_CONFIGURATION_ENABLE
+
+	RCC->APBENR1 |= RCC_APBENR1_PWREN;
+
+	configureVoltageScaling(DISTORTOS_CHIP_PWR_VOLTAGE_SCALING_RANGE);
+
+#ifdef DISTORTOS_CHIP_RCC_HSE_ENABLE
+	enableHse(hseClockBypass);
+#endif	// def DISTORTOS_CHIP_RCC_HSE_ENABLE
+
+#ifdef DISTORTOS_CHIP_RCC_HSI16_ENABLE
+	enableHsi16();
+#endif	// def DISTORTOS_CHIP_RCC_HSI16_ENABLE
+
+#ifdef DISTORTOS_CHIP_RCC_LSE_ENABLE
+
+#if defined(DISTORTOS_CHIP_RCC_LSE_DRIVE_CAPABILITY_LOW) || defined(DISTORTOS_CHIP_RCC_LSE_CLOCK_BYPASS)
+	constexpr LseDriveCapability lseDriveCapability {LseDriveCapability::low};
+#elif defined(DISTORTOS_CHIP_RCC_LSE_DRIVE_CAPABILITY_MEDIUMLOW)
+	constexpr LseDriveCapability lseDriveCapability {LseDriveCapability::mediumLow};
+#elif defined(DISTORTOS_CHIP_RCC_LSE_DRIVE_CAPABILITY_MEDIUMHIGH)
+	constexpr LseDriveCapability lseDriveCapability {LseDriveCapability::mediumHigh};
+#elif defined(DISTORTOS_CHIP_RCC_LSE_DRIVE_CAPABILITY_HIGH)
+	constexpr LseDriveCapability lseDriveCapability {LseDriveCapability::high};
+#else
+	#error "LSE drive capability not configured!"
+#endif
+
+	disableRtcDomainWriteProtection();
+	enableLse(lseClockBypass, lseDriveCapability);
+	enableRtcDomainWriteProtection();
+
+#endif	// def DISTORTOS_CHIP_RCC_LSE_ENABLE
+
+#ifdef DISTORTOS_CHIP_RCC_PLL_ENABLE
+
+#if defined(DISTORTOS_CHIP_RCC_PLLSRC_HSE)
+	constexpr PllClockSource pllClockSource {PllClockSource::hse};
+#elif defined(DISTORTOS_CHIP_RCC_PLLSRC_HSI16)
+	constexpr PllClockSource pllClockSource {PllClockSource::hsi16};
+#endif	// defined(DISTORTOS_CHIP_RCC_PLLSRC_MSI)
+
+#ifdef STM32G0_RCC_HAS_PLLQ
+
+	enablePll(pllClockSource, DISTORTOS_CHIP_RCC_PLLM, DISTORTOS_CHIP_RCC_PLLN, DISTORTOS_CHIP_RCC_PLLP,
+			DISTORTOS_CHIP_RCC_PLLQ, DISTORTOS_CHIP_RCC_PLLR);
+
+#else	// !def STM32G0_RCC_HAS_PLLQ
+
+	enablePll(pllClockSource, DISTORTOS_CHIP_RCC_PLLM, DISTORTOS_CHIP_RCC_PLLN, DISTORTOS_CHIP_RCC_PLLP,
+			DISTORTOS_CHIP_RCC_PLLR);
+
+#endif	// !def STM32G0_RCC_HAS_PLLQ
+
+#ifdef DISTORTOS_CHIP_RCC_PLLP_ENABLE
+	enablePllOutput(PllOutput::p, true);
+#endif	// def DISTORTOS_CHIP_RCC_PLLP_ENABLE
+#ifdef DISTORTOS_CHIP_RCC_PLLQ_ENABLE
+	enablePllOutput(PllOutput::q, true);
+#endif	// def DISTORTOS_CHIP_RCC_PLLQ_ENABLE
+#ifdef DISTORTOS_CHIP_RCC_PLLR_ENABLE
+	enablePllOutput(PllOutput::r, true);
+#endif	// def DISTORTOS_CHIP_RCC_PLLR_ENABLE
+
+#endif	// def DISTORTOS_CHIP_RCC_PLL_ENABLE
+
+	configureAhbClockDivider(DISTORTOS_CHIP_RCC_HPRE);
+	configureApbClockDivider(DISTORTOS_CHIP_RCC_PPRE);
+
+#if DISTORTOS_CHIP_PWR_VOLTAGE_SCALING_RANGE == 1
+	constexpr uint32_t frequencyThreshold {24000000};
+	constexpr uint8_t flashLatencyUpperBound {2};
+#elif DISTORTOS_CHIP_PWR_VOLTAGE_SCALING_RANGE == 2
+	constexpr uint32_t frequencyThreshold {8000000};
+	constexpr uint8_t flashLatencyUpperBound {1};
+#endif
+
+	constexpr uint8_t flashLatency {(ahbFrequency - 1) / frequencyThreshold};
+	static_assert(flashLatency <= maxFlashLatency, "Invalid flash latency!");
+	configureFlashLatency(std::min(flashLatency, flashLatencyUpperBound));
+
+#if defined(DISTORTOS_CHIP_RCC_SYSCLK_HSE)
+	switchSystemClock(SystemClockSource::hse);
+#elif defined(DISTORTOS_CHIP_RCC_SYSCLK_HSISYS)
+	switchSystemClock(SystemClockSource::hsisys);
+#elif defined(DISTORTOS_CHIP_RCC_SYSCLK_LSE)
+	switchSystemClock(SystemClockSource::lse);
+#elif defined(DISTORTOS_CHIP_RCC_SYSCLK_LSI)
+	switchSystemClock(SystemClockSource::lsi);
+#elif defined(DISTORTOS_CHIP_RCC_SYSCLK_PLLR)
+	switchSystemClock(SystemClockSource::pllr);
+#endif	// defined(DISTORTOS_CHIP_RCC_SYSCLK_PLLR)
+
+#endif	// def DISTORTOS_CHIP_STANDARD_CLOCK_CONFIGURATION_ENABLE
 
 	RCC->IOPENR |=
 #ifdef DISTORTOS_CHIP_GPIOA_ENABLE
