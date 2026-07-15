88f03e06aa171bf6297c2e353c2c9ae1fb774795
Kamil Szczygiel <distortec@distortec.com>
2026-07-15 22:23:11 +0200

    Silence some GCC-15 warnings for littlefs1

--- source/FileSystem/littlefs1/external/littlefs1-sources.cmake
+++ source/FileSystem/littlefs1/external/littlefs1-sources.cmake
@@ -1,7 +1,7 @@
 #
 # file: littlefs1-sources.cmake
 #
-# author: Copyright (C) 2018-2019 Kamil Szczygiel https://distortec.com https://freddiechopin.info
+# author: Copyright (C) 2018-2026 Kamil Szczygiel https://distortec.com https://freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -18,3 +18,5 @@ target_compile_definitions(littlefs1 PUBLIC
 		LFS1_NO_WARN)
 target_include_directories(littlefs1 INTERFACE
 		${CMAKE_CURRENT_LIST_DIR}/littlefs1)
+target_compile_options(littlefs1 PRIVATE
+		-Wno-unterminated-string-initialization)
